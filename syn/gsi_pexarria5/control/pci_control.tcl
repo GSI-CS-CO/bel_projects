0f52fa6bf1c400ca506b30a95a29ee895b6b5e87
Wesley W. Terpstra <w.terpstra@gsi.de>
2013-09-30 18:38:37 +0200

    build-id: include platform information in all designs

--- syn/gsi_pexarria5/control/pci_control.tcl
+++ syn/gsi_pexarria5/control/pci_control.tcl
@@ -1,3 +1,5 @@
+set platform "pexaria5 +febex-ad6 +wrex1"
+source ../../../modules/build_id/build_id.tcl
 source ../../../ip_cores/general-cores/platform/altera/networks/arria5.tcl
 source ../../../ip_cores/general-cores/platform/altera/wb_pcie/arria5.tcl
 source ../../../ip_cores/wr-cores/platform/altera/arria5_pll/arria5_pll.tcl
