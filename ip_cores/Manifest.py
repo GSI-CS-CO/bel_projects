8344279c4c287b5cc5eaae69eb10369c8dc07cc6
Cesar Prados <c.prados@gsi.de>
2013-10-04 16:00:43 +0200

    add legacy-vme64x-core

--- ip_cores/Manifest.py
+++ ip_cores/Manifest.py
@@ -3,5 +3,6 @@ modules = {
     "git://ohwr.org/hdl-core-lib/etherbone-core.git",
     "git://ohwr.org/hdl-core-lib/general-cores.git",
     "git://ohwr.org/hdl-core-lib/wr-cores.git",
+    "git://ohwr.org/hdl-core-lib/vme64x-core/legacy-vme64x-core.git",
   ]
 }
