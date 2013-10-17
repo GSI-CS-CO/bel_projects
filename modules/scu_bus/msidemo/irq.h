638b22270ae3b643fb96bcf92099ad14fb23f0ae
Stefan Rauch <s.rauch@gsi.de>
2013-10-17 16:44:31 +0200

    initial commit of msidemo for fg

---
+++ modules/scu_bus/msidemo/irq.h
@@ -0,0 +1,32 @@
+/** @file scu_bus.h
+ *  
+ *  Copyright (C) 2011-2012 GSI Helmholtz Centre for Heavy Ion Research GmbH 
+ *
+ *  @author Stefan Rauch <s.rauch@gsi.de>
+ *
+ *
+ *******************************************************************************
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 3 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library. If not, see <http://www.gnu.org/licenses/>.
+ *******************************************************************************
+ */
+
+#ifndef __SCU_H_
+#define __SCU_H_
+
+
+#define GLOBAL_IRQ_ENA  0x4
+#define SRQ_ENA         0xc
+
+
+#endif
