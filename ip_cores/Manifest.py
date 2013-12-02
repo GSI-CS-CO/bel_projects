6930b52124e45bb59de819bf972f725d9ddfc946
Wesley W. Terpstra <w.terpstra@gsi.de>
2013-12-02 13:57:47 +0100

    build: stop hdlmake from even thinking of invoking git

--- ip_cores/Manifest.py
+++ ip_cores/Manifest.py
@@ -1,8 +1,8 @@
 modules = {
-  "git" : [
-    "git://ohwr.org/hdl-core-lib/etherbone-core.git",
-    "git://ohwr.org/hdl-core-lib/general-cores.git",
-    "git://ohwr.org/hdl-core-lib/wr-cores.git",
-    "git://ohwr.org/hdl-core-lib/vme64x-core/legacy-vme64x-core.git",
+  "local" : [
+    "etherbone-core",
+    "general-cores",
+    "wr-cores",
+    "legacy-vme64x-core",
   ]
 }
