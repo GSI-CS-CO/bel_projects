872ec3d1896954d1db884807c8a02a9028582503
Stefan Rauch <s.rauch@gsi.de>
2014-02-06 11:45:13 +0100

    scu_diob: added code for lm32, connected wb_scu_reg to read multiplexer

--- top/scu_diob/Manifest.py
+++ top/scu_diob/Manifest.py
@@ -1,9 +1,13 @@
 files = [
-   "scu_addac.vhd",
-   "scu_addac.sdc",
-   "IO_4x8.vhd",
+   "scu_diob.vhd",
+   "scu_diob.sdc",
    "flash_loader_v01.vhd",
-   "adda_pll.vhd"  
+   "adda_pll.vhd",
+   "Test_User_Reg.vhd",
+   "Zeitbasis.vhd",
+   "SysClock.vhd",
+   "AW_IO_Reg.vhd",
+   "PLL_SIO.vhd"         
 ]
 
 modules = {
---
+++ top/scu_diob/include/sockitowm/eep43.h
@@ -0,0 +1,4 @@
+int Write(int, uchar*, uchar*);
+int Copy2Mem43(int, uchar*);
+int ReadScratch43(int, uchar*, uchar*);
+int ReadMem43(int, uchar*, uchar*);
