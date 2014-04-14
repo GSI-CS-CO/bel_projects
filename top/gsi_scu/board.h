86b1c5be8118ff43578f69f604686180bf4ac738
Wesley W. Terpstra <w.terpstra@gsi.de>
2014-04-14 11:34:15 +0200

    scu: fix onewire base address -- real fix pending please!
    The onewire base addess is hard-coded AND wrong.
    Stefan is working on a proper fix.

--- top/gsi_scu/board.h
+++ top/gsi_scu/board.h
@@ -5,7 +5,7 @@
 
 #define BASE_PIO	    0x100400
 #define BASE_OW_WR    0x80060600
-#define BASE_OW_EXT   0x80000200
+#define BASE_OW_EXT   0x80000300
 #define BASE_SCU_REG  0x100800
 
 #define UART_BAUDRATE 115200ULL /* not a real UART */
