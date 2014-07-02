0199b8bf2a50494f9fd17c5d9882c5eb83a792e8
Wesley W. Terpstra <w.terpstra@gsi.de>
2014-07-02 18:03:01 +0200

    iotest: support all form factors with same test procedure

--- tools/io-test.cpp
+++ tools/io-test.cpp
@@ -4,39 +4,18 @@
 #include <cassert>
 #include <cstdio>
 #include <cstdlib>
+#include <cstring>
 #include <unistd.h> // sleep
 
 using namespace GSI_ECA;
 using namespace GSI_TLU;
 
-#define HIGH_NS		100000000
-#define LOW_NS		100000000
-#define CPU_DELAY_MS	1000
+#define HIGH_NS		100000
+#define LOW_NS		100000
+#define CPU_DELAY_MS	20
 #define EVENTS		16
-
-void validate(uint64_t start, int in, int out, const std::vector<uint64_t>& queue) {
-  /* Now test phase quality */
-  int64_t phase[EVENTS];
-  int64_t avg = 0;
-  for (int event = 0; event < EVENTS; ++event) {
-    uint64_t target = (event%8) + start + (HIGH_NS+LOW_NS)*event;
-    phase[event] = queue[event] - target;
-    avg += phase[event];
-  }
-  avg /= EVENTS;
-  
-  printf("Phase offset from %d=>%d: %dns\n", out, in, (int)avg);
-  for (int event = 0; event < EVENTS; ++event) {
-    if (phase[event] > avg + 1 ||
-        phase[event] + 1 < avg) {
-      fprintf(stderr, "Too much phase noise on %d=>%d! %dns differs too far from average %dns\n",
-        out, in, (int)phase[event], (int)avg);
-      for (int event = 0; event < EVENTS; ++event)
-        printf("Phase: %d\n", (int)phase[event]);
-      exit(1);
-    }
-  }
-}
+#define MAX_CHANNELS	32
+#define TAG_BIT		0x40000000
 
 int main(int argc, const char** argv) {
   Socket socket;
@@ -100,10 +79,12 @@ int main(int argc, const char** argv) {
   assert (devs.size() == 1);
   address_t ioconf = devs[0].sdb_component.addr_first + 4;
   
-  int cable1 = -1, cable2 = -1;
+  int inputs = 0;
+  int connection[MAX_CHANNELS][MAX_CHANNELS];
+  memset(connection, 0, sizeof(connection));
   
   /* For each output, generate a pulse chain and confirm it's validity */
-  for (int out = 0; out < 3; ++out) {
+  for (int out = 0; out < MAX_CHANNELS; ++out) {
     device.write(ioconf, EB_DATA32, 1 << out);
     /* Generate pulse quickly */
     eca.refresh();
@@ -115,63 +96,71 @@ int main(int argc, const char** argv) {
     std::vector<std::vector<uint64_t> > queues;
     tlu.pop_all(queues);
     
+    assert (queues.size() < MAX_CHANNELS);
+    if ((int)queues.size() > inputs) inputs = queues.size();
+    
     start <<= 3; // TLU is in 1ns steps, ECA in 8ns steps
-    for (int in = 0; in < 3; ++in) {
+    for (int in = 0; in < (int)queues.size(); ++in) {
       std::vector<uint64_t>& queue = queues[in];
       
-      /* Detect cable */
-      if (in != out) {
-        if (!queue.empty()) {
-          if (cable1 == -1) {
-            printf("Detected cable bridging %d-%d\n", in, out);
-            cable1 = out;
-            cable2 = in;
-          } else if ((cable1 != in  && cable2 != in) ||
-                     (cable1 != out && cable2 != out)) {
-            fprintf(stderr, "Impossible! Cable connects more than two IOs %d-%d and %d-%d\n",
-              cable1, cable2, in, out);
-            return 1;
-          }
-        } else {
-          if ((cable1 == in  || cable2 == in) &&
-              (cable1 == out || cable2 == out)) {
-            fprintf(stderr, "Direction check failure! IO %d=>%d works, but not %d=>%d\n",
-              cable1, cable2, out, in);
-            return 1;
-          }
-          continue;
-        }
-      }
+      /* No source of data? */
+      if (queue.empty()) continue;
       
       if (queue.size() != EVENTS) {
         fprintf(stderr, "Input %d did not record %d events from output %d!\n", in, EVENTS, out);
         return 1;
       }
       
-      validate(start, in, out, queue);
+      /* Now test phase quality */
+      int64_t phase[EVENTS];
+      int64_t avg = 0;
+      for (int event = 0; event < EVENTS; ++event) {
+        uint64_t target = (event%8) + start + (HIGH_NS+LOW_NS)*event;
+        phase[event] = queue[event] - target;
+        avg += phase[event];
+      }
+      avg /= EVENTS;
+      
+      // printf("Phase offset from %d=>%d: %dns\n", out, in, (int)avg);
+      for (int event = 0; event < EVENTS; ++event) {
+        if (phase[event] > avg + 1 ||
+            phase[event] + 1 < avg) {
+          fprintf(stderr, "Too much phase noise on %d=>%d! %dns differs too far from average %dns\n",
+            out, in, (int)phase[event], (int)avg);
+          for (int event = 0; event < EVENTS; ++event)
+            printf("Phase: %d\n", (int)phase[event]);
+          exit(1);
+        }
+      }
+      
+      connection[out][in] = avg + TAG_BIT;
+    }
+  }
+  
+  /* Confirm that all inputs were tested */
+  int ret = 0;
+  for (int in = 0; in < inputs; ++in) {
+    int ok = 0;
+    printf("Input %d <=", in);
+    for (int out = 0; out < MAX_CHANNELS; ++out) {
+      if (connection[out][in]) {
+        ok = 1;
+        printf(" %d (%dns)", out, connection[out][in] - TAG_BIT);
+      }
+    }
+    if (!ok) {
+      printf(" nothing connected! - Test invalid.");
+      ret = 1;
     }
     
-    for (int lvds = 3; lvds < 5; ++lvds) {
-      std::vector<uint64_t>& queue = queues[lvds];
-      
-      if (queue.empty()) {
-        fprintf(stderr, "!!! warning: failed to detect cable on LVDS input\n");
-        continue;
-      }
-      
-      if (queue.size() != EVENTS) {
-        fprintf(stderr, "Input %d did not record %d events!\n", lvds, EVENTS);
-        return 1;
-      }
-      validate(start, lvds, -1, queue);
-    }
+    printf("\n");
   }
   
-  if (cable1 == -1) {
-    fprintf(stderr, "Failed to detect loopback cable!\n");
-    return 1;
+  if (!ret) {
+    printf(">>> test successful <<<\n");
+  } else {
+    printf("!!! test failed !!!\n");
   }
   
-  printf(">>> testing complete <<<\n");
-  return 0;
+  return ret;
 }
