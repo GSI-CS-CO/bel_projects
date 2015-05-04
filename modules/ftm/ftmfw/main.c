c01a8c20b4add889eb0e9d041d0e0c3814ab2a02
Mathias Kreider <m.kreider@gsi.de>
2015-05-04 14:25:10 +0200

    ftm: Changed firmware to newest dev version

--- modules/ftm/ftmfw/main.c
+++ modules/ftm/ftmfw/main.c
@@ -2,11 +2,10 @@
 #include <string.h>
 #include <inttypes.h>
 #include <stdint.h>
-
+#include "mprintf.h"
 #include "mini_sdb.h"
 #include "irq.h"
 #include "ftm.h"
-#include "timer.h"
 #include "ebm.h"
 #include "aux.h"
 #include "dbg.h"
@@ -35,18 +34,16 @@ void isr1()
 
 void ebmInit()
 {
-   ebm_config_if(LOCAL,   "hw/08:00:30:e3:b0:5a/udp/192.168.191.254/port/60368");
+   ebm_init();
+   ebm_config_if(LOCAL,   "hw/08:00:30:e3:b0:5a/udp/192.168.0.100/port/60368");
    //ebm_config_if(REMOTE,  "hw/00:14:d1:fa:01:aa/udp/192.168.191.131/port/60368");
    //ebm_config_if(REMOTE,  "hw/00:26:7b:00:04:08/udp/192.168.191.72/port/60368");
-   ebm_config_if(REMOTE,  "hw/ff:ff:ff:ff:ff:ff/udp/192.168.191.255/port/60368");
+   ebm_config_if(REMOTE,  "hw/ff:ff:ff:ff:ff:ff/udp/192.168.0.101/port/60368");
    ebm_config_meta(1500, 0x11, 255, 0x00000000 );
 }
 
 void init()
 { 
-   char buffer[] = "\nFTM Core #00 rdy ________"; 
-   char itoaBuffer[3];
-   
    discoverPeriphery();
    uart_init_hw();
    ebmInit();
@@ -54,21 +51,13 @@ void init()
  
    cmdCnt = 0;
    cpuId = getCpuIdx();
-   itoa(cpuId, itoaBuffer, 10);
-   itoa((uint32_t)&pFtmIf->cmd, &buffer[18], 8);
-   if(itoaBuffer[1]) {buffer[12] = itoaBuffer[1]; buffer[11] = itoaBuffer[0];}
-   if(itoaBuffer[0]) {buffer[12] = itoaBuffer[0];}
-
+   
    isr_table_clr();
    isr_ptr_table[0] = isr0; //timer
    isr_ptr_table[1] = isr1;   
    irq_set_mask(0x03);
    irq_enable();
    
-   
-   atomic_on(); 
-   uart_write_string(buffer);
-   atomic_off(); 
 }
 
 
@@ -118,14 +107,64 @@ int insertFpqEntry()
 void main(void) {
    
    int j;
-
+   
    init();
-   for (j = 0; j < (125000000/4); ++j) { asm("nop"); }
-   mprintf("\n");
-
+   uint32_t test = &pFtmIf->tPrep;
+   
+   //for (j = 0; j < (125000000/4); ++j) { asm("nop"); }
+   atomic_on();
+      
+   mprintf("#%02u: Core ready. Hi! \n", cpuId);
+   #if DEBUGLEVEL != 0
+      mprintf("#%02u: Debuglevel %u. Don't expect timeley delivery with console outputs on!\n", cpuId, DEBUGLEVEL);
+   #endif   
+   #if DEBUGTIME == 1
+      mprintf("#%02u: Debugtime mode ON. Par Field of Msgs will be overwritten be dispatch time at lm32\n", cpuId);
+   #endif
+   #if DEBUGPRIOQ == 1
+      mprintf("#%02u: Priority Queue Debugmode ON, timestamps will be written to 0x%08x on receivers", cpuId, DEBUGPRIOQDST);
+   #endif
+    
+   atomic_off();
+   mprintf("#%02u: Tprep @ 0x%08x\n", cpuId, test);
+   //hexDump ("Plan 0 Chain 0 : \n", (void*)pFtmIf->pAct->plans[0], 128);
+   /*
+   t_time now, later;
+   int64_t diff, diffSum;
+   int64_t diffMin, diffMax, diffAvg;
+   int64_t div = 1000000;
+   diffMin =  0x7fffffff;
+   diffMax =  0xffffffff;
+   diffAvg =  0;
+   */
+   
    while (1) {
       cmdEval();
       processFtm();
+      
+      //mprintf("pAct 0x%08x, Qty 0x%08x, pStart 0x%08x, lans[0] 0x%08x,\n", pFtmIf->pAct, pFtmIf->pAct->planQty, pFtmIf->pAct->pStart, pFtmIf->pAct->plans[0]);
+      
+      //for (j = 0; j < (125000000/4); ++j) { asm("nop"); }
+      /*
+      ebm_hi(0x0);
+      atomic_on();
+      ebm_op(0x0, 0x0BEEBABE, EBM_WRITE);
+      ebm_op(0x0, 0x1BEEBABE, EBM_WRITE);
+      atomic_off();
+      ebm_flush();
+      
+      for (j = 0; j < div; ++j) {
+         now   = getSysTime();
+         later = getSysTime();
+         diff  = (int64_t) later - (int64_t) now;
+         //mprintf("Min: %d Max: %d\n", (int32_t)diffMin, (int32_t)diffMax);
+         if(diffMin > diff) diffMin = diff;
+         if(diffMax < diff) diffMax = diff;
+         diffSum += diff;
+      }
+      mprintf("Min: %d Max: %d Avg: %d\n", (int32_t)diffMin, (int32_t)diffMax, ((int32_t)(diffSum / div)));
+      diffSum = 0;
+      */
    }
 
 }
