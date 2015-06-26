9cf87fb2ee8a0a44bce38ce415dc4c502de29d88
Rolf Hartmann <r.hartmann@gsi.de>
2015-06-26 11:40:57 +0200

    scu_diob: variable debounce added, adc dac interface added, new options for input register

--- top/scu_diob/config_status.vhd
+++ top/scu_diob/config_status.vhd
@@ -32,6 +32,8 @@ ENTITY config_status IS
     AW_Config1:           out  std_logic_vector(15 downto 0);    -- Daten-Reg. AWOut3
     AW_Config2:           out  std_logic_vector(15 downto 0);    -- Daten-Reg. AWOut4
 
+    Mirr_OutReg_Maske:    out  std_logic_vector(15 downto 0);    -- Maske für den Spiegel-Modus des Ausgangsregisters
+    
     Diob_Config1_wr:      out  std_logic;                        -- write-Strobe, Daten-Reg. Diob_Config1
     Diob_Config2_wr:      out  std_logic;                        -- write-Strobe, Daten-Reg. Diob_Config2
     AW_Config1_wr:        out  std_logic;                        -- write-Strobe, Daten-Reg. AW_Config1  
@@ -50,61 +52,69 @@ ARCHITECTURE Arch_config_status OF config_status IS
 
 
 
-constant  addr_width:               INTEGER := Adr_from_SCUB_LA'length;
+constant  addr_width:                 INTEGER := Adr_from_SCUB_LA'length;
 --
-constant  Diob_Config1_addr_offset: INTEGER := 00;    -- Offset zur Base_addr zum Setzen oder Rücklesen des Diob_Config1 Registers
-constant  Diob_Config2_addr_offset: INTEGER := 01;    -- Offset zur Base_addr zum Setzen oder Rücklesen des Diob_Config2 Registers
-constant  Diob_Status1_addr_offset: INTEGER := 02;    -- Offset zur Base_addr zum Rücklesen des AWIN_Port1
-constant  Diob_Status2_addr_offset: INTEGER := 03;    -- Offset zur Base_addr zum Rücklesen des AWIN_Port2
+constant  Diob_Config1_addr_offset:   INTEGER := 16#00#;    -- Offset zur Base_addr zum Setzen oder Rücklesen des Diob_Config1 Registers
+constant  Diob_Config2_addr_offset:   INTEGER := 16#01#;    -- Offset zur Base_addr zum Setzen oder Rücklesen des Diob_Config2 Registers
+constant  Diob_Status1_addr_offset:   INTEGER := 16#02#;    -- Offset zur Base_addr zum Rücklesen des AWIN_Port1
+constant  Diob_Status2_addr_offset:   INTEGER := 16#03#;    -- Offset zur Base_addr zum Rücklesen des AWIN_Port2
+  
+constant  AW_Config1_addr_offset:     INTEGER := 16#07#;    -- Offset zur Base_addr zum Setzen oder Rücklesen des AW_Config1 Registers
+constant  AW_Config2_addr_offset:     INTEGER := 16#08#;    -- Offset zur Base_addr zum Setzen oder Rücklesen des AW_Config2 Registers
+constant  AW_Status1_addr_offset:     INTEGER := 16#09#;    -- Offset zur Base_addr zum Rücklesen des AWIN_Port3
+constant  AW_Status2_addr_offset:     INTEGER := 16#0A#;    -- Offset zur Base_addr zum Rücklesen des AWIN_Port4
 
-constant  AW_Config1_addr_offset:   INTEGER := 04;    -- Offset zur Base_addr zum Setzen oder Rücklesen des AW_Config1 Registers
-constant  AW_Config2_addr_offset:   INTEGER := 05;    -- Offset zur Base_addr zum Setzen oder Rücklesen des AW_Config2 Registers
-constant  AW_Status1_addr_offset:   INTEGER := 06;    -- Offset zur Base_addr zum Rücklesen des AWIN_Port3
-constant  AW_Status2_addr_offset:   INTEGER := 07;    -- Offset zur Base_addr zum Rücklesen des AWIN_Port4
+constant  Mirr_OutReg_Maske_offset:   INTEGER := 16#0E#;    -- Offset zur Base_addr zum Setzen oder Rücklesen der Maske für den Spiegel-Modus des Ausgangsregisters 1
 
-constant  C_Diob_Config1_Addr: unsigned(addr_width-1 downto 0) := to_unsigned((CS_Base_addr + Diob_Config1_addr_offset), addr_width);  -- Adresse zum Setzen oder Rücklesen des Diob_Config1 Registers
-constant  C_Diob_Config2_Addr: unsigned(addr_width-1 downto 0) := to_unsigned((CS_Base_addr + Diob_Config2_addr_offset), addr_width);  -- Adresse zum Setzen oder Rücklesen des Diob_Config2 Registers
-constant  C_AW_Config1_Addr:   unsigned(addr_width-1 downto 0) := to_unsigned((CS_Base_addr + AW_Config1_addr_offset), addr_width);  -- Adresse zum Setzen oder Rücklesen des AW_Config1 Registers
-constant  C_AW_Config2_Addr:   unsigned(addr_width-1 downto 0) := to_unsigned((CS_Base_addr + AW_Config2_addr_offset), addr_width);  -- Adresse zum Setzen oder Rücklesen des AW_Config2 Registers
 
-constant  C_Diob_Status1_Addr: unsigned(addr_width-1 downto 0) := to_unsigned((CS_Base_addr + Diob_Status1_addr_offset), addr_width);  -- Adresse zum Lesen des Diob_Status1
-constant  C_Diob_Status2_Addr: unsigned(addr_width-1 downto 0) := to_unsigned((CS_Base_addr + Diob_Status2_addr_offset), addr_width);  -- Adresse zum Lesen des Diob_Status2
-constant  C_AW_Status1_Addr:   unsigned(addr_width-1 downto 0) := to_unsigned((CS_Base_addr + AW_Status1_addr_offset), addr_width);  -- Adresse zum Lesen des AW_Status1
-constant  C_AW_Status2_Addr:   unsigned(addr_width-1 downto 0) := to_unsigned((CS_Base_addr + AW_Status2_addr_offset), addr_width);  -- Adresse zum Lesen des AW_Status2
+constant  C_Diob_Config1_Addr:      unsigned(addr_width-1 downto 0) := to_unsigned((CS_Base_addr + Diob_Config1_addr_offset), addr_width);  -- Adresse zum Setzen oder Rücklesen des Diob_Config1 Registers
+constant  C_Diob_Config2_Addr:      unsigned(addr_width-1 downto 0) := to_unsigned((CS_Base_addr + Diob_Config2_addr_offset), addr_width);  -- Adresse zum Setzen oder Rücklesen des Diob_Config2 Registers
+constant  C_AW_Config1_Addr:        unsigned(addr_width-1 downto 0) := to_unsigned((CS_Base_addr + AW_Config1_addr_offset), addr_width);  -- Adresse zum Setzen oder Rücklesen des AW_Config1 Registers
+constant  C_AW_Config2_Addr:        unsigned(addr_width-1 downto 0) := to_unsigned((CS_Base_addr + AW_Config2_addr_offset), addr_width);  -- Adresse zum Setzen oder Rücklesen des AW_Config2 Registers
+      
+constant  C_Diob_Status1_Addr:      unsigned(addr_width-1 downto 0) := to_unsigned((CS_Base_addr + Diob_Status1_addr_offset), addr_width);  -- Adresse zum Lesen des Diob_Status1
+constant  C_Diob_Status2_Addr:      unsigned(addr_width-1 downto 0) := to_unsigned((CS_Base_addr + Diob_Status2_addr_offset), addr_width);  -- Adresse zum Lesen des Diob_Status2
+constant  C_AW_Status1_Addr:        unsigned(addr_width-1 downto 0) := to_unsigned((CS_Base_addr + AW_Status1_addr_offset), addr_width);  -- Adresse zum Lesen des AW_Status1
+constant  C_AW_Status2_Addr:        unsigned(addr_width-1 downto 0) := to_unsigned((CS_Base_addr + AW_Status2_addr_offset), addr_width);  -- Adresse zum Lesen des AW_Status2
 
-signal    S_Diob_Config1:      std_logic_vector(15 downto 0);
-signal    S_Diob_Config1_Rd:   std_logic;
-signal    S_Diob_Config1_Wr:   std_logic;
+constant  C_Mirr_OutReg_Maske_Addr: unsigned(addr_width-1 downto 0) := to_unsigned((CS_Base_addr + Mirr_OutReg_Maske_offset), addr_width);  -- Adresse zum Lesen der Maske für den Spiegel-Modus des Ausgangsregisters
+
+signal    S_Diob_Config1:           std_logic_vector(15 downto 0);
+signal    S_Diob_Config1_Rd:        std_logic;
+signal    S_Diob_Config1_Wr:        std_logic;
+      
+signal    S_Diob_Config2:           std_logic_vector(15 downto 0);
+signal    S_Diob_Config2_Rd:        std_logic;
+signal    S_Diob_Config2_Wr:        std_logic;
+      
+signal    S_AW_Config1:             std_logic_vector(15 downto 0);
+signal    S_AW_Config1_Rd:          std_logic;
+signal    S_AW_Config1_Wr:          std_logic;
+          
+signal    S_AW_Config2:             std_logic_vector(15 downto 0);
+signal    S_AW_Config2_Rd:          std_logic;
+signal    S_AW_Config2_Wr:          std_logic;
+      
+signal    S_Diob_Status1_Rd:        std_logic;
+signal    S_Diob_Status1_Wr:        std_logic;
+signal    S_Diob_Status2_Rd:        std_logic;
+signal    S_Diob_Status2_Wr:        std_logic;
+signal    S_AW_Status1_Rd:          std_logic;
+signal    S_AW_Status2_Rd:          std_logic;
+
+signal    S_Mirr_OutReg_Maske:      std_logic_vector(15 downto 0);
+signal    S_Mirr_OutReg_Maske_Rd:   std_logic;
+signal    S_Mirr_OutReg_Maske_Wr:   std_logic;
+
+signal    DIOB_Sts1_rd_Out:         std_logic_vector(15 downto 0); -- Zwischenspeicher
+signal    DIOB_Sts1_rd_Strobe_o:    std_logic;        -- Output 
+signal    DIOB_Sts1_rd_shift:       std_logic_vector(2  downto 0); -- Shift-Reg.
+  
+signal    s_Diob_Status1:           std_logic_vector(15 downto 0);
+signal    s_Diob_Status2:           std_logic_vector(15 downto 0);
  
-signal    S_Diob_Config2:      std_logic_vector(15 downto 0);
-signal    S_Diob_Config2_Rd:   std_logic;
-signal    S_Diob_Config2_Wr:   std_logic;
- 
-signal    S_AW_Config1:        std_logic_vector(15 downto 0);
-signal    S_AW_Config1_Rd:     std_logic;
-signal    S_AW_Config1_Wr:     std_logic;
-     
-signal    S_AW_Config2:        std_logic_vector(15 downto 0);
-signal    S_AW_Config2_Rd:     std_logic;
-signal    S_AW_Config2_Wr:     std_logic;
-
-signal    S_Diob_Status1_Rd:   std_logic;
-signal    S_Diob_Status1_Wr:   std_logic;
-signal    S_Diob_Status2_Rd:   std_logic;
-signal    S_Diob_Status2_Wr:   std_logic;
-signal    S_AW_Status1_Rd:     std_logic;
-signal    S_AW_Status2_Rd:     std_logic;
-
-
-signal    DIOB_Sts1_rd_Out:       std_logic_vector(15 downto 0); -- Zwischenspeicher
-signal    DIOB_Sts1_rd_Strobe_o:  std_logic;        -- Output 
-signal    DIOB_Sts1_rd_shift:     std_logic_vector(2  downto 0); -- Shift-Reg.
-
-signal    s_Diob_Status1:         std_logic_vector(15 downto 0);
-signal    s_Diob_Status2:         std_logic_vector(15 downto 0);
- 
-signal    S_Dtack:             std_logic;
-signal    S_Read_Port:         std_logic_vector(Data_to_SCUB'range);
+signal    S_Dtack:                  std_logic;
+signal    S_Read_Port:              std_logic_vector(Data_to_SCUB'range);
 
 
 begin
@@ -113,30 +123,33 @@ P_Adr_Deco:  process (nReset, clk)
   begin
     if nReset = '0' then
       
-      S_Diob_Config1_Rd <= '0';  S_Diob_Config1_Wr <= '0';
-      S_Diob_Config2_Rd <= '0';  S_Diob_Config2_Wr <= '0';
-      S_AW_Config1_Rd   <= '0';  S_AW_Config1_Wr   <= '0';
-      S_AW_Config2_Rd   <= '0';  S_AW_Config2_Wr   <= '0';
+      S_Diob_Config1_Rd       <= '0';  S_Diob_Config1_Wr <= '0';
+      S_Diob_Config2_Rd       <= '0';  S_Diob_Config2_Wr <= '0';
+      S_AW_Config1_Rd         <= '0';  S_AW_Config1_Wr   <= '0';
+      S_AW_Config2_Rd         <= '0';  S_AW_Config2_Wr   <= '0';
+      
+      S_Diob_Status1_Rd       <= '0';  S_Diob_Status1_Wr <= '0';
+      S_Diob_Status2_Rd       <= '0';  S_Diob_Status2_Wr <= '0';
+      S_AW_Status1_Rd         <= '0';
+      S_AW_Status2_Rd         <= '0';
 
-      S_Diob_Status1_Rd <= '0';  S_Diob_Status1_Wr <= '0';
-      S_Diob_Status2_Rd <= '0';  S_Diob_Status2_Wr <= '0';
-      S_AW_Status1_Rd   <= '0';
-      S_AW_Status2_Rd   <= '0';
+      S_Mirr_OutReg_Maske_Rd  <= '0';  S_Mirr_OutReg_Maske_Wr   <= '0';
       
       S_Dtack <= '0';
       Rd_active <= '0';
     
     elsif rising_edge(clk) then
 
-      S_Diob_Config1_Rd <= '0';  S_Diob_Config1_Wr <= '0';
-      S_Diob_Config2_Rd <= '0';  S_Diob_Config2_Wr <= '0';
-      S_AW_Config1_Rd   <= '0';  S_AW_Config1_Wr   <= '0';
-      S_AW_Config2_Rd   <= '0';  S_AW_Config2_Wr   <= '0';
-
-      S_Diob_Status1_Rd <= '0';  S_Diob_Status1_Wr <= '0';
-      S_Diob_Status2_Rd <= '0';  S_Diob_Status2_Wr <= '0';
-      S_AW_Status1_Rd   <= '0';
-      S_AW_Status2_Rd   <= '0';
+      S_Diob_Config1_Rd       <= '0';  S_Diob_Config1_Wr        <= '0';
+      S_Diob_Config2_Rd       <= '0';  S_Diob_Config2_Wr        <= '0';
+      S_AW_Config1_Rd         <= '0';  S_AW_Config1_Wr          <= '0';
+      S_AW_Config2_Rd         <= '0';  S_AW_Config2_Wr          <= '0';
+              
+      S_Diob_Status1_Rd       <= '0';  S_Diob_Status1_Wr        <= '0';
+      S_Diob_Status2_Rd       <= '0';  S_Diob_Status2_Wr        <= '0';
+      S_AW_Status1_Rd         <= '0';
+      S_AW_Status2_Rd         <= '0';
+      S_Mirr_OutReg_Maske_Rd  <= '0';  S_Mirr_OutReg_Maske_Wr   <= '0';
 
       S_Dtack <= '0';
       Rd_active <= '0';
@@ -231,7 +244,18 @@ P_Adr_Deco:  process (nReset, clk)
               S_AW_Status2_Rd   <= '1';
               Rd_active <= '1';
             end if;
-         
+            
+            when C_Mirr_OutReg_Maske_Addr =>
+            if Ext_Wr_active = '1' then
+              S_Dtack <= '1';        
+              S_Mirr_OutReg_Maske_Wr   <= '1';
+            end if;
+            if Ext_Rd_active = '1' then
+              S_Dtack     <= '1';
+              S_Mirr_OutReg_Maske_Rd   <= '1';
+              Rd_active <= '1';
+            end if;
+   
           when others => 
             
             S_Diob_Config1_Rd <= '0';  S_Diob_Config1_Wr <= '0';
@@ -242,6 +266,8 @@ P_Adr_Deco:  process (nReset, clk)
             S_Diob_Status1_Rd <= '0';  S_Diob_Status1_Wr <= '0'; 
             S_Diob_Status2_Rd <= '0';  S_Diob_Status2_Wr <= '0';   
             S_AW_Status1_Rd   <= '0';  S_AW_Status2_Rd   <= '0';
+
+            S_Mirr_OutReg_Maske_Rd  <= '0';  S_Mirr_OutReg_Maske_Wr   <= '0';
                       
             S_Dtack <= '0';
             Rd_active <= '0';
@@ -269,37 +295,42 @@ P_AWOut_Reg:  process (nReset, clk,
         S_Diob_Config1(15 downto 1) <= Data_from_SCUB_LA(15 downto 1);   -- Output-Daten vom SCU-Bus
         S_Diob_Config1(0)           <= '0';                              -- mit Bit-0 werden die Tag-Masken gelöscht, wird nicht gespeichert
       end if;
-      if S_Diob_Config2_Wr = '1' then  S_Diob_Config2 <= Data_from_SCUB_LA;
+      if S_Diob_Config2_Wr      = '1' then  S_Diob_Config2 <= Data_from_SCUB_LA;
       end if;
-      if S_AW_Config1_Wr = '1'   then  S_AW_Config1 <= Data_from_SCUB_LA;
+      if S_AW_Config1_Wr        = '1' then  S_AW_Config1 <= Data_from_SCUB_LA;
+      end if;       
+      if S_AW_Config2_Wr        = '1' then  S_AW_Config2 <= Data_from_SCUB_LA;
       end if;
-      if S_AW_Config2_Wr = '1'   then  S_AW_Config2 <= Data_from_SCUB_LA;
+      if S_Mirr_OutReg_Maske_Wr = '1' then  S_Mirr_OutReg_Maske <= Data_from_SCUB_LA;
       end if;
 
   end if;
   end process P_AWOut_Reg;
   
 
-  P_read_mux:  process (S_Diob_Config1_Rd,  S_Diob_Config1,
-                        S_Diob_Config2_Rd,  S_Diob_Config2,
-                        S_AW_Config1_Rd,    S_AW_Config1,
-                        S_AW_Config2_Rd,    S_AW_Config2,
-                        S_Diob_Status1_Rd,  s_Diob_Status1,
-                        S_Diob_Status2_Rd,  s_Diob_Status2,
-                        S_AW_Status1_Rd,    AW_Status1,
-                        S_AW_Status2_Rd,    AW_Status2)
+  P_read_mux:  process (S_Diob_Config1_Rd,      S_Diob_Config1,
+                        S_Diob_Config2_Rd,      S_Diob_Config2,
+                        S_AW_Config1_Rd,        S_AW_Config1,
+                        S_AW_Config2_Rd,        S_AW_Config2,
+                        S_Diob_Status1_Rd,      s_Diob_Status1,
+                        S_Diob_Status2_Rd,      s_Diob_Status2,
+                        S_AW_Status1_Rd,        AW_Status1,
+                        S_AW_Status2_Rd,        AW_Status2,
+                        S_Mirr_OutReg_Maske_Rd, S_Mirr_OutReg_Maske)
 
   begin
 
-    if    S_Diob_Config1_Rd = '1' then  S_Read_port <= S_Diob_Config1;
-    elsif S_Diob_Config2_Rd = '1' then  S_Read_port <= S_Diob_Config2;
-    elsif S_AW_Config1_Rd   = '1' then  S_Read_port <= S_AW_Config1;
-    elsif S_AW_Config2_Rd   = '1' then  S_Read_port <= S_AW_Config2;
+    if    S_Diob_Config1_Rd       = '1' then  S_Read_port <= S_Diob_Config1;
+    elsif S_Diob_Config2_Rd       = '1' then  S_Read_port <= S_Diob_Config2;
+    elsif S_AW_Config1_Rd         = '1' then  S_Read_port <= S_AW_Config1;
+    elsif S_AW_Config2_Rd         = '1' then  S_Read_port <= S_AW_Config2;
+      
+    elsif S_Diob_Status1_Rd       = '1' then  S_Read_port <= s_Diob_Status1;    -- read Input-Port1
+    elsif S_Diob_Status2_Rd       = '1' then  S_Read_port <= s_Diob_Status2;
+    elsif S_AW_Status1_Rd         = '1' then  S_Read_port <= AW_Status1;
+    elsif S_AW_Status2_Rd         = '1' then  S_Read_port <= AW_Status2;
 
-    elsif S_Diob_Status1_Rd = '1' then  S_Read_port <= s_Diob_Status1;    -- read Input-Port1
-    elsif S_Diob_Status2_Rd = '1' then  S_Read_port <= s_Diob_Status2;
-    elsif S_AW_Status1_Rd   = '1' then  S_Read_port <= AW_Status1;
-    elsif S_AW_Status2_Rd   = '1' then  S_Read_port <= AW_Status2;
+    elsif S_Mirr_OutReg_Maske_Rd  = '1' then  S_Read_port <= s_Mirr_OutReg_Maske;
 
   else
       S_Read_Port <= (others => '-');
@@ -468,10 +499,13 @@ LA <=      (x"000" &
 Dtack_to_SCUB <= S_Dtack;
 Data_to_SCUB <= S_Read_Port;
 
-Diob_Config1      <=  S_Diob_Config1;     -- Daten-Reg. AWOut1
-Diob_Config2      <=  S_Diob_Config2;     -- Daten-Reg. AWOut2
-AW_Config1        <=  S_AW_Config1;       -- Daten-Reg. AWOut3
-AW_Config2        <=  S_AW_Config2;       -- Daten-Reg. AWOut4
+Diob_Config1      <=  s_Diob_Config1;       -- Daten-Reg. AWOut1
+Diob_Config2      <=  s_Diob_Config2;       -- Daten-Reg. AWOut2
+AW_Config1        <=  s_AW_Config1;         -- Daten-Reg. AWOut3
+AW_Config2        <=  s_AW_Config2;         -- Daten-Reg. AWOut4
+
+Mirr_OutReg_Maske <=  s_Mirr_OutReg_Maske;  -- Maskierung für Spiegel-Modus des Ausgangsregisters
+
 
 
 Diob_Config1_wr   <=  S_Diob_Config1_Wr;  -- write-Strobe, Daten-Reg. AWOut1
