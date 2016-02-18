de2ea88cea80a6d42ce9abf0ecb1467ab223455d
Stefan Rauch <s.rauch@gsi.de>
2016-02-18 13:49:09 +0100

    scu slaves: added missing tcl files

---
+++ syn/scu_sio3/scu_sio3.tcl
@@ -0,0 +1,7 @@
+set platform "SIO"        
+source ../autogen.tcl
+source ../../modules/build_id/build_id_short.tcl 
+source ../../ip_cores/general-cores/platform/altera/networks/arria2gx.tcl
+source ../../ip_cores/general-cores/platform/altera/wb_pcie/arria2.tcl
+source ../../modules/pll/arria2/arria2_pll.tcl
+source ../../ip_cores/wr-cores/platform/altera/wr_arria2_phy/wr_arria2_phy.tcl
