cbb3165e5fe8233f8ab3eada300c2b24484d6da4
A. Hahn <a.hahn@gsi.de>
2016-05-24 15:53:56 +0200

    avsoc: empty initial build project

---
+++ top/gsi_avsoc/av_rocket_board/Manifest.py
@@ -0,0 +1,11 @@
+files = [
+   "av_rocket_board.vhd",
+   "av_rocket_board.sdc",
+   "ramsize_pkg.vhd",
+]
+
+modules = {
+  "local" : [
+    "../../..",
+  ]
+}
---
+++ top/gsi_avsoc/av_rocket_board/av_rocket_board.vhd
@@ -0,0 +1,22 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+library work;
+use work.monster_pkg.all;
+use work.ramsize_pkg.c_lm32_ramsizes;
+
+entity av_rocket_board is
+end av_rocket_board;
+
+architecture rtl of av_rocket_board is
+
+  -- Constants and signals ...
+
+begin
+
+  -- Monster ...
+
+  -- SOC ...
+  
+end rtl;
