bf3d5bf51f6cc66d6933ee8e0c65b8b297c21b96
Stefan Rauch <s.rauch@gsi.de>
2016-10-07 14:24:15 +0200

    diob_module2: set unused leds to off

--- top/scu_diob_v2/diob_module2_arch.vhd
+++ top/scu_diob_v2/diob_module2_arch.vhd
@@ -8,11 +8,16 @@ use work.scu_bus_slave_pkg.all;
 use work.scu_diob_pkg.all;
 
 architecture diob_module2_arch of diob_module is
-  signal en:    std_logic;
-  signal sreg:  std_logic_vector(7 downto 0) := "10000000";
+
+  alias A_nLED7: std_logic is vect_en(17);
+  alias A_nLED6: std_logic is vect_en(19);
+  alias A_nLED5: std_logic is vect_en(21);
+  alias A_nLED4: std_logic is vect_en(23);
+  alias A_nLED3: std_logic is vect_en(25);
+  alias A_nLED2: std_logic is vect_en(27);
+  alias A_nLED1: std_logic is vect_en(29);
+  alias A_nLED0: std_logic is vect_en(31);
 begin
-  vect_i  <= x"0000000000000000000000000000000"& "000";
-  
   vect_o(17) <= '1'; --A_nLED7
   vect_o(19) <= '1'; --A_nLED6
   vect_o(21) <= '1'; --A_nLED5
@@ -28,8 +33,16 @@ begin
     if rising_edge(clk) then
         count := count - 1;
     end if;
-    vect_en(19) <= count(26);
+    A_nLED0 <= count(26);
   end process; 
+  
+  A_nLED7 <= '1'; --led off
+  A_nLED6 <= '1'; --led off
+  A_nLED5 <= '1'; --led off
+  A_nLED4 <= '1'; --led off
+  A_nLED3 <= '1'; --led off
+  A_nLED2 <= '1'; --led off
+  A_nLED1 <= '1'; --led off
   
   irq_o <= (others => '0'); -- no irqs for this module
 
