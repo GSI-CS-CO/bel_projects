c5a8d9c5430c4f3263e517c9217cb41c2d7f61ea
Stefan Rauch <s.rauch@gsi.de>
2016-10-07 14:26:09 +0200

    diob_v2: use muxes instead of tristate logic

--- top/scu_diob_v2/diob_pio_tristate.vhd
+++ top/scu_diob_v2/diob_pio_tristate.vhd
@@ -17,7 +17,8 @@ entity diob_pio_tristate is
     pio:                inout std_logic_vector(pio_max downto pio_min);
     bit_vector_config:  in    t_pio_bit_vectors_array;
     module_io_i:        out   std_logic_vector(pio_max downto pio_min);
-    irq_o:              out   std_logic_vector(15 downto 1));
+    irq_o:              out   std_logic_vector(15 downto 1);
+    mod_en:             out   std_logic_vector(module_count-1 downto 0));
 end entity diob_pio_tristate;
 
 architecture arch of diob_pio_tristate is
@@ -27,21 +28,42 @@ architecture arch of diob_pio_tristate is
   
 
 begin
-  vector_mux:
-  for i in 0 to module_count-1 generate
-  --for i in 5 to 5 generate
-    sel_bit_vector_in  <= bit_vector_config(i).vect_in  when module_list(i).id = module_id else (others => 'Z');
-    sel_bit_vector_out <= bit_vector_config(i).vect_out when module_list(i).id = module_id else (others => 'Z');
-    sel_bit_vector_en  <= bit_vector_config(i).vect_en  when module_list(i).id = module_id else (others => 'Z');
-    irq_o              <= bit_vector_config(i).irq      when module_list(i).id = module_id else (others => 'Z');
-  end generate vector_mux; 
-
+  vector_mux: process (module_id, bit_vector_config)
+    variable sel_bit_vector_in_tmp:   std_logic_vector(pio_max downto pio_min);
+    variable sel_bit_vector_out_tmp:  std_logic_vector(pio_max downto pio_min);
+    variable sel_bit_vector_en_tmp:   std_logic_vector(pio_max downto pio_min);
+    variable irq_tmp:                 std_logic_vector(15 downto 1);
+  begin
+    sel_bit_vector_in_tmp   := (others => '0');
+    sel_bit_vector_out_tmp  := (others => '0');
+    sel_bit_vector_en_tmp   := (others => '0');
+    irq_tmp                 := (others => '0');
+    for i in 0 to module_count-1 loop
+      if module_list(i).id = module_id then
+        sel_bit_vector_in_tmp   := sel_bit_vector_in_tmp or bit_vector_config(i).vect_in;
+        sel_bit_vector_out_tmp  := sel_bit_vector_out_tmp or bit_vector_config(i).vect_out;
+        sel_bit_vector_en_tmp   := sel_bit_vector_en_tmp or bit_vector_config(i).vect_en;
+        irq_tmp                 := irq_tmp or bit_vector_config(i).irq;
+      end if;
+    end loop;
+    irq_o               <= irq_tmp;
+    sel_bit_vector_in   <= sel_bit_vector_in_tmp;
+    sel_bit_vector_out  <= sel_bit_vector_out_tmp;
+    sel_bit_vector_en   <= sel_bit_vector_en_tmp;
+    
+  end process;
+  
   module_io_i <= pio;
   
   pio_out:
   for i in pio_min to pio_max generate
     pio(i) <= sel_bit_vector_en(i) when sel_bit_vector_out(i) = '1' else 'Z';
   end generate pio_out; 
+  
+  en:
+  for i in 0 to module_count-1 generate
+    mod_en(i) <= '1' when module_list(i).id = module_id else '0';
+  end generate en;
 
 
 end architecture;
