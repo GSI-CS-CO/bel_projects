fd7c34bad0d2098f000957d3972614b09cfbf52d
Mathias Kreider <mkreider@gsi.de>
2016-10-21 14:57:47 +0200

    ftm: fixed branch points to only kick in after full repetitions

--- modules/ftm/ftmfw/ftm.c
+++ modules/ftm/ftmfw/ftm.c
@@ -314,7 +314,23 @@ inline t_ftmChain* processChainAux(t_ftmChain* c)
             if(c->flags & FLAGS_IS_SIG_ALL)  pFtmIf->sema.sig  = 1;
             if(c->flags & FLAGS_IS_COND_ALL) pFtmIf->sema.cond = 1;
 
+            if((c->flags & FLAGS_IS_BP) && (pFtmIf->pAct->pBp != NULL))       
+           { 
+              pCur = pFtmIf->pAct->pBp;  //BP? go to alt chain
+              
+              pFtmIf->sctr      = 0;
+              pFtmIf->pAct->pBp = NULL;
+              pFtmIf->sema.sig  = 1;
+              pFtmIf->sema.cond = 1;
+              
+              DBPRINT3("BP SemaCond: %u\n", pFtmIf->sema.cond);
+           }
+
          }
+
+                 //is c a branchpoint? if so, jump, reset sequence counter (sctr), semaphores and BP
+         
+       
       } 
       else
       {
@@ -330,21 +346,12 @@ inline t_ftmChain* processChainAux(t_ftmChain* c)
          DBPRINT3("NC SemaCond: %u\n", pFtmIf->sema.cond);
         
       }
-      
+
+
+    
    }
    
-   //is c a branchpoint? if so, jump, reset sequence counter (sctr), semaphores and BP
-   if((c->flags & FLAGS_IS_BP) && (pFtmIf->pAct->pBp != NULL))       
-   { 
-      pCur = pFtmIf->pAct->pBp;  //BP? go to alt chain
-      
-      pFtmIf->sctr      = 0;
-      pFtmIf->pAct->pBp = NULL;
-      pFtmIf->sema.sig  = 1;
-      pFtmIf->sema.cond = 1;
-      
-      DBPRINT3("BP SemaCond: %u\n", pFtmIf->sema.cond);
-   }
+   
     
    return pCur;    
 }
