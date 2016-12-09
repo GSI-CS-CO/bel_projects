778275fcc3480186bd743631ae76e66741de0b23
A. Hahn <a.hahn@gsi.de>
2016-12-09 14:54:36 +0100

    arria2, ifa8, nau8811: fixed build flow

--- modules/nau8811/Manifest.py
+++ modules/nau8811/Manifest.py
@@ -1,7 +1,7 @@
-files = [ 
-  "src/hdl/generic_iis_master_pkg.vhd",
-  "src/hdl/generic_iis_master.vhd",
-  "src/hdl/wb_nau8811_audio_driver_pkg.vhd",
-  "src/hdl/wb_nau8811_audio_driver.vhd",
-  "src/hdl/altera_pll/audio_pll_ref.qip",
-]
+def __audio_check():
+  syn_files = []
+  if syn_device[:1] == "5": syn_files = [ "src/hdl/generic_iis_master_pkg.vhd", "src/hdl/generic_iis_master.vhd", "src/hdl/wb_nau8811_audio_driver_pkg.vhd", "src/hdl/wb_nau8811_audio_driver.vhd", "src/hdl/altera_pll/audio_pll_ref.qip" ]
+  else:                     syn_files = [ "src/hdl/wb_nau8811_audio_driver_pkg.vhd" ]
+  return syn_files
+
+files = __audio_check()
