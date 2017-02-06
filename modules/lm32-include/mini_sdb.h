31b793f9c4b13e38706084435059469c172080de
Stefan Rauch <s.rauch@gsi.de>
2017-02-06 10:25:08 +0100

    dev-bus: added special sdb record for dev-bus irq master

--- modules/lm32-include/mini_sdb.h
+++ modules/lm32-include/mini_sdb.h
@@ -67,6 +67,7 @@
 #define SCU_BUS_MASTER        0x9602eb6f
 #define SCU_IRQ_CTRL          0x9602eb70
 #define WB_FG_IRQ_CTRL        0x9602eb71
+#define MIL_IRQ_CTRL          0x9602eb72
 
 #define SCU_BUS_MASTER        0x9602eb6f
 #define WR_1Wire              0x779c5443
