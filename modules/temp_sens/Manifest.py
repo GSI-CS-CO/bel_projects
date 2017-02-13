3f71858f3fa43c855728223273638be98b6c4d21
A.Suresh <a.suresh@gsi.de>
2017-02-13 14:09:56 +0100

    temp_sense:Added temperature sensor IP core for merging with proposed master

---
+++ modules/temp_sens/Manifest.py
@@ -0,0 +1,7 @@
+files = [
+  "hdl/temp_sensor_pkg.vhd",
+  "hdl/wb_temp_sense_pkg.vhd",
+  "hdl/wb_temp_sense.vhd", 
+  "synthesis/temp_sens.vhd",
+  "synthesis/submodules/temp_sens_temp_sense_0.v",
+]
