f822bab09293eed3152273ff3a2ef444eca2264c
Mathias Kreider <mkreider@gsi.de>
2017-03-03 15:30:39 +0100

    add defines for blocks and events

--- modules/ftm/ftm_common.h
+++ modules/ftm/ftm_common.h
@@ -2,18 +2,20 @@
 #define _FTM_COMMON_H_
 #include <inttypes.h>
 #include <stdint.h>
+#include "ftm_common_legacy.h"
 
-
-
-#define FTM_RST_FPGA        0x0
-#define FTM_RST_FPGA_CMD    0xDEADBEEF
-#define FTM_RST_GET         (FTM_RST_FPGA + 4)
-#define FTM_RST_SET         (FTM_RST_GET  + 4)
-#define FTM_RST_CLR         (FTM_RST_SET  + 4)
-
-#define FTM_SHARED_OFFSET_NEW 0x0500
-#define FTM_SHARED_OFFSET     FTM_SHARED_OFFSET_NEW
-
+//////////////////////////////////////////////////////////////////////
+//  DM FPGA Control                                                 //
+//////////////////////////////////////////////////////////////////////
+#define FTM_RST_FPGA        0x0                                     //              
+#define FTM_RST_FPGA_CMD    0xDEADBEEF                              //
+#define FTM_RST_GET         (FTM_RST_FPGA + 4)                      //
+#define FTM_RST_SET         (FTM_RST_GET  + 4)                      //
+#define FTM_RST_CLR         (FTM_RST_SET  + 4)                      //
+                                                                    //
+#define FTM_SHARED_OFFSET_NEW 0x0500                                //
+#define FTM_SHARED_OFFSET     FTM_SHARED_OFFSET_NEW                 //
+                                                                    //
 //////////////////////////////////////////////////////////////////////
 //  DM Configuration                                                //
 //////////////////////////////////////////////////////////////////////
@@ -39,7 +41,9 @@
 #define _STAT_SIZE_     (QTY_STATUS_FIELDS * _32b_SIZE_)
 #define _IBS_SIZE_      (QTY_CORES * QTY_THREADS * _32b_SIZE_)
 
-
+//////////////////////////////////////////////////////////////////////
+//  Dynamic Memory Allocation System                                //
+//////////////////////////////////////////////////////////////////////
 //Live Block
 #define _LB_START_    0
 #define LB_PTR        (_LB_START_)
@@ -47,7 +51,6 @@
 #define _LB_SIZE_     (LB_SIZE  + _OFFS_SIZE_)
 
 //Live Block Table
-
 //Live Block Table Bitmap
 #define _LBT_BMP_SIZE_  ((QTY_BLOCKS + QTY_BLOCKS-1)/32 * _32b_SIZE_)
 //Live Block Table
@@ -117,131 +120,117 @@
 #define SHCTL_THR_CTL    (SHCTL_U_REQ        + _UR_SIZE_  ) //Thread Control Registers (Start Stop Status)
 #define SHCTL_THR_DAT    (SHCTL_THR_CTL      + _TC_SIZE_  ) //Thread Data (1 per Thread )
 #define SHCTL_INBOXES    (SHCTL_THR_DAT      + _TDS_SIZE_ ) //Inboxes for MSI (1 per Core in System )
-
-/// LEGACY SHIT
-#define FTM_IDLE_OFFSET  (SHCTL_INBOXES      + _IBS_SIZE_ ) 
-#define FTM_DEBUG_DATA   (FTM_IDLE_OFFSET    + _32b_SIZE_ )     //c8
-#define _SHCTL_SIZE_     (FTM_DEBUG_DATA     + _32b_SIZE_ )
-
-/////////////////////////////////////////////////////////////////////////////////
-// Backward compatible layouts
-/////////////////////////////////////////////////////////////////////////////////
-
-#define THR_SHARED_SIZE     16
-#define CPU_SHARED_SIZE     (8*THR_SHARED_SIZE + 40) 
-
-#define FTM_PAGE_QTY        0
-#define FTM_PLAN_MAX        16
-#define FTM_PAGEDATA        0x0600
-#define FTM_PAGEMETA        (4 + 4 * FTM_PLAN_MAX + 4 + 4)   
-#define FTM_PAGESIZE        (FTM_PAGEDATA + FTM_PAGEMETA)
-
-#define BUF_SIZE            FTM_PAGESIZE
+//////////////////////////////////////////////////////////////////////
 
 
+//////////////////////////////////////////////////////////////////////
+// Payload Data (Blocks and Events                                  //
+//////////////////////////////////////////////////////////////////////
+// Block Hdr
+#define BLOCK_PERIOD        0
+#define BLOCK_START_TIME    (BLOCK_PERIOD       + 8)
+#define BLOCK_FLAGS         (BLOCK_START_TIME   + 8)
+#define BLOCK_Q_OFF         (BLOCK_FLAGS        + 2)
+#define BLOCK_NEXT_IDX      (BLOCK_Q_OFF        + 2)
+// this will be removed > v4.0 /////////////////////////
+#define BLOCK_EVT_QTY       (BLOCK_NEXT_IDX     + 4)  //
+#define BLOCK_EVT_CNT       (BLOCK_EVT_QTY      + 4)  //
+#define BLOCK_CURRENT_TIME  (BLOCK_EVT_CNT      + 4)  //
+#define _BLOCK_HDR_SIZE     (BLOCK_CURRENT_TIME + 8)  //
+////////////////////////////////////////////////////////
+//#define _BLOCK_HDR_SIZE     (BLOCK_NEXT_IDX     + 4)
+
+// Event Hdr
+#define OFFS_TIME_NOW       0xFFFFFFFFFFFFFFFFULL
+#define OFFS_TIME_PPS       0xFFFFFFFFFFFFFFFEULL
+
+#define EVT_OFFS_TIME       0
+#define EVT_FLAGS           (EVT_OFFS_TIME  + 8)
+#define _EVT_HDR_SIZE       (EVT_FLAGS      + 4)    
+
+// Timing Message Event
+#define EVT_TM_ID           (_EVT_HDR_SIZE)
+#define EVT_TM_PAR          (EVT_TM_ID      + 8)
+#define EVT_TM_TEF          (EVT_TM_PAR     + 8)
+
+// Command Message Event
+#define EVT_CM_TIME         (_EVT_HDR_SIZE)
+#define EVT_CM_ACT          (EVT_CM_TIME    + 8)
+
+#define _EVT_SIZE           (_EVT_HDR_SIZE  + 20)
+
+//////////////////////////////////////////////////////////////////////
 
 
 
-   
+//////////////////////////////////////////////////////////////////////
+//Command Queue System structures                                   //
+//////////////////////////////////////////////////////////////////////
+// 3 priorities, Low, High, Interlock
+#define PRIO_LO 0
+#define PRIO_HI 1
+#define PRIO_IL 2
 
+//Command structure, Action & Timestamp
+#define CMD_TS_OFF            0x0
+#define CMD_ACT_OFF           (CMD_TS_OFF  + 8)
+#define _CMD_SIZE             (CMD_ACT_OFF + 4)
 
-#define DBG_DISP_DUR_MIN      0
-#define DBG_DISP_DUR_MAX      DBG_DISP_DUR_MIN+1
-#define DBG_DISP_DUR_AVG      DBG_DISP_DUR_MAX+1
+//Action substructure
+//FIXME this is very odd... add unshifted values?
+#define CMD_TYPE_MSK          0xf0000000
+#define ACT_TYPE_NOP          0x00000000
+#define ACT_TYPE_FLOW         0x10000000
+#define ACT_TYPE_FLUSH        0x20000000 // save the bitshift
+#define ACT_TYPE_POS          28
 
+//Action of Nop type
+#define ACT_FNF_QTY_MSK       0x0000ffff
+#define ACT_FNF_QTY_POS       0
+//Action of Flow type
+#define ACT_FLOW_NEXT_MSK     0x0fff0000
+#define ACT_FLOW_NEXT_POS     16
+//Action of Flush type
+#define ACT_FLUSH_LO_MSK      0x00010000
+#define ACT_FLUSH_LO_POS      16 
+#define ACT_FLUSH_HI_MSK      0x00020000
+#define ACT_FLUSH_HI_POS      17   
+#define ACT_FLUSH_LO_RNG_MSK  0x000000ff
+#define ACT_FLUSH_LO_RNG_POS  0
+#define ACT_FLUSH_HI_RNG_MSK  0x0000ff00
+#define ACT_FLUSH_HI_RNG_POS  8
 
-//masks & constants
-#define CMD_RST           		0x0001	//Reset FTM status and counters
-#define CMD_START      		    0x0002	//Start FTM
-#define CMD_IDLE   		        0x0004	//Jump into IDLE at next BP
-#define CMD_STOP_REQ          0x0008	//Stop FTM if when it reaches IDLE state
-#define CMD_STOP_NOW          0x0010	//Stop FTM immediately
+//Command Queue structure
+#define CMDQ_QTY              4 ///
+#define _CMDQ_BUF_SIZE        (CMDQ_QTY * _CMD_SIZE)
+#define CMDQ_IDX_OF_MSK       (2 * CMDQ_QTY -1) // x2 for overflow bit
+#define CMDQ_IDX_MSK          (CMDQ_QTY -1) // 
+#define _CMDQ_IDX_SIZE        4
+#define CMDQ_OFF              0
+#define CMDQ_RD_OFF           (CMDQ_OFF)
+#define CMDQ_WR_OFF           (CMDQ_RD_OFF  + _CMDQ_IDX_SIZE)
+#define CMDQ_BUF_OFF          (CMDQ_WR_OFF  + _CMDQ_IDX_SIZE)
+#define _CMDQ_SIZE            (CMDQ_BUF_OFF + _CMDQ_BUF_SIZE)
 
-#define CMD_COMMIT_PAGE       0x0100  //Commmit new data and validate
-#define CMD_COMMIT_BP         0x0200  //Commit alt Plan pointer. Will be selected at next BP if not NULL
-#define CMD_PAGE_SWAP         0x0400  //swap Page at next BP
-//#define CMD_SHOW_ACT          0x0800
-//#define CMD_SHOW_INA          0x1000
-#define CMD_DBG_0             0x2000  //DBG case 0
-#define CMD_DBG_1             0x4000  //DBG case 1
-
-#define STAT_RUNNING          (1<<0)   //the FTM is running
-#define STAT_IDLE             (1<<1)   //the FTM is idling  
-#define STAT_STOP_REQ         (1<<2)   //alt ptr has been set to IDLE 
-#define STAT_ERROR            (1<<3)   //FTM encountered an error, check error register
-#define STAT_WAIT             (1<<4)   //FTM waiting on condition
-#define STAT_STOPPED          (1<<5)   //FTM is Stopped - will not execute anything 
-
-
-#define ID_MSK_B16            0xffff
-#define ID_FID_LEN            4
-#define ID_GID_LEN            12
-#define ID_EVTNO_LEN          12
-#define ID_SID_LEN            12
-#define ID_BPID_LEN           14
-#define ID_SCTR_LEN           10
-#define ID_FID_POS            (ID_GID_LEN + ID_EVTNO_LEN + ID_SID_LEN + ID_BPID_LEN + ID_SCTR_LEN)
-#define ID_GID_POS            (ID_EVTNO_LEN + ID_SID_LEN + ID_BPID_LEN + ID_SCTR_LEN)
-#define ID_EVTNO_POS          (ID_SID_LEN + ID_BPID_LEN + ID_SCTR_LEN)
-#define ID_SID_POS            (ID_BPID_LEN + ID_SCTR_LEN)
-#define ID_BPID_POS           (ID_SCTR_LEN)
-#define ID_SCTR_POS           0
-
-#define FLAGS_IS_BP           (1<<0)
-
-#define FLAGS_IS_COND_MSI     (1<<4)
-#define FLAGS_IS_COND_SHARED  (1<<5)
-#define FLAGS_IS_COND_ADR     (1<<6)
-#define FLAGS_IS_COND_TIME    (1<<7)
-#define FLAGS_IS_COND_ALL     (1<<8)
-
-#define FLAGS_IS_SIG_MSI      (1<<12)
-#define FLAGS_IS_SIG_SHARED   (1<<13)
-#define FLAGS_IS_SIG_ADR      (1<<14)
-#define FLAGS_IS_SIG_TIME     (1<<15)
-
-#define FLAGS_IS_SIG_FIRST    (1<<16)
-#define FLAGS_IS_SIG_LAST     (1<<17)
-#define FLAGS_IS_SIG_ALL      (1<<18)
-
-#define FLAGS_IS_START        (1<<20) 
-#define FLAGS_IS_END          (1<<21) 
-#define FLAGS_IS_ENDLOOP      (1<<22)
-#define FLAGS_IS_PERS_REP_CNT (1<<23)
-
-
-#define FTM_TIME_SIZE         8
-#define FTM_DWORD_SIZE        8
-#define FTM_WORD_SIZE         4
-#define FTM_PTR_SIZE          4
-#define FTM_NULL              0x0
-
-#define FTM_MSG_ID            0
-#define FTM_MSG_PAR           (FTM_MSG_ID    + 8)
-#define FTM_MSG_TEF           (FTM_MSG_PAR   + 8)
-#define FTM_MSG_RES           (FTM_MSG_TEF   + 4)
-#define FTM_MSG_TS            (FTM_MSG_RES   + 4)
-#define FTM_MSG_OFFS          (FTM_MSG_TS    + 8)
-#define FTM_MSG_END_          (FTM_MSG_OFFS  + 8)
-
-#define FTM_CHAIN_TTRN          0
-#define FTM_CHAIN_TSTART        0
-#define FTM_CHAIN_TPERIOD       (FTM_CHAIN_TSTART         + 8)
-#define FTM_CHAIN_TEXEC         (FTM_CHAIN_TPERIOD        + 8)
-#define FTM_CHAIN_FLAGS         (FTM_CHAIN_TEXEC          + 8)
-#define FTM_CHAIN_CONDSRC       (FTM_CHAIN_FLAGS          + 4)
-#define FTM_CHAIN_CONDVAL       (FTM_CHAIN_CONDSRC        + 4)
-#define FTM_CHAIN_CONDMSK       (FTM_CHAIN_CONDVAL        + 4)
-#define FTM_CHAIN_SIGDST        (FTM_CHAIN_CONDMSK        + 4)
-#define FTM_CHAIN_SIGVAL        (FTM_CHAIN_SIGDST         + 4)
-#define FTM_CHAIN_REPQTY        (FTM_CHAIN_SIGVAL         + 4)
-#define FTM_CHAIN_REPCNT        (FTM_CHAIN_REPQTY         + 4)
-#define FTM_CHAIN_MSGQTY        (FTM_CHAIN_REPCNT         + 4)
-#define FTM_CHAIN_MSGIDX        (FTM_CHAIN_MSGQTY         + 4)
-#define FTM_CHAIN_PMSG          (FTM_CHAIN_MSGIDX         + 4)
-#define FTM_CHAIN_PNEXT         (FTM_CHAIN_PMSG           + 4)
-#define FTM_CHAIN_PIDX          (FTM_CHAIN_PNEXT          + 4)
-#define FTM_CHAIN_END_          (FTM_CHAIN_PIDX           + 4)
+//Command Queue System structure (Triple CMDQ)
+//Interlock Queue
+#define CMDQS_OFF             0
+#define CMDQ_LO_RD_OFF        (CMDQS_OFF       + 0)
+#define CMDQ_LO_OFF           (CMDQ_LO_RD_OFF)
+#define CMDQ_LO_WR_OFF        (CMDQ_LO_RD_OFF  + _CMDQ_IDX_SIZE)
+#define CMDQ_LO_BUF_OFF       (CMDQ_LO_WR_OFF  + _CMDQ_IDX_SIZE)
+//High Priority Queue
+#define CMDQ_HI_RD_OFF        (CMDQ_LO_BUF_OFF + _CMDQ_BUF_SIZE)
+#define CMDQ_HI_OFF           (CMDQ_HI_RD_OFF)
+#define CMDQ_HI_WR_OFF        (CMDQ_HI_RD_OFF  + _CMDQ_IDX_SIZE)
+#define CMDQ_HI_BUF_OFF       (CMDQ_HI_WR_OFF  + _CMDQ_IDX_SIZE)
+//Low Priority Queue
+#define CMDQ_IL_RD_OFF        (CMDQ_HI_BUF_OFF + _CMDQ_BUF_SIZE)
+#define CMDQ_IL_OFF           (CMDQ_IL_RD_OFF)
+#define CMDQ_IL_WR_OFF        (CMDQ_IL_RD_OFF  + _CMDQ_IDX_SIZE)
+#define CMDQ_IL_BUF_OFF       (CMDQ_IL_WR_OFF  + _CMDQ_IDX_SIZE)
+#define CMDQS_SIZE            (CMDQ_IL_BUF_OFF + _CMDQ_BUF_SIZE)
+//////////////////////////////////////////////////////////////////////
 
 #endif
 
