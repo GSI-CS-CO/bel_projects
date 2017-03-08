ff02286b83628bde59dd8b5f8463718e994e8153
Mathias Kreider <mkreider@gsi.de>
2017-03-08 16:30:44 +0100

    dm: event class working

--- modules/ftm/ftmx86/event.cpp
+++ modules/ftm/ftmx86/event.cpp
@@ -2,48 +2,42 @@
 #include <stdio.h>
 #include "event.h"
 
-Event::Event(void) {
-  printf("Create Event Base\n");
+
+
+//TimingMsg::TimingMsg(uint64_t tOffs, uint16_t flags, uint64_t id, uint64_t par, uint32_t tef) : Event (tOffs, flags), id(id), par(par), tef(tef) {}
+
+void TimingMsg::show(void) {
+  TimingMsg::show(0, "");
 }
 
-TimingMsg::TimingMsg(uint64_t tOffs, uint16_t flags, uint64_t id, uint64_t par, uint32_t tef) : Event (tOffs, flags) id(id), par(par), tef(tef) {}
-
-TimingMsg::show() {
-  TimingMsg::Show(0, "");
-}
-
-TimingMsg::show(uint32_t cnt, const char* prefix) {
+void TimingMsg::show(uint32_t cnt, const char* prefix) {
   char* p;
-  if (prefix == NULL) p = "";
+  if (prefix == NULL) p = (char*)"";
   else p = (char*)prefix;
   printf("%s***------- %3u -------\n", p, cnt);
-  printf("%s*** TimingMsg @ %llu\n", p, (long long unsigned int)e->tOffs);
-  printf("%sID 0x%08x%08x, Par 0x%08x%08x, Tef 0x%08x\n", p, (uint32_t)(e->t.id >> 32),
-  (uint32_t)e->t.id, (uint32_t)(e->t.par >> 32), (uint32_t)e->t.par, e->t.tef);
+  printf("%s*** TimingMsg @ %llu\n", p, (long long unsigned int)this->tOffs);
+  printf("%sID 0x%08x%08x, Par 0x%08x%08x, Tef 0x%08x\n", p, (uint32_t)(this->id >> 32),
+  (uint32_t)this->id, (uint32_t)(this->par >> 32), (uint32_t)this->par, this->tef);
 }
 
-TimingMsg::~TimingMsg() {}
 
 
-Command::Command(uint64_t tOffs, uint16_t flags, uint64_t tValid, uint32_t act) : Event (tOffs, flags), tValid, act{
-  this->tValid  = tValid;
-  this->act     = act;
+//Command::Command(uint64_t tOffs, uint16_t flags, uint64_t tValid, uint32_t act) : Event (tOffs, flags), tValid(tValid), act(act) {}
+
+void Command::show(void) {
+  Command::show(0, "");
 }
 
-Command::show() {
-  Command::Show(0, "");
-}
-
-Command::show(uint32_t cnt, const char* prefix) {
+void Command::show(uint32_t cnt, const char* prefix) {
   char* p;
-  if (prefix == NULL) p = "";
+  if (prefix == NULL) p = (char*)"";
   else p = (char*)prefix;
   printf("%s***------- %3u -------\n", p, cnt);
-  printf("%s*** Command   @ %llu\n", p, (long long unsigned int)e->tOffs);
-  printf("%sValid @ %llu, Action 0x%08x\n", p, (long long unsigned int)e->c.tValid, e->c.act);
+  printf("%s*** Command   @ %llu\n", p, (long long unsigned int)this->tOffs);
+  printf("%sValid @ %llu, Action 0x%08x\n", p, (long long unsigned int)this->tValid, this->act);
 }
 
-Command::~Command() {}
+
 
 
 
--- modules/ftm/ftmx86/event.h
+++ modules/ftm/ftmx86/event.h
@@ -6,14 +6,17 @@
 #include <stdint.h>
 
 class Event {
+protected:  
   uint64_t tOffs;
+  uint16_t flags;
 
 public:
-  Event();
-  Event(uint64_t tOffs, uint16_t flags);
-  virtual ~Event();
-  virtual void show();
-  virtual uint8_t* serialise(uint8_t *buf);
+  Event() {}
+  Event(uint64_t tOffs, uint16_t flags) : tOffs(tOffs), flags(flags) {}
+  ~Event() {};
+  virtual void show(void) = 0;
+  virtual void show(uint32_t cnt, const char* sPrefix) = 0;
+  //virtual uint8_t* serialise(uint8_t *buf);
 
 };
 
@@ -23,11 +26,11 @@ class TimingMsg : public Event {
   uint32_t tef;
 
 public:
-  TimingMsg(uint64_t tOffs, uint16_t flags, uint64_t id, uint64_t par, uint32_t tef) : Event (tOffs, flags);
-  ~TimingMsg();
-  void show();
+  TimingMsg(uint64_t tOffs, uint16_t flags, uint64_t id, uint64_t par, uint32_t tef) : Event (tOffs, flags), id(id), par(par), tef(tef) {}
+  ~TimingMsg() {};
+  void show(void);
   void show(uint32_t cnt, const char* sPrefix);
-  uint8_t* serialise(uint8_t *buf);
+  //uint8_t* serialise(uint8_t *buf);
 };
 
 class Command : public Event {
@@ -35,11 +38,11 @@ class Command : public Event {
   uint32_t act;
 
 public:
-  Command(uint64_t tOffs, uint16_t flags, uint64_t tValid, uint32_t act) : Event (tOffs, flags);
-  ~Command();
-  void show();
+  Command(uint64_t tOffs, uint16_t flags, uint64_t tValid, uint32_t act) : Event (tOffs, flags), tValid(tValid), act(act) {}
+  ~Command() {};
+  void show(void);
   void show(uint32_t cnt, const char* sPrefix);
-  uint8_t* serialise(uint8_t *buf);
+  //uint8_t* serialise(uint8_t *buf);
 };
 
 
