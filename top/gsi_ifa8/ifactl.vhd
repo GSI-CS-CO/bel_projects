2054a449a4afcd5546fa9ee5885bf3ff140696dd
Stefan Rauch <s.rauch@gsi.de>
2017-05-12 12:11:40 +0200

    ifa8: fixed merge mixup with ifactl

--- top/gsi_ifa8/ifactl.vhd
+++ top/gsi_ifa8/ifactl.vhd
@@ -5,125 +5,125 @@ use ieee.numeric_std.all;
 library work;
 use work.ifa8_pkg.all;
 
-entity ifactl_c is
+entity ifactl is
 port (
-  clk:                in std_logic;                       -- system clock
-  sclr:               in std_logic;                       -- synchronous clear
-  ifa_adr:            in std_logic_vector(7 downto 0);    -- IFK address
-  fc_str:             in std_logic;                       -- function code strobe
-  clr_after_rd:       in std_logic;                       -- function code strobe
-  fc:                 in std_logic_vector(7 downto 0);    -- function code
-  di:                 in std_logic_vector(15 downto 0);   -- data set value
-  diw:                in std_logic_vector(15 downto 0);   -- data actual value
-  sts:                in std_logic_vector(7 downto 0);    -- 8 Bit status from VG connector
-  inrm:               in std_logic_vector(7 downto 0);    -- 8 Bit interrupt mask
-  ifa_ctrl:           in std_logic_vector(7 downto 0);    -- IFA internal control status
-  ifa_id:             in std_logic_vector(7 downto 0);    -- IFA identification code
-  ifp_id:             in std_logic_vector(7 downto 0);    -- IF-Piggy identification code
-  ifp_in:             in std_logic;                       -- IF-Piggy input
-  ifa_epld_vers:      in std_logic_vector(7 downto 0);    -- IFA EPLD version number
-  i2c_data:           in std_logic_vector(12 downto 0);   -- data i2c bus
-  fwl_data_sts:       in std_logic_vector(15 downto 0);   -- data or status from IFA firmware loader
-  me_vw_err:          in std_logic_vector(15 downto 0);   -- error counter ME-VW
-  me_data_err:        in std_logic_vector(15 downto 0);   -- error counter ME-Data
-  if_mode:            in std_logic_vector(15 downto 0);   -- mode of interface card
+  clk                : in std_logic;                       -- system clock
+  sclr               : in std_logic;                       -- synchronous clear
+  ifa_adr            : in std_logic_vector(7 downto 0);    -- IFK address
+  fc_str             : in std_logic;                       -- function code strobe
+  clr_after_rd       : in std_logic;                       -- function code strobe
+  fc                 : in std_logic_vector(7 downto 0);    -- function code
+  di                 : in std_logic_vector(15 downto 0);   -- data set value
+  diw                : in std_logic_vector(15 downto 0);   -- data actual value
+  sts                : in std_logic_vector(7 downto 0);    -- 8 Bit status from VG connector
+  inrm               : in std_logic_vector(7 downto 0);    -- 8 Bit interrupt mask
+  ifa_ctrl           : in std_logic_vector(7 downto 0);    -- IFA internal control status
+  ifa_id             : in std_logic_vector(7 downto 0);    -- IFA identification code
+  ifp_id             : in std_logic_vector(7 downto 0);    -- IF-Piggy identification code
+  ifp_in             : in std_logic;                       -- IF-Piggy input
+  ifa_epld_vers      : in std_logic_vector(7 downto 0);    -- IFA EPLD version number
+  i2c_data           : in std_logic_vector(12 downto 0);   -- data i2c bus
+  fwl_data_sts       : in std_logic_vector(15 downto 0);   -- data or status from IFA firmware loader
+  me_vw_err          : in std_logic_vector(15 downto 0);   -- error counter ME-VW
+  me_data_err        : in std_logic_vector(15 downto 0);   -- error counter ME-Data
+  if_mode            : in std_logic_vector(15 downto 0);   -- mode of interface card
   
-  ifa_sd_mux_sel:     out std_logic_vector(3 downto 0);   -- mux select for status and data of the IFA
-  ifa_sd_mux:         out std_logic_vector(15 downto 0);  -- mux output for status and data of the IFA
-  vg_data:            out std_logic_vector(15 downto 0);  -- data output in IFA-Mode to VG connector
-  send_str:           out std_logic;                      -- send strobe for EE-FlipFlop of the sender 6408
-  fc_ext:             out std_logic;                      -- signals external data access
-  ifa_fc_int:         out std_logic;                      -- function codes for IFA
-  wr_i2c:             out std_logic;                      -- write data of the i2c bus
-  wr_irm:             out std_logic;                      -- write interrupt mask
-  wr_fwl_data:        out std_logic;                      -- write data to IFA firmware loader
-  wr_fwl_ctrl:        out std_logic;                      -- write ctrl to IFA firmware loader
-  rd_fwl_data:        out std_logic;                      -- read data from IFA firmware loader
-  rd_fwl_sts:         out std_logic;                      -- read status from IFA firmware loader
-  rd_me_vw_err:       out std_logic;                      -- error counter for ME-VW-Error
-  rd_me_data_err:     out std_logic;                      -- error counter for ME-Data-Error
-  wr_clr_me_vw_err:   out std_logic;                      -- clear ME-VR error counter
-  wr_clr_me_data_err: out std_logic;                      -- clear ME-Data error counter
-  ifp_led:            out std_logic_vector(15 downto 0);  -- IF-Piggy 'FG 380.751' LED port
-  ifp_led_out:        out std_logic_vector(15 downto 0);  -- IF-Piggy 'FG 380.751' out/leds
-  broad_en:           out std_logic;                      -- flag for broadcast enable
-  reset_cmd:          out std_logic;                      -- reset command (fc = 0x01)
-  res_vw_err:         out std_logic);                     -- reset VW error
-end entity ifactl_c;
+  ifa_sd_mux_sel     : out std_logic_vector(3 downto 0);   -- mux select for status and data of the IFA
+  ifa_sd_mux         : out std_logic_vector(15 downto 0);  -- mux output for status and data of the IFA
+  vg_data            : out std_logic_vector(15 downto 0);  -- data output in IFA-Mode to VG connector
+  send_str           : out std_logic;                      -- send strobe for EE-FlipFlop of the sender 6408
+  fc_ext             : out std_logic;                      -- signals external data access
+  ifa_fc_int         : out std_logic;                      -- function codes for IFA
+  wr_i2c             : out std_logic;                      -- write data of the i2c bus
+  wr_irm             : out std_logic;                      -- write interrupt mask
+  wr_fwl_data        : out std_logic;                      -- write data to IFA firmware loader
+  wr_fwl_ctrl        : out std_logic;                      -- write ctrl to IFA firmware loader
+  rd_fwl_data        : out std_logic;                      -- read data from IFA firmware loader
+  rd_fwl_sts         : out std_logic;                      -- read status from IFA firmware loader
+  rd_me_vw_err       : out std_logic;                      -- error counter for ME-VW-Error
+  rd_me_data_err     : out std_logic;                      -- error counter for ME-Data-Error
+  wr_clr_me_vw_err   : out std_logic;                      -- clear ME-VR error counter
+  wr_clr_me_data_err : out std_logic;                      -- clear ME-Data error counter
+  ifp_led            : out std_logic_vector(15 downto 0);  -- IF-Piggy 'FG 380.751' LED port
+  ifp_led_out        : out std_logic_vector(15 downto 0);  -- IF-Piggy 'FG 380.751' out/leds
+  broad_en           : out std_logic;                      -- flag for broadcast enable
+  reset_cmd          : out std_logic;                      -- reset command (fc = 0x01)
+  res_vw_err         : out std_logic);                     -- reset VW error
+end entity ifactl;
 
-architecture arch of ifactl_c is
+architecture arch of ifactl is
 
-constant c_rd_irm:          std_logic_vector(7 downto 0) := x"c9";
-constant c_rd_cntrl:        std_logic_vector(7 downto 0) := x"ca";
-constant c_rd_id:     		  std_logic_vector(7 downto 0) := x"cc";
-constant c_rd_vers:         std_logic_vector(7 downto 0) := x"cd";
-constant c_rd_buildid:      std_logic_vector(7 downto 0) := x"ce";
-constant c_rst_buildid:     std_logic_vector(7 downto 0) := x"cf";
-constant c_rd_echo:         std_logic_vector(7 downto 0) := x"89";
-constant c_rd_if_mode:      std_logic_vector(7 downto 0) := x"97";
-constant c_rd_i2c:          std_logic_vector(7 downto 0) := x"98";
-constant c_rd_fwl_data:     std_logic_vector(7 downto 0) := x"9c"; -- data for firmware loader of the IFA
-constant c_rd_fwl_sts:      std_logic_vector(7 downto 0) := x"9d"; -- status of the IFA firmware loader
-constant c_rd_me_vw_err:    std_logic_vector(7 downto 0) := x"d3"; -- error counter ME-VW
-constant c_rd_me_data_err:  std_logic_vector(7 downto 0) := x"d4"; -- error counter ME-Data
-constant c_rd_ifp_led:      std_logic_vector(7 downto 0) := x"9e"; -- IF-Piggy 'FG 380.751' LED port
-constant c_rd_ifp_io:       std_logic_vector(7 downto 0) := x"9f"; -- IF-Piggy 'FG 380.751' Out/LEDs/In
-constant c_rd_ifp_id:       std_logic_vector(7 downto 0) := x"8e"; -- IF-Piggy-ID
+constant c_rd_irm         : std_logic_vector(7 downto 0) := x"c9";
+constant c_rd_cntrl       : std_logic_vector(7 downto 0) := x"ca";
+constant c_rd_id          : std_logic_vector(7 downto 0) := x"cc";
+constant c_rd_vers        : std_logic_vector(7 downto 0) := x"cd";
+constant c_rd_buildid     : std_logic_vector(7 downto 0) := x"ce";
+constant c_rst_buildid    : std_logic_vector(7 downto 0) := x"cf";
+constant c_rd_echo        : std_logic_vector(7 downto 0) := x"89";
+constant c_rd_if_mode     : std_logic_vector(7 downto 0) := x"97";
+constant c_rd_i2c         : std_logic_vector(7 downto 0) := x"98";
+constant c_rd_fwl_data    : std_logic_vector(7 downto 0) := x"9c"; -- data for firmware loader of the IFA
+constant c_rd_fwl_sts     : std_logic_vector(7 downto 0) := x"9d"; -- status of the IFA firmware loader
+constant c_rd_me_vw_err   : std_logic_vector(7 downto 0) := x"d3"; -- error counter ME-VW
+constant c_rd_me_data_err : std_logic_vector(7 downto 0) := x"d4"; -- error counter ME-Data
+constant c_rd_ifp_led     : std_logic_vector(7 downto 0) := x"9e"; -- IF-Piggy 'FG 380.751' LED port
+constant c_rd_ifp_io      : std_logic_vector(7 downto 0) := x"9f"; -- IF-Piggy 'FG 380.751' Out/LEDs/In
+constant c_rd_ifp_id      : std_logic_vector(7 downto 0) := x"8e"; -- IF-Piggy-ID
 
-constant c_wr_sw1:          std_logic_vector(7 downto 0) := x"06";
-constant c_wr_irm:          std_logic_vector(7 downto 0) := x"12";
-constant c_wr_echo:         std_logic_vector(7 downto 0) := x"13";
-constant c_wr_if_mode:      std_logic_vector(7 downto 0) := x"60";
-constant c_wr_fwl_data:     std_logic_vector(7 downto 0) := x"65"; -- data for IFA firmware loader
-constant c_wr_fwl_ctrl:     std_logic_vector(7 downto 0) := x"66"; -- ctrl for IFA firmware loader
-constant c_wr_i2c:          std_logic_vector(7 downto 0) := x"61";
-constant c_wr_ifp_led:      std_logic_vector(7 downto 0) := x"69"; -- IF-Piggy 'FG 380.751' LED-Port
-constant c_wr_ifp_out:      std_logic_vector(7 downto 0) := x"6a"; -- IF-Piggy 'FG 380.751' Out/LED's/In
+constant c_wr_sw1         : std_logic_vector(7 downto 0) := x"06";
+constant c_wr_irm         : std_logic_vector(7 downto 0) := x"12";
+constant c_wr_echo        : std_logic_vector(7 downto 0) := x"13";
+constant c_wr_if_mode     : std_logic_vector(7 downto 0) := x"60";
+constant c_wr_fwl_data    : std_logic_vector(7 downto 0) := x"65"; -- data for IFA firmware loader
+constant c_wr_fwl_ctrl    : std_logic_vector(7 downto 0) := x"66"; -- ctrl for IFA firmware loader
+constant c_wr_i2c         : std_logic_vector(7 downto 0) := x"61";
+constant c_wr_ifp_led     : std_logic_vector(7 downto 0) := x"69"; -- IF-Piggy 'FG 380.751' LED-Port
+constant c_wr_ifp_out     : std_logic_vector(7 downto 0) := x"6a"; -- IF-Piggy 'FG 380.751' Out/LED's/In
 
-constant c_reset_cmd:       std_logic_vector(7 downto 0) := x"01";
-constant c_res_vw_err:      std_logic_vector(7 downto 0) := x"7d";
-constant c_res_broadcast:   std_logic_vector(7 downto 0) := x"7e";
-constant c_set_broadcast:   std_logic_vector(7 downto 0) := x"7f";
+constant c_reset_cmd      : std_logic_vector(7 downto 0) := x"01";
+constant c_res_vw_err     : std_logic_vector(7 downto 0) := x"7d";
+constant c_res_broadcast  : std_logic_vector(7 downto 0) := x"7e";
+constant c_set_broadcast  : std_logic_vector(7 downto 0) := x"7f";
 
 
-signal s_fc_int:      std_logic;
-signal s_fc_ext:      std_logic;
-signal s_rd_irm:      std_logic;
-signal s_rd_cntrl:    std_logic;
-signal s_rd_id:       std_logic;
-signal s_rd_vers:     std_logic;
-signal s_rd_buildid:  std_logic;
-signal s_rst_buildid: std_logic;
-signal s_rd_sts:      std_logic;
-signal s_rd_echo:     std_logic;
-signal s_send_str:    std_logic;
-signal s_wr_irm:      std_logic;
-signal s_res_vw_err:  std_logic;
-signal s_reset_cmd:   std_logic;
-signal s_rd_i2c:      std_logic;
-signal s_wr_i2c:      std_logic;
-signal s_wr_fwl_data: std_logic;
-signal s_wr_fwl_ctrl: std_logic;
-signal s_rd_fwl_data: std_logic;
-signal s_rd_fwl_sts:  std_logic;
-signal s_wr_ifp_led:  std_logic;
-signal s_wr_ifp_out:  std_logic;
-signal s_rd_ifp_led:  std_logic;
-signal s_rd_ifp_io:   std_logic;
-signal s_rd_ifp_id:   std_logic;
-signal s_rd_if_mode:  std_logic;
-signal s_broad_en:    std_logic;
-signal s_ed:          std_logic_vector(15 downto 0);
-signal s_ifp_led:     std_logic_vector(15 downto 0);
-signal s_ifp_out:     std_logic_vector(15 downto 0);
-signal s_vg_data:     std_logic_vector(15 downto 0);
+signal s_fc_int             : std_logic;
+signal s_fc_ext             : std_logic;
+signal s_rd_irm             : std_logic;
+signal s_rd_cntrl           : std_logic;
+signal s_rd_id              : std_logic;
+signal s_rd_vers            : std_logic;
+signal s_rd_buildid         : std_logic;
+signal s_rst_buildid        : std_logic;
+signal s_rd_sts             : std_logic;
+signal s_rd_echo            : std_logic;
+signal s_send_str           : std_logic;
+signal s_wr_irm             : std_logic;
+signal s_res_vw_err         : std_logic;
+signal s_reset_cmd          : std_logic;
+signal s_rd_i2c             : std_logic;
+signal s_wr_i2c             : std_logic;
+signal s_wr_fwl_data        : std_logic;
+signal s_wr_fwl_ctrl        : std_logic;
+signal s_rd_fwl_data        : std_logic;
+signal s_rd_fwl_sts         : std_logic;
+signal s_wr_ifp_led         : std_logic;
+signal s_wr_ifp_out         : std_logic;
+signal s_rd_ifp_led         : std_logic;
+signal s_rd_ifp_io          : std_logic;
+signal s_rd_ifp_id          : std_logic;
+signal s_rd_if_mode         : std_logic;
+signal s_broad_en           : std_logic;
+signal s_ed                 : std_logic_vector(15 downto 0);
+signal s_ifp_led            : std_logic_vector(15 downto 0);
+signal s_ifp_out            : std_logic_vector(15 downto 0);
+signal s_vg_data            : std_logic_vector(15 downto 0);
 
-signal s_rd_me_vw_err:      std_logic;
-signal s_rd_me_data_err:    std_logic;
-signal s_wr_clr_me_vw_err:  std_logic;
-signal s_wr_clr_me_data_err: std_logic;
+signal s_rd_me_vw_err       : std_logic;
+signal s_rd_me_data_err     : std_logic;
+signal s_wr_clr_me_vw_err   : std_logic;
+signal s_wr_clr_me_data_err : std_logic;
 
-signal build_id_out:  std_logic_vector(15 downto 0);
+signal build_id_out         : std_logic_vector(15 downto 0);
 
 
 begin
