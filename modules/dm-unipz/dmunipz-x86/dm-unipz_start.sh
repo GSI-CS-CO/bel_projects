73f99c9c65038a0844d6aef493d36420cce057f5
Dietrich Beck <d.beck@gsi.de>
2017-05-17 14:55:50 +0200

    dm-unipz: start to add tools for host system

---
+++ modules/dm-unipz/dmunipz-x86/dm-unipz_start.sh
@@ -0,0 +1,65 @@
+#!/bin/sh
+# starts and configures the firmware (lm32) and software (host) of dm-unipz 
+
+# set -x
+
+###########################################
+# clean up stuff
+###########################################
+echo -e bring possibly resident firmware to idle state
+eb-write dev/wbm0 0x20090504/4 0x3
+sleep 5
+eb-write dev/wbm0 0x20090504/4 0x4
+sleep 5
+echo -e destroy all unowned conditions for lm32 channel of ECA
+saft-ecpu-ctl baseboard -x
+
+
+###########################################
+# load firmware to lm32
+###########################################
+echo -e load firmware
+eb-fwload dev/wbm0 u 0x0 dmunipz.bin
+
+###########################################
+# start software on hostsystem 
+###########################################
+# to be implemented: command with pipe to logger (logstash)
+
+###########################################
+# configure firmware and make it operational 
+###########################################
+ 
+# do some write actions to set register values
+# to be implemented
+
+echo -e make firmware operational
+# send CONFIGURE command to firmware
+sleep 5
+eb-write dev/wbm0 0x20090504/4 0x1
+
+# send START OPERATATION command to firmware
+sleep 5
+eb-write dev/wbm0 0x20090504/4 0x2
+
+###########################################
+# configure ECA
+###########################################
+echo -e configure lm32 channel of ECA
+
+# configure ECA for lm32 channel: here action for TK request, tag "0x2"
+saft-ecpu-ctl baseboard -c 0x2222000000000000 0xffff000000000000 0 0x2 -d
+
+# configure ECA for lm32 channel: here action for TK request, tag "0x3"
+saft-ecpu-ctl baseboard -c 0x3333000000000000 0xffff000000000000 0 0x3 -d
+
+###########################################
+# testing without datamaster
+###########################################
+# saft-dm bla -fp -n 3600000 schedule.txt
+# alternative : 
+# - saft-ctl bla -fp inject 0x2222000000000002 0x0 0
+# - saft-ctl bla -fp inject 0x3333000000000002 0x0 0
+# note that action of firmware is triggered by tag and
+# that virtual accelerator is specified by low bits of EvtID
+
