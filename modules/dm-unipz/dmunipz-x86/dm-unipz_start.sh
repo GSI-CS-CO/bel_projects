8a6a85a5853f07212088e7e71b77c50f91cb07e6
Dietrich Beck <d.beck@gsi.de>
2017-05-31 14:29:33 +0200

    dm-unipz: fixed a bug in the startup script

--- modules/dm-unipz/dmunipz-x86/dm-unipz_start.sh
+++ modules/dm-unipz/dmunipz-x86/dm-unipz_start.sh
@@ -6,20 +6,20 @@
 ###########################################
 # clean up stuff
 ###########################################
-echo -e dm-unipz (start): bring possibly resident firmware to idle state
+echo -e dm-unipz - start: bring possibly resident firmware to idle state
 dmunipz-ctl dev/wbm0 stopop
 sleep 5
 
 dmunipz-ctl dev/wbm0 idle
 sleep 5
-echo -e dm-unipz (start): destroy all unowned conditions for lm32 channel of ECA
+echo -e dm-unipz - start: destroy all unowned conditions for lm32 channel of ECA
 saft-ecpu-ctl baseboard -x
 
 
 ###########################################
 # load firmware to lm32
 ###########################################
-echo -e dm-unipz (start): load firmware
+echo -e dm-unipz - start: load firmware
 eb-fwload dev/wbm0 u 0x0 dmunipz.bin
 
 ###########################################
@@ -34,7 +34,7 @@ eb-fwload dev/wbm0 u 0x0 dmunipz.bin
 # do some write actions to set register values
 # to be implemented
 
-echo -e dm-unipz (start): make firmware operational
+echo -e dm-unipz - start: make firmware operational
 # send CONFIGURE command to firmware
 sleep 5
 dmunipz-ctl dev/wbm0 configure
@@ -46,7 +46,7 @@ dmunipz-ctl dev/wbm0 startop
 ###########################################
 # configure ECA
 ###########################################
-echo -e dm-unipz (start): configure lm32 channel of ECA
+echo -e dm-unipz - start: configure lm32 channel of ECA
 
 # configure ECA for lm32 channel: here action for TK request, tag "0x2"
 saft-ecpu-ctl baseboard -c 0x2222000000000000 0xffff000000000000 0 0x2 -d
@@ -57,7 +57,7 @@ saft-ecpu-ctl baseboard -c 0x3333000000000000 0xffff000000000000 0 0x3 -d
 # configure ECA for lm32 channel: here action for TK request, tag "0x4"
 saft-ecpu-ctl baseboard -c 0x4444000000000000 0xffff000000000000 0 0x4 -d
 
-echo -e dm-unipz (start): startup script finished
+echo -e dm-unipz - start: startup script finished
 
 ###########################################
 # testing without datamaster
