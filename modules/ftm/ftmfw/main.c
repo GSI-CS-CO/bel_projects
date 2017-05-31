ad2811bac1361bf11c30907837307c01281153f2
Mathias Kreider <mkreider@gsi.de>
2017-05-31 17:47:07 +0200

    firmware can now do 'proper' action for node and check queue state

--- modules/ftm/ftmfw/main.c
+++ modules/ftm/ftmfw/main.c
@@ -80,6 +80,50 @@ void init()
 }
 
 
+
+uint32_t* event(uint32_t* node) {
+  node[NODE_FLAGS >> 2] |= NFLG_PAINT_LM32_SMSK;
+  mprintf("#%02u: Sending Evt 0x%08x, next: 0x%08x\n", cpuId, node[NODE_HASH >> 2], node[NODE_DEF_DEST_PTR >> 2]);
+  return (uint32_t*)node[NODE_DEF_DEST_PTR >> 2];
+}
+
+uint32_t* block(uint32_t* node) {
+  node[NODE_FLAGS >> 2] |= NFLG_PAINT_LM32_SMSK;
+  mprintf("#%02u: Checking Block 0x%08x ...", cpuId, node[NODE_HASH >> 2]);
+  uint32_t ardOffs = node[BLOCK_CMDQ_RD_IDXS >> 2], awrOffs = node[BLOCK_CMDQ_WR_IDXS >> 2], rdOffs, bufOffs, cmdOffs, idxPrio, ptrPrio;
+  uint32_t *qbl, *qb, *prd;
+
+  if( awrOffs != ardOffs ) {
+    mprintf(" pending Cmd @");
+    if (((awrOffs ^ ardOffs) >> (PRIO_LO * 8)) & 0xff) { idxPrio = PRIO_LO; ptrPrio = BLOCK_CMDQ_LO_PTR; }
+    if (((awrOffs ^ ardOffs) >> (PRIO_HI * 8)) & 0xff) { idxPrio = PRIO_HI; ptrPrio = BLOCK_CMDQ_HI_PTR; }
+    if (((awrOffs ^ ardOffs) >> (PRIO_IL * 8)) & 0xff) { idxPrio = PRIO_IL; ptrPrio = BLOCK_CMDQ_IL_PTR; }
+    mprintf(" Prio %u\n", idxPrio);
+
+    rdOffs  = (ardOffs >> (idxPrio * 8)) & 0xff;
+    bufOffs =  rdOffs / (_MEM_BLOCK_SIZE / _T_CMD_SIZE  ) * _PTR_SIZE_;
+    cmdOffs =  rdOffs % (_MEM_BLOCK_SIZE / _T_CMD_SIZE  ) * _T_CMD_SIZE;
+    qbl = (uint32_t*)node[BLOCK_CMDQ_IL_PTR >> 2];
+    qb  = (uint32_t*)qbl[bufOffs >> 2];
+    prd = (uint32_t*)&qb[cmdOffs >> 2];
+    
+    mprintf(" Reading from 0x%08x -> 0x%08x\n", prd, *prd);
+
+    node[BLOCK_CMDQ_RD_IDXS >> 2] = 
+  } else {
+    mprintf(" nothing pending\n");
+    
+  }  
+  return (uint32_t*)node[NODE_DEF_DEST_PTR >> 2];
+  
+}
+
+typedef uint32_t* (*nodeFuncPtr)( uint32_t* );
+nodeFuncPtr nodeFuncs[8];
+
+
+
+
 void paintPath(uint32_t* node) {
   mprintf("#%02u: Received 0x%08x\n", cpuId, (uint32_t)node);
   mprintf("#%02u: @ 0x%08x FL 0x%08x # 0x%08x DD 0x%08x \n", cpuId, (uint32_t)(uint32_t*)&node[NODE_HASH >> 2], node[NODE_FLAGS >> 2], node[NODE_HASH >> 2], node[NODE_DEF_DEST_PTR >> 2]);
@@ -98,7 +142,16 @@ void main(void) {
    uint32_t   *lbtIdx, *lbtPtr, *tcGet;
    uint32_t   start;
     
-   uint32_t** test; 
+   uint32_t** test;
+
+  nodeFuncs[NODE_TYPE_UNKNOWN]  = ( NULL );
+  nodeFuncs[NODE_TYPE_RAW]      = ( NULL );
+  nodeFuncs[NODE_TYPE_TMSG]     = event;
+  nodeFuncs[NODE_TYPE_CNOOP]    = event;
+  nodeFuncs[NODE_TYPE_CFLOW]    = event;
+  nodeFuncs[NODE_TYPE_CFLUSH]   = event;
+  nodeFuncs[NODE_TYPE_CWAIT]    = event;
+  nodeFuncs[NODE_TYPE_BLOCK]    = block; 
 
    init();
    //uint32_t test = &pFtmIf->tPrep;
@@ -127,16 +180,27 @@ void main(void) {
    uint32_t *p  = (uint32_t*)_startshared; 
    
    while (1) {
+
+
     uint32_t* start = p + (( SHCTL_THR_CTL + T_TC_START )>> 2);
     for(i=0;i<8;i++) {
       if (*start & 1<<i) {
         //mprintf("#%02u: Start Thr #%u\n", cpuId, i);
         //mprintf("#%02u: Looking at Node Ptr @ 0x%08x\n", cpuId, (uint32_t)(uint32_t*)&p[( SHCTL_THR_STA + i * _T_TS_SIZE_ + T_TD_NODE_PTR )>> 2] );
-        uint32_t* np = p + (( SHCTL_THR_STA + i * _T_TS_SIZE_ + T_TD_NODE_PTR )>> 2);
-        mprintf("#%02u: Node Ptr is 0x%08x\n", cpuId, *np);
+        uint32_t* np = (uint32_t*)*(p + (( SHCTL_THR_STA + i * _T_TS_SIZE_ + T_TD_NODE_PTR )>> 2));
+        
         *start &= ~(1<<i);
-        paintPath((uint32_t*)*np);
-        mprintf("#%02u: Done painting\n", cpuId);
+        
+        while(np != NULL) {
+          //mprintf("#%02u: Node Ptr is 0x%08x\n", cpuId, np);
+          uint32_t flags = np[NODE_FLAGS >> 2];
+          uint8_t  type = (flags >> NFLG_TYPE_POS) & NFLG_TYPE_MSK;
+          //mprintf("#%02u: flags @ 0x%08x are 0x%08x, Type is 0x%08x\n", cpuId, (uint32_t)&np[NODE_FLAGS >> 2], flags, type);
+          np = nodeFuncs[type](np);
+
+
+        }
+
       }
       
     } 
