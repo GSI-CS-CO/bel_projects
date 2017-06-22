89e4dd03cc6293759e14232a419c9e644f977579
Mathias Kreider <mkreider@gsi.de>
2017-06-22 18:02:25 +0200

    using carpeDM library for unipz test now

--- modules/ftm/ftmx86/dm-unipz-test.sh
+++ modules/ftm/ftmx86/dm-unipz-test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 eb-fwload tcp/tsl008.acc.gsi.de u0 0 /home/mkreider/projects/bel_projects/modules/ftm/ftmfw/ftm.bin 
-./gread tcp/tsl008.acc.gsi.de unipz.dot -w
+./test tcp/tsl008.acc.gsi.de unipz.dot -w
 eb-write tcp/tsl008.acc.gsi.de 0x4110538/4 0x10001310; eb-write tcp/tsl008.acc.gsi.de 0x4110528/4 0x1
 
