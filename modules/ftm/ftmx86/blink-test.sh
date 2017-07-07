031559b6a0196320d46a8ef380f3ac319de8032e
Mathias Kreider <mkreider@gsi.de>
2017-07-07 14:35:30 +0200

    mutual call blink test successful

--- modules/ftm/ftmx86/blink-test.sh
+++ modules/ftm/ftmx86/blink-test.sh
@@ -5,8 +5,8 @@ eb-fwload $1 u0 0 ../ftmfw/ftm.bin
 sleep 1
 ./dm-sched $1 $file -w
 sleep 1
-./dm-cmd $1 $file origin -t0 Evt_POLICE
-./dm-cmd $1 $file origin -t1 Evt_FIREF
+./dm-cmd $1 $file origin -t0 Evt_POLICE0
+./dm-cmd $1 $file origin -t1 Evt_FIREF0
 ./dm-cmd $1 $file preptime -t0 1000000000
 ./dm-cmd $1 $file preptime -t1 1000000000
 
