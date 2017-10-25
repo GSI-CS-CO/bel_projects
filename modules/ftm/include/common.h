5ec0a23a23fa3663dbeb76bab0b12ccecf73da26
Mathias Kreider <mkreider@gsi.de>
2017-10-25 14:12:08 +0200

    DM: lib: made boost 1.62+ optional for endian conversion

--- modules/ftm/include/common.h
+++ modules/ftm/include/common.h
@@ -11,9 +11,39 @@
 #include <boost/function.hpp>
 #include <boost/bind.hpp>
 #include <boost/graph/graphviz.hpp>
-#include <boost/endian/conversion.hpp> //FIXME this isn't wrong, but it needs Boost 1.62+. ASL cluster only has 1.54, write an alternative endian swap and use depending on version
 #include <boost/optional.hpp>
 #include "ftm_common.h"
+
+#if BOOST_VERSION >= 106200 //endian conversian was included in boost 1.62
+  #include <boost/endian/conversion.hpp>
+  using namespace boost::endian;
+#else
+   // avoiding boost 1.62+, stolen from boost/endian/conversion.hpp to  //////////
+  inline uint16_t endian_reverse(uint16_t x)
+  {
+    return (x << 8)
+      | (x >> 8);
+  }
+
+  inline uint32_t endian_reverse(uint32_t x)                          
+  {
+    uint32_t step16;
+    step16 = x << 16 | x >> 16;
+    return
+        ((step16 << 8) & 0xff00ff00)
+      | ((step16 >> 8) & 0x00ff00ff);
+  }
+
+  inline uint64_t endian_reverse(uint64_t x)
+  {
+    uint64_t step32, step16;
+    step32 = x << 32 | x >> 32;
+    step16 = (step32 & 0x0000FFFF0000FFFFULL) << 16
+           | (step32 & 0xFFFF0000FFFF0000ULL) >> 16;
+    return   (step16 & 0x00FF00FF00FF00FFULL) << 8
+           | (step16 & 0xFF00FF00FF00FF00ULL) >> 8;
+  }
+#endif
 
 
 #define BYTE_TO_BINARY_PATTERN "%c%c%c%c%c%c%c%c"
@@ -46,11 +76,13 @@ typedef boost::container::vector<uint8_t> vBuf;
 typedef boost::container::vector<uint32_t> vAdr;
 typedef boost::container::vector<uint32_t> ebBuf;
 
-//FIXME this isn't wrong, but it needs Boost 1.62+. ASL cluster only has 1.54, write an alternative endian swap and use depending on version
+
+
+
 
 template<typename T>
 inline void writeLeNumberToBeBytes(uint8_t* pB, T val) {
-  T x = boost::endian::endian_reverse(val);
+  T x = endian_reverse(val);
   std::copy(static_cast<const uint8_t*>(static_cast<const void*>(&x)),
             static_cast<const uint8_t*>(static_cast<const void*>(&x)) + sizeof x,
             pB);
@@ -58,8 +90,9 @@ inline void writeLeNumberToBeBytes(uint8_t* pB, T val) {
 
 template<typename T>
 inline T writeBeBytesToLeNumber(uint8_t* pB) {
-  return boost::endian::endian_reverse(*((T*)pB));
+  return endian_reverse(*((T*)pB));
 }
+
 
 template<typename T>
 inline T s2u(const std::string& s) {
@@ -67,7 +100,7 @@ inline T s2u(const std::string& s) {
 
 }
 
-//FIXME just fuckin overload it, why are there two hexdump functions again ?
+//FIXME just fuckin overload it, why are there two hexdump functions here ?
 inline void hexDump (const char *desc, const void *addr, int len) {
     int i;
     unsigned char buff[17];
