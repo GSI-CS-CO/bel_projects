8bbb09e5d1a5c37cd634e1ac54ffc206ed38f0b6
Cesar Prados <c.prados@gsi.de>
2017-11-20 10:38:02 +0100

    wb_fec: add component to bel_projects

--- Manifest.py
+++ Manifest.py
@@ -8,7 +8,8 @@ modules = {
     "git://ohwr.org/hdl-core-lib/general-cores.git",
     "git://ohwr.org/hdl-core-lib/etherbone-core.git",
     "git://ohwr.org/hdl-core-lib/pci-core.git",
-    "git://ohwr.org/hdl-core-lib/wr-cores.git"
+    "git://ohwr.org/hdl-core-lib/wr-cores.git",
+    "git@bitbucket.org:bradomyn/wb_fec.git"
   ],
   "gitsm" : [
     "git://ohwr.org/hdl-core-lib/fpga-config-space.git"
