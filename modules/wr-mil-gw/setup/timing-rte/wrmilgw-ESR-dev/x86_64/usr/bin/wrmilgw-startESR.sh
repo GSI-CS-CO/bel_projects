82a275cbbb7fde62ab812fa4581520229e7ebd7f
Michael Reese <m.reese@gsi.de>
2018-01-08 15:52:28 +0100

    wr-mil-gw: add a deployment of production version to toplevel Makefile

--- modules/wr-mil-gw/setup/timing-rte/wrmilgw-ESR-dev/x86_64/usr/bin/wrmilgw-startESR.sh
+++ modules/wr-mil-gw/setup/timing-rte/wrmilgw-ESR-dev/x86_64/usr/bin/wrmilgw-startESR.sh
@@ -15,7 +15,7 @@ eb-fwload dev/wbm0 u1 0 wr_mil.bin
 # destoy all unowned conditions
 saft-ecpu-ctl baseboard -x
 # create new condition for all MIL events ('0' in first four bits of EVTNO)
-#      GROUP-ID for ESR? --------|||
+#      GROUP-ID for ESR  --------|||
 #                                VVV
 saft-ecpu-ctl baseboard -g -z -c 0x1154000000000000 20 100000 4 -d 
 sleep 1
--- modules/wr-mil-gw/setup/timing-rte/wrmilgw-SIS-dev/x86_64/usr/bin/wrmilgw-startSIS.sh
+++ modules/wr-mil-gw/setup/timing-rte/wrmilgw-SIS-dev/x86_64/usr/bin/wrmilgw-startSIS.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# starts and configures the firmware (lm32) and software (host) of wr-mil gateway for ESR operation
+# starts and configures the firmware (lm32) and software (host) of wr-mil gateway for SIS operation
 
 # set -x
 
@@ -15,12 +15,12 @@ eb-fwload dev/wbm0 u1 0 wr_mil.bin
 # destoy all unowned conditions
 saft-ecpu-ctl baseboard -x
 # create new condition for all MIL events ('0' in first four bits of EVTNO)
-#      GROUP-ID for SIS? --------|||
+#      GROUP-ID for SIS  --------|||
 #                                VVV
 saft-ecpu-ctl baseboard -g -z -c 0x112c000000000000 20 100000 4 -d 
 sleep 1
 
-# configure as ESR gateway and run it
+# configure as SIS gateway and run it
 wr-mil-gw-ctl dev/wbm0 -s
 
 # done
