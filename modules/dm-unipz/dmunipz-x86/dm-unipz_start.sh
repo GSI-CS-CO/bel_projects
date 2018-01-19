38d95694dc3af25b25103e4bf91d0910ce8e8555
Dietrich Beck <d.beck@gsi.de>
2018-01-19 11:26:10 +0100

    dm-unipz: adding some stuff for deployment to ASL

--- modules/dm-unipz/dmunipz-x86/dm-unipz_start.sh
+++ modules/dm-unipz/dmunipz-x86/dm-unipz_start.sh
@@ -35,8 +35,12 @@ dmunipz-ctl -s2 dev/wbm0 | logger -t dmunipz-ctl -sp local0.info &
  
 # do some write actions to set register values
 echo -e dm-unipz - set MAC and IP of gateway and Data Master
-dmunipz-ctl dev/wbm0 ebmdm 0x00267b000422 0xc0a80c04
-dmunipz-ctl dev/wbm0 ebmlocal 0x00267b000321 0xc0a80cea
+# development: Programmentwicklungsraum, tsl008, scuxl0033
+#dmunipz-ctl dev/wbm0 ebmdm 0x00267b000422 0xc0a80c04
+#dmunipz-ctl dev/wbm0 ebmlocal 0x00267b000321 0xc0a80cea
+# production: BG2, tsl017, scuxl0223
+dmunipz-ctl dev/wbm0 ebmdm 0x00267b000407 0xc0a8803f
+dmunipz-ctl dev/wbm0 ebmlocal 0x00267b0003f1 0xc0a8a0e5
 
 echo -e dm-unipz - start: make firmware operational
 # send CONFIGURE command to firmware
@@ -53,16 +57,16 @@ dmunipz-ctl dev/wbm0 startop
 echo -e dm-unipz - start: configure lm32 channel of ECA
 
 # configure ECA for lm32 channel: here action for TK request, tag "0x1"
-# no longer needed, here: prep_dm saft-ecpu-ctl baseboard -c 0x1111000000000000 0xffff000000000000 0 0x5 -d
+# no longer needed, here: prep_dm saft-ecpu-ctl tr0 -c 0x1111000000000000 0xffff000000000000 0 0x5 -d
 
 # configure ECA for lm32 channel: here action for TK request, tag "0x2"
-saft-ecpu-ctl baseboard -c 0x2222000000000000 0xffff000000000000 0 0x2 -d
+saft-ecpu-ctl tr0 -c 0x0222000000000000 0xffff000000000000 0 0x2 -d
 
 # configure ECA for lm32 channel: here action for TK request, tag "0x3"
-saft-ecpu-ctl baseboard -c 0x3333000000000000 0xffff000000000000 0 0x3 -d
+saft-ecpu-ctl tr0 -c 0x0333000000000000 0xffff000000000000 0 0x3 -d
 
 # configure ECA for lm32 channel: here action for TK request, tag "0x4"
-saft-ecpu-ctl baseboard -c 0x4444000000000000 0xffff000000000000 0 0x4 -d
+saft-ecpu-ctl tr0 -c 0x0444000000000000 0xffff000000000000 0 0x4 -d
 
 echo -e dm-unipz - start: startup script finished
 
