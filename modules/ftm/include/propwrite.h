0dc67423abf3e045badaff5dd07ebfc80c7f3372
Michael Reese <m.reese@gsi.de>
2018-02-06 09:57:36 +0100

    DM: lib: use auto instead of explicit type to make it compile on asl740

--- modules/ftm/include/propwrite.h
+++ modules/ftm/include/propwrite.h
@@ -154,7 +154,7 @@ template <class Name>
     static_eq(typeMap type) : type(type) {}
     template <class Edge>
     bool operator()(const Edge& e) const {
-      vStrC allowedTypes = {det::sDefDst, det::sResFlowDst, det::sDynFlowDst};
+      auto allowedTypes = {det::sDefDst, det::sResFlowDst, det::sDynFlowDst};
       for(auto& it : allowedTypes ) { if (type[e] == it) return true; } //true if edge is of allowed types
       return false;  
     }
