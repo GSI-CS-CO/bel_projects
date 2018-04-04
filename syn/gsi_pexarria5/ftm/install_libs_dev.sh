2c7bfbb143708a43f9ad91dacb517b85d9704721
Michael Reese <m.reese@gsi.de>
2018-04-04 12:12:23 +0200

    generator libs: update install scripts

--- syn/gsi_pexarria5/ftm/install_libs_dev.sh
+++ syn/gsi_pexarria5/ftm/install_libs_dev.sh
@@ -5,3 +5,7 @@ cp /usr/lib64/libboost_*                       /common/export/timing-rte/generat
 cp ../../../modules/ftm/lib/libcarpedm.so      /common/export/timing-rte/generator-dev/lib/
 cp dm-cmd                                      /common/export/timing-rte/generator-dev/bin/
 cp dm-sched                                    /common/export/timing-rte/generator-dev/bin/
+
+chmod g+w /common/export/timing-rte/generator-dev/lib64/*
+chmod g+w /common/export/timing-rte/generator-dev/lib/*
+chmod g+w /common/export/timing-rte/generator-dev/bin/*
