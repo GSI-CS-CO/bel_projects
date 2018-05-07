91acea53bb267b90de6c92eb7889c2b6e9a223f7
Stefan Rauch <s.rauch@gsi.de>
2018-05-07 11:02:12 +0200

    sio3: generate clk region cores

--- syn/scu_sio3/scu_sio3.tcl
+++ syn/scu_sio3/scu_sio3.tcl
@@ -1,4 +1,5 @@
 set platform "SIO"        
 source ../autogen.tcl
 source ../../modules/build_id/build_id_short.tcl 
+source ../../ip_cores/general-cores/platform/altera/networks/arria2gx.tcl
 source ../../modules/pll/arria2/arria2_pll.tcl
