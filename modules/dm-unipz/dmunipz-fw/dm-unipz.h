d22a04cdfde6e571053ae7ce193ea90481fa1875
Dietrich Beck <d.beck@gsi.de>
2018-07-19 15:25:02 +0200

    dm-unipz: fixing a minor issue, polishin CLI, version set to 0.4.1

--- modules/dm-unipz/dmunipz-fw/dm-unipz.h
+++ modules/dm-unipz/dmunipz-fw/dm-unipz.h
@@ -12,7 +12,7 @@
 #define  DMUNIPZ_SAFETYMARGIN     1000000     // saftey margin required by DM+network, 1ms @ 2018
 #define  DMUNIPZ_MATCHWINDOW       100000     // used for comparing timestamps: 1 TS from TLU->ECA matches event from MIL FIFO, 2: synch EVT_MB_TRIGGER, ...
 #define  DMUNIPZ_OFFSETFLEX       1500000     // offset added to obtain TS "flex wait" [ns]
-#define  DMUNIPZ_OFFSETINJECT    10000000     // offset added to obtain expected time of injection [ns], used for diagnostic only
+#define  DMUNIPZ_OFFSETINJECT     9980000     // offset added to obtain expected time of injection [ns], used for diagnostic only
 #define  DMUNIPZ_EVT_READY2SIS    0x1e        // event number EVT_READY_TO_SIS (HEX)
 #define  DMUNIPZ_ECA_ADDRESS      0x7ffffff0  // address of ECA input
 #define  DMUNIPZ_EB_HACKISH       0x12345678  // value for EB read handshake
