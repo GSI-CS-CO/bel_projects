33b57963096c52fe50cacf35b447ecb883f23667
Mathias Kreider <mkreider@gsi.de>
2018-10-09 11:41:04 +0200

    diag module: fixed swapped loss/acquire, added deadtime to as workaround for WR state glitch

--- modules/dm_diag/dm_diag.vhd
+++ modules/dm_diag/dm_diag.vhd
@@ -44,11 +44,11 @@ Port(
   cyc_diag_i                    : std_logic_vector(g_cores-1 downto 0);
   stall_diag_i                  : std_logic_vector(g_cores-1 downto 0);
   wr_lock_i                     : std_logic;
-  
+
   ctrl_i                        : in  t_wishbone_slave_in;
   ctrl_o                        : out t_wishbone_slave_out
 
-  
+
 );
 end dm_diag;
 
@@ -72,11 +72,11 @@ architecture rtl of dm_diag is
   -- Regs/Sigs Diagnostics Module
   type u32_array is array (natural range <>) of unsigned(31 downto 0);
   type u64_array is array (natural range <>) of unsigned(63 downto 0);
-  
+
   signal r_tai_observer_cnt                   : unsigned(63 downto 0) := (others => '0');
   signal s_tai_observer_dec                   : unsigned(63 downto 0) := (others => '0');
   signal r_tai_old                            : signed(63 downto 0)   := (others => '0');
-  signal r_time_dif, r_time_dif_pos, 
+  signal r_time_dif, r_time_dif_pos,
          r_time_dif_neg                       : signed(63 downto 0)             := (others => '0');
   signal r_time_dif_pos_ts                    : std_logic_vector(64-1 downto 0) := (others => '0'); -- (approximate) timestamp of last pos dif update
   signal r_time_dif_neg_ts                    : std_logic_vector(64-1 downto 0) := (others => '0'); -- (approximate) timestamp of last neg dif update
@@ -84,16 +84,25 @@ architecture rtl of dm_diag is
   signal r_wr_lock_acqu_last_ts, r_wr_lock_loss_last_ts : std_logic_vector(64-1 downto 0) := (others => '0');
   signal s_wr_lock_cnt_i                      : std_logic_vector(64-1 downto 0) := (others => '0');
   signal r_wr_lock_cnt                        : unsigned(64-1 downto 0)         := (others => '0');
-  signal r_wr_lock, s_wr_lock_loss, 
+  signal r_wr_lock, s_wr_lock_loss,
          s_wr_lock_acqu                       : std_logic := '0';
 
   signal s_stall_observation_cycles           : unsigned(31 downto 0)           := (others => '0');
-  signal ra_stall_cnt, ra_stall_observer_cnt, 
+  signal ra_stall_cnt, ra_stall_observer_cnt,
          ra_stall_max                         : u32_array(g_cores-1 downto 0)   := (others => (others => '0'));
   signal s_stall_observer_dec, s_stall_inc    : std_logic_vector(g_cores-1 downto 0) := (others => '0');
   signal ra_stall_max_ts                      : u64_array(g_cores-1 downto 0)   := (others => (others => '0')); -- Timestamp of last max update
-    
+
   signal s_selector                           : natural;
+
+  ---------------------------------------------------------------------------------
+  --Workaround (deadtime) for weirdly jittering WR core status signals
+  signal s_wr_lock_scan,
+         s_deadtime_start,
+         s_deadtime_stop : std_logic := '0';
+  signal r_deadtime : unsigned(15 downto 0);
+  signal r_deadtime_run : std_logic_vector(0 downto 0);
+  ---------------------------------------------------------------------------------
 
 
 begin
@@ -106,11 +115,11 @@ begin
   s_ctrl_time_dif_pos_ts_i  <= r_time_dif_pos_ts;
   s_ctrl_time_dif_neg_i     <= std_logic_vector(r_time_dif_neg);
   s_ctrl_time_dif_neg_ts_i  <= r_time_dif_neg_ts;
-  
+
   --TAI diff observer process
   tai_observer : process (clk_ref_i)
   begin
- 
+
     if rising_edge(clk_ref_i) then
       if(rst_ref_n_i = '0' OR s_ctrl_reset_o(0) = '1' ) then
         r_tai_old         <= signed(tm_tai8ns_i);
@@ -124,7 +133,7 @@ begin
         if (r_tai_observer_cnt(r_tai_observer_cnt'left) = '1') then
           --re-init observer count down
           r_tai_observer_cnt <= "000" & unsigned(s_ctrl_time_observation_interval_o(63 downto 3)); -- ticks are 8ns, shift by 3b
-          
+
           --assign new extreme values penending on diff sign, timestamp the update (probably not exact, but enough to correlate with other log files)
           if (r_time_dif > r_time_dif_pos) then
             r_time_dif_pos    <= r_time_dif;
@@ -140,7 +149,7 @@ begin
           r_tai_observer_cnt <= r_tai_observer_cnt - s_tai_observer_dec;
         end if;
         --TODO: ECA adder might be a good idea, this requires some fast 64b magic...
-        r_time_dif <= signed(tm_tai8ns_i) - r_tai_old; 
+        r_time_dif <= signed(tm_tai8ns_i) - r_tai_old;
       end if;
     end if;
   end process;
@@ -163,7 +172,7 @@ begin
 
     stall_observer : process (clk_ref_i)
     begin
-   
+
       if rising_edge(clk_ref_i) then
         if(rst_ref_n_i = '0' OR s_ctrl_reset_o(0) = '1') then
           ra_stall_max(I)          <= (others => '0');
@@ -172,7 +181,7 @@ begin
           ra_stall_max_ts(I)       <= unsigned(tm_tai8ns_i);
         else
           if (ra_stall_observer_cnt(I)(ra_stall_observer_cnt(I)'left) = '1') then -- observer count down is over
-            
+
             if (ra_stall_cnt(I) > ra_stall_max(I)) then -- if the stall cnt is a new record, we save the value
               ra_stall_max(I)     <= ra_stall_cnt(I);
               ra_stall_max_ts(I)  <= unsigned(tm_tai8ns_i);
@@ -181,31 +190,33 @@ begin
             -- re-init observer countdown, clear stall cnt
             ra_stall_observer_cnt(I) <= unsigned(s_ctrl_stall_observation_interval_o);
             ra_stall_cnt(I)          <= (others => '0');
-            
+
           else
             -- countdown and stall count
             ra_stall_observer_cnt(I) <= ra_stall_observer_cnt(I)  - resize(unsigned(s_stall_observer_dec(I downto I)), ra_stall_observer_cnt(I)'length);
-            ra_stall_cnt(I)          <= ra_stall_cnt(I)           + resize(unsigned(s_stall_inc(I downto I)),          ra_stall_cnt(I)'length);         
+            ra_stall_cnt(I)          <= ra_stall_cnt(I)           + resize(unsigned(s_stall_inc(I downto I)),          ra_stall_cnt(I)'length);
           end if;
         end if;
       end if;
     end process;
 
-  end generate G1;   
+  end generate G1;
 
- 
+
+
+
  -- --WR state observer process
-  s_wr_lock_loss  <= not wr_lock_i and     r_wr_lock; --falling edge of wr state is lock lost
-  s_wr_lock_acqu  <=     wr_lock_i and not r_wr_lock; --rising edge of wr state is lock acquired
+  s_wr_lock_loss  <= not wr_lock_i and     r_wr_lock and s_wr_lock_scan; --falling edge of wr state is lock lost
+  s_wr_lock_acqu  <=     wr_lock_i and not r_wr_lock and s_wr_lock_scan; --rising edge of wr state is lock acquired
   s_wr_lock_cnt_i <= std_logic_vector(r_wr_lock_cnt);
 
- 
+
   wr_observer : process (clk_ref_i)
   begin
     if rising_edge(clk_ref_i) then
       r_wr_lock <= wr_lock_i;
       if(rst_ref_n_i = '0' OR s_ctrl_reset_o(0) = '1' ) then
-        r_wr_lock_cnt          <= (others => '0'); 
+        r_wr_lock_cnt          <= (others => '0');
         r_wr_lock_loss_last_ts <= (others => '0');
         r_wr_lock_acqu_last_ts <= (others => '0');
       else
@@ -219,6 +230,26 @@ begin
       end if;
     end if;
   end process;
+
+  ---------------------------------------------------------------------------------
+  --Workaround (deadtime) for weirdly jittering WR core status signals
+
+  s_wr_lock_scan    <= not r_deadtime_run(0);
+  s_deadtime_start  <= wr_lock_i xor r_wr_lock;
+  s_deadtime_stop   <= r_deadtime(r_deadtime'left); --overflow bit
+  workaround_wr_status_bug : process(clk_ref_i)
+  begin
+    if rising_edge(clk_ref_i) then
+      if(rst_ref_n_i = '0' OR s_ctrl_reset_o(0) = '1' OR s_deadtime_stop = '1') then
+        r_deadtime     <= "0" & to_unsigned(-1, r_deadtime'length-1);
+        r_deadtime_run <= "0";
+      else
+        r_deadtime_run(0) <= (r_deadtime_run(0) OR s_deadtime_start);
+        r_deadtime <= r_deadtime - resize(unsigned(r_deadtime_run), r_deadtime'length);
+      end if;
+    end if;
+  end process;
+  ---------------------------------------------------------------------------------
 
 
     INST_dm_diag_auto : dm_diag_auto
@@ -238,12 +269,12 @@ begin
     time_dif_neg_i                => s_ctrl_time_dif_neg_i,
     time_dif_neg_ts_V_i           => "1",
     time_dif_neg_ts_i             => s_ctrl_time_dif_neg_ts_i,
-    wr_lock_acqu_last_ts_i        => r_wr_lock_acqu_last_ts, 
+    wr_lock_acqu_last_ts_i        => r_wr_lock_acqu_last_ts,
     wr_lock_acqu_last_ts_V_i      => "1",
-    wr_lock_cnt_i                 => s_wr_lock_cnt_i, 
-    wr_lock_cnt_V_i               => "1", 
-    wr_lock_loss_last_ts_i        => r_wr_lock_loss_last_ts, 
-    wr_lock_loss_last_ts_V_i      => "1", 
+    wr_lock_cnt_i                 => s_wr_lock_cnt_i,
+    wr_lock_cnt_V_i               => "1",
+    wr_lock_loss_last_ts_i        => r_wr_lock_loss_last_ts,
+    wr_lock_loss_last_ts_V_i      => "1",
     stall_observation_interval_o  => s_ctrl_stall_observation_interval_o,
     stall_stat_select_WR_o        => s_ctrl_stall_stat_select_WR_o,
     stall_stat_select_RD_o        => open,
@@ -256,5 +287,5 @@ begin
     stall_max_ts_i                => s_ctrl_stall_max_ts_i,
     ctrl_i                        => ctrl_i,
     ctrl_o                        => ctrl_o  );
-  
+
 end rtl;
