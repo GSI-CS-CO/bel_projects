78f398771d18605ec7a12c01e3345026afec9f36
ubecker <ubecker@asl740.acc.gsi.de>
2018-11-05 17:01:18 +0100

    Added lm32_assert.h

---
+++ modules/lm32-include/lm32_assert.h
@@ -0,0 +1,85 @@
+/*!
+ *
+ * @brief     Definition of the macro LM32_ASSERT similar to ANSI-macro "assert"
+ *
+ * @file      lm32_assert.h
+ * @copyright GSI Helmholtz Centre for Heavy Ion Research GmbH
+ * @author    Ulrich Becker <u.becker@gsi.de>
+ * @date      05.11.2018
+ *******************************************************************************
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 3 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library. If not, see <http://www.gnu.org/licenses/>.
+ *******************************************************************************
+*/
+#ifndef _LM32_ASSERT_H
+#define _LM32_ASSERT_H
+
+#ifndef NDEBUG
+ #include "mprintf.h"
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef NDEBUG           /* required by ANSI standard */
+# define LM32_ASSERT(__e) ((void)0)
+#else
+# define LM32_ASSERT(__e) ((__e) ? (void)0 : __lm32_assert_func (__FILE__, __LINE__, \
+                                                                 __ASSERT_FUNC, #__e))
+
+# ifndef __ASSERT_FUNC
+  /* Use g++'s demangled names in C++.  */
+#  if defined __cplusplus && defined __GNUC__
+#   define __ASSERT_FUNC __PRETTY_FUNCTION__
+
+  /* C99 requires the use of __func__.  */
+#  elif __STDC_VERSION__ >= 199901L
+#   define __ASSERT_FUNC __func__
+
+  /* Older versions of gcc don't have __func__ but can use __FUNCTION__.  */
+#  elif __GNUC__ >= 2
+#   define __ASSERT_FUNC __FUNCTION__
+
+  /* failed to detect __func__ support.  */
+#  else
+#   define __ASSERT_FUNC ((char *) 0)
+#  endif
+# endif /* !__ASSERT_FUNC */
+
+#define __ESC_BOLD   "\e[1m"
+#define __ESC_RED    "\e[31m"
+#define __ESC_NORMAL "\e[0m"
+
+static inline void __lm32_assert_func( const char* fileName,
+                                       int lineNumber,
+                                       const char* functionName,
+                                       const char* conditionStr )
+{
+   mprintf( __ESC_BOLD __ESC_RED
+            "Assertion failed in file: \"%s\" line: %d function: \"%s\" condition: \"%s\"\n"
+            "System stopped!\n"
+            __ESC_NORMAL,
+            fileName, lineNumber, functionName, conditionStr );
+   while( 1 );
+}
+
+#endif /* !NDEBUG */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // _LM32_ASSERT_H
+/*================================== EOF ====================================*/
+
---
+++ modules/lm32-test/assert/assert_test.c
@@ -0,0 +1,48 @@
+/*!
+ *
+ * @brief     Testprogram for the macro LM32_ASSERT similar to ANSI-macro "assert"
+ *
+ * @file      assert_test.c
+ * @copyright GSI Helmholtz Centre for Heavy Ion Research GmbH
+ * @author    Ulrich Becker <u.becker@gsi.de>
+ * @date      05.11.2018
+ *******************************************************************************
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 3 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library. If not, see <http://www.gnu.org/licenses/>.
+ *******************************************************************************
+*/
+#include <stdbool.h>
+#include "mprintf.h"
+#include "mini_sdb.h"
+
+//#define NDEBUG
+
+#include "lm32_assert.h"
+
+void init( void )
+{
+   discoverPeriphery();   // mini-sdb: get info on important Wishbone infrastructure
+   uart_init_hw();        // init UART, required for printf...
+}
+
+void main( void )
+{
+   init();
+   mprintf("\nFarewell, you cruel world!\n");
+   LM32_ASSERT( true );
+   LM32_ASSERT( false );
+   mprintf("\nI survived!\n");
+   while( true );
+}
+
+/*================================== EOF ====================================*/
