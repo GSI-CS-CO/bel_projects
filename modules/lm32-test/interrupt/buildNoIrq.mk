97449c28e6e22cb8ff63eb4233f0deb3eb96afc7
ubecker <ubecker@asl740.acc.gsi.de>
2018-11-16 17:16:36 +0100

    Initial

---
+++ modules/lm32-test/interrupt/buildNoIrq.mk
@@ -0,0 +1,102 @@
+
+# Without irq.c
+
+TOP		:= $(dir $(lastword $(MAKEFILE_LIST)))../../..
+QUARTUS		?= /opt/quartus
+QUARTUS_BIN	=  $(QUARTUS)/bin
+HDLMAKE := PATH=$(TOP)/bin:$(QUARTUS_BIN):$(PATH) PYTHONPATH=$(TOP)/lib/python2.7/site-packages hdlmake
+PATHPKG		?= $(shell $(HDLMAKE) list-mods | grep -G '^[^\#]' | grep top | grep -o '^\S*')
+SPI_LANES	?= ASx1
+
+CROSS_COMPILE	?= lm32-elf-
+CC		=  $(CROSS_COMPILE)gcc
+SHELL = /bin/sh
+OBJCOPY		=  $(CROSS_COMPILE)objcopy
+GENRAMMIF	?= $(TOP)/ip_cores/wrpc-sw/tools/genrammif
+INCPATH	:= $(TOP)/modules/lm32-include
+EBPATH  := $(TOP)/ip_cores/etherbone-core/hdl/eb_master_core
+W1    	:= $(TOP)/ip_cores/wrpc-sw
+USRCPUCLK	?= 62500
+CFLAGS	+= 	-mmultiply-enabled -mbarrel-shift-enabled -Os -DUSRCPUCLK=$(USRCPUCLK) -I$(INCPATH) -I$(W1)/include \
+		-I$(W1)/sdb-lib -I$(W1)/pp_printf -I$(EBPATH) -std=gnu99 -DCONFIG_WR_NODE -DCONFIG_PRINT_BUFSIZE=128 -DSDBFS_BIG_ENDIAN
+
+
+STUBD	?= $(TOP)/modules/lm32_stub
+STUBS	?= $(STUBD)/stubs.c $(STUBD)/crt0.S
+INCLUDES  += 	$(INCPATH)/dbg.c $(INCPATH)/mini_sdb.c $(INCPATH)/mprintf.c \
+		$(W1)/dev/uart.c $(W1)/lib/usleep.c $(W1)/dev/devicelist.c $(W1)/dev/syscon.c $(W1)/pp_printf/printf.c \
+		$(W1)/sdb-lib/glue.c $(W1)/pp_printf/vsprintf-mini.c $(INCPATH)/sdb_add.S
+LDFLAGS		?= -nostdlib -T ram.ld -Wl,--defsym,_fstack=$$(($(RAM_SIZE)-4)) -lgcc -lc 
+
+ifndef RAM_SIZE
+$(error Missing mandatory RAM_SIZE parameter! Quitting ...)
+endif
+
+.PHONY: ram.ld buildid.c $(PATHPKG)/ramsize_pkg.vhd $(TARGET)_shared_mmap.h
+.PRECIOUS: $(TARGET).bin
+
+include $(INCPATH)/build_lm32.mk
+ 
+
+all:	$(TARGET).mif $(TARGET)_stub.mif $(TARGET).sof $(TARGET).jic $(TARGET).rpd
+
+$(TARGET)_shared_mmap.h:
+	@(printf %b $(SMM)) > $@
+
+buildid.c:
+	@(printf %b $(CBR)) > $@
+
+ram.ld:
+	@(printf %b $(LDS)) > $@
+
+$(PATHPKG)/ramsize_pkg.vhd:
+	@(printf %b $(PKG)) > $@
+
+clean::
+	rm -rf db incremental_db PLLJ_PLLSPE_INFO.txt
+	rm -f $(TARGET).*.rpt $(TARGET).*.summary $(TARGET).map* $(TARGET).fit.* $(TARGET).pin $(TARGET).jdi $(TARGET)*.qdf $(TARGET).done $(TARGET).qws
+	rm -f $(TARGET).rpd $(TARGET).jic $(TARGET).pof $(TARGET).sof $(TARGET).dep $(TARGET).elf $(TARGET).o *.mif *.elf
+	rm -f ram.ld buildid.c $(TARGET)_shared_mmap.h 
+	rm -f project project.tcl files.tcl
+
+prog:
+	@read -p "If you have multiple USB-Programmer connected, choose the one you want to use: " BLASTER; \
+	[ -z "$$BLASTER" ] && BLASTER=1 ; \
+	$(QUARTUS_BIN)/quartus_pgm -c "$$BLASTER" -m jtag -o 'p;$(TARGET).sof'
+
+%_stub.elf:  ram.ld
+	$(CC) $(CFLAGS) -o $@ $^ $(STUBS) $(LDFLAGS)
+
+%.elf:	buildid.c ram.ld $(TARGET)_shared_mmap.h
+	$(CC) $(CFLAGS) -o $@ $^ $(STUBS) $(INCLUDES) $(LDFLAGS)
+
+%.bin:	%.elf
+	$(OBJCOPY) -O binary $< $@
+
+%.mif:	%.bin
+	$(GENRAMMIF) $< $(RAM_SIZE) > $@
+
+%.sof:	%.qsf %.mif $(PATHPKG)/ramsize_pkg.vhd 
+	$(HDLMAKE) makefile -f hdlmake.mk ; make -f hdlmake.mk project
+	find $(TOP) -name Manifest.py > $*.dep
+	sed -n -e 's/"//g;s/quartus_sh://;s/set_global_assignment.*-name.*_FILE //p' < $< >> $*.dep
+	echo "$*.sof $@:	$< " `cat $*.dep` > $*.dep
+	$(QUARTUS_BIN)/quartus_sh --tcl_eval load_package flow \; project_open $* \; execute_flow -compile
+
+%.opt:	%.sof
+	rm -f $@.tmp
+	[ $$($(QUARTUS_BIN)/quartus_cpf --version | sed -ne 's/^Version \([0-9]*\).*$$/\1/p') -lt 13 ] || \
+		echo "IGNORE_EPCS_ID_CHECK=ON" >> $@.tmp
+	echo "BITSTREAM_COMPRESSION=ON" >> $@.tmp
+	mv $@.tmp $@
+
+%.jic:	%.sof %.opt
+	$(QUARTUS_BIN)/quartus_cpf -c -o $*.opt -d $(FLASH) -s $(DEVICE) $< $@
+
+%.pof:	%.sof %.opt
+	$(QUARTUS_BIN)/quartus_cpf -c -o $*.opt -d $(FLASH) -m $(SPI_LANES) $< $@
+
+%.rpd:	%.pof %.opt
+	$(QUARTUS_BIN)/quartus_cpf -c -o $*.opt $< $@
+
+-include $(TARGET).dep
---
+++ modules/lm32-test/interrupt/irq_test.c
@@ -0,0 +1,190 @@
+/*!
+ *  @file irq_test.c
+ *  @brief Listen to all 32 external interrupts of LM32
+ *  @date 16.11.2018
+ *  @copyright (C) 2018 GSI Helmholtz Centre for Heavy Ion Research GmbH
+ *
+ *  @author Ulrich Becker <u.becker@gsi.de>
+ *
+ *
+ *******************************************************************************
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 3 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT AqNY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library. If not, see <http://www.gnu.org/licenses/>.
+ *******************************************************************************
+ */
+#include <string.h>
+#include <stdbool.h>
+#include "mini_sdb.h"
+//#include "../../top/gsi_scu/scu_bus.h"
+#include "eb_console_helper.h"
+#include "lm32_assert.h"
+#include "helper_macros.h"
+#include "lm32_hexdump.h"
+#include "dbg.h"
+
+volatile unsigned int g_aEvents[32];
+static void onIrq( uint8_t n );
+
+// ----------------------------------------------------------------------------
+static inline  uint32_t  irq_get_mask(void)
+{
+    //read IRQ mask
+    uint32_t im;
+    asm ( "rcsr %0, im": "=&r" (im));
+    return im;
+}
+
+// ----------------------------------------------------------------------------
+static inline void irq_set_mask( uint32_t im)
+{
+    //write IRQ mask
+    asm (   "wcsr im, %0" \
+            :             \
+            : "r" (im)    \
+        );
+}
+
+// ----------------------------------------------------------------------------
+static inline  uint32_t  irq_get_enable(void)
+{
+    //read global IRQ enable bit
+    uint32_t eie;
+    asm ( "rcsr %0, ie\n"  \
+          "srui %0, %0, 1\n" \
+          "andi %0, %0, 1" \
+         : "=&r" (eie));
+    return eie;
+}
+
+// ----------------------------------------------------------------------------
+static inline void irq_disable(void)
+{
+   //globally disable interrupts
+   unsigned foo;
+   asm volatile   (  "rcsr %0, IE\n"            \
+                     "andi  %0, %0, 0xFFFE\n"   \
+                     "wcsr IE, %0"              \
+                     : "=r" (foo)               \
+                     :                          \
+                     :
+                     );
+}
+
+// -----------------------------------------------------------------------------
+static inline void irq_enable(void)
+{
+   //globally enable interrupts
+   unsigned foo;
+   asm volatile   (  "rcsr %0, IE\n"      \
+                     "ori  %0, %0, 1\n"   \
+                     "wcsr IE, %0"        \
+                     : "=r" (foo)         \
+                     :                    \
+                     :                    \
+                     );
+}
+
+// -----------------------------------------------------------------------------
+static inline void irq_clear( uint32_t mask)
+{
+    //clear pending interrupt flag(s)
+    asm           (  "wcsr ip, %0"  \
+                     :              \
+                     : "r" (mask)   \
+                     :              \
+                     );
+}
+
+// -----------------------------------------------------------------------------
+void _irq_entry(void)
+{
+  uint32_t  ip;
+  unsigned char irq_no = 0;
+
+#if NESTED_IRQS
+  uint32_t  msk;
+#endif
+  asm ("rcsr %0, ip": "=r"(ip)); //get pending flags
+  while(ip)
+  {
+    if(ip & 1) //check if irq with lowest number is pending
+    {
+#if NESTED_IRQS
+      msk = irq_get_mask();
+      irq_set_mask(msk & ((1<<irq_no)-1) ); //mask out all priorities matching and below current
+      irq_enable();
+#endif
+      //irq_pop_msi(irq_no);      //pop msg from msi queue into global_msi variable
+      onIrq( irq_no );
+      irq_clear(1<<irq_no);     //clear pending bit
+#if NESTED_IRQS
+      irq_set_mask(msk);
+      irq_disable();
+#endif
+    }
+    irq_no++;
+    ip = ip >> 1; //process next irq
+  }
+}
+
+// ----------------------------------------------------------------------------
+static void onIrq( uint8_t n )
+{
+   LM32_ASSERT( n < ARRAY_SIZE( g_aEvents ) );
+   g_aEvents[n]++;
+}
+
+// ----------------------------------------------------------------------------
+static unsigned int getEventOf( unsigned int n )
+{
+   unsigned int ret;
+   LM32_ASSERT( n < ARRAY_SIZE( g_aEvents ) );
+   irq_disable();
+   ret = g_aEvents[n];
+   irq_enable();
+   return ret;
+}
+
+#define LINE_START 3
+
+//=============================================================================
+void main( void )
+{
+   unsigned int oldEvents[sizeof(g_aEvents)];
+   memset( (void*)g_aEvents, 0, sizeof(g_aEvents) );
+   memset( oldEvents, 0, sizeof(oldEvents) );
+   bool first = true;
+
+   discoverPeriphery();
+   uart_init_hw();
+   gotoxy( 1, 1 );
+   clrscr();
+   mprintf( ESC_BOLD "Interrupt listener\n" ESC_NORMAL );
+   irq_enable();
+   while( true )
+   {
+      for( int i = 0; i < ARRAY_SIZE( g_aEvents ); i++ )
+      {
+         unsigned int newEvent = getEventOf( i );
+         if( first || (oldEvents[i] != newEvent) )
+         {
+            oldEvents[i] = newEvent;
+            gotoxy( 1, LINE_START + i );
+            mprintf( "IRQ %02d: %04d", i, newEvent );
+         }
+      }
+      first = false;
+   }
+}
+
+/*================================== EOF ====================================*/
