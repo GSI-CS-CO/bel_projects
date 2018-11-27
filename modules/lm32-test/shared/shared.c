d413e845c3211dc216e889e9651731fe25fd7c49
ubecker <ubecker@asl740.acc.gsi.de>
2018-11-27 16:59:03 +0100

    Current state of develop

--- modules/lm32-test/shared/shared.c
+++ modules/lm32-test/shared/shared.c
@@ -21,18 +21,74 @@
  *  License along with this library. If not, see <http://www.gnu.org/licenses/>.
  *******************************************************************************
 */
-
+#include <string.h>
 #include <stdbool.h>
 #include "mini_sdb.h"
+#include "aux.h"
+#include "../../tools/wb_slaves.h"
 #include "eb_console_helper.h"
 #include "helper_macros.h"
+#include "lm32_assert.h"
+#include "shared_shared_mmap.h"
 
 #define SHARED __attribute__((section(".shared")))
 
-extern uint32_t*       _startshared[];
-uint32_t SHARED dummy0 = 0;
-uint32_t SHARED dummy1 = 0;
-uint32_t SHARED dummy2 = 0;
+typedef struct
+{
+   uint32_t a;
+   uint32_t b;
+   uint32_t c;
+} IO_T;
+
+
+//extern uint32_t*       _startshared[];
+volatile IO_T SHARED io;
+
+
+void getWishboneTAI()
+{
+  uint32_t *pPPSGen;   // WB address of PPS_GEN
+  uint32_t taiSecs;    // TAI full seconds
+  uint32_t taiNsecs;   // TAI nanoseconds part
+
+  // find Wishbone address of WR PPS GEN
+  pPPSGen   = find_device_adr(WR_PPS_GEN_VENDOR, WR_PPS_GEN_PRODUCT);
+
+  taiSecs  = *((uint32_t*)(((uint8_t*)pPPSGen) + WR_PPS_GEN_CNTR_UTCLO));
+  taiNsecs = *((uint32_t*)(((uint8_t*)pPPSGen) + WR_PPS_GEN_CNTR_NSEC));
+
+  //print TAI to UART
+  mprintf("TAI: %08u.%09u\n", taiSecs, taiNsecs);
+
+} // getWishboneTAI
+
+
+
+#define LM32_INTERN_OFFSET 0x80000000
+
+/*! ---------------------------------------------------------------------------
+ * @brief Gets the pointer to shared memory for
+ *        external perspective from host bridge.
+ * @retval !=NULL Base pointer for external access via host
+ * @retval ==NULL Error
+ */
+uint32_t* getEtherBoneBaseAddressToSharedMemory( const uint32_t sharedOffset )
+{
+   uint32_t idx;
+   sdb_location aFoundSdb[10]; //! @todo Check array size!
+   sdb_location foundClu;
+   unsigned int cpuId = getCpuIdx();
+
+   idx = 0;
+   find_device_multi( &foundClu, &idx, 1, GSI, LM32_CB_CLUSTER );
+   idx = 0;
+   find_device_multi_in_subtree( &foundClu, aFoundSdb, &idx, ARRAY_SIZE(aFoundSdb), GSI, LM32_RAM_USER);
+   if(idx < cpuId)
+      return NULL;
+
+   LM32_ASSERT( cpuId < ARRAY_SIZE(aFoundSdb) );
+   return (uint32_t*)(getSdbAdr(&aFoundSdb[cpuId]) + sharedOffset - LM32_INTERN_OFFSET);
+}
 
 void main( void )
 {
@@ -41,10 +97,30 @@ void main( void )
 
    gotoxy( 0, 0 );
    clrscr();
-   mprintf( "Shared memory test 0x%x\n", _startshared );
-   mprintf( "Addr 0 = 0x%x\n", &dummy0 );
-   mprintf( "Addr 1 = 0x%x\n", &dummy1 );
-   mprintf( "Addr 2 = 0x%x\n", &dummy2 );
+   mprintf( "Shared memory test\n");
+
+   getWishboneTAI();
+
+   uint32_t* pCpuRamExternal = getEtherBoneBaseAddressToSharedMemory(SHARED_OFFS);
+   if( pCpuRamExternal == NULL )
+   {
+      mprintf( ESC_FG_RED "ERROR: Could not find external WB address of my own RAM !\n" ESC_NORMAL);
+      return;
+   }
+
+   mprintf( "intern: &io.a: 0x%x extern: 0x%x\n", &io.a, ((uint32_t)pCpuRamExternal) + offsetof( IO_T, a ) );
+   mprintf( "intern: &io.b: 0x%x extern: 0x%x\n", &io.b, ((uint32_t)pCpuRamExternal) + offsetof( IO_T, b ) );
+   mprintf( "intern: &io.c: 0x%x extern: 0x%x\n", &io.c, ((uint32_t)pCpuRamExternal) + offsetof( IO_T, c ) );
+
+   while( true )
+   {
+      io.a = 0;
+      while( io.a == 0 );
+      mprintf( "io.a: 0x%x\n", io.a );
+      mprintf( "io.b: 0x%x\n", io.b );
+      mprintf( "io.c: 0x%x\n", io.c );
+   }
+
 }
 
 /*================================== EOF ====================================*/
