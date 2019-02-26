d510597815e478c2b2281e5b2e50b7a3fa89b36e
Michael Reese <m.reese@gsi.de>
2019-02-26 17:43:28 +0100

    wr-mil-gw: add OLED output

---
+++ modules/wr-mil-gw/firmware/wr_mil_msi.h
@@ -0,0 +1,10 @@
+#ifndef WR_MIL_MSI_H_
+#define WR_MIL_MSI_H_
+
+#include <stdint.h>
+
+void send_MSI(uint32_t slot, uint32_t msg);
+
+
+#endif
+
---
+++ modules/wr-mil-gw/firmware/wr_mil_oled.h
@@ -0,0 +1,27 @@
+#ifndef WR_MIL_OLED_H_
+#define WR_MIL_OLED_H_
+
+#include <inttypes.h>
+#include <stdint.h>
+#include "wr_mil_config.h"
+
+#define OLED_PID 0x93a6f3c4
+
+
+// this function is intended to be called from the main loop. Most of the time it does nothing,
+// but after ~4 sec, it updates the display. This happens one character at a time, i.e. one 
+// character per function call. 
+// It was done like this to minimize the waiting time for the function to return (now ~7-8 us) and
+// so that the real-time behavior of the system is not compromized.
+//
+// returns 1 if a character was written, 0 otherwise
+int oled_loop(volatile WrMilConfig *config, volatile uint32_t *oled);
+
+// for internal use only
+extern uint32_t oled_loop_counter;
+extern uint32_t oled_char_counter;
+void oled_write_one_char(volatile WrMilConfig *config, volatile uint32_t *oled, int ch);
+
+
+#endif
+
