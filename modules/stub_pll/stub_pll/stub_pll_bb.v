e1c7bfd11018978d02702e606bd9553dedd0c149
A. Hahn <a.hahn@gsi.de>
2019-03-06 14:17:13 +0100

    modules: added stub_pll for testing (arria 10)

---
+++ modules/stub_pll/stub_pll/stub_pll_bb.v
@@ -0,0 +1,18 @@
+
+module stub_pll (
+	rst,
+	refclk,
+	locked,
+	outclk_0,
+	outclk_1,
+	outclk_2,
+	outclk_3);	
+
+	input		rst;
+	input		refclk;
+	output		locked;
+	output		outclk_0;
+	output		outclk_1;
+	output		outclk_2;
+	output		outclk_3;
+endmodule
