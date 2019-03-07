2b864650b6362cf1a964182c4ff77c41ecde7276
A. Hahn <a.hahn@gsi.de>
2019-03-07 15:18:47 +0100

    pexarria10 and pexarria10_soc: fixed pci_control.tcl (clocking)

--- syn/gsi_pexarria10/control/pci_control.tcl
+++ syn/gsi_pexarria10/control/pci_control.tcl
@@ -1,7 +1,7 @@
 set platform "pexaria10"
 source ../../autogen.tcl
 source ../../../modules/build_id/build_id.tcl
-source ../../../ip_cores/general-cores/platform/altera/networks/arria10gx.tcl
+source ../../../ip_cores/general-cores/platform/altera/networks/arria10gx/arria10gx.tcl
 source ../../../ip_cores/general-cores/platform/altera/wb_pcie/arria10gx.tcl
 source ../../../modules/stub_pll/stub_pll.tcl
 source ../../../modules/pll/arria10/arria10_pll.tcl
--- syn/gsi_pexarria10_soc/control/pci_control.tcl
+++ syn/gsi_pexarria10_soc/control/pci_control.tcl
@@ -1,7 +1,7 @@
 set platform "pexaria10_soc"
 source ../../autogen.tcl
 source ../../../modules/build_id/build_id.tcl
-source ../../../ip_cores/general-cores/platform/altera/networks/arria10gx.tcl
+source ../../../ip_cores/general-cores/platform/altera/networks/arria10gx/arria10gx.tcl
 source ../../../ip_cores/general-cores/platform/altera/wb_pcie/arria10sx.tcl
 source ../../../modules/stub_pll/stub_pll.tcl
 source ../../../modules/pll/arria10/arria10_pll.tcl
