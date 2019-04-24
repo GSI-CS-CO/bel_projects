210bd53edf68c76c0e28b8ad9e50bc7844fa4795
Dietrich Beck <d.beck@gsi.de>
2019-04-24 18:45:02 +0200

    b2b-test: cleaning up refactoring hack (probably containing many bugs)

--- modules/b2b-test/b2btest-fw/b2b-pm.c
+++ modules/b2b-test/b2btest-fw/b2b-pm.c
@@ -48,27 +48,8 @@
 #include "mini_sdb.h"
 
 /* includes specific for bel_projects */
-//#include "irq.h"
-//#include "ebm.h"
-//#include "aux.h"
 #include "dbg.h"
 #include "../../../top/gsi_scu/scu_mil.h"                               // register layout of 'MIL macro' /* chk */
-//#include "../../../ip_cores/wr-cores/modules/wr_eca/eca_queue_regs.h"   // register layout ECA queue
-//#include "../../../ip_cores/wr-cores/modules/wr_eca/eca_regs.h"         // register layout ECA control
-/*                                                                      
-#include "../../../ip_cores/wr-cores/modules/wr_pps_gen/pps_gen_regs.h" // useless register layout, I can't handle this wbgen stuff here
-*/
-/*#define WR_PPS_GEN_ESCR         0x1c                                    // External Sync Control Register
-#define WR_PPS_GEN_ESCR_MASK    0xC                                     // bit 2: PPS valid, bit 3: timestamp valid
-
-#define WR_ENDPOINT             0x650c2d4f                              // WR-endpoint
-#define WR_ENDPOINT_MACHI       0x24                                    // MAC high bytes
-#define WR_ENDPOINT_MACLO       0x28                                    // MAC low bytes
-
-#include "../../../ip_cores/saftlib/drivers/eca_flags.h"                // definitions for ECA queue
-
-#include "../../../tools/wb_slaves.h"                                   // Dietrichs hackish solution for defining register layout
-*/
 
 #include <b2b-common.h>                                                 // common stuff for b2b
 #include <b2b-test.h>                                                   // defs
@@ -87,21 +68,6 @@ volatile uint32_t *pPPSGen;             // WB address of PPS Gen
 volatile uint32_t *pIOCtrl;             // WB address of IO Control
 
 volatile uint32_t *pShared;             // pointer to begin of shared memory region
-/*uint32_t *pSharedVersion;               // pointer to a "user defined" u32 register; here: publish version
-uint32_t *pSharedSumStatus;             // pointer to a "user defined" u32 register; here: publish OR of all (actual) error bits
-uint32_t *pSharedNBadStatus;            // pointer to a "user defined" u32 register; here: publish # of bad status (=error) incidents
-uint32_t *pSharedNBadState;             // pointer to a "user defined" u32 register; here: publish # of bad state (=FATAL, ERROR, UNKNOWN) incidents
-volatile uint32_t *pSharedCmd;          // pointer to a "user defined" u32 register; here: get command from host
-uint32_t *pSharedState;                 // pointer to a "user defined" u32 register; here: publish status
-volatile uint32_t *pSharedData4EB;      // pointer to a n x u32 register; here: memory region for receiving EB return values
-uint32_t *pSharedMacHi;                 // pointer to a "user defined" u32 register; here: high bits of MAC
-uint32_t *pSharedMacLo;                 // pointer to a "user defined" u32 register; here: low bits of MAC
-uint32_t *pSharedIp;                    // pointer to a "user defined" u32 register; here: IP
-uint32_t *pSharedTDiagHi;               // pointer to a "user defined" u32 register; here: time when diag was cleared, high bits
-uint32_t *pSharedTDiagLo;               // pointer to a "user defined" u32 register; here: time when diag was cleared, low bits
-uint32_t *pSharedTS0Hi;                 // pointer to a "user defined" u32 register; here: time when FW was in S0 state, high bits
-uint32_t *pSharedTS0Lo;                 // pointer to a "user defined" u32 register; here: time when FW was in S0 state, low bits
-*/
 uint32_t *pSharedNTransfer;             // pointer to a "user defined" u32 register; here: # of transfers
 
 uint32_t *pCpuRamExternal;              // external address (seen from host bridge) of this CPU's RAM            
@@ -110,35 +76,6 @@ uint32_t *pCpuRamExternalData4EB;       // external address (seen from host brid
 uint32_t sumStatus;                     // all status infos are ORed bit-wise into sum status, sum status is then published
 uint32_t nBadStatus;                    // # of bad status (=error) incidents
 uint32_t nBadState;                     // # of bad state (=FATAL, ERROR, UNKNOWN) incidents*/
-
-
-/*uint32_t ioCtrlSetGate(uint32_t enable, uint32_t io)  // set gate of LVDS input
-{
-  uint32_t offset;
-  
-  if ((enable != 1) && (enable != 0)) return COMMON_STATUS_OUTOFRANGE;
-  if (io > 31)                        return COMMON_STATUS_OUTOFRANGE;
-
-  if (enable) offset = IO_CTRL_LVDSINGATESETLOW;
-  else        offset = IO_CTRL_LVDSINGATERESETLOW;
-
-  *(pIOCtrl + (offset >> 2)) = (1 << io);
-
-  return COMMON_STATUS_OK;
-} // ioCtrlSetGate
-*/
-
-/*uint32_t wrCheckSyncState() //check status of White Rabbit (link up, tracking)
-{
-  uint32_t syncState;
-
-  syncState =  *(pPPSGen + (WR_PPS_GEN_ESCR >> 2));                         // read status
-  syncState = syncState & WR_PPS_GEN_ESCR_MASK;                             // apply mask
-
-  if ((syncState == WR_PPS_GEN_ESCR_MASK)) return COMMON_STATUS_OK;        // check if all relevant bits are set
-  else                                     return COMMON_STATUS_WRBADSYNC;
-} //wrCheckSyncState 
-*/
 
 
 void init() // typical init for lm32
@@ -196,248 +133,6 @@ void initSharedMem() // determine address and clear shared mem
 
 } // initSharedMem 
 
-/*
-uint32_t findPPSGen() //find WB address of WR PPS Gen
-{
-  pPPSGen = 0x0;
-  
-  // get Wishbone address for PPS Gen
-  pPPSGen = find_device_adr(CERN, WR_PPS_GEN);
-
-  if (!pPPSGen) {DBPRINT1("b2b-test: can't find WR PPS Gen\n"); return COMMON_STATUS_ERROR;}
-  else                                                          return COMMON_STATUS_OK;
-} // findPPSGen
-*/
-
-/*
-uint32_t findIOCtrl() // find WB address of IO Control
-{
-  pIOCtrl = 0x0;
-
-  pIOCtrl = find_device_adr(IO_CTRL_VENDOR, IO_CTRL_PRODUCT);
-
-  if (!pIOCtrl) {DBPRINT1("b2b-test: can't find IO Control\n"); return COMMON_STATUS_ERROR;}
-  else                                                          return COMMON_STATUS_OK;    
-} // find IOCtrol
-*/
-
-/*
-uint32_t findECAQueue() // find WB address of ECA channel for LM32
-{
-#define ECAQMAX           4     // max number of ECA channels in the system
-#define ECACHANNELFORLM32 2     // this is a hack! suggest implementing finding via sdb-record and info
-
-  // stuff below needed to get WB address of ECA queue
-  sdb_location ECAQ_base[ECAQMAX];
-  uint32_t ECAQidx = 0;         
-  uint32_t *tmp;                
-  int i;
-
-  // get Wishbone address of ECA queue 
-  // get list of ECA queues
-  find_device_multi(ECAQ_base, &ECAQidx, ECAQMAX, ECA_QUEUE_SDB_VENDOR_ID, ECA_QUEUE_SDB_DEVICE_ID);
-  pECAQ = 0x0;
-
-  // find ECA queue connected to ECA chanel for LM32
-  for (i=0; i < ECAQidx; i++) {
-    tmp = (uint32_t *)(getSdbAdr(&ECAQ_base[i]));  
-    if ( *(tmp + (ECA_QUEUE_QUEUE_ID_GET >> 2)) == ECACHANNELFORLM32) pECAQ = tmp;
-  }
-
-  if (!pECAQ) {DBPRINT1("b2b-test: can't find ECA queue\n"); return COMMON_STATUS_ERROR;}
-  else                                                       return COMMON_STATUS_OK;
-} // findECAQueue
-*/
-
-/*void initCmds() // init stuff for handling commands, trivial for now, will be extended
-{
-  //  initalize command value: 0x0 means 'no command'
-  *pSharedCmd     = 0x0;
-} // initCmds
-
- 
-void clearDiag() // clears all statistics
-{
-  uint64_t now;
-  
-  sumStatus      = 0;
-  nBadStatus     = 0;
-
-  now = getSysTime();
-  *pSharedTDiagHi = (uint32_t)(now >> 32);
-  *pSharedTDiagLo = (uint32_t)now & 0xffffffff;
-
-} // clearDiag
-*/
-
-
-/*uint32_t doActionS0()
-{
-  uint32_t status = COMMON_STATUS_OK;
-  uint64_t now;
-
-  if (findECAQueue() != COMMON_STATUS_OK) status = COMMON_STATUS_ERROR; 
-  if (findPPSGen()   != COMMON_STATUS_OK) status = COMMON_STATUS_ERROR;
-  if (findWREp()     != COMMON_STATUS_OK) status = COMMON_STATUS_ERROR;
-  if (findIOCtrl()   != COMMON_STATUS_OK) status = COMMON_STATUS_ERROR;
-
-  nBadState     = 0;
-  now           = getSysTime();
-  *pSharedTS0Hi = (uint32_t)(now >> 32);
-  *pSharedTS0Lo = (uint32_t)now & 0xffffffff;
-  
-  initCmds();                    
-
-  return status;
-} // doActionS0
-*/
-
-/*
-uint32_t entryActionConfigured(uint64_t destMac, uint32_t destIp)
-{
-  uint32_t status = COMMON_STATUS_OK;
-  uint64_t mac;
-  uint32_t ip;
-
-  // configure EB master (SRC and DST MAC/IP are set from host)
-  //  ebmInit(100, 0xffffffffffff, 0xffffffff, EBM_NOREPLY);
-  if ((status = common_ebmInit(2000, 0xffffffffffff, 0xffffffff, EBM_NOREPLY)) != COMMON_STATUS_OK) {
-    DBPRINT1("b2b-test: ERROR - init of EB master failed! %d\n", status);
-    return status;
-  } 
-
-  // get and publish NIC data
-  common_publishNICData();
-
-  return status;
-} // entryActionConfigured
-*/
-
-/*uint32_t entryActionOperation()
-{
-  int      i;
-  uint64_t tDummy;
-  uint64_t pDummy;
-  uint32_t flagDummy;
-
-  // clear diagnostics
-  common_clearDiag();                  
-
-  // flush ECA queue for lm32
-  i = 0;
-  while (common_wait4ECAEvent(1, &tDummy, &pDummy, &flagDummy) !=  B2BTEST_ECADO_TIMEOUT) {i++;}
-  DBPRINT1("b2b-test: ECA queue flushed - removed %d pending entries from ECA queue\n", i);
-
-  return COMMON_STATUS_OK;
-} // entryActionOperation
-*/
-
-/*
-uint32_t exitActionOperation(){
-  return COMMON_STATUS_OK;
-} // exitActionOperation
-*/
-
-/*
-uint32_t exitActionError()
-{
-  return COMMON_STATUS_OK;
-} // exitActionError
-*/
-
-/*void cmdHandler(uint32_t *reqState) // handle commands from the outside world
-{
-  uint32_t cmd;
-
-  cmd = *pSharedCmd;
-  // check, if the command is valid and request state change
-  if (cmd) {
-    switch (cmd) {
-    case COMMON_CMD_CONFIGURE :
-      *reqState =  COMMON_STATE_CONFIGURED;
-      DBPRINT3("b2b-test: received cmd %d\n", cmd);
-      break;
-    case COMMON_CMD_STARTOP :
-      *reqState = COMMON_STATE_OPREADY;
-      DBPRINT3("b2b-test: received cmd %d\n", cmd);
-      break;
-    case COMMON_CMD_STOPOP :
-      *reqState = COMMON_STATE_STOPPING;
-      DBPRINT3("b2b-test: received cmd %d\n", cmd);
-      break;
-    case COMMON_CMD_IDLE :
-      *reqState = COMMON_STATE_IDLE;
-      DBPRINT3("b2b-test: received cmd %d\n", cmd);
-      break;
-    case COMMON_CMD_RECOVER :
-      *reqState = COMMON_STATE_IDLE;
-      DBPRINT3("b2b-test: received cmd %d\n", cmd);
-      break;
-    case COMMON_CMD_CLEARDIAG :
-      DBPRINT3("b2b-test: received cmd %d\n", cmd);
-      clearDiag();
-      break;
-    default:
-      DBPRINT3("b2b-test: received unknown command '0x%08x'\n", cmd);
-    } // switch 
-    *pSharedCmd = 0x0; // reset cmd value in shared memory 
-  } // if command 
-} // cmdHandler
-*/
-
-/*uint32_t changeState(uint32_t *actState, uint32_t *reqState, uint32_t actStatus)   //state machine; see b2b-test.h for possible states and transitions
-{
-  uint32_t statusTransition= COMMON_STATUS_OK;
-  uint32_t status;
-  uint32_t nextState;                   
-
-  // if something severe happened, perform implicitely allowed transition to ERROR or FATAL states
-  // else                        , handle explicitcely allowed transitions
-
-  if ((*reqState == COMMON_STATE_ERROR) || (*reqState == COMMON_STATE_FATAL)) {statusTransition = actStatus; nextState = *reqState;}
-  else {
-    nextState = *actState;                       // per default: remain in actual state without exit or entry action
-    switch (*actState) {                         // check for allowed transitions: 1. determine next state, 2. perform exit or entry actions if required
-    case COMMON_STATE_S0:
-      if      (*reqState == COMMON_STATE_IDLE)       {                                             nextState = *reqState;}      
-      break;
-    case COMMON_STATE_IDLE:
-      if      (*reqState == COMMON_STATE_CONFIGURED)  {statusTransition = entryActionConfigured(); nextState = *reqState;}
-      break;
-    case COMMON_STATE_CONFIGURED:
-      if      (*reqState == COMMON_STATE_IDLE)       {                                             nextState = *reqState;}
-      else if (*reqState == COMMON_STATE_CONFIGURED) {statusTransition = entryActionConfigured();  nextState = *reqState;}
-      else if (*reqState == COMMON_STATE_OPREADY)    {statusTransition = entryActionOperation();   nextState = *reqState;}
-      break;
-    case COMMON_STATE_OPREADY:
-      if      (*reqState == COMMON_STATE_STOPPING)   {statusTransition = exitActionOperation();    nextState = *reqState;}
-      break;
-    case COMMON_STATE_STOPPING:
-      nextState = COMMON_STATE_CONFIGURED;      //automatic transition but without entryActionConfigured
-    case COMMON_STATE_ERROR:
-      if      (*reqState == COMMON_STATE_IDLE)       {statusTransition = exitActionError();        nextState = *reqState;}
-      break;
-    default: 
-      nextState = COMMON_STATE_S0;
-    } // switch actState
-  }  // else something severe happened
-  
-  // if the transition failed, transit to error state (except we are already in FATAL state)
-  if ((statusTransition != COMMON_STATUS_OK) && (nextState != COMMON_STATE_FATAL)) nextState = COMMON_STATE_ERROR;
-
-  // if the state changes
-  if (*actState != nextState) {                   
-    mprintf("b2b-test: changed to state %d\n", nextState);
-    *actState = nextState;                      
-    status = statusTransition;
-  } // if state change
-  else  status = actStatus;
-
-  *reqState = COMMON_STATE_UNKNOWN;              // reset requested state (= no change state requested)  
-
-  return status;
-} //changeState
-*/
 
 uint32_t doActionOperation(uint64_t *tAct,                    // actual time
                            uint32_t actStatus)                // actual status of firmware
@@ -508,30 +203,6 @@ uint32_t doActionOperation(uint64_t *tAct,                    // actual time
   return status;
 } // doActionOperation
 
-/*
-uint32_t doAutoRecovery(uint32_t actState, uint32_t *reqState)                    // do autorecovery from error state
-{
-  switch (actState)
-    {
-    case COMMON_STATE_ERROR :
-      DBPRINT3("b2b-common: attempting autorecovery ERROR -> IDLE\n");
-      usleep(10000000);
-      *reqState = COMMON_STATE_IDLE; 
-      break;
-    case COMMON_STATE_IDLE :
-      DBPRINT3("b2b-common: attempting autorecovery IDLE -> CONFIGURED\n");
-      usleep(5000000);
-      *reqState = COMMON_STATE_CONFIGURED;
-      break;
-    case COMMON_STATE_CONFIGURED :
-      DBPRINT3("b2b-common: attempting autorecovery CONFIGURED -> OPREADY\n");
-      usleep(5000000);
-      *reqState = COMMON_STATE_OPREADY;
-      break;
-    default : ;
-    } // switch actState
-} // doAutoRecovery
-*/
 
 void main(void) {
  
