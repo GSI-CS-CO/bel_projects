2ac787c2d1352c6a5a6f0e8d420984c1a78e442b
Alessandro Rubini <rubini@gnudd.com>
2019-05-02 15:07:15 +0200

    syn/build.mk: sort qsf file to minimize differences
    The qsf file is a list of global assignments, and every line
    is independent. Unfortunately, the content is sometimes re-saved
    by quartus in a completely different order, but the content is
    equivalent.
    
    For example, commit cad275d reports 719 changes in scu_control.qsf,
    wherease it only grew by 3 new lines.
    
    After this commit, and respective builds, we'll have a single big
    diff for each qsf file, but later on the changes will be minimal and
    user-readable.
    
    Signed-off-by: Alessandro Rubini <rubini@gnudd.com>

--- syn/build.mk
+++ syn/build.mk
@@ -75,6 +75,7 @@ prog:
 	$(GENRAMMIF) $< $(RAM_SIZE) > $@
 
 %.sof:	%.qsf %.mif $(PATHPKG)/ramsize_pkg.vhd 
+	mv $*.qsf $*.qsf-tmp; sort $*.qsf-tmp > $*.qsf; rm $*.qsf-tmp
 	$(HDLMAKE) makefile -f hdlmake.mk ; make -f hdlmake.mk project
 	find $(TOP) -name Manifest.py > $*.dep
 	sed -n -e 's/"//g;s/quartus_sh://;s/set_global_assignment.*-name.*_FILE //p' < $< >> $*.dep
