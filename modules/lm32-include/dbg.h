e464052cef90dd8463ed9f8df16421b5d400f73a
Dietrich Beck <d.beck@gsi.de>
2019-05-16 11:26:05 +0200

    dbg: cleaning up 'mprintf'

--- modules/lm32-include/dbg.h
+++ modules/lm32-include/dbg.h
@@ -5,7 +5,7 @@
 #include <string.h>
 #include <inttypes.h>
 #include <stdint.h>
-#include "mprintf.h"
+#include "pp-printf.h"
 
 // Provides some control on how talkative your dbg statements get
 #ifndef DEBUGLEVEL
@@ -14,21 +14,21 @@
 //print macro for debuglevel 1-3
 #ifdef DEBUGLEVEL
    #if DEBUGLEVEL>=1
-      #define DBPRINT1 mprintf
-      #define DBPRINT  mprintf
+      #define DBPRINT1 pp_printf
+      #define DBPRINT  pp_printf
    #else
       #define DBPRINT1(...)
       #define DBPRINT(...)
    #endif
 
    #if DEBUGLEVEL>=2
-      #define DBPRINT2 mprintf
+      #define DBPRINT2 pp_printf
    #else
       #define DBPRINT2(...)
    #endif
 
    #if DEBUGLEVEL>=3
-      #define DBPRINT3 mprintf
+      #define DBPRINT3 pp_printf
    #else
       #define DBPRINT3(...)
    #endif
