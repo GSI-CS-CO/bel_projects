721b7adf567d9e1f230b71c9b4408c802ecb9084
Dietrich Beck <d.beck@gsi.de>
2019-06-04 17:16:37 +0200

    stack check: write to lm32 id rom instead of WR console

--- modules/lm32-include/stack-check.c
+++ modules/lm32-include/stack-check.c
@@ -1,5 +1,7 @@
 #include <stack.h>
 #include <assert.h>
+#include <pp-printf.h>
+#include <string.h>
 
 void check_stack(void)
 {
@@ -11,3 +13,26 @@ void check_stack(void)
 	}
 	assert(_endram == ENDRAM_MAGIC, "Stack overflow! (%x)\n", _endram);
 }
+
+
+// routine writes info to a dedicated offset within the lm32 firmware ID
+void check_stack_fwid(uint32_t *fwid)
+{
+#define STACKSTATUS_OFFSET 24
+  
+  // lazily, initialize at first invocation (no init function is there) 
+  static int  inited;
+  char help[64];
+  int i;
+  
+  if (!inited) {
+    inited++;
+    _endram = ENDRAM_MAGIC;
+  }
+  if (_endram != ENDRAM_MAGIC) {
+    // avoid trailing '/0'
+    pp_sprintf(help, "Stack overflow! (0x%x)", _endram);
+    for (i=0; i<strlen(help); i++) ((char *)fwid)[i+STACKSTATUS_OFFSET] = help[i];
+  } // if _endram
+} // check_stack_fwid
+
