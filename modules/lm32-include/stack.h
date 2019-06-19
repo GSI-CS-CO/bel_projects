1940c883b435994d9c89337c675fcc740917c5d0
Dietrich Beck <d.beck@gsi.de>
2019-06-19 11:26:00 +0200

    stack-check: changed variable name

--- modules/lm32-include/stack.h
+++ modules/lm32-include/stack.h
@@ -6,6 +6,6 @@
 
 extern uint32_t _endram;
 extern void check_stack(void);
-extern void check_stack_fwid(uint32_t *stackStatus);
+extern void check_stack_fwid(uint32_t *fwid);
 
 #endif /* __STACK_H__ */
