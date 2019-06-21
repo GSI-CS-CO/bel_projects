59535e35d60f2aa6ee09ccd785c3ed4bdb5ee0d0
Ulrich Becker <U.Becker@gsi.de>
2019-06-21 15:29:53 +0200

    wb_ddr3ram2 added

--- top/gsi_daq/feSupport/scu/etherbone/Constants.hpp
+++ top/gsi_daq/feSupport/scu/etherbone/Constants.hpp
@@ -3,6 +3,8 @@
 
 #include <string>
 
+
+
 namespace FeSupport {
   namespace Scu {
     namespace Etherbone {
@@ -24,12 +26,13 @@ namespace FeSupport {
         scu_bus_master = 0x9602eb6f,
         gsi_gpio_32    = 0x35aa6b95,
         oled_display   = 0x93a6f3c4,
-	mil_interface  = 0x35aa6b96,   //??? hat da vielleicht das ganze piggy eine eigene ID ???
-	pcie_Msi_Tgt   = 0x8a670e73,
-	irq_endpoint   = 0x10050082,
-	irq_control    = 0x10040084,
-	wb4_blockram   = 0x66cfeb52,
+        mil_interface  = 0x35aa6b96,   //??? hat da vielleicht das ganze piggy eine eigene ID ???
+        pcie_Msi_Tgt   = 0x8a670e73,
+        irq_endpoint   = 0x10050082,
+        irq_control    = 0x10040084,
+        wb4_blockram   = 0x66cfeb52,
         wb_ddr3ram     = 0x20150828,
+        wb_ddr3ram2    = 0x20160525,
         lm32_ram_user  = 0x54111351
       };
     } // namespace Etherbone
