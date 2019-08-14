3599aeffc1e54ababfa1452ebd2f1f79ae458f2a
A. Hahn <a.hahn@gsi.de>
2019-08-14 11:08:54 +0200

    lvds: added arria10 files to Manifest.py

--- modules/lvds/Manifest.py
+++ modules/lvds/Manifest.py
@@ -1,19 +1,21 @@
 def __helper():
   dirs = []
-  if syn_device[:1] == "5":    dirs.extend(["arria5"])
-  if syn_device[:4] == "ep2a": dirs.extend(["arria2"])
+  if syn_device[:1] == "5":       dirs.extend(["arria5"])
+  if syn_device[:4] == "ep2a":    dirs.extend(["arria2"])
+  if syn_device[:7] == "10ax027": dirs.extend(["arria10_scu4"])
   return dirs
-  
+
 modules = {"local": __helper() }
 
-files = [ 
+files = [
   "altera_lvds.vhd",
-  "altera_lvds_pkg.vhd", 
+  "altera_lvds_pkg.vhd",
   "arria2_lvds_pkg.vhd",
   "arria5_lvds_pkg.vhd",
   "altera_lvds_ibuf.vhd",
   "altera_lvds_obuf.vhd",
   "altera_lvds_rx.vhd",
   "altera_lvds_tx.vhd",
+  "arria10_lvds_pkg.vhd",
   "eca_lvds_channel.vhd"
   ]
