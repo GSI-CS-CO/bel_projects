bd7e234adb3e982ef4b38f39c5e8d24cb83f6f53
Dietrich Beck <d.beck@gsi.de>
2019-09-27 07:41:46 +0200

    eb-mon: extend stats for wr, add stats for cpu stalls, add stats for eca tap

--- tools/monitoring/eb-mon.c
+++ tools/monitoring/eb-mon.c
@@ -3,7 +3,7 @@
 //
 //  created : 2015
 //  author  : Dietrich Beck, GSI-Darmstadt
-//  version : 22-Oct-2018
+//  version : 27-Sep-2019
 //
 // Command-line interface for WR monitoring via Etherbone.
 //
@@ -34,7 +34,7 @@
 // For all questions and ideas contact: d.beck@gsi.de
 // Last update: 25-April-2015
 //////////////////////////////////////////////////////////////////////////////////////////////
-#define EBMON_VERSION "1.7.1"
+#define EBMON_VERSION "2.0.0"
 
 // standard includes
 #include <unistd.h> // getopt
@@ -73,19 +73,29 @@ static void help(void) {
   fprintf(stderr, "  -d               display WR time\n");
   fprintf(stderr, "  -e               display etherbone version\n");
   fprintf(stderr, "  -f<familyCode>   specify family code of 1-wire slave (0x43: EEPROM; 0x28,0x42: temperature)\n");
-  fprintf(stderr, "  -g               display WR lock statistics\n");
+  fprintf(stderr, "  -g               display WR statistics (lock, time continuity)\n");
   fprintf(stderr, "  -h               display this help and exit\n");
   fprintf(stderr, "  -i               display WR IP\n");
+  fprintf(stderr, "  -j<cpu>          display lm32 stall info; # of CPU starts with '0'\n");
+  fprintf(stderr, "  -k               display 'ECA-Tap' statistics\n");
   fprintf(stderr, "  -l               display WR link status\n");
   fprintf(stderr, "  -m               display WR MAC\n");
   fprintf(stderr, "  -o               display offset between WR time and system time [ms]\n");
-  fprintf(stderr, "  -s               display WR sync status\n");
+  fprintf(stderr, "  -s<number>       snoop  for information continuously\n");
+  fprintf(stderr, "                      0: print all messages (default)\n");
+  fprintf(stderr, "                      1..N print after N number of seconds\n");
   fprintf(stderr, "  -t<busIndex>     display temperature of sensor on the specified 1-wire bus\n");
   fprintf(stderr, "  -u<index>        user 1-wire: specify WB device in case multiple WB devices of the same type exist (default: u0)\n");
   fprintf(stderr, "  -v               display verbose information\n");
   fprintf(stderr, "  -w<index>        WR 1-wire: specify WB device in case multiple WB devices of the same type exist (default: u0)\n");
+  fprintf(stderr, "  -y               display WR sync status\n");
   fprintf(stderr, "  -z               display FPGA uptime [h]\n");
   fprintf(stderr, "\n");
+  fprintf(stderr, "  wrstatclear  <tWrObs> <tStallObs>  command clears statics and sets observation times for 'WR statistics' and 'CPU stall' [ns]\n");
+  fprintf(stderr, "  ecatapclear                        command clears statics on ECA Tap module\n");
+  fprintf(stderr, "  ecatapenable                       command enables capture on ECA Tap module\n");
+  fprintf(stderr, "  ecatapdisable                      command enables capture on ECA Tap module\n");
+  fprintf(stderr, "\n");  
   fprintf(stderr, "Use this tool to get some info about WR enabled hardware.\n");
   fprintf(stderr, "Example1: '%s -v dev/wbm0' display typical information.\n", program);
   fprintf(stderr, "Example2: '%s -b0 -f0x43 dev/wbm0' read ID of EEPROM connected to 1st (user) 1-wire bus\n", program);
@@ -110,6 +120,7 @@ int main(int argc, char** argv) {
 
   const char* devName;
   const char* devNameOther=NULL;
+  const char* command;      
 
   int         getEBVersion=0;
   int         getWRDate=0;
@@ -118,16 +129,19 @@ int main(int argc, char** argv) {
   int         getWRMac=0;
   int         getWRLink=0;
   int         getWRIP=0;
-  int         getWRLockStats=0;
+  int         getWRStats=0;
   int         getBoardID=0;
   int         getBoardTemp=0;
   int         getWRDateOther=0;
   int         getWRUptime=0;
   int         getBuildType=0;
+  int         getCPUStall=0;
+  int         getECATap=0;
   int         exitCode=0;
 
-  unsigned int family = 0;       // 1-Wire: familyCode
+  unsigned int family    = 0;    // 1-Wire: familyCode
   unsigned int user1Wire = 1;    // 1-Wire: 1 - User-1Wire; 0 - WR-Periph-1Wire
+  unsigned int ecpu      = 0;    // # of embedded cpu (for 'stall' statistics) 
 
   uint64_t    nsecs64, nsecsOther64;
   uint64_t    nsecsSum64, nsecsSumOther64;
@@ -137,9 +151,23 @@ int main(int argc, char** argv) {
   uint64_t    hostmsecs64;
   int64_t     offset;
   uint64_t    mac;
-  uint64_t    nsecsLockLoss;
-  uint64_t    nsecsLockAcq;
-  uint32_t    nLockAcq;
+  uint64_t    lockLossTS;
+  uint64_t    lockAcqTS;
+  uint32_t    lockNAcq;
+  uint64_t    contObsT;
+  int64_t     contMaxPosDT;
+  uint64_t    contMaxPosTS;
+  int64_t     contMaxNegDT;
+  uint64_t    contMaxNegTS;
+  uint64_t    stallObsT;
+  uint32_t    stallMaxStreak;
+  uint32_t    stallN;
+  uint64_t    stallTS;
+  uint64_t    ecaNMessage;
+  int64_t     ecaDtSum;
+  int64_t     ecaDtMin;
+  int64_t     ecaDtMax;
+  
   int         link;
   uint32_t    uptime;
   int         syncState;
@@ -159,107 +187,122 @@ int main(int argc, char** argv) {
 
   program = argv[0];
 
-  while ((opt = getopt(argc, argv, "t:u:w:f:b:c:adgosmlievhz")) != -1) {
+  while ((opt = getopt(argc, argv, "t:u:w:f:b:c:j:adgoymlievhzk")) != -1) {
     switch (opt) {
-    case 'a':
-      getBuildType=1;
-      break;
-    case 'b':
-      getBoardID=1;
-      busIndex = strtol(optarg, &tail, 0);
-      if (*tail != 0) {
-        fprintf(stderr, "Specify a proper number, not '%s'!\n", optarg);
-        exit(1);
-      } // if *tail
-      break;
-    case 'c':
-      getWRDateOther=1;
-      devNameOther = optarg;
-      break;
-    case 'd':
-      getWRDate=1;
-      break;
-    case 'f':
-      family = strtol(optarg, &tail, 0);
-      if (*tail != 0) {
-        fprintf(stderr, "Specify a proper number, not '%s'!\n", optarg);
-        exit(1);
-      } // if *tail
-      break;
-    case 'g':
-      getWRLockStats=1;
-      break;
-    case 'o':
-      getWROffset=1;
-      break;
-    case 'm':
-      getWRMac=1;
-      break;
-    case 'l':
-      getWRLink=1;
-      break;
-    case 'i':
-      getWRIP=1;
-      break;
-    case 's':
-      getWRSync=1;
-      break;
-    case 'z':
-      getWRUptime=1;
-      break;
-    case 't':
-      getBoardTemp=1;
-      busIndex = strtol(optarg, &tail, 0);
-      if (*tail != 0) {
-        fprintf(stderr, "Specify a proper number, not '%s'!\n", optarg);
-        exit(1);
-      } // if *tail
-      break;
-    case 'e':
-      getEBVersion=1;
-      break;
-    case 'u':
-      user1Wire = 1;
-      devIndex  = strtol(optarg, &tail, 0);
-      if (*tail != 0) {
-        fprintf(stderr, "Specify a proper number, not '%s'!\n", optarg);
-        exit(1);
-      } // if *tail
-      break;
-    case 'v':
-      getWRDate=1;
-      getWROffset=1;
-      getWRLockStats=1;
-      getWRSync=1;
-      getWRMac=1;
-      getWRLink=1;
-      getWRIP=1;
-      getWRUptime=1;
-      getEBVersion=1;
-      getBuildType=1;
-      verbose=1;
-      break;
-    case 'w':
-      user1Wire = 0;
-      devIndex  = strtol(optarg, &tail, 0);
-      if (*tail != 0) {
-        fprintf(stderr, "Specify a proper number, not '%s'!\n", optarg);
-        exit(1);
-      } // if *tail
-      break;
-    case 'h':
-      help();
-      return 0;
+      case 'a':
+        getBuildType=1;
+        break;
+      case 'b':
+        getBoardID=1;
+        busIndex = strtol(optarg, &tail, 0);
+        if (*tail != 0) {
+          fprintf(stderr, "Specify a proper number, not '%s'!\n", optarg);
+          exit(1);
+        } // if *tail
+        break;
+      case 'c':
+        getWRDateOther=1;
+        devNameOther = optarg;
+        break;
+      case 'd':
+        getWRDate=1;
+        break;
+      case 'f':
+        family = strtol(optarg, &tail, 0);
+        if (*tail != 0) {
+          fprintf(stderr, "Specify a proper number, not '%s'!\n", optarg);
+          exit(1);
+        } // if *tail
+        break;
+      case 'g':
+        getWRStats=1;
+        break;
+      case 'j':
+        getCPUStall=1;
+        ecpu   = strtol(optarg, &tail, 0);
+        if (ecpu > 16) {
+          fprintf(stderr, "# of cpu '%d' unreasonable large!\n", ecpu);
+          exit(1);
+        } // if ecpu
+        if (*tail != 0) {
+          fprintf(stderr, "Specify a proper number, not '%s'!\n", optarg);
+          exit(1);
+        } // if *tail
+        break;
+      case 'k':
+        getECATap=1;
+        break;
+      case 'o':
+        getWROffset=1;
+        break;
+      case 'm':
+        getWRMac=1;
+        break;
+      case 'l':
+        getWRLink=1;
+        break;
+      case 'i':
+        getWRIP=1;
+        break;
+      case 'y':
+        getWRSync=1;
+        break;
+      case 'z':
+        getWRUptime=1;
+        break;
+      case 't':
+        getBoardTemp=1;
+        busIndex = strtol(optarg, &tail, 0);
+        if (*tail != 0) {
+          fprintf(stderr, "Specify a proper number, not '%s'!\n", optarg);
+          exit(1);
+        } // if *tail
+        break;
+      case 'e':
+        getEBVersion=1;
+        break;
+      case 'u':
+        user1Wire = 1;
+        devIndex  = strtol(optarg, &tail, 0);
+        if (*tail != 0) {
+          fprintf(stderr, "Specify a proper number, not '%s'!\n", optarg);
+          exit(1);
+        } // if *tail
+        break;
+      case 'v':
+        getWRDate=1;
+        getWROffset=1;
+        getWRStats=1;
+        getWRSync=1;
+        getWRMac=1;
+        getWRLink=1;
+        getWRIP=1;
+        getWRUptime=1;
+        getEBVersion=1;
+        getBuildType=1;
+        verbose=1;
+        break;
+      case 'w':
+        user1Wire = 0;
+        devIndex  = strtol(optarg, &tail, 0);
+        if (*tail != 0) {
+          fprintf(stderr, "Specify a proper number, not '%s'!\n", optarg);
+          exit(1);
+        } // if *tail
+        break;
+      case 'h':
+        help();
+        return 0;
       case ':':
       case '?':
         error = 1;
-      break;
-    default:
-      fprintf(stderr, "%s: bad getopt result\n", program);
-      return 1;
+        break;
+      default:
+        fprintf(stderr, "%s: bad getopt result\n", program);
+        return 1;
     } // switch opt
   } // while opt
-
+  
   if (error) {
     help();
     return 1;
@@ -274,6 +317,10 @@ int main(int argc, char** argv) {
 
   devName = argv[optind];
   if (devIndex < 0) devIndex = 0; // default: grab first device of the requested type on the wishbone bus
+
+  if (optind+1 < argc)  command = argv[++optind];
+  else command = NULL;
+  
   
   if (getEBVersion) {
     if (verbose) fprintf(stdout, "EB version / EB source: ");
@@ -371,7 +418,7 @@ int main(int argc, char** argv) {
       fprintf(stdout, "%s (%3lu ms)", timestr, msecs64 - secs * 1000);
       fprintf(stdout, ", %"PRIu64" us\n", nsecs64 / 1000);
     }
-  }
+  } // if getWRDate
 
   if (getWRSync) {
     if ((status = wb_wr_get_sync_state(device, devIndex, &syncState)) != EB_OK) die("WR get sync state", status);
@@ -385,31 +432,7 @@ int main(int argc, char** argv) {
       sprintf(syncStr, "NO SYNC");
     if (verbose) fprintf(stdout, "Sync Status: ");
     fprintf(stdout, "%s\n", syncStr);
-  }
-
-  if (getWRLockStats) {
-    if ((status = wb_wr_get_lock_stats(device, devIndex, &nsecsLockLoss, &nsecsLockAcq, &nLockAcq)) != EB_OK) die("WR get lock statistics", status);
-
-    fprintf(stdout, "# of acquired locks: %d\n", nLockAcq);
-
-    if (nsecsLockLoss == 0xffffffffffffffff)
-      fprintf(stdout, "WR lock lost       : N/A\n");
-    else {
-      secs = (unsigned long)((double)nsecsLockLoss / 1000000000.0);
-      tm = gmtime(&secs);
-      strftime(timestr, sizeof(timestr), "%Y-%m-%d %H:%M:%S %Z", tm);
-      fprintf(stdout, "WR lock lost       : %s\n", timestr);
-    }
-
-    if (nsecsLockAcq == 0xffffffffffffffff)
-      fprintf(stdout, "WR lock acquired   : N/A\n");
-    else {
-      secs = (unsigned long)((double)nsecsLockAcq / 1000000000.0);
-      tm = gmtime(&secs);
-      strftime(timestr, sizeof(timestr), "%Y-%m-%d %H:%M:%S %Z", tm);
-      fprintf(stdout, "WR lock aquired    : %s\n", timestr);
-    }
-  }
+  } // getWRSync
 
   if (getWRMac) {
     if ((status = wb_wr_get_mac(device, devIndex, &mac)) != EB_OK) die("WR get MAC", status);
@@ -452,13 +475,108 @@ int main(int argc, char** argv) {
     fprintf(stdout, "0x%016"PRIx64"\n", id);
   }
 
- if (getBoardTemp) {
-   if (!family) die("family code not specified (1-wire)", EB_OOM);
-   if ((status = wb_1wire_get_temp(device, devIndex, busIndex, family, user1Wire, &temp)) != EB_OK) die("WR get board temperature", status);
-   if (verbose) fprintf(stdout, "temp: ");
-   fprintf(stdout, "%.4f\n", (float)temp);
- } 
+  if (getBoardTemp) {
+    if (!family) die("family code not specified (1-wire)", EB_OOM);
+    if ((status = wb_1wire_get_temp(device, devIndex, busIndex, family, user1Wire, &temp)) != EB_OK) die("WR get board temperature", status);
+    if (verbose) fprintf(stdout, "temp: ");
+    fprintf(stdout, "%.4f\n", (float)temp);
+  } 
 
+  if (getWRStats || getCPUStall || getECATap) {
+    fprintf(stdout, "\n");
+    fprintf(stdout, "=============\n");
+    fprintf(stdout, "Statistics\n");
+    fprintf(stdout, "=============\n");
+  } // if getWRStats || ...
+  
+  if (getWRStats) {
+    fprintf(stdout, "= White Rabbit\n");
+    
+    // WR stats: lock
+    fprintf(stdout, "-- locks...\n");
+    if ((status = wb_wr_stats_get_lock(device, devIndex, &lockLossTS, &lockAcqTS, &lockNAcq)) != EB_OK) die("WR get lock statistics", status);
+    fprintf(stdout, "---# of acquired locks: %d\n", lockNAcq);
+    
+    if (lockLossTS == 0xffffffffffffffff)
+      fprintf(stdout, "---WR lock lost       : N/A\n");
+    else {
+      secs = (unsigned long)((double)lockLossTS / 1000000000.0);
+      tm = gmtime(&secs);
+      strftime(timestr, sizeof(timestr), "%Y-%m-%d %H:%M:%S %Z", tm);
+      fprintf(stdout, "---WR lock lost       : %s\n", timestr);
+    }
+    
+    if (lockAcqTS == 0xffffffffffffffff)
+      fprintf(stdout, "---WR lock acquired   : N/A\n");
+    else {
+      secs = (unsigned long)((double)lockAcqTS / 1000000000.0);
+      tm = gmtime(&secs);
+      strftime(timestr, sizeof(timestr), "%Y-%m-%d %H:%M:%S %Z", tm);
+      fprintf(stdout, "---WR lock aquired    : %s\n", timestr);
+    }
+    
+    // WR stats: time continuity
+    fprintf(stdout, "-- time continuity [ns]\n");
+    if ((status = wb_wr_stats_get_continuity(device, devIndex, &contObsT, &contMaxPosDT, &contMaxPosTS, &contMaxNegDT, &contMaxNegTS)) != EB_OK) die("WR get lock statistics", status);
+    fprintf(stdout, "--- observation period: %"PRIu64"\n", contObsT);
+    fprintf(stdout, "---- max pos diff     : %"PRId64"\n", contMaxPosDT);
+    secs = (unsigned long)((double)contMaxPosTS / 1000000000.0);
+    tm = gmtime(&secs);
+    strftime(timestr, sizeof(timestr), "%Y-%m-%d %H:%M:%S %Z", tm);
+    fprintf(stdout, "---- TS of pos  diff  : %s\n", timestr);
+    fprintf(stdout, "---- max neg diff     : %"PRId64"\n", contMaxNegDT);
+    secs = (unsigned long)((double)contMaxNegTS / 1000000000.0);
+    tm = gmtime(&secs);
+    strftime(timestr, sizeof(timestr), "%Y-%m-%d %H:%M:%S %Z", tm);
+    fprintf(stdout, "---- TS of neg diff   : %s\n", timestr);
+  } // if getWRStats
+  
+  if (getCPUStall) {
+    fprintf(stdout, "= lm32 stalls for CPU %d [ns]\n", ecpu);
+    if ((status = wb_wr_stats_get_stall(device, devIndex, ecpu, &stallObsT, &stallMaxStreak, &stallN, &stallTS)) != EB_OK) die("WR get CPU stall statistics", status);
+    fprintf(stdout, "--- observation period: %"PRIu64"\n", stallObsT);
+    fprintf(stdout, "---- stall time max   : %u\n", stallMaxStreak);
+    fprintf(stdout, "---- stall time act   : %u\n", stallN);
+    secs = (unsigned long)((double)stallTS / 1000000000.0);
+    tm = gmtime(&secs);
+    strftime(timestr, sizeof(timestr), "%Y-%m-%d %H:%M:%S %Z", tm);
+    fprintf(stdout, "---- TS of neg diff   : %s\n", timestr); 
+  } // if getCPUStall
+
+  if (getECATap) {
+    int32_t aheadt = 1000000;
+    fprintf(stdout, "= ECA-Tap (input) [ns]\n");
+    if ((status = wb_eca_stats_get(device, devIndex, &ecaNMessage, &ecaDtSum, &ecaDtMin, &ecaDtMax)) != EB_OK) fprintf(stdout, "warning: can't ECA-Tap statistics (can't find in gateware)\n");
+    fprintf(stdout, "--- # of messages     : %"PRIu64"\n", ecaNMessage);
+    fprintf(stdout, "--- ave (dl - ts)     : %d\n", (int32_t)((double)ecaDtSum/(double)ecaNMessage));
+    fprintf(stdout, "--- min (dl - ts)     : %"PRId64"\n", ecaDtMin);
+    fprintf(stdout, "--- max (dl - ts)     : %"PRId64"\n", ecaDtMax);
+    fprintf(stdout, "--- calc latency      : %d\n", aheadt - (int32_t)((double)ecaDtSum/(double)ecaNMessage));
+  } // getECATap
+  
+  if (command) {
+    if (!strcasecmp(command, "wrstatclear")) {
+      if (optind+3  != argc) {printf("expecting exactly two arguments: wrstatclear  <tWrObs> <tStallObs>\n"); return 1;}
+      contObsT = strtoul(argv[optind+1], &tail, 0);
+      stallObsT = strtoul(argv[optind+2], &tail, 0);
+      wb_wr_stats_reset(device, devIndex, contObsT, stallObsT);
+    } // wrstatclear
+
+    if (!strcasecmp(command, "ecatapclear")) {
+      wb_eca_stats_reset(device, devIndex);
+    } // ecatapclear
+
+    if (!strcasecmp(command, "ecatapenable")) {
+      wb_eca_stats_enable(device, devIndex, 0x1);
+    } // ecatapenable
+
+    if (!strcasecmp(command, "ecatapdisable")) {
+      wb_eca_stats_enable(device, devIndex, 0x0);
+    } // ecatapdisable
+
+    
+  } // if command
+  
   wb_close(device, socket);
   wb_close(deviceOther, socket);
   
