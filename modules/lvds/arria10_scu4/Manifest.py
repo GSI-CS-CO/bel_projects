cba8319c11956e83d75a7cbf851cdbe838705bcc
A. Hahn <a.hahn@gsi.de>
2019-10-10 10:16:25 +0200

    manifest.py/lvds: added a10/scu4 components

--- modules/lvds/arria10_scu4/Manifest.py
+++ modules/lvds/arria10_scu4/Manifest.py
@@ -3,4 +3,5 @@ files = [
   "arria10_scu4_lvds_obuf/arria10_scu4_lvds_obuf_inst.vhd",
   "arria10_scu4_lvds_rx/arria10_scu4_lvds_rx_inst.vhd",
   "arria10_scu4_lvds_tx/arria10_scu4_lvds_tx_inst.vhd",
+  "arria10_scu4_lvds_pll/arria10_scu4_lvds_pll_inst.vhd",
   ]
