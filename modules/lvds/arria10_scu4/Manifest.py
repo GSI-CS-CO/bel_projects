94fe9203862aee145f226021399f54a76ee857fb
A. Hahn <a.hahn@gsi.de>
2019-10-11 11:15:24 +0200

    scu4: updated lvds makefile

--- modules/lvds/arria10_scu4/Manifest.py
+++ modules/lvds/arria10_scu4/Manifest.py
@@ -1,7 +1,7 @@
 files = [
-  "arria10_scu4_lvds_ibuf/arria10_scu4_lvds_ibuf_inst.vhd",
-  "arria10_scu4_lvds_obuf/arria10_scu4_lvds_obuf_inst.vhd",
-  "arria10_scu4_lvds_rx/arria10_scu4_lvds_rx_inst.vhd",
-  "arria10_scu4_lvds_tx/arria10_scu4_lvds_tx_inst.vhd",
-  "arria10_scu4_lvds_pll/arria10_scu4_lvds_pll_inst.vhd",
+  "arria10_scu4_lvds_ibuf/arria10_scu4_lvds_ibuf/arria10_scu4_lvds_ibuf_inst.vhd",
+  "arria10_scu4_lvds_obuf/arria10_scu4_lvds_obuf/arria10_scu4_lvds_obuf_inst.vhd",
+  "arria10_scu4_lvds_rx/arria10_scu4_lvds_rx/arria10_scu4_lvds_rx_inst.vhd",
+  "arria10_scu4_lvds_tx/arria10_scu4_lvds_tx/arria10_scu4_lvds_tx_inst.vhd",
+  "arria10_scu4_lvds_pll/arria10_scu4_lvds_pll/arria10_scu4_lvds_pll_inst.vhd",
   ]
