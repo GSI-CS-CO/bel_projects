6049353badb2bc0ac8762d9ee9dbf21a46cef92a
A. Hahn <a.hahn@gsi.de>
2019-11-13 16:44:05 +0100

    beam_dump: initial

--- modules/monster/monster_pkg.vhd
+++ modules/monster/monster_pkg.vhd
@@ -106,6 +106,7 @@ package monster_pkg is
       g_en_nau8811           : boolean := false;
       g_en_user_ow           : boolean := false;
       g_en_psram             : boolean := false;
+      g_en_beam_dump         : boolean := false;
       g_io_table             : t_io_mapping_table_arg_array(natural range <>);
       g_en_pmc               : boolean := false;
       g_lm32_cores           : natural := 1;
