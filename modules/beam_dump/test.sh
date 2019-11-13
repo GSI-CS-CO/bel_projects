867db1c54abcf9f26616dbd30ef8ba7e9fe78e90
Restyled.io <commits@restyled.io>
2019-11-13 15:52:45 +0000

    Restyled by shellharden

--- modules/beam_dump/test.sh
+++ modules/beam_dump/test.sh
@@ -5,14 +5,14 @@ device_id="0x11223344"
 base_addr="0x7fff0000"
 
 # check if device is available
-test -e /$dev
+test -e /"$dev"
 if [ $? -ne 0 ]; then
   echo "Error: Device $dev does not exsist!"
   exit 1
 fi
 
 # check address
-addr_check=$(eb-find $dev $vendor_id $device_id)
+addr_check=$(eb-find "$dev" "$vendor_id" "$device_id")
 if [[ "$addr_check" != "$base_addr" ]]; then
   echo "Error: Address does not match!"
   echo "Expected: $base_addr"
@@ -26,18 +26,18 @@ echo "=================================================="
 counter=1
 hex_offset=0
 hex_addr=0
-while [ $counter -le 8 ]; do
+while [ "$counter" -le 8 ]; do
   echo "Iteration: $counter -> 0x$hex_offset (offset) -> 0x$hex_addr (destination)"
   dword_set="0xffffffff"
   dword_get="0x00000000"
   hex_addr=$(echo $(($base_addr + $hex_offset)))
-  hex_addr=$(echo 16o${hex_addr}p |dc)
+  hex_addr=$(echo 16o"${hex_addr}"p |dc)
 
   # get random hex value, cut off spaces and translate upper to lower chars
   dword_set=$(hexdump -n 4 -e '4/4 "%08X" 1 "\n"' /dev/urandom | awk '{$1=$1};1' | tr '[:upper:]' '[:lower:]')
-  eb-write $dev 0x$hex_addr/4 0x$dword_set
+  eb-write "$dev" 0x"$hex_addr"/4 0x"$dword_set"
   echo "Write: 0x$dword_set to   0x$hex_addr/4 "
-  dword_get=$(eb-read $dev 0x$hex_addr/4)
+  dword_get=$(eb-read "$dev" 0x"$hex_addr"/4)
   echo "Read:  0x$dword_get from 0x$hex_addr/4 "
 
   # compare write and read
@@ -58,8 +58,8 @@ echo ""
 echo "Testing streaming access mode..."
 echo "=================================================="
 dd if=/dev/urandom of=random_file_set bs=32 count=1
-eb-put $dev $base_addr random_file_set
-eb-get $dev $base_addr/32 random_file_get
+eb-put "$dev" "$base_addr" random_file_set
+eb-get "$dev" "$base_addr"/32 random_file_get
 diff random_file_set random_file_get
 if [ $? -ne 0 ]; then
   echo "Error: Streaming mode borken!"
