1786685300c3bffdd67ac4ee924369a9d5295a7c
A. Hahn <a.hahn@gsi.de>
2020-01-16 11:54:50 +0100

    prioq2/arbiter: removed int signal

--- modules/prioq2/arbiter.vhd
+++ modules/prioq2/arbiter.vhd
@@ -14,7 +14,7 @@ use work.matrix_pkg.all;
 entity arbiter is
 generic(
   g_depth         : natural := 16;
-  g_num_masters   : natural := 8 
+  g_num_masters   : natural := 8
 );
 port(
   clk_i         : in  std_logic;
@@ -29,18 +29,18 @@ port(
   cnt_ch_in_o   : out matrix(g_num_masters-1 downto 0, 31 downto 0);
   cnt_ch_out_o  : out matrix(g_num_masters-1 downto 0, 31 downto 0);
   cnt_all_out_o : out std_logic_vector(63 downto 0);
-  
+
 -- ts_out
   ts_o          : out std_logic_vector(63 downto 0);
   ts_valid_o    : out std_logic;
-  
+
 
 -- wb ifs
   slaves_i      : in t_wishbone_slave_in_array(g_num_masters-1 downto 0);
   slaves_o      : out t_wishbone_slave_out_array(g_num_masters-1 downto 0);
   master_o      : out t_wishbone_master_out;
   master_i      : in t_wishbone_master_in
-  
+
 );
 begin
 assert (g_num_masters >= 1 or g_num_masters <= 9) report "Timing Priority Queue must have 1-9 slave ports" severity failure;
@@ -49,12 +49,12 @@ end entity;
 
 architecture behavioral of arbiter is
 
-  -- we have N masters - 1 slave  
+  -- we have N masters - 1 slave
   constant c_num_slaves : natural := 1;
-  constant c_slave : natural := 0; 
+  constant c_slave : natural := 0;
 
 
- 
+
   subtype slave_row is std_logic_vector(g_num_masters-1 downto 0);
   type slave_matrix is array (natural range <>) of slave_row;
 
@@ -64,18 +64,18 @@ architecture behavioral of arbiter is
   signal master_ie      : t_wishbone_master_in_array(c_num_slaves-1 downto 0);
 
   signal s_master_o     : t_wishbone_master_out;
-  signal r_master_o_cyc : std_logic; 
+  signal r_master_o_cyc : std_logic;
   signal s_emptys       : std_logic_vector(8 downto 0);
-  signal s_valids,      
+  signal s_valids,
          r_valids       : std_logic_vector(8 downto 0);
-        
+
   signal s_ts_prio      : std_logic_vector(8 downto 0);
   signal s_ts_array     : slv64_array(8 downto 0);
   signal matrix_new,
          matrix_old,
          granted        : matrix(g_num_masters-1 downto 0, c_num_slaves-1 downto 0);
-  
-  
+
+
   signal s_full         : std_logic_vector(8 downto 0);
 
   --statistics counters
@@ -83,13 +83,13 @@ architecture behavioral of arbiter is
   signal r_aux_out      : slv32_array(g_num_masters-1 downto 0);
 
   signal r_count_all  : std_logic_vector(63 downto 0);
-  
+
   signal ts_debug0 : std_logic_vector(63 downto 0);
   signal ts_debug1 : std_logic_vector(63 downto 0);
   signal ts_debug2 : std_logic_vector(63 downto 0);
   signal ts_debug3 : std_logic_vector(63 downto 0);
   signal ts_debug4 : std_logic_vector(63 downto 0);
-  signal ts_debug5 : std_logic_vector(63 downto 0);  
+  signal ts_debug5 : std_logic_vector(63 downto 0);
   signal ts_debug6 : std_logic_vector(63 downto 0);
   signal ts_debug7 : std_logic_vector(63 downto 0);
   signal ts_debug8 : std_logic_vector(63 downto 0);
@@ -98,27 +98,27 @@ architecture behavioral of arbiter is
 
 
 ---------------------------------------------------------------------------
--- Based on Wesley Terpstra's fast Wishbone Crossbar Code, thanks 
+-- Based on Wesley Terpstra's fast Wishbone Crossbar Code, thanks
 ---------------------------------------------------------------------------
-  
+
   -- If any of the bits are '1', the whole thing is '1'
   -- This function makes the check explicitly have logarithmic depth.
   function vector_OR(x : std_logic_vector)
-    return std_logic 
+    return std_logic
   is
     constant len : integer := x'length;
     constant mid : integer := len / 2;
     alias y : std_logic_vector(len-1 downto 0) is x;
   begin
-    if len = 1 
+    if len = 1
     then return y(0);
     else return vector_OR(y(len-1 downto mid)) or
                 vector_OR(y(mid-1 downto 0));
     end if;
   end vector_OR;
-  
-  
-  
+
+
+
   function slave_matrix_OR(x : slave_matrix)
     return std_logic_vector is
     variable result : std_logic_vector(x'LENGTH-1 downto 0);
@@ -128,9 +128,9 @@ architecture behavioral of arbiter is
     end loop;
     return result;
   end slave_matrix_OR;
-  
-  
-  
+
+
+
   -- Select the master pins the slave will receive
   function slave_logic(slave   : integer;
                        adr     : t_wishbone_address;
@@ -160,7 +160,7 @@ architecture behavioral of arbiter is
         DAT_matrix(bit)(master) := slave_i(master).DAT(bit) and granted(master, slave);
       end loop;
     end loop;
-    
+
     return (
        CYC => vector_OR(CYC_row),
        STB => vector_OR(STB_row),
@@ -172,7 +172,7 @@ architecture behavioral of arbiter is
 
   subtype master_row is std_logic_vector(c_num_slaves-1 downto 0);
   type master_matrix is array (natural range <>) of master_row;
-  
+
   function master_matrix_OR(x : master_matrix)
     return std_logic_vector is
     variable result : std_logic_vector(x'LENGTH-1 downto 0);
@@ -182,7 +182,7 @@ architecture behavioral of arbiter is
     end loop;
     return result;
   end master_matrix_OR;
-  
+
   -- Select the slave pins the master will receive
   function master_logic(master    : integer;
                         granted   : matrix(g_num_masters-1 downto 0, c_num_slaves-1 downto 0);
@@ -205,16 +205,15 @@ architecture behavioral of arbiter is
         DAT_matrix(bit)(slave) := master_ie(slave).DAT(bit) and granted(master, slave);
       end loop;
     end loop;
-    
+
     return (
       ACK => vector_OR(ACK_row),
       ERR => vector_OR(ERR_row),
       RTY => vector_OR(RTY_row),
       STALL => not vector_OR(STALL_row),
-      DAT => master_matrix_OR(DAT_matrix),
-      INT => '0');
+      DAT => master_matrix_OR(DAT_matrix));
   end master_logic;
-  
+
 
   function matrix_logic(
     ts_priority : slave_row;
@@ -225,10 +224,10 @@ architecture behavioral of arbiter is
   is
     subtype row    is std_logic_vector(g_num_masters-1 downto 0);
     subtype column is std_logic_vector(c_num_slaves-1  downto 0);
-    
+
     variable tmp_column : column;
     variable tmp_row    : row;
-    
+
     variable selected   : matrix(g_num_masters-1 downto 0, c_num_slaves-1 downto 0);  -- Which master wins arbitration  log(M) request
     variable sbusy      : column;  -- Does the slave's  previous connection persist?
     variable mbusy      : row;     -- Does the master's previous connection persist?
@@ -239,7 +238,7 @@ architecture behavioral of arbiter is
         tmp_row(master) := matrix_old(master, c_slave) and slave_i(master).CYC;
       end loop;
       sbusy(c_slave) := vector_OR(tmp_row);
-    
+
     -- A master is busy iff it services an in-progress cycle
     for master in g_num_masters-1 downto 0 loop
       tmp_column(c_slave) := matrix_old(master, c_slave);
@@ -250,7 +249,7 @@ architecture behavioral of arbiter is
     for master in g_num_masters-1 downto 0 loop
       selected(master, c_slave) := slave_i(master).CYC and slave_i(master).STB and ts_priority(master) and en;
     end loop;
-     
+
     -- Determine the master granted access
     -- Policy: if cycle still in progress, preserve the previous choice
     for master in g_num_masters-1 downto 0 loop
@@ -261,7 +260,7 @@ architecture behavioral of arbiter is
       end if;
     end loop;
 
-    
+
     return matrix_new;
   end matrix_logic;
   --------------------------------------------------------
@@ -269,10 +268,10 @@ architecture behavioral of arbiter is
 begin
 
   ---------------------------------------------------------------------------
--- Generate some statistics 
----------------------------------------------------------------------------    
+-- Generate some statistics
+---------------------------------------------------------------------------
 
-Count : for i in g_num_masters-1 downto 0 generate   
+Count : for i in g_num_masters-1 downto 0 generate
 
   cnt_ch_out : process(clk_i)
     variable v_inc : unsigned(31 downto 0);
@@ -282,19 +281,19 @@ Count : for i in g_num_masters-1 downto 0 generate
         r_valids(i) <= '0';
         r_aux_out(i) <= (others => '0');
       else
-        r_valids(i) <= s_valids(i);        
+        r_valids(i) <= s_valids(i);
         v_inc := (others => '0');
         v_inc(0) := s_valids(i) and not r_valids(i);
-        r_aux_out(i) <= std_logic_vector(unsigned(r_aux_out(i)) + v_inc); 
+        r_aux_out(i) <= std_logic_vector(unsigned(r_aux_out(i)) + v_inc);
       end if;
     end if;
   end process cnt_ch_out;
 
-  -- fucking awkward, but best I can do. An assignment procedure would create drivers for all bits at once - can't have that 
-  bitsO : for j in 31 downto 0 generate 
+  -- fucking awkward, but best I can do. An assignment procedure would create drivers for all bits at once - can't have that
+  bitsO : for j in 31 downto 0 generate
     cnt_ch_out_o(i, j)   <= r_aux_out(i)(j);
   end generate;
-  
+
   cnt_ch_in : process(clk_i)
     variable v_inc : unsigned(34 downto 0);
   begin
@@ -302,20 +301,20 @@ Count : for i in g_num_masters-1 downto 0 generate
       v_inc := (others => '0');
       v_inc(0) := slaves_i(i).cyc and slaves_i(i).stb and not s_slaves_o(i).stall;
 
-      
+
       if rst_n_i = '0' or cnt_clear_i = '1' then
         r_aux_in(i) <= (others => '0');
       else
-        r_aux_in(i) <= std_logic_vector(unsigned(r_aux_in(i)) + v_inc); 
+        r_aux_in(i) <= std_logic_vector(unsigned(r_aux_in(i)) + v_inc);
       end if;
-      
+
     end if;
   end process cnt_ch_in;
 
 
   -- again: fuckin awkward and ugly...
   bitsI : for j in 31+3 downto 3 generate
-          -- each telegram has 8 words. Divide by 8 - right shift by 3b 
+          -- each telegram has 8 words. Divide by 8 - right shift by 3b
     cnt_ch_in_o(i, j-3)   <= r_aux_in(i)(j);
   end generate;
 
@@ -360,8 +359,8 @@ end generate;
   s_emptys <= not s_valids;
 
   --the input queues
-  queues : for master in 0 to g_num_masters-1 generate 
-    qu : queue_unit 
+  queues : for master in 0 to g_num_masters-1 generate
+    qu : queue_unit
     generic map(
       g_depth => g_depth,
       g_words => 8
@@ -398,20 +397,20 @@ end generate;
       end if;
     end if;
   end process main;
-  
+
   -- Make the "crossbar" registered
   granted <= matrix_old;
-  
+
   master_o <= s_master_o;
-  --- Master Muxing ---  
+  --- Master Muxing ---
   -- n - 1 master out
   s_master_o         <= slave_logic(c_slave, dst_adr_i, granted, s_masters_o);
   -- 1 - n master in
   master_ie(c_slave) <= master_i;
   master_ins : for master in g_num_masters-1 downto 0 generate
     s_masters_i(master) <= master_logic(master, granted, master_ie);
-  end generate;  
- 
+  end generate;
+
   ts_debug0 <= s_ts_array(0);
   ts_debug1 <= s_ts_array(1);
   ts_debug2 <= s_ts_array(2);
