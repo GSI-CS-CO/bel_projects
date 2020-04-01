2867881c1280ffc9f2a78b9344bde8f689c1ecbc
Ulrich Becker <>
2020-04-01 17:37:14 +0200

    Current state of develop

--- modules/FreeRTOS/Demo/SCU_LM32_GCC/test1/lm32RtosTest1Config.h
+++ modules/FreeRTOS/Demo/SCU_LM32_GCC/test1/lm32RtosTest1Config.h
@@ -1,32 +1,12 @@
 /*!
- * @file      FreeRTOSConfig.h
- * @brief  Project specific definitions for using FreeRTOS in SCU
- *
+ * @file lm32RtosTest1Config.h
+ * @brief FreeRTOS configuration file for test program lm32RtosTest1
  * @copyright GSI Helmholtz Centre for Heavy Ion Research GmbH
  * @author    Ulrich Becker <u.becker@gsi.de>
- * @date      14.01.2020
- ******************************************************************************
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 3 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library. If not, see <http://www.gnu.org/licenses/>.
- ******************************************************************************
+ * @date      01.04.2020
  */
-#ifndef FREERTOS_CONFIG_H
-#define FREERTOS_CONFIG_H
-
-/*
- * Here is a good place to include header files that are required across
- * your application.
- */
+#ifndef _LM32RTOSTEST1CONFIG_H
+#define _LM32RTOSTEST1CONFIG_H
 
 #include <stdbool.h>
 
@@ -131,7 +111,5 @@
 #define INCLUDE_xTaskGetHandle                  0
 #define INCLUDE_xTaskResumeFromISR              1
 
-/* A header file that defines trace macro can be included here. */
-
-#endif /* FREERTOS_CONFIG_H */
+#endif /* ifndef _LM32RTOSTEST1CONFIG_H */
 /*================================== EOF ====================================*/
