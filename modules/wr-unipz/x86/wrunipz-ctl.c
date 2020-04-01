f3bcbcc59c29fcee790649aef8d9d880e3d6bc64
Dietrich Beck <d.beck@gsi.de>
2020-04-01 13:31:43 +0200

    wr-unipz: minor fix

--- modules/wr-unipz/x86/wrunipz-ctl.c
+++ modules/wr-unipz/x86/wrunipz-ctl.c
@@ -513,8 +513,9 @@ int main(int argc, char** argv) {
   */
   if (getVersion) {
     wrunipz_version_library(&verLib);
+    printf("wr-unipz: library (firmware) version %s",  wrunipz_version_text(verLib));     
     wrunipz_version_firmware(ebDevice, &verFw);
-    printf("wr-unipz: library (firmware) version %s (%s)\n",  wrunipz_version_text(verLib), wrunipz_version_text(verFw));     
+    printf("(%s)\n",  wrunipz_version_text(verFw));     
   } // if getEBVersion
 
   if (getInfo) {
