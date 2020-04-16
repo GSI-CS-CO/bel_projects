dacfd202a6dba4420ab8bb0053714c204ca87756
Michael Reese <michaelate@gmail.com>
2020-04-16 12:09:17 +0200

    virtual-tr: compiles with no unbounded warnings. But it does not work yet
    /opt/altera/18.1/quartus/eda/sim_lib/altera_mf.vhd:41182:13:@0ms:(assertion warning): Warning: DONT_CARE value for read_during_write_mode_port_a is not supported in Stratix device family, it might cause incorrect behavioural simulation result
    /opt/altera/18.1/quartus/eda/sim_lib/altera_mf.vhd:41190:13:@0ms:(assertion warning): Warning: DONT_CARE value for read_during_write_mode_port_b is not supported in Stratix device family, it might cause incorrect behavioural simulation result
    /opt/altera/18.1/quartus/eda/sim_lib/altera_mf.vhd:41198:13:@0ms:(assertion error): Error: DONT_CARE value for read_during_write_mode_port_a is not supported in Stratix device family for AUTO ram block type in BIDIR_DUAL_PORT operation mode
    ./testbench:error: bound check failure at /opt/altera/18.1/quartus/eda/sim_lib/altera_mf.vhd:40537

---
+++ testbench/common/xwb_lm32.vhd
@@ -0,0 +1,49 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;  
+
+use work.wishbone_pkg.all;
+use work.interface_lm32.all;
+
+entity xwb_lm32 is
+generic(
+	g_profile      : string := "dontcare";
+	g_reset_vector : std_logic_vector(31 downto 0) := x"00000000";
+	g_sdb_address  : std_logic_vector(31 downto 0) := x"00000000"
+);
+port(
+	clk_sys_i : in  std_logic;
+	rst_n_i   : in  std_logic;
+	irq_i     : in  std_logic_vector(31 downto 0);
+	dwb_o     : out t_wishbone_master_out;
+	dwb_i     : in  t_wishbone_master_in;
+	iwb_o     : out t_wishbone_master_out;
+	iwb_i     : in  t_wishbone_master_in
+);
+end entity;
+
+architecture verilator_interface of xwb_lm32 is
+  signal lm32_idx         : integer   := interface_lm32_init;
+  signal rst              : std_logic := '0';
+begin
+  
+  rst <= not rst_n_i;
+
+  main: process
+  begin
+    wait until rising_edge(clk_sys_i);
+    interface_lm32_reset(lm32_idx, rst);
+    interface_lm32_clock(lm32_idx, clk_sys_i);
+
+    -- get the output from verilator simulation
+    iwb_o <= interface_lm32_i_wb_mosi(lm32_idx); -- instruction bus
+    dwb_o <= interface_lm32_d_wb_mosi(lm32_idx); -- data bus
+
+    wait until falling_edge(clk_sys_i);
+    interface_lm32_i_wb_miso(lm32_idx, iwb_i); -- instruction bus
+    interface_lm32_d_wb_miso(lm32_idx, dwb_i); -- data bus
+
+    interface_lm32_clock(lm32_idx, clk_sys_i);
+  end process;
+	
+end architecture;
