97b7e96138147d56744c695258b6cd75328c1cb9
Ulrich Becker <>
2020-04-22 09:14:19 +0200

    No RTOS timer

--- modules/FreeRTOS/Demo/SCU_LM32_GCC/FreeRTOS+DDR3/rtosDdr3TestConfig.h
+++ modules/FreeRTOS/Demo/SCU_LM32_GCC/FreeRTOS+DDR3/rtosDdr3TestConfig.h
@@ -61,7 +61,7 @@ void ddr3Unlock( void );
 #define configMAX_CO_ROUTINE_PRIORITIES         1
 
 /* Software timer related definitions. */
-#define configUSE_TIMERS                        1
+#define configUSE_TIMERS                        0
 #define configTIMER_TASK_PRIORITY               3
 #define configTIMER_QUEUE_LENGTH                10
 #define configTIMER_TASK_STACK_DEPTH            configMINIMAL_STACK_SIZE
