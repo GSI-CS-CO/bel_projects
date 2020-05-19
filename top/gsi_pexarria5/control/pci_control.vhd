e39fc20104cc0bc655990aa17c6182e7d2991772
alex-cscotg <alex-cscotg@users.noreply.github.com>
2020-05-19 10:57:45 +0200

    pexarria5: added lm32 timer

--- top/gsi_pexarria5/control/pci_control.vhd
+++ top/gsi_pexarria5/control/pci_control.vhd
@@ -273,6 +273,7 @@ begin
       g_en_user_ow        => true,
       g_en_tempsens       => true,
       g_delay_diagnostics => true,
+      g_en_timer          => true,
       g_io_table          => io_mapping_table,
       g_lm32_cores        => c_cores,
       g_lm32_ramsizes     => c_lm32_ramsizes/4,
