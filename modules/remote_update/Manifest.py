e0e5bfccf921601737bf3bcf8eee1edb3a9ddc01
Stefan Rauch <s.rauch@gsi.de>
2020-05-27 13:49:17 +0200

    initial commit wb_asmi_a10

--- modules/remote_update/Manifest.py
+++ modules/remote_update/Manifest.py
@@ -9,7 +9,10 @@ files = [
   "altasmi.vhd",
   "wb_remote_update.vhd",
   "wb_asmi.vhd",
-  "asmi_arriaII/synthesis/asmi_arriaII.vhd"
+  "asmi_arriaII/synthesis/asmi_arriaII.vhd",
+  "asmi10/asmi10/synth/asmi10.vhd",
+  "asmi10/asmi10/altera_asmi_parallel_181/synth/asmi10_pkg.vhd",
+  "asmi10/asmi10/altera_asmi_parallel_181/synth/asmi10_altera_asmi_parallel_181_svbigkq.v"
 ]
 
 modules = {"local": __helper() }
