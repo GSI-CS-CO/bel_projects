9110d4b546e33cdd1f09614c19f378bea4e543ad
Stefan Rauch <s.rauch@gsi.de>
2020-06-29 15:40:20 +0200

    asmi support for arria5

--- modules/remote_update/Manifest.py
+++ modules/remote_update/Manifest.py
@@ -12,7 +12,9 @@ files = [
   "asmi_arriaII/synthesis/asmi_arriaII.vhd",
   "asmi10/asmi10/synth/asmi10.vhd",
   "asmi10/asmi10/altera_asmi_parallel_181/synth/asmi10_pkg.vhd",
-  "asmi10/asmi10/altera_asmi_parallel_181/synth/asmi10_altera_asmi_parallel_181_svbigkq.v"
+  "asmi10/asmi10/altera_asmi_parallel_181/synth/asmi10_altera_asmi_parallel_181_svbigkq.v",
+  "asmi5/synthesis/asmi5.vhd",
+  "asmi5/synthesis/submodules/asmi5_asmi_parallel_0.v"
 ]
 
 modules = {"local": __helper() }
