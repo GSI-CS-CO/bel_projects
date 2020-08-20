e543f62e3ba586d0923d1e9ecd204c3b9d3c019e
Dietrich Beck <d.beck@gsi.de>
2020-08-20 16:08:09 +0200

    wr-unipz: library version set to 00.21.03

--- modules/wr-unipz/include/wrunipzlib.h
+++ modules/wr-unipz/include/wrunipzlib.h
@@ -3,7 +3,7 @@
  *
  *  created : 2020
  *  author  : Dietrich Beck, GSI-Darmstadt
- *  version : 06-April-2020
+ *  version : 20-August-2020
  *
  * library for wrunipz
  *
@@ -41,7 +41,7 @@
 extern "C" {
 #endif
 
-#define WRUNIPZLIB_VERSION 0x002102
+#define WRUNIPZLIB_VERSION 0x002103
 
 // (error) codes; duplicated to avoid the need of joining bel_projects and acc git repos
 #define  WRUNIPZLIB_STATUS_OK                 0    // OK
