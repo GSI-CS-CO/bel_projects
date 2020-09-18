437a3930f37a1438ad01b4f7e316117caa85eb1a
Stefan Rauch <s.rauch@gsi.de>
2020-09-18 11:17:40 +0200

    wb_asmi: remove generated files from Manifest

--- modules/remote_update/Manifest.py
+++ modules/remote_update/Manifest.py
@@ -1,20 +1,13 @@
-def __helper():
-  dirs = []
-  dirs.extend(["asmi10/asmi10/altera_asmi_parallel_181"])
-  return dirs
-
 files = [ 
   "remote_update_pkg.vhd",
   "remote_update.vhd",
   "wb_remote_update.vhd",
   "wb_asmi.vhd",
-  "asmi10/asmi10/synth/asmi10.vhd",
-  "asmi10/asmi10/altera_asmi_parallel_181/synth/asmi10_pkg.vhd",
-  "asmi10/asmi10/altera_asmi_parallel_181/synth/asmi10_altera_asmi_parallel_181_svbigkq.v",
-  "asmi5/asmi5/synthesis/asmi5.vhd",
-  "asmi5/asmi5/synthesis/submodules/asmi5_asmi_parallel_0.v",
-  "asmi_arriaII/asmi_arriaII/synthesis/asmi_arriaII.vhd",
-  "asmi_arriaII/asmi_arriaII/synthesis/submodules/asmi_arriaII_asmi_parallel_0.v"
+  #"asmi10/asmi10/synth/asmi10.vhd",
+  #"asmi10/asmi10/altera_asmi_parallel_181/synth/asmi10_pkg.vhd",
+  #"asmi10/asmi10/altera_asmi_parallel_181/synth/asmi10_altera_asmi_parallel_181_svbigkq.v",
+  #"asmi5/asmi5/synthesis/asmi5.vhd",
+  #"asmi5/asmi5/synthesis/submodules/asmi5_asmi_parallel_0.v",
+  #"asmi_arriaII/asmi_arriaII/synthesis/asmi_arriaII.vhd",
+  #"asmi_arriaII/asmi_arriaII/synthesis/submodules/asmi_arriaII_asmi_parallel_0.v"
 ]
-
-modules = {"local": __helper() }
--- modules/remote_update/asmi10/asmi10/altera_asmi_parallel_181/Manifest.py
+++
@@ -1,6 +0,0 @@
-library = "altera_asmi_parallel_181"
-
-files = [
-        "synth/asmi10_altera_asmi_parallel_181_gdoqleq.v",
-        "synth/asmi10_pkg.vhd"
-        ]
