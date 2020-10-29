948a47a0270b1d98ca4a146f24e7510cd456efef
Martin Skorsky <m.skorsky@gsi.de>
2020-10-29 14:31:56 +0100

    dynamic/async/test.py: wait longer (1.1 sec) before check

--- modules/ftm/ftmx86/full_test/dynamic/async/test.py
+++ modules/ftm/ftmx86/full_test/dynamic/async/test.py
@@ -13,7 +13,7 @@ testcase = TestCase(
     Op("Test1_4_flow", "dm-cmd", "-i", "0.0",                        "test1_0c_cmd.dot"),
     Op("Test1_5_Queue", "dm-cmd", "rawqueue BLOCK_B", "0.0", None, "test1_5_exp.txt", ["reltime", "cmdqty"],),
     Op("Test1_6_unlock", "dm-cmd", "-i", "0.0",                    "test1_0d_cmd.dot"),
-    Op("Test1_7_CheckVis", "dm-sched", "rawvisited", "1.0", None,  "test1_7_exp.txt"),
+    Op("Test1_7_CheckVis", "dm-sched", "rawvisited", "1.1", None,  "test1_7_exp.txt"),
     Op("Test1_8_CheckLock", "dm-cmd", "showlocks", "0.0", None,      "test1_8_exp.txt"),
     Op("Test1_9_Queue", "dm-cmd", "rawqueue BLOCK_B", "0.0", None, "test1_9_exp.txt", ["reltime", "cmdqty"],)
   ]
