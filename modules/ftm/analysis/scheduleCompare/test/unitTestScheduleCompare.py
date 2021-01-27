e3b7c31a72baf964893c38ac5aff3588eec98c0d
Martin Skorsky <m.skorsky@gsi.de>
2021-01-27 15:37:17 +0100

    unitTestScheduleCompare.py: first 7 tests working OK

--- modules/ftm/analysis/scheduleCompare/test/unitTestScheduleCompare.py
+++ modules/ftm/analysis/scheduleCompare/test/unitTestScheduleCompare.py
@@ -19,30 +19,53 @@ class TestScheduleCompare(unittest.TestCase):
     """
     self.binary = test_binary
 
-  def test_1print_args(self):
-    print(f'Binary: {self.binary}.')
+  def t1est_1print_args(self):
+    print(f'Binary: {self.binary}.', end='')
 
-  def callScheduleCompare(self, file1, file2, options):
+  def callScheduleCompare(self, file1, file2, options='', expectedReturnCode=-1, linesCout=-1, linesCerr=-1):
     """
     Common method for test cases: run scheduleCompare.
     Start scheduleCompare with the arguments and check the output on stdout and stderr and the return code as well.
     """
     # pass cmd and args to the function
-    process = subprocess.Popen([self.binary, file1, file2, options], stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+    if len(options) > 0:
+      process = subprocess.Popen([self.binary, file1, file2, options], stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+    else:
+      process = subprocess.Popen([self.binary, file1, file2], stderr=subprocess.PIPE, stdout=subprocess.PIPE)
     # get command output and error
     stdout, stderr = process.communicate()
-    print(f'Returncode: {process.returncode} of {self.binary}.')
-    lines = stderr.decode('utf-8').splitlines()
-    print(f'Lines:\n{lines}')
-    test_result = False
-    for line in lines:
-      if 'Target node is NULL, target missing.' in line:
-        test_result = True
-    self.assertTrue(test_result, f'stderr: {lines}')
-    self.assertEqual(len(lines), 1)
+    # print(f'Returncode: {process.returncode} of {self.binary}.')
+    if expectedReturnCode > -1:
+      self.assertEqual(process.returncode, expectedReturnCode, f'wrong return code {process.returncode}')
+    if linesCerr > -1:
+      lines = stderr.decode('utf-8').splitlines()
+      self.assertEqual(len(lines), linesCerr, f'stderr: {lines}')
+    # print(f'stderr Lines:\n{lines}')
+    if linesCout > -1:
+      lines = stdout.decode('utf-8').splitlines()
+      self.assertEqual(len(lines), linesCout, f'stdout: {lines}')
+    # print(f'stdout Lines:\n{lines}')
 
   def test_first_isomorphism(self):
-    self.callScheduleCompare('test0.dot', 'test0.dot', '-v')
+    self.callScheduleCompare('test0.dot', 'test0.dot', expectedReturnCode=0, linesCerr=0, linesCout=3)
+
+  def test_first_isomorphism_verbose(self):
+    self.callScheduleCompare('test0.dot', 'test0.dot', '-v', expectedReturnCode=0, linesCerr=0, linesCout=25)
+
+  def test_subgraph_isomorphism(self):
+    self.callScheduleCompare('x0.dot', 'x1.dot', expectedReturnCode=2, linesCerr=0, linesCout=3)
+
+  def test_subgraph_isomorphism_verbose(self):
+    self.callScheduleCompare('x0.dot', 'x1.dot', '-v', expectedReturnCode=2, linesCerr=0, linesCout=31)
+
+  def test_subgraph_isomorphism_superverbose(self):
+    self.callScheduleCompare('x0.dot', 'x1.dot', '-vv', expectedReturnCode=2, linesCerr=0, linesCout=51)
+
+  def test_subgraph_isomorphism_silent(self):
+    self.callScheduleCompare('x0.dot', 'x1.dot', '-s', expectedReturnCode=2, linesCerr=0, linesCout=0)
+
+  def test_usage_message(self):
+    self.callScheduleCompare('', '', '-h', expectedReturnCode=14, linesCerr=16, linesCout=0)
 
 
   def targetName(self, command, count_out=0, count_err=0, options=''):
