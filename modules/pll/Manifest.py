cf3624d27212a6bbc467e79d9bc1c6eb3aea4957
A. Hahn <a.hahn@gsi.de>
2021-02-16 15:14:32 +0100

    pll: Manifest.py update

--- modules/pll/Manifest.py
+++ modules/pll/Manifest.py
@@ -4,7 +4,6 @@ def __helper():
   if syn_device[:9] == "10ax027e3": dirs.extend(["arria10_scu4"])
   if syn_device[:9] == "10ax027h2": dirs.extend(["arria10_pex10"])
   if syn_device[:9] == "10ax066h2": dirs.extend(["arria10_ftm10"])
-  if syn_device[:7] == "10ax066":   dirs.extend(["arria10"])
   if syn_device[:7] == "10ax115":   dirs.extend(["arria10_e3p1"])
   if syn_device[:1] == "5":         dirs.extend(["arria5"])
   if syn_device[:4] == "ep2a":      dirs.extend(["arria2"])
