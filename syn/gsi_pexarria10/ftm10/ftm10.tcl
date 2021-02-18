0576befad686243003bca4d1d2050c455b787d82
A. Hahn <a.hahn@gsi.de>
2021-02-18 12:53:37 +0100

    ftm10: tcl update

--- syn/gsi_pexarria10/ftm10/ftm10.tcl
+++ syn/gsi_pexarria10/ftm10/ftm10.tcl
@@ -4,5 +4,5 @@ source ../../../modules/build_id/build_id.tcl
 source ../../../modules/pll/arria10_ftm10/arria10_ftm10_pll.tcl
 source ../../../ip_cores/general-cores/platform/altera/wb_pcie/arria10gx_ftm10.tcl
 source ../../../ip_cores/general-cores/platform/altera/networks/arria10gx/arria10gx.tcl
-#source ../../../ip_cores/wr-cores/platform/altera/wr_arria10_phy/wr_arria10_ftm10_phy/wr_arria10_ftm10_phy.tcl
+source ../../../ip_cores/wr-cores/platform/altera/wr_arria10_phy/wr_arria10_ftm10_phy/wr_arria10_ftm10_phy.tcl
 
