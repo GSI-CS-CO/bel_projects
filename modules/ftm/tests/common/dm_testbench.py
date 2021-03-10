32fdeced29cd267852a7029646d0fd08afeb51f1
Martin Skorsky <m.skorsky@gsi.de>
2021-03-10 15:13:44 +0100

    add startAndCheckSubprocess, snoopToCsv, snoopToCsvWithAction, analyseFrequencyFromCsv to dm_testbench.

--- modules/ftm/tests/common/dm_testbench.py
+++ modules/ftm/tests/common/dm_testbench.py
@@ -1,8 +1,12 @@
-import subprocess
-import unittest
-import difflib
+import subprocess  # used in startAllPatterns
+import unittest    # contains super class
+import difflib     # used in compareExpectedResult
 import os
 import pathlib
+import csv         # used in analyseFrequencyFromCsv
+import contextlib
+import datetime    # used in analyseFrequencyFromCsv
+import collections # used in analyseFrequencyFromCsv
 
 """
 Module dm_testbench collects functions to handle patterns for the data master testbench.
@@ -54,28 +58,31 @@ class DmTestbench(unittest.TestCase):
       if lines[i] == 'Patterns:':
         patterns = True
 
-  def startAndCheckSubprocess(self, binary, command, parameter1, options='', expectedReturnCode=-1, linesCout=-1, linesCerr=-1):
+  def startAndCheckSubprocess(self, argumentsList, expectedReturnCode=-1, linesCout=-1, linesCerr=-1):
     """
-    Common method to start a subprocess and check the return code, .
-    Start dm-cmd and dm-sched for the test steps with the arguments and check the output on stdout and stderr and the return code as well.
+    Common method to start a subprocess and check the return code.
+    The <argumentsList> contains the binary to execute and all arguments in one list.
+    Start the binary for the test step with the arguments and check the output on stdout and stderr and the return code as well.
     """
     # pass cmd and args to the function
-    if len(options) > 0:
-      process = subprocess.Popen([binary, command, parameter1, options], stderr=subprocess.PIPE, stdout=subprocess.PIPE)
-    else:
-      process = subprocess.Popen([binary, command, parameter1], stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+    process = subprocess.Popen([*argumentsList], stderr=subprocess.PIPE, stdout=subprocess.PIPE)
     # get command output and error
     stdout, stderr = process.communicate()
     if expectedReturnCode > -1:
-      self.assertEqual(process.returncode, expectedReturnCode, f'wrong return code {process.returncode}, Command line: {binary} {command} {parameter1} {options}\nstderr: {stderr.decode("utf-8").splitlines()}\nstdout: {stdout.decode("utf-8").splitlines()}')
+      self.assertEqual(process.returncode, expectedReturnCode, f'wrong return code {process.returncode}, Command line: {argumentsList}\nstderr: {stderr.decode("utf-8").splitlines()}\nstdout: {stdout.decode("utf-8").splitlines()}')
     if linesCerr > -1:
       lines = stderr.decode('utf-8').splitlines()
-      self.assertEqual(len(lines), linesCerr, f'wrong stderr, expected {linesCerr} lines, Command line: {binary} {command} {parameter1} {options}\nstderr: {lines}\nstdout: {stdout.decode("utf-8").splitlines()}')
+      self.assertEqual(len(lines), linesCerr, f'wrong stderr, expected {linesCerr} lines, Command line: {argumentsList}\nstderr: {lines}\nstdout: {stdout.decode("utf-8").splitlines()}')
     if linesCout > -1:
       lines = stdout.decode('utf-8').splitlines()
-      self.assertEqual(len(lines), linesCout, f'wrong stdout, expected {linesCout} lines, Command line: {binary} {command} {parameter1} {options}\nstderr: {stderr.decode("utf-8").splitlines()}\nstdout: {lines}')
+      self.assertEqual(len(lines), linesCout, f'wrong stdout, expected {linesCout} lines, Command line: {argumentsList}\nstderr: {stderr.decode("utf-8").splitlines()}\nstdout: {lines}')
 
   def compareExpectedResult(self, file_current, file_expected, exclude):
+    """
+    Compare a file with a test result with an expected result contained in <file_expected>.
+    Lines with the string <exclude> are removed from <file_current> before checking against <file_expected>.
+    Assert that a unified diff has no lines.
+    """
     with open(file_current) as f_current:
       current = f_current.readlines()
     if len(exclude) > 0:
@@ -87,6 +94,73 @@ class DmTestbench(unittest.TestCase):
   #  print(f'Diff: {diffLines}')
     self.assertEqual(len(diffLines), 0, f'Diff: {diffLines}')
 
+  def snoopToCsv(self, csv_file_name, duration=1.0):
+    """
+    Snoop timing messages with saft-ctl for <duration> seconds (default = 1.0).
+    Write the messages to <csv_file_name>.
+    Details: start saft-ctl with Popen, wait for <duration> seconds.
+    When the TimeoutExpired exception pops up, kill saft-ctl and wait for at most 5 seconds.
+    At least assert the return code -9 of saft-ctl.
+    """
+    with open(csv_file_name, 'wb') as file1:
+      try:
+        process = subprocess.Popen(['saft-ctl', 'x', '-fvx', 'snoop', '0', '0', '0'], stdout=file1)
+        process.wait(duration)
+      except subprocess.TimeoutExpired as excep:
+        process.kill()
+        process.wait(5)
+        self.assertEqual(process.returncode, -9, f'Returncode: {process.returncode}')
+
+  def snoopToCsvWithAction(self, csv_file_name, action, duration=1.0):
+    """
+    Snoop timing messages with saft-ctl for <duration> seconds (default = 1.0).
+    Write the messages to <csv_file_name>.
+    Details: start saft-ctl with Popen, wait for <duration> seconds.
+    When the TimeoutExpired exception pops up, kill saft-ctl and wait for at most 5 seconds.
+    At least assert the return code -9 of saft-ctl.
+    """
+    with open(csv_file_name, 'wb') as file1:
+      try:
+        process = subprocess.Popen(['saft-ctl', 'x', '-fvx', 'snoop', '0', '0', '0'], stdout=file1)
+        action()
+        process.wait(duration)
+      except subprocess.TimeoutExpired as excep:
+        process.kill()
+        process.wait(5)
+        self.assertEqual(process.returncode, -9, f'Returncode: {process.returncode}')
+
+  def analyseFrequencyFromCsv(self, csv_file_name, column=20, printTable=True):
+    """
+    Analyse the frequency of the values in the specified column. Defaule column is 20 (parameter of the timing message).
+    Prints (if printtable=True) the table of values, counters, and frequency over the whole time span.
+    """
+    line_count = 0
+    maxTime = datetime.datetime.strptime("2000-01-01", '%Y-%m-%d')
+    minTime = datetime.datetime.strptime("2500-01-01", '%Y-%m-%d')
+    listParam = []
+    # Read this file snoop_protocol.csv as csv
+    with open(csv_file_name) as csv_file:
+      csv_reader = csv.reader(csv_file, delimiter=' ')
+      for row in csv_reader:
+        line_count += 1
+        time1 = datetime.datetime.strptime(row[1] + ' ' + row[2][:-3], '%Y-%m-%d %H:%M:%S.%f')
+        if minTime > time1:
+          minTime = time1
+        if time1 > maxTime:
+          maxTime = time1
+        listParam.append(row[column])
+      timeSpan = (maxTime-minTime).total_seconds()
+      listCounter = sorted(collections.Counter(listParam).items())
+      if printTable:
+        print()
+        print(f'{"Value":^20s}  {"Count":>6s}   {"Frequency":>9s}')
+        for key, value in listCounter:
+          print(f'{key:>20s}: {value:6d} {value/timeSpan:9.3f}Hz')
+        print(f'{"All":>20s}: {line_count:6d} {line_count/timeSpan: >9.3f}Hz, time span: {timeSpan:0.6f}sec')
+
   def deleteFile(self, file_name):
+    """
+    Delete file <file_name>.
+    """
     file_to_rem = pathlib.Path(file_name)
     file_to_rem.unlink()
--- modules/ftm/tests/safe2remove/unitTestSafe2Remove.py
+++ modules/ftm/tests/safe2remove/unitTestSafe2Remove.py
@@ -4,6 +4,13 @@ import unittest
 import sys
 import pathlib
 import dm_testbench
+import subprocess
+#import time
+#import signal
+import csv
+import contextlib
+import datetime
+import collections
 
 """
 Class collects unit tests for safe2remove.
@@ -28,21 +35,41 @@ class UnitTestSafe2Remove(dm_testbench.DmTestbench):
     self.datamaster = test_datamaster
 
   def safe2removeTestcase(self, dot_file1, pattern_to_remove):
-      self.startAllPattern(self.datamaster, dot_file1 + '.dot')
-      self.startAndCheckSubprocess('dm-cmd', self.datamaster, 'chkrem', pattern_to_remove, expectedReturnCode=0)
-      self.compareExpectedResult('debug.dot', dot_file1 + '-forbidden.dot', 'Created')
-      self.deleteFile('debug.dot')
-      self.startAndCheckSubprocess('dm-cmd', self.datamaster, 'abortpattern', pattern_to_remove, expectedReturnCode=0)
-      self.startAndCheckSubprocess('dm-cmd', self.datamaster, 'chkrem', pattern_to_remove, expectedReturnCode=0)
-      self.compareExpectedResult('debug.dot', dot_file1 + '-safe.dot', 'Created')
-      self.deleteFile('debug.dot')
-      self.startAndCheckSubprocess('dm-sched', self.datamaster, 'remove', dot_file1 + '-remove.dot', expectedReturnCode=0)
-      self.startAndCheckSubprocess('dm-sched', self.datamaster, 'status', '-ostatus.dot', expectedReturnCode=0)
-      self.compareExpectedResult('status.dot', dot_file1 + '-status.dot', '')
-      self.deleteFile('status.dot')
+    self.startAllPattern(self.datamaster, dot_file1 + '.dot')
+    self.startAndCheckSubprocess(('dm-cmd', self.datamaster, 'chkrem', pattern_to_remove), expectedReturnCode=0)
+    self.compareExpectedResult('debug.dot', dot_file1 + '-forbidden.dot', 'Created')
+    self.deleteFile('debug.dot')
+    self.startAndCheckSubprocess(('dm-cmd', self.datamaster, 'abortpattern', pattern_to_remove), expectedReturnCode=0)
+    self.startAndCheckSubprocess(('dm-cmd', self.datamaster, 'chkrem', pattern_to_remove), expectedReturnCode=0)
+    self.compareExpectedResult('debug.dot', dot_file1 + '-safe.dot', 'Created')
+    self.deleteFile('debug.dot')
+    self.startAndCheckSubprocess(('dm-sched', self.datamaster, 'remove', dot_file1 + '-remove.dot'), expectedReturnCode=0)
+    self.startAndCheckSubprocess(('dm-sched', self.datamaster, 'status', '-ostatus.dot'), expectedReturnCode=0)
+    self.compareExpectedResult('status.dot', dot_file1 + '-status.dot', '')
+    self.deleteFile('status.dot')
 
   def test_safe2remove_blockalign1(self):
     self.safe2removeTestcase('blockalign1', 'PPS1_TEST')
+    
+  def doBranch1(self):
+    self.startAndCheckSubprocess(('dm-cmd', self.datamaster, 'flow', '-q', '100', 'Block1', 'B1'), expectedReturnCode=0)
+    self.startAndCheckSubprocess(('dm-cmd', self.datamaster, 'flow', '-q', '100', 'Block1', 'C1'), expectedReturnCode=0)
+  
+  def test_branch1(self):
+    self.startAllPattern(self.datamaster, 'branch1.dot')
+    file_name = 'snoop_branch1.csv'
+    parameter_column = 20
+    self.snoopToCsvWithAction(file_name, self.doBranch1, 3)
+    self.analyseFrequencyFromCsv(file_name, parameter_column)
+    self.deleteFile(file_name)
+
+  def test_safe2remove_blockflow1(self):
+    self.startAllPattern(self.datamaster, 'block-flow1.dot')
+    file_name = 'snoop_block-flow1.csv'
+    parameter_column = 20
+    self.snoopToCsv(file_name, 5.0)
+    self.analyseFrequencyFromCsv(file_name, parameter_column)
+    self.deleteFile(file_name)
 
 if __name__ == '__main__':
   if len(sys.argv) > 1:
