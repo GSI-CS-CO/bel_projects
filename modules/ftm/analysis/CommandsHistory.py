602268d63e9aec08be320f2b4e5bf7ba2ab18256
Martin Skorsky <m.skorsky@gsi.de>
2021-03-15 16:10:20 +0100

    CommandsHistory.py: implement begin / end timestamp. Add files for first test case.

--- modules/ftm/analysis/CommandsHistory.py
+++ modules/ftm/analysis/CommandsHistory.py
@@ -1,8 +1,10 @@
-#!/usr/bin/python
+#! /usr/bin/env python3
 import sys
 import re
 import stat
 import os
+from datetime import datetime as dt
+import argparse
 
 def createFolder(folder):
     try:
@@ -16,7 +18,7 @@ Take a generator command history file and extract all commands and the input dot
 Output: 1. bash script with similar name as history file
         2. folder with extracted dot files
 '''
-def extractScript(commands_history_file):
+def extractScript(commands_history_file, dtBegin, dtEnd, verbose):
     script_file = 'commands.sh'
     if 'GeneratorCommandsHistory' in commands_history_file:
         script_file = f'{commands_history_file[9:-4]}.sh'
@@ -28,10 +30,19 @@ def extractScript(commands_history_file):
     file_h = open(commands_history_file, 'r')
     lines = file_h.readlines()
     pattern = re.compile('# log entry ([0-9]+) ------------')
+    # Match something like '# Fri_Feb__5_13.36.22_2021'
+    timestampPattern = re.compile('# ([FMSTW][a-z]{2}_[A-Z][a-z]{2}_+[0-9]{1,2}_[0-9]{1,2}\.[0-9]{1,2}\.[0-9]{1,2}_[0-9]{4})')
     entry_no = -1
     dot_file_name = ''
     collect_lines = False
+    # recordEntries is set to True, iff the time stamp of the entry is between Begin and End.
+    recordEntries = False
+    # find the first and the last entry
+    dtFirstEntry = dt(2100,1,1)
+    dtLastEntry = dt(1970,1,1)
+    # collector for the grapd written to a dot file.
     graph_lines = []
+    countDotFiles = 0
     script = open(script_file, 'w')
     # Write header of script file
     script.write(f'#! /bin/bash\n\n')
@@ -41,106 +52,156 @@ def extractScript(commands_history_file):
     script.write(f'# Example: ./commands.sh -v\n')
     script.write(f'OPTIONS=$1\n')
     for i in range(len(lines)):
-        match = pattern.match(lines[i])
-        if match:
-            # write graph_lines to dot file only if there is a graph.
-            # improvement: check for 'digraph' in the first line.
-            if int(entry_no) > -1 and len(graph_lines) > 2 and len(graph_lines[1]) > 6:
-                with open(dot_file_name, 'w') as writer:
-                    writer.write(''.join(graph_lines[1:]))
-            entry_no = match.group(1)
-            if len(entry_no) < 6:
-                entry_no = '000000'[0:6-len(entry_no)] + entry_no
-            dot_file_name = f'{prefix}-{entry_no}.dot'
-            collect_lines = False
-            graph_lines = []
-            print(f'Entry: {entry_no} {lines[i+2][:-1]}')
+      match = pattern.match(lines[i])
+      if match:
+          # write graph_lines to dot file only if there is a graph.
+          # improvement: check for 'digraph' in the first line.
+          if recordEntries and int(entry_no) > -1 and len(graph_lines) > 2 and len(graph_lines[1]) > 6:
+            with open(dot_file_name, 'w') as writer:
+              writer.write(''.join(graph_lines[1:]))
+              countDotFiles += 1
+          # extract the entry number.
+          entry_no = match.group(1)
+          if len(entry_no) < 6:
+            entry_no = '000000'[0:6-len(entry_no)] + entry_no
+          dot_file_name = f'{prefix}-{entry_no}.dot'
+          timestampMatch = timestampPattern.match(lines[i+1])
+          if timestampMatch:
+            testdate = timestampMatch.group(1).replace('__', '_0')
+            dtTestdate = dt.strptime(testdate, "%a_%b_%d_%H.%M.%S_%Y")
+            if dtFirstEntry > dtTestdate:
+              dtFirstEntry = dtTestdate
+            if dtLastEntry < dtTestdate:
+              dtLastEntry = dtTestdate
+            recordEntries = (dtBegin < dtTestdate) and (dtTestdate < dtEnd)
+          collect_lines = False
+          graph_lines = []
+          if verbose:
+            print(f'Entry: {entry_no} {timestampMatch.group(1)} {lines[i+2][:-1]}')
+      if recordEntries:
         if 'schedule clear' in lines[i]:
-            collect_lines = False
-            script.write(f'# entry {entry_no}\n')
-            script.write(f'dm-cmd $DM $OPTIONS halt\n')
-            script.write(f'dm-sched $DM $OPTIONS clear\n')
+          collect_lines = False
+          script.write(f'# entry {entry_no}\n')
+          script.write(f'dm-cmd $DM $OPTIONS halt\n')
+          script.write(f'dm-sched $DM $OPTIONS clear\n')
         elif 'schedule add' in lines[i]:
-            collect_lines = True
-            script.write(f'# entry {entry_no}\n')
-            script.write(f'dm-sched $DM $OPTIONS add {dot_file_name}\n')
+          collect_lines = True
+          script.write(f'# entry {entry_no}\n')
+          script.write(f'dm-sched $DM $OPTIONS add {dot_file_name}\n')
         elif 'schedule keep' in lines[i]:
-            collect_lines = True
-            script.write(f'# entry {entry_no}\n')
-            script.write(f'dm-sched $DM $OPTIONS keep {dot_file_name}\n')
+          collect_lines = True
+          script.write(f'# entry {entry_no}\n')
+          script.write(f'dm-sched $DM $OPTIONS keep {dot_file_name}\n')
         elif 'schedule remove' in lines[i]:
-            collect_lines = True
-            script.write(f'# entry {entry_no}\n')
-            script.write(f'dm-sched $DM $OPTIONS remove {dot_file_name}\n')
+          collect_lines = True
+          script.write(f'# entry {entry_no}\n')
+          script.write(f'dm-sched $DM $OPTIONS remove {dot_file_name}\n')
         elif 'schedule dump' in lines[i]:
-            collect_lines = True
-            script.write(f'# entry {entry_no}\n')
-            script.write(f'dm-sched $DM $OPTIONS status {dot_file_name}\n')
+          collect_lines = True
+          script.write(f'# entry {entry_no}\n')
+          script.write(f'dm-sched $DM $OPTIONS status {dot_file_name}\n')
         elif 'table check result:' in lines[i]:
-            collect_lines = False
-            script.write(f'# entry {entry_no}, lines[i]')
+          collect_lines = False
+          script.write(f'# entry {entry_no}, {lines[i]}')
         elif 'command execute' in lines[i]:
-            collect_lines = True
-            script.write(f'# entry {entry_no}\n')
-            script.write(f'dm-cmd $DM $OPTIONS -i {dot_file_name}\n')
+          collect_lines = True
+          script.write(f'# entry {entry_no}\n')
+          script.write(f'dm-cmd $DM $OPTIONS -i {dot_file_name}\n')
         elif 'is safe to remove' in lines[i]:
-            collect_lines = False
-            try:
-              pattern_name = re.search('is safe to remove: (.+)$', lines[i]).group(1)
-              script.write(f'# entry {entry_no}\n')
-              script.write(f'dm-cmd $DM $OPTIONS chkrem {pattern_name}\n')
-            except AttributeError:
-              print(f'Pattern name not found: {lines[i]}, entry {entry_no}')
+          collect_lines = False
+          try:
+            pattern_name = re.search('is safe to remove: (.+)$', lines[i]).group(1)
+            script.write(f'# entry {entry_no}\n')
+            script.write(f'dm-cmd $DM $OPTIONS chkrem {pattern_name}\n')
+          except AttributeError:
+            print(f'Pattern name not found: {lines[i]}, entry {entry_no}')
         elif 'result' in lines[i]:
-            collect_lines = False
-            script.write(f'# entry {entry_no}, {lines[i]}')
+          collect_lines = False
+          script.write(f'# entry {entry_no}, {lines[i]}')
         elif 'queue status for pattern' in lines[i]:
-            collect_lines = True
-            script.write(f'# entry {entry_no}\n')
-            script.write(f'# dm- $DM $OPTIONS  {dot_file_name}\n')
+          collect_lines = True
+          script.write(f'# entry {entry_no}\n')
+          script.write(f'# dm- $DM $OPTIONS  {dot_file_name}\n')
         elif 'status report' in lines[i]:
-            collect_lines = True
-            script.write(f'# entry {entry_no}\n')
-            script.write(f'# dm- $DM $OPTIONS  {dot_file_name}\n')
+          collect_lines = True
+          script.write(f'# entry {entry_no}\n')
+          script.write(f'# dm- $DM $OPTIONS  {dot_file_name}\n')
         elif 'static flush pattern' in lines[i]:
-            collect_lines = True
-            script.write(f'# entry {entry_no}\n')
-            script.write(f'# dm- $DM $OPTIONS  {dot_file_name}\n')
+          collect_lines = True
+          script.write(f'# entry {entry_no}\n')
+          script.write(f'# dm- $DM $OPTIONS  {dot_file_name}\n')
         elif 'queue report for pattern' in lines[i]:
-            collect_lines = False
-            script.write(f'# entry {entry_no}, lines[i]')
+          collect_lines = False
+          script.write(f'# entry {entry_no}, lines[i]')
         elif 'set maintenance mode to true' in lines[i]:
-            collect_lines = True
-            script.write(f'# entry {entry_no}\n')
-            script.write(f'# dm- $DM $OPTIONS  {dot_file_name}\n')
+          collect_lines = True
+          script.write(f'# entry {entry_no}\n')
+          script.write(f'# dm- $DM $OPTIONS  {dot_file_name}\n')
         elif 'set maintenance mode to false' in lines[i]:
-            collect_lines = True
-            script.write(f'# entry {entry_no}\n')
-            script.write(f'# dm- $DM $OPTIONS  {dot_file_name}\n')
+          collect_lines = True
+          script.write(f'# entry {entry_no}\n')
+          script.write(f'# dm- $DM $OPTIONS  {dot_file_name}\n')
         elif 'set force schedule actions to true' in lines[i]:
-            collect_lines = True
-            script.write(f'# entry {entry_no}\n')
-            script.write(f'# dm- $DM $OPTIONS  {dot_file_name}\n')
+          collect_lines = True
+          script.write(f'# entry {entry_no}\n')
+          script.write(f'# dm- $DM $OPTIONS  {dot_file_name}\n')
         elif 'set force schedule actions to false' in lines[i]:
-            collect_lines = True
-            script.write(f'# entry {entry_no}\n')
-            script.write(f'# dm- $DM $OPTIONS  {dot_file_name}\n')
+          collect_lines = True
+          script.write(f'# entry {entry_no}\n')
+          script.write(f'# dm- $DM $OPTIONS  {dot_file_name}\n')
         elif 'set optimizeSafeToRemove to true' in lines[i]:
-            collect_lines = True
-            script.write(f'# entry {entry_no}\n')
-            script.write(f'# dm- $DM $OPTIONS  {dot_file_name}\n')
+          collect_lines = True
+          script.write(f'# entry {entry_no}\n')
+          script.write(f'# dm- $DM $OPTIONS  {dot_file_name}\n')
         elif 'set optimizeSafeToRemove to false' in lines[i]:
-            collect_lines = True
-            script.write(f'# entry {entry_no}\n')
-            script.write(f'# dm- $DM $OPTIONS  {dot_file_name}\n')
-        if collect_lines:
-            graph_lines.append(lines[i])
-    print(f'Lines: {len(lines)}.')
+          collect_lines = True
+          script.write(f'# entry {entry_no}\n')
+          script.write(f'# dm- $DM $OPTIONS  {dot_file_name}\n')
+      if collect_lines:
+        graph_lines.append(lines[i])
+    print(f'Lines: {len(lines)}, dot files: {countDotFiles}, first eintry: {dtFirstEntry}, last entry: {dtLastEntry}.')
     st = os.stat(script_file)
     os.chmod(script_file, st.st_mode | stat.S_IXUSR | stat.S_IXGRP | stat.S_IXOTH)
 
-if __name__ == '__main__':
-    if len(sys.argv) > 1:
-        extractScript(sys.argv[1])
+def convertTimestamp(dtString):
+  """
+  Parse a timestamp (date or date and time).
+  """
+  try:
+    if len(dtString) > 19:
+      # parse 2021-03-12 12:20:07.6
+      # parse 2021-03-12 12:20:07.000006
+      # parse 2021-03-12 12:20:07.691156
+      return dt.strptime(dtString, "%Y-%m-%d %H:%M:%S.%f")
+    if len(dtString) > 17:
+      # parse 2021-03-12 12:20:07
+      return dt.strptime(dtString, "%Y-%m-%d %H:%M:%S")
+    if len(dtString) > 13:
+      # parse 2021-03-12
+      return dt.strptime(dtString, "%Y-%m-%d %H:%M")
+    if len(dtString) > 11:
+      # parse 2021-03-12 11
+      return dt.strptime(dtString, "%Y-%m-%d %H")
+    if len(dtString) > 9:
+      # parse 2021-03-12
+      return dt.strptime(dtString, "%Y-%m-%d")
     else:
-        print(f'File name for command history missing')
+      return dt.now()
+  except ValueError as v:
+    print(v)
+    return dt(1, 1, 1)
+  
+if __name__ == '__main__':
+  dtBegin = '1970-01-01'
+  dtEnd = '2100-01-01'
+  parser = argparse.ArgumentParser("CommandHistory reads datamaster log files and generates a script and a series of dot files for the script.",
+  epilog="For questions and bugs contact Martin Skorsky (m.skorsky@gsi.de).")
+  parser.add_argument('--version', action='version', version='%(prog)s 1.0')
+  parser.add_argument('--verbose', '-v', help='print each log entry')
+  parser.add_argument('log_file_name', help='the name of the datamaster log file')
+  parser.add_argument('begin_date', help='the first date to log')
+  parser.add_argument('begin_time', help='the time part of the first date to log')
+  parser.add_argument('end_date', help='the last date to log')
+  parser.add_argument('end_time', help='the time part of the last date to log')
+  args = parser.parse_args()
+  extractScript(args.log_file_name, convertTimestamp(args.begin_date + " " + args.begin_time), convertTimestamp(args.end_date + " " + args.end_time), args.verbose)
---
+++ modules/ftm/analysis/test/CommandsHistory_Fri_Feb__5_13.33.22_2021.sh
@@ -0,0 +1,158 @@
+#! /bin/bash
+
+# Usage: CommandsHistory_Fri_Feb__5_13.33.22_2021.sh [options]
+#        where options are used for dm-cmd and dm-sched
+#        Data Master name is expected in $DM.
+# Example: ./commands.sh -v
+OPTIONS=$1
+# entry 000000
+dm-sched $DM $OPTIONS status CommandsHistory_Fri_Feb__5_13.33.22_2021/graph-entry-000000.dot
+# entry 000001, table check result: true
+# entry 000002
+dm-sched $DM $OPTIONS status CommandsHistory_Fri_Feb__5_13.33.22_2021/graph-entry-000002.dot
+# entry 000003, table check result: true
+# entry 000004
+dm-cmd $DM $OPTIONS halt
+dm-sched $DM $OPTIONS clear
+# entry 000005
+dm-sched $DM $OPTIONS status CommandsHistory_Fri_Feb__5_13.33.22_2021/graph-entry-000005.dot
+# entry 000006, table check result: true
+# entry 000007
+dm-cmd $DM $OPTIONS halt
+dm-sched $DM $OPTIONS clear
+# entry 000008
+dm-sched $DM $OPTIONS status CommandsHistory_Fri_Feb__5_13.33.22_2021/graph-entry-000008.dot
+# entry 000009, table check result: true
+# entry 000010
+dm-sched $DM $OPTIONS status CommandsHistory_Fri_Feb__5_13.33.22_2021/graph-entry-000010.dot
+# entry 000011, table check result: true
+# entry 000012
+dm-sched $DM $OPTIONS status CommandsHistory_Fri_Feb__5_13.33.22_2021/graph-entry-000012.dot
+# entry 000013, table check result: true
+# entry 000014
+dm-sched $DM $OPTIONS remove CommandsHistory_Fri_Feb__5_13.33.22_2021/graph-entry-000014.dot
+# entry 000015
+dm-sched $DM $OPTIONS status CommandsHistory_Fri_Feb__5_13.33.22_2021/graph-entry-000015.dot
+# entry 000016, table check result: true
+# entry 000017
+dm-sched $DM $OPTIONS status CommandsHistory_Fri_Feb__5_13.33.22_2021/graph-entry-000017.dot
+# entry 000018, table check result: true
+# entry 000019
+dm-sched $DM $OPTIONS status CommandsHistory_Fri_Feb__5_13.33.22_2021/graph-entry-000019.dot
+# entry 000020, table check result: true
+# entry 000021
+dm-sched $DM $OPTIONS remove CommandsHistory_Fri_Feb__5_13.33.22_2021/graph-entry-000021.dot
+# entry 000022
+dm-sched $DM $OPTIONS status CommandsHistory_Fri_Feb__5_13.33.22_2021/graph-entry-000022.dot
+# entry 000023, table check result: true
+# entry 000024
+dm-sched $DM $OPTIONS status CommandsHistory_Fri_Feb__5_13.33.22_2021/graph-entry-000024.dot
+# entry 000025, table check result: true
+# entry 000026
+dm-sched $DM $OPTIONS add CommandsHistory_Fri_Feb__5_13.33.22_2021/graph-entry-000026.dot
+# entry 000027
+dm-sched $DM $OPTIONS status CommandsHistory_Fri_Feb__5_13.33.22_2021/graph-entry-000027.dot
+# entry 000028, table check result: true
+# entry 000029
+dm-cmd $DM $OPTIONS -i CommandsHistory_Fri_Feb__5_13.33.22_2021/graph-entry-000029.dot
+# entry 000030
+dm-cmd $DM $OPTIONS -i CommandsHistory_Fri_Feb__5_13.33.22_2021/graph-entry-000030.dot
+# entry 000031
+dm-sched $DM $OPTIONS status CommandsHistory_Fri_Feb__5_13.33.22_2021/graph-entry-000031.dot
+# entry 000032, table check result: true
+# entry 000033
+dm-sched $DM $OPTIONS add CommandsHistory_Fri_Feb__5_13.33.22_2021/graph-entry-000033.dot
+# entry 000034
+dm-sched $DM $OPTIONS status CommandsHistory_Fri_Feb__5_13.33.22_2021/graph-entry-000034.dot
+# entry 000035, table check result: true
+# entry 000036
+dm-cmd $DM $OPTIONS -i CommandsHistory_Fri_Feb__5_13.33.22_2021/graph-entry-000036.dot
+# entry 000037
+dm-cmd $DM $OPTIONS -i CommandsHistory_Fri_Feb__5_13.33.22_2021/graph-entry-000037.dot
+# entry 000038
+dm-sched $DM $OPTIONS status CommandsHistory_Fri_Feb__5_13.33.22_2021/graph-entry-000038.dot
+# entry 000039, table check result: true
+# entry 000040
+dm-sched $DM $OPTIONS add CommandsHistory_Fri_Feb__5_13.33.22_2021/graph-entry-000040.dot
+# entry 000041
+dm-sched $DM $OPTIONS status CommandsHistory_Fri_Feb__5_13.33.22_2021/graph-entry-000041.dot
+# entry 000042, table check result: true
+# entry 000043
+dm-cmd $DM $OPTIONS -i CommandsHistory_Fri_Feb__5_13.33.22_2021/graph-entry-000043.dot
+# entry 000044
+dm-cmd $DM $OPTIONS -i CommandsHistory_Fri_Feb__5_13.33.22_2021/graph-entry-000044.dot
+# entry 000045
+dm-sched $DM $OPTIONS status CommandsHistory_Fri_Feb__5_13.33.22_2021/graph-entry-000045.dot
+# entry 000046, table check result: true
+# entry 000047
+dm-sched $DM $OPTIONS add CommandsHistory_Fri_Feb__5_13.33.22_2021/graph-entry-000047.dot
+# entry 000048
+dm-sched $DM $OPTIONS status CommandsHistory_Fri_Feb__5_13.33.22_2021/graph-entry-000048.dot
+# entry 000049, table check result: true
+# entry 000050
+dm-cmd $DM $OPTIONS -i CommandsHistory_Fri_Feb__5_13.33.22_2021/graph-entry-000050.dot
+# entry 000051
+dm-cmd $DM $OPTIONS -i CommandsHistory_Fri_Feb__5_13.33.22_2021/graph-entry-000051.dot
+# entry 000052
+dm-cmd $DM $OPTIONS -i CommandsHistory_Fri_Feb__5_13.33.22_2021/graph-entry-000052.dot
+# entry 000053
+dm-cmd $DM $OPTIONS chkrem SIS18_FAST_HHD_20210201_085701
+# entry 000054, result:  -> true
+# entry 000055
+dm-cmd $DM $OPTIONS -i CommandsHistory_Fri_Feb__5_13.33.22_2021/graph-entry-000055.dot
+# entry 000056
+dm-cmd $DM $OPTIONS chkrem SIS18_SLOW_HFS_20210114_132445
+# entry 000057, result:  -> true
+# entry 000058
+dm-cmd $DM $OPTIONS -i CommandsHistory_Fri_Feb__5_13.33.22_2021/graph-entry-000058.dot
+# entry 000059
+dm-sched $DM $OPTIONS status CommandsHistory_Fri_Feb__5_13.33.22_2021/graph-entry-000059.dot
+# entry 000060, table check result: true
+# entry 000061
+dm-cmd $DM $OPTIONS -i CommandsHistory_Fri_Feb__5_13.33.22_2021/graph-entry-000061.dot
+# entry 000062
+dm-cmd $DM $OPTIONS chkrem SL_ESR_DRYRUN20_2CRYRING_no_line
+# entry 000063, result:  -> true
+# entry 000064
+dm-cmd $DM $OPTIONS -i CommandsHistory_Fri_Feb__5_13.33.22_2021/graph-entry-000064.dot
+# entry 000065
+dm-cmd $DM $OPTIONS -i CommandsHistory_Fri_Feb__5_13.33.22_2021/graph-entry-000065.dot
+# entry 000066
+dm-cmd $DM $OPTIONS chkrem SIS100_TEST_PROTON_20210114
+# entry 000067, result:  -> true
+# entry 000068
+dm-cmd $DM $OPTIONS -i CommandsHistory_Fri_Feb__5_13.33.22_2021/graph-entry-000068.dot
+# entry 000069
+dm-cmd $DM $OPTIONS -i CommandsHistory_Fri_Feb__5_13.33.22_2021/graph-entry-000069.dot
+# entry 000070
+dm-cmd $DM $OPTIONS chkrem SIS18_FAST_HHT_20201211_132855
+# entry 000071, result:  -> true
+# entry 000072
+dm-cmd $DM $OPTIONS -i CommandsHistory_Fri_Feb__5_13.33.22_2021/graph-entry-000072.dot
+# entry 000073
+dm-cmd $DM $OPTIONS -i CommandsHistory_Fri_Feb__5_13.33.22_2021/graph-entry-000073.dot
+# entry 000074
+dm-cmd $DM $OPTIONS chkrem SIS18_FAST_HHT_20210201_132940
+# entry 000075, result:  -> true
+# entry 000076
+dm-cmd $DM $OPTIONS -i CommandsHistory_Fri_Feb__5_13.33.22_2021/graph-entry-000076.dot
+# entry 000077
+dm-sched $DM $OPTIONS status CommandsHistory_Fri_Feb__5_13.33.22_2021/graph-entry-000077.dot
+# entry 000078, table check result: true
+# entry 000079
+dm-cmd $DM $OPTIONS -i CommandsHistory_Fri_Feb__5_13.33.22_2021/graph-entry-000079.dot
+# entry 000080
+dm-cmd $DM $OPTIONS chkrem CRYRING_D_local
+# entry 000081, result:  -> true
+# entry 000082
+dm-cmd $DM $OPTIONS -i CommandsHistory_Fri_Feb__5_13.33.22_2021/graph-entry-000082.dot
+# entry 000083
+dm-cmd $DM $OPTIONS -i CommandsHistory_Fri_Feb__5_13.33.22_2021/graph-entry-000083.dot
+# entry 000084
+dm-cmd $DM $OPTIONS -i CommandsHistory_Fri_Feb__5_13.33.22_2021/graph-entry-000084.dot
+# entry 000085
+dm-cmd $DM $OPTIONS -i CommandsHistory_Fri_Feb__5_13.33.22_2021/graph-entry-000085.dot
+# entry 000086
+dm-cmd $DM $OPTIONS -i CommandsHistory_Fri_Feb__5_13.33.22_2021/graph-entry-000086.dot
+# entry 000087
+dm-cmd $DM $OPTIONS -i CommandsHistory_Fri_Feb__5_13.33.22_2021/graph-entry-000087.dot
