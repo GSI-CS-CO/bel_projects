815b079af1648388db9c91f59d376b849360adfb
Martin Skorsky <m.skorsky@gsi.de>
2021-03-16 17:07:37 +0100

    CommandsHistory.py: call tested method directly. All cleanup of generated files.

--- modules/ftm/analysis/CommandsHistory.py
+++ modules/ftm/analysis/CommandsHistory.py
@@ -27,8 +27,9 @@ def extractScript(commands_history_file, dtBegin, dtEnd, verbose):
     prefix = script_file[:-3] + '/graph-entry'
     print(f'Schedules written to {prefix}-n.dot, where n is the entry number.')
     # Read comand history file
-    file_h = open(commands_history_file, 'r')
-    lines = file_h.readlines()
+    lines = []
+    with open(commands_history_file, 'r') as file_h:
+      lines = file_h.readlines()
     pattern = re.compile('# log entry ([0-9]+) ------------')
     # Match something like '# Fri_Feb__5_13.36.22_2021'
     timestampPattern = re.compile('# ([FMSTW][a-z]{2}_[A-Z][a-z]{2}_+[0-9]{1,2}_[0-9]{1,2}\.[0-9]{1,2}\.[0-9]{1,2}_[0-9]{4})')
@@ -43,122 +44,122 @@ def extractScript(commands_history_file, dtBegin, dtEnd, verbose):
     # collector for the grapd written to a dot file.
     graph_lines = []
     countDotFiles = 0
-    script = open(script_file, 'w')
-    # Write header of script file
-    script.write(f'#! /bin/bash\n\n')
-    script.write(f'# Usage: {script_file} [options]\n')
-    script.write(f'#        where options are used for dm-cmd and dm-sched\n')
-    script.write(f'#        Data Master name is expected in $DM.\n')
-    script.write(f'# Example: ./commands.sh -v\n')
-    script.write(f'OPTIONS=$1\n')
-    for i in range(len(lines)):
-      match = pattern.match(lines[i])
-      if match:
-          # write graph_lines to dot file only if there is a graph.
-          # improvement: check for 'digraph' in the first line.
-          if recordEntries and int(entry_no) > -1 and len(graph_lines) > 2 and len(graph_lines[1]) > 6:
-            with open(dot_file_name, 'w') as writer:
-              writer.write(''.join(graph_lines[1:]))
-              countDotFiles += 1
-          # extract the entry number.
-          entry_no = match.group(1)
-          if len(entry_no) < 6:
-            entry_no = '000000'[0:6-len(entry_no)] + entry_no
-          dot_file_name = f'{prefix}-{entry_no}.dot'
-          timestampMatch = timestampPattern.match(lines[i+1])
-          if timestampMatch:
-            testdate = timestampMatch.group(1).replace('__', '_0')
-            dtTestdate = dt.strptime(testdate, "%a_%b_%d_%H.%M.%S_%Y")
-            if dtFirstEntry > dtTestdate:
-              dtFirstEntry = dtTestdate
-            if dtLastEntry < dtTestdate:
-              dtLastEntry = dtTestdate
-            recordEntries = (dtBegin < dtTestdate) and (dtTestdate < dtEnd)
-          collect_lines = False
-          graph_lines = []
-          if verbose:
-            print(f'Entry: {entry_no} {timestampMatch.group(1)} {lines[i+2][:-1]}')
-      if recordEntries:
-        if 'schedule clear' in lines[i]:
-          collect_lines = False
-          script.write(f'# entry {entry_no}\n')
-          script.write(f'dm-cmd $DM $OPTIONS halt\n')
-          script.write(f'dm-sched $DM $OPTIONS clear\n')
-        elif 'schedule add' in lines[i]:
-          collect_lines = True
-          script.write(f'# entry {entry_no}\n')
-          script.write(f'dm-sched $DM $OPTIONS add {dot_file_name}\n')
-        elif 'schedule keep' in lines[i]:
-          collect_lines = True
-          script.write(f'# entry {entry_no}\n')
-          script.write(f'dm-sched $DM $OPTIONS keep {dot_file_name}\n')
-        elif 'schedule remove' in lines[i]:
-          collect_lines = True
-          script.write(f'# entry {entry_no}\n')
-          script.write(f'dm-sched $DM $OPTIONS remove {dot_file_name}\n')
-        elif 'schedule dump' in lines[i]:
-          collect_lines = True
-          script.write(f'# entry {entry_no}\n')
-          script.write(f'dm-sched $DM $OPTIONS status {dot_file_name}\n')
-        elif 'table check result:' in lines[i]:
-          collect_lines = False
-          script.write(f'# entry {entry_no}, {lines[i]}')
-        elif 'command execute' in lines[i]:
-          collect_lines = True
-          script.write(f'# entry {entry_no}\n')
-          script.write(f'dm-cmd $DM $OPTIONS -i {dot_file_name}\n')
-        elif 'is safe to remove' in lines[i]:
-          collect_lines = False
-          try:
-            pattern_name = re.search('is safe to remove: (.+)$', lines[i]).group(1)
+    with open(script_file, 'w') as script:
+      # Write header of script file
+      script.write(f'#! /bin/bash\n\n')
+      script.write(f'# Usage: {script_file} [options]\n')
+      script.write(f'#        where options are used for dm-cmd and dm-sched\n')
+      script.write(f'#        Data Master name is expected in $DM.\n')
+      script.write(f'# Example: ./commands.sh -v\n')
+      script.write(f'OPTIONS=$1\n')
+      for i in range(len(lines)):
+        match = pattern.match(lines[i])
+        if match:
+            # write graph_lines to dot file only if there is a graph.
+            # improvement: check for 'digraph' in the first line.
+            if recordEntries and int(entry_no) > -1 and len(graph_lines) > 2 and len(graph_lines[1]) > 6:
+              with open(dot_file_name, 'w') as writer:
+                writer.write(''.join(graph_lines[1:]))
+                countDotFiles += 1
+            # extract the entry number.
+            entry_no = match.group(1)
+            if len(entry_no) < 6:
+              entry_no = '000000'[0:6-len(entry_no)] + entry_no
+            dot_file_name = f'{prefix}-{entry_no}.dot'
+            timestampMatch = timestampPattern.match(lines[i+1])
+            if timestampMatch:
+              testdate = timestampMatch.group(1).replace('__', '_0')
+              dtTestdate = dt.strptime(testdate, "%a_%b_%d_%H.%M.%S_%Y")
+              if dtFirstEntry > dtTestdate:
+                dtFirstEntry = dtTestdate
+              if dtLastEntry < dtTestdate:
+                dtLastEntry = dtTestdate
+              recordEntries = (dtBegin < dtTestdate) and (dtTestdate < dtEnd)
+            collect_lines = False
+            graph_lines = []
+            if verbose:
+              print(f'Entry: {entry_no} {timestampMatch.group(1)} {lines[i+2][:-1]}')
+        if recordEntries:
+          if 'schedule clear' in lines[i]:
+            collect_lines = False
             script.write(f'# entry {entry_no}\n')
-            script.write(f'dm-cmd $DM $OPTIONS chkrem {pattern_name}\n')
-          except AttributeError:
-            print(f'Pattern name not found: {lines[i]}, entry {entry_no}')
-        elif 'result' in lines[i]:
-          collect_lines = False
-          script.write(f'# entry {entry_no}, {lines[i]}')
-        elif 'queue status for pattern' in lines[i]:
-          collect_lines = True
-          script.write(f'# entry {entry_no}\n')
-          script.write(f'# dm- $DM $OPTIONS  {dot_file_name}\n')
-        elif 'status report' in lines[i]:
-          collect_lines = True
-          script.write(f'# entry {entry_no}\n')
-          script.write(f'# dm- $DM $OPTIONS  {dot_file_name}\n')
-        elif 'static flush pattern' in lines[i]:
-          collect_lines = True
-          script.write(f'# entry {entry_no}\n')
-          script.write(f'# dm- $DM $OPTIONS  {dot_file_name}\n')
-        elif 'queue report for pattern' in lines[i]:
-          collect_lines = False
-          script.write(f'# entry {entry_no}, lines[i]')
-        elif 'set maintenance mode to true' in lines[i]:
-          collect_lines = True
-          script.write(f'# entry {entry_no}\n')
-          script.write(f'# dm- $DM $OPTIONS  {dot_file_name}\n')
-        elif 'set maintenance mode to false' in lines[i]:
-          collect_lines = True
-          script.write(f'# entry {entry_no}\n')
-          script.write(f'# dm- $DM $OPTIONS  {dot_file_name}\n')
-        elif 'set force schedule actions to true' in lines[i]:
-          collect_lines = True
-          script.write(f'# entry {entry_no}\n')
-          script.write(f'# dm- $DM $OPTIONS  {dot_file_name}\n')
-        elif 'set force schedule actions to false' in lines[i]:
-          collect_lines = True
-          script.write(f'# entry {entry_no}\n')
-          script.write(f'# dm- $DM $OPTIONS  {dot_file_name}\n')
-        elif 'set optimizeSafeToRemove to true' in lines[i]:
-          collect_lines = True
-          script.write(f'# entry {entry_no}\n')
-          script.write(f'# dm- $DM $OPTIONS  {dot_file_name}\n')
-        elif 'set optimizeSafeToRemove to false' in lines[i]:
-          collect_lines = True
-          script.write(f'# entry {entry_no}\n')
-          script.write(f'# dm- $DM $OPTIONS  {dot_file_name}\n')
-      if collect_lines:
-        graph_lines.append(lines[i])
+            script.write(f'dm-cmd $DM $OPTIONS halt\n')
+            script.write(f'dm-sched $DM $OPTIONS clear\n')
+          elif 'schedule add' in lines[i]:
+            collect_lines = True
+            script.write(f'# entry {entry_no}\n')
+            script.write(f'dm-sched $DM $OPTIONS add {dot_file_name}\n')
+          elif 'schedule keep' in lines[i]:
+            collect_lines = True
+            script.write(f'# entry {entry_no}\n')
+            script.write(f'dm-sched $DM $OPTIONS keep {dot_file_name}\n')
+          elif 'schedule remove' in lines[i]:
+            collect_lines = True
+            script.write(f'# entry {entry_no}\n')
+            script.write(f'dm-sched $DM $OPTIONS remove {dot_file_name}\n')
+          elif 'schedule dump' in lines[i]:
+            collect_lines = True
+            script.write(f'# entry {entry_no}\n')
+            script.write(f'dm-sched $DM $OPTIONS status {dot_file_name}\n')
+          elif 'table check result:' in lines[i]:
+            collect_lines = False
+            script.write(f'# entry {entry_no}, {lines[i]}')
+          elif 'command execute' in lines[i]:
+            collect_lines = True
+            script.write(f'# entry {entry_no}\n')
+            script.write(f'dm-cmd $DM $OPTIONS -i {dot_file_name}\n')
+          elif 'is safe to remove' in lines[i]:
+            collect_lines = False
+            try:
+              pattern_name = re.search('is safe to remove: (.+)$', lines[i]).group(1)
+              script.write(f'# entry {entry_no}\n')
+              script.write(f'dm-cmd $DM $OPTIONS chkrem {pattern_name}\n')
+            except AttributeError:
+              print(f'Pattern name not found: {lines[i]}, entry {entry_no}')
+          elif 'result' in lines[i]:
+            collect_lines = False
+            script.write(f'# entry {entry_no}, {lines[i]}')
+          elif 'queue status for pattern' in lines[i]:
+            collect_lines = True
+            script.write(f'# entry {entry_no}\n')
+            script.write(f'# dm- $DM $OPTIONS  {dot_file_name}\n')
+          elif 'status report' in lines[i]:
+            collect_lines = True
+            script.write(f'# entry {entry_no}\n')
+            script.write(f'# dm- $DM $OPTIONS  {dot_file_name}\n')
+          elif 'static flush pattern' in lines[i]:
+            collect_lines = True
+            script.write(f'# entry {entry_no}\n')
+            script.write(f'# dm- $DM $OPTIONS  {dot_file_name}\n')
+          elif 'queue report for pattern' in lines[i]:
+            collect_lines = False
+            script.write(f'# entry {entry_no}, lines[i]')
+          elif 'set maintenance mode to true' in lines[i]:
+            collect_lines = True
+            script.write(f'# entry {entry_no}\n')
+            script.write(f'# dm- $DM $OPTIONS  {dot_file_name}\n')
+          elif 'set maintenance mode to false' in lines[i]:
+            collect_lines = True
+            script.write(f'# entry {entry_no}\n')
+            script.write(f'# dm- $DM $OPTIONS  {dot_file_name}\n')
+          elif 'set force schedule actions to true' in lines[i]:
+            collect_lines = True
+            script.write(f'# entry {entry_no}\n')
+            script.write(f'# dm- $DM $OPTIONS  {dot_file_name}\n')
+          elif 'set force schedule actions to false' in lines[i]:
+            collect_lines = True
+            script.write(f'# entry {entry_no}\n')
+            script.write(f'# dm- $DM $OPTIONS  {dot_file_name}\n')
+          elif 'set optimizeSafeToRemove to true' in lines[i]:
+            collect_lines = True
+            script.write(f'# entry {entry_no}\n')
+            script.write(f'# dm- $DM $OPTIONS  {dot_file_name}\n')
+          elif 'set optimizeSafeToRemove to false' in lines[i]:
+            collect_lines = True
+            script.write(f'# entry {entry_no}\n')
+            script.write(f'# dm- $DM $OPTIONS  {dot_file_name}\n')
+        if collect_lines:
+          graph_lines.append(lines[i])
     print(f'Lines: {len(lines)}, dot files: {countDotFiles}, first entry: {dtFirstEntry}, last entry: {dtLastEntry}.')
     st = os.stat(script_file)
     os.chmod(script_file, st.st_mode | stat.S_IXUSR | stat.S_IXGRP | stat.S_IXOTH)
@@ -190,12 +191,10 @@ def convertTimestamp(dtString):
   except ValueError as v:
     print(v)
     return dt(1, 1, 1)
-  
-if __name__ == '__main__':
-  dtBegin = '1970-01-01'
-  dtEnd = '2100-01-01'
+
+def main(argv):
   parser = argparse.ArgumentParser("CommandHistory reads datamaster log files and generates a script and a series of dot files for the script.",
-  epilog="For questions and bugs contact Martin Skorsky (m.skorsky@gsi.de).")
+                                    epilog="For questions and bugs contact Martin Skorsky (m.skorsky@gsi.de).")
   parser.add_argument('--version', action='version', version='%(prog)s 1.0')
   parser.add_argument('--verbose', '-v', help='print each log entry')
   parser.add_argument('log_file_name', help='the name of the datamaster log file')
@@ -203,5 +202,8 @@ if __name__ == '__main__':
   parser.add_argument('begin_time', help='the time part of the first date to log')
   parser.add_argument('end_date', help='the last date to log')
   parser.add_argument('end_time', help='the time part of the last date to log')
-  args = parser.parse_args()
+  args = parser.parse_args(argv)
   extractScript(args.log_file_name, convertTimestamp(args.begin_date + " " + args.begin_time), convertTimestamp(args.end_date + " " + args.end_time), args.verbose)
+    
+if __name__ == '__main__':
+  main(argv=sys.argv[1:])
--- modules/ftm/analysis/test/testCommandsHistory.py
+++ modules/ftm/analysis/test/testCommandsHistory.py
@@ -1,8 +1,26 @@
 #! /usr/bin/env python3
 
-import unittest
-import subprocess
 import CommandsHistory as C
+import pathlib
+import shutil
+import subprocess
+import sys
+import unittest
+from io import StringIO 
+
+"""
+Capture stdout with a context manager in a list.
+Used to capture stdout during a method call.
+"""
+class Capturing(list):
+  def __enter__(self):
+    self._stdout = sys.stdout
+    sys.stdout = self._stringio = StringIO()
+    return self
+  def __exit__(self, *args):
+    self.extend(self._stringio.getvalue().splitlines())
+    del self._stringio    # free up some memory
+    sys.stdout = self._stdout
 
 """
 Usage:
@@ -10,6 +28,36 @@ python3 -m unittest testCommandsHistory.py -v
 Add folder with CommandsHistory.py to the PYTHONPATH.
 """
 class TestCommandsHistory(unittest.TestCase):
+
+  def deleteFile(self, file_name):
+    """Delete file <file_name>.
+    """
+    file_to_rem = pathlib.Path(file_name)
+    file_to_rem.unlink()
+
+  def test_CommandsHistoryX(self):
+    """Test CommandHistory.py with a log file and a time interval.
+    The interval starts before the log file starts and ends before the end of the log file.
+    This means that the first part of the log file is rewritten to a script.
+    The output (stdout, generated script) is compared with the expected outut.
+    """
+    history_file = 'GeneratorCommandsHistory_Fri_Feb__5_13.33.22_2021'
+    with Capturing() as stdOutLines:
+      C.main([history_file + '.log', '2021-02-04', '12:00', '2021-02-05', '13:50'])
+    self.assertEqual(stdOutLines, ['Rewrite command history file GeneratorCommandsHistory_Fri_Feb__5_13.33.22_2021.log to script CommandsHistory_Fri_Feb__5_13.33.22_2021.sh.', 
+      'Schedules written to CommandsHistory_Fri_Feb__5_13.33.22_2021/graph-entry-n.dot, where n is the entry number.', 
+      'Lines: 3416, dot files: 32, first entry: 2021-02-05 13:36:22, last entry: 2021-02-05 14:07:52.'])
+    lines = []
+    shell_file = history_file[9:] + '.sh'
+    with open(shell_file) as script:
+      lines = script.readlines()
+    expectedLines = []
+    with open('expected_' + shell_file) as expected:
+      expectedLines = expected.readlines()
+    self.assertEqual(lines, expectedLines)
+    self.deleteFile(shell_file)
+    shutil.rmtree(history_file[9:] + '/')
+
   def startAndGetSubprocessStdout(self, argumentsList, expectedReturnCode=-1, linesCout=-1, linesCerr=-1):
     """
     Common method to start a subprocess and check the return code.
