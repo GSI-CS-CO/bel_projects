8e97d6f4195c60af94d1a50988d22ad71f645dd1
Martin Skorsky <m.skorsky@gsi.de>
2021-03-19 16:50:00 +0100

    add more test schedules for safe2remove

--- modules/ftm/tests/common/dm_testbench.py
+++ modules/ftm/tests/common/dm_testbench.py
@@ -13,21 +13,25 @@ Module dm_testbench collects functions to handle patterns for the data master te
 """
 class DmTestbench(unittest.TestCase):
 
-  def startpattern(self, data_master, pattern_file):
+  def addSchedule(self, data_master, schedule_file):
+    """Connect to the given data master and load the schedule file (dot format).
+    The data master is halted, cleared, and statistics is reset.
     """
-    Connect to the given data master and load the pattern file (dot format).
+    self.startAllPattern(data_master, schedule_file, start=False)
+
+  def startPattern(self, data_master, schedule_file):
+    """Connect to the given data master and load the schedule file (dot format).
     The data master is halted, cleared, and statistics is reset.
     Search for the first pattern in the data master with 'dm-sched' and start it.
     """
-    self.startAllPattern(data_master, pattern_file, onePattern=True)
+    self.startAllPattern(data_master, schedule_file, onePattern=True)
 
-  def startAllPattern(self, data_master, pattern_file, onePattern=False):
-    """
-    Connect to the given data master and load the pattern file (dot format).
+  def startAllPattern(self, data_master, schedule_file, onePattern=False, start=True):
+    """Connect to the given data master and load the schedule file (dot format).
     The data master is halted, cleared, and statistics is reset.
     Search for all pattern in the data master with 'dm-sched' and start these.
     """
-    print (f"Connect to device '{data_master}', pattern file '{pattern_file}'.   ", end='', flush=True)
+    print (f"Connect to device '{data_master}', schedule file '{schedule_file}'.   ", end='', flush=True)
     process = subprocess.Popen(['dm-cmd', data_master, 'halt'])
     process.wait()
     self.assertEqual(process.returncode, 0, f'wrong return code {process.returncode}, Command line: dm-cmd {data_master} halt')
@@ -37,26 +41,27 @@ class DmTestbench(unittest.TestCase):
     process = subprocess.Popen(['dm-cmd', data_master, 'cleardiag'])
     process.wait()
     self.assertEqual(process.returncode, 0, f'wrong return code {process.returncode}, Command line: dm-cmd {data_master} cleardiag')
-    process = subprocess.Popen(['dm-sched', data_master, 'add', pattern_file])
+    process = subprocess.Popen(['dm-sched', data_master, 'add', schedule_file])
     process.wait()
-    self.assertEqual(process.returncode, 0, f'wrong return code {process.returncode}, Command line: dm-sched {data_master} add {pattern_file}')
-    # run 'dm-sched data_master' as a sub process.
-    process = subprocess.Popen(['dm-sched', data_master], stderr=subprocess.PIPE, stdout=subprocess.PIPE)
-    # get command output and error
-    stdout, stderr = process.communicate()
-    lines = stdout.decode('utf-8').splitlines()
-    # start the first pattern found in the data master.
-    patterns = False
-    for i in range(len(lines)):
-      if patterns and len(lines[i]) > 0:
-        # print (f"Pattern: \n{lines[i]} {lines}")
-        process = subprocess.Popen(['dm-cmd', data_master, 'startpattern', lines[i]])
-        process.wait()
-        self.assertEqual(process.returncode, 0, f'wrong return code {process.returncode}, Command line: dm-cmd {data_master} startpattern {lines[i]}')
-        if onePattern:
-          patterns = False
-      if lines[i] == 'Patterns:':
-        patterns = True
+    self.assertEqual(process.returncode, 0, f'wrong return code {process.returncode}, Command line: dm-sched {data_master} add {schedule_file}')
+    if start:
+      # run 'dm-sched data_master' as a sub process.
+      process = subprocess.Popen(['dm-sched', data_master], stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+      # get command output and error
+      stdout, stderr = process.communicate()
+      lines = stdout.decode('utf-8').splitlines()
+      # start the first pattern found in the data master.
+      patterns = False
+      for i in range(len(lines)):
+        if patterns and len(lines[i]) > 0:
+          # print (f"Pattern: \n{lines[i]} {lines}")
+          process = subprocess.Popen(['dm-cmd', data_master, 'startpattern', lines[i]])
+          process.wait()
+          self.assertEqual(process.returncode, 0, f'wrong return code {process.returncode}, Command line: dm-cmd {data_master} startpattern {lines[i]}')
+          if onePattern:
+            patterns = False
+        if lines[i] == 'Patterns:':
+          patterns = True
 
   def startAndCheckSubprocess(self, argumentsList, expectedReturnCode=-1, linesCout=-1, linesCerr=-1):
     """
@@ -97,27 +102,40 @@ class DmTestbench(unittest.TestCase):
       self.assertEqual(len(lines), linesCout, f'wrong stdout, expected {linesCout} lines, Command line: {argumentsList}\nstderr: {stderr.decode("utf-8").splitlines()}\nstdout: {lines}')
     return stdout.decode("utf-8").splitlines()
 
-  def compareExpectedResult(self, file_current, file_expected, exclude):
+  def removePaintedFlags(self, dot_lines):
+    """Remove flags and fillcolors of painted nodes from dot string (parameter dot_lines).
     """
-    Compare a file with a test result with an expected result contained in <file_expected>.
+    for index, line in enumerate(dot_lines):
+      dot_lines[index] = line.replace('flags="0x00000107"', 'flags="0x00000007"') \
+      .replace('flags="0x00002107"', 'flags="0x00002007"') \
+      .replace('flags="0x00108107"', 'flags="0x00108007"') \
+      .replace('flags="0x00708107"', 'flags="0x00708007"') \
+      .replace('flags="0x00100107"', 'flags="0x00100007"') \
+      .replace('fillcolor = "green"', 'fillcolor = "white"') \
+      .replace('flags="0x00020007"', 'flags="0x00000007"') \
+      .replace('flags="0x00022007"', 'flags="0x00002007"') \
+      .replace('flags="0x00120007"', 'flags="0x00100007"') \
+      .replace('flags="0x00128007"', 'flags="0x00108007"') \
+      .replace(', fontname="Times-Bold", fontcolor = "blue2", fontsize="16"', '')
+    return dot_lines
+
+  def compareExpectedResult(self, file_current, file_expected, exclude=''):
+    """Compare a file with a test result with an expected result contained in <file_expected>.
     Lines with the string <exclude> are removed from <file_current> before checking against <file_expected>.
     Assert that a unified diff has no lines.
     """
-    with open(file_current) as f_current:
+    with open(file_current, 'r') as f_current:
       current = f_current.readlines()
     if len(exclude) > 0:
       current = [ x for x in current if exclude not in x ]
-  #  print(f'current: {current}')
-    with open(file_expected) as f_expected:
+    current = self.removePaintedFlags(current)
+    with open(file_expected, 'r') as f_expected:
       expected = f_expected.readlines()
-    diffLines = list(difflib.unified_diff(current, expected))
-  #  print(f'Diff: {diffLines}')
-    self.assertEqual(len(diffLines), 0, f'Diff: {diffLines}')
+    diffLines = list(difflib.unified_diff(current, expected, n=0))
+    self.assertEqual(len(diffLines), 0, f'Diff: file {file_expected}\n{diffLines}')
 
   def snoopToCsv(self, csv_file_name, duration=1.0):
-    """
-    Snoop timing messages with saft-ctl for <duration> seconds (default = 1.0).
-    Write the messages to <csv_file_name>.
+    """Snoop timing messages with saft-ctl for <duration> seconds (default = 1.0) and write the messages to <csv_file_name>.
     Details: start saft-ctl with Popen, wait for <duration> seconds.
     When the TimeoutExpired exception pops up, kill saft-ctl and wait for at most 5 seconds.
     At least assert the return code -9 of saft-ctl.
@@ -179,9 +197,10 @@ class DmTestbench(unittest.TestCase):
           print(f'{key:>20s}: {value:6d} {value/timeSpan:9.3f}Hz')
         print(f'{"All":>20s}: {line_count:6d} {line_count/timeSpan: >9.3f}Hz, time span: {timeSpan:0.6f}sec')
 
-  def deleteFile(self, file_name):
+  def deleteFile(self, fileName):
     """
-    Delete file <file_name>.
+    Delete file <fileName>.
     """
-    file_to_rem = pathlib.Path(file_name)
-    file_to_rem.unlink()
+    fileToRemove = pathlib.Path(fileName)
+    if fileToRemove.exists():
+      fileToRemove.unlink()
--- modules/ftm/tests/safe2remove/unitTestSafe2Remove.py
+++ modules/ftm/tests/safe2remove/unitTestSafe2Remove.py
@@ -5,12 +5,8 @@ import sys
 import pathlib
 import dm_testbench
 import subprocess
-#import time
-#import signal
-#import csv
-#import contextlib
-import datetime
-#import collections
+from datetime import datetime as dt
+from datetime import timedelta as delta
 
 """
 Class collects unit tests for safe2remove.
@@ -34,6 +30,10 @@ class UnitTestSafe2Remove(dm_testbench.DmTestbench):
     """
     self.datamaster = test_datamaster
 
+  def tearDown(self):
+    self.deleteFile('debug.dot')
+    self.deleteFile('status.dot')
+
   def safe2removeTestcase(self, dot_file1, pattern_to_remove):
     self.startAllPattern(self.datamaster, dot_file1 + '.dot')
     self.startAndCheckSubprocess(('dm-cmd', self.datamaster, 'chkrem', pattern_to_remove), expectedReturnCode=0)
@@ -42,40 +42,66 @@ class UnitTestSafe2Remove(dm_testbench.DmTestbench):
     self.startAndCheckSubprocess(('dm-cmd', self.datamaster, 'abortpattern', pattern_to_remove), expectedReturnCode=0)
     self.startAndCheckSubprocess(('dm-cmd', self.datamaster, 'chkrem', pattern_to_remove), expectedReturnCode=0)
     self.compareExpectedResult('debug.dot', dot_file1 + '-safe.dot', 'Created')
-    self.deleteFile('debug.dot')
     self.startAndCheckSubprocess(('dm-sched', self.datamaster, 'remove', dot_file1 + '-remove.dot'), expectedReturnCode=0)
-    self.startAndCheckSubprocess(('dm-sched', self.datamaster, 'status', '-ostatus.dot'), expectedReturnCode=0)
-    self.compareExpectedResult('status.dot', dot_file1 + '-status.dot', '')
-    self.deleteFile('status.dot')
+    self.startAndCheckSubprocess(('dm-sched', self.datamaster, 'status', '-o', 'status.dot'), expectedReturnCode=0)
+    self.compareExpectedResult('status.dot', dot_file1 + '-status.dot')
 
-  def t1est_safe2remove_blockalign1(self):
+  def test_safe2remove_blockalign1(self):
     self.safe2removeTestcase('blockalign1', 'PPS1_TEST')
-    
-  def doBranch1(self):
-    output = self.startAndGetSubprocessStdout(('dm-cmd', self.datamaster, 'deadline'), expectedReturnCode=0)
-    print()
-    print(f'Result deadline: {output}')
-    microseconds = float(output[0][21:-3])
-    deadlineTime = datetime.datetime.fromtimestamp(microseconds/1000000.0)
-    print(f'{output[0][21:-3]}, {deadlineTime}')
-    deadlineTime1 = datetime.datetime.fromtimestamp(microseconds/1000000.0 + 1.0)
-    print(f'{output[0][21:-3]}, {deadlineTime1}')
-    offsetTime = (deadlineTime1-datetime.datetime(1970,1,1)).total_seconds()
-    offsetNanoseconds = offsetTime * 1000000000
-    offsetNanosecondsStr = "{:.0f}".format(offsetNanoseconds)
-    print(f'{offsetTime}, {offsetNanoseconds} {offsetNanoseconds:.0f}, {offsetNanosecondsStr}')
-    self.startAndCheckSubprocess(('dm-cmd', self.datamaster, 'flow', '-q', '10', '-a', '-l', offsetNanosecondsStr, 'Block1', 'B1'), expectedReturnCode=0)
-    self.startAndCheckSubprocess(('dm-cmd', self.datamaster, 'flow', '-q', '10', '-a', '-l', offsetNanosecondsStr, 'Block1', 'C1'), expectedReturnCode=0)
-  
-  def test_branch1(self):
-    self.startAllPattern(self.datamaster, 'branch1.dot')
-    file_name = 'snoop_branch1.csv'
-    parameter_column = 20
-    self.snoopToCsvWithAction(file_name, self.doBranch1, 2)
-    self.analyseFrequencyFromCsv(file_name, parameter_column)
-    self.deleteFile(file_name)
 
-  def t1est_safe2remove_blockflow1(self):
+  def safe2removeTestcasePerformance(self, dot_file1, limit):
+    start = dt.now()
+    self.safe2removeTestcase(dot_file1, 'G1_P1')
+    self.startAndCheckSubprocess(('dm-sched', self.datamaster, 'add', 'g1_p1_update_schedule.dot'), expectedReturnCode=0)
+    duration = dt.now() - start
+    self.assertTrue(duration <= limit, f'Duration of test too long, duration: {duration}, limit: {limit}.')
+
+  def test_safe2remove_group_1_1_1(self):
+    self.safe2removeTestcasePerformance('groups_1_nonDefaultPatterns_1_blocksPerPattern_1', delta(seconds=1))
+
+  def test_safe2remove_group_1_2_1(self):
+    self.safe2removeTestcasePerformance('groups_1_nonDefaultPatterns_2_blocksPerPattern_1', delta(seconds=1))
+
+  def test_safe2remove_group_1_4_1(self):
+    self.safe2removeTestcasePerformance('groups_1_nonDefaultPatterns_4_blocksPerPattern_1', delta(seconds=1.5))
+
+  def test_safe2remove_group_1_9_1(self):
+    self.safe2removeTestcasePerformance('groups_1_nonDefaultPatterns_9_blocksPerPattern_1', delta(seconds=2.6))
+
+  def test_safe2remove_group_1_9_10(self):
+    self.safe2removeTestcasePerformance('groups_1_nonDefaultPatterns_9_blocksPerPattern_10', delta(seconds=3.1))
+
+  def test_safe2remove_group_1_9_150(self):
+    self.safe2removeTestcasePerformance('groups_1_nonDefaultPatterns_9_blocksPerPattern_150', delta(seconds=12))
+
+  def test_safe2remove_group_2_9_1(self):
+    self.safe2removeTestcasePerformance('groups_2_nonDefaultPatterns_9_blocksPerPattern_1', delta(seconds=5.5))
+
+  def test_safe2remove_group_2_9_10(self):
+    self.safe2removeTestcasePerformance('groups_2_nonDefaultPatterns_9_blocksPerPattern_10', delta(seconds=7))
+
+  def test_safe2remove_group_2_9_150(self):
+    self.safe2removeTestcasePerformance('groups_2_nonDefaultPatterns_9_blocksPerPattern_150', delta(seconds=30))
+
+  def test_safe2remove_group_3_9_1(self):
+    self.safe2removeTestcasePerformance('groups_3_nonDefaultPatterns_9_blocksPerPattern_1', delta(seconds=9))
+
+  def test_safe2remove_group_3_9_10(self):
+    self.safe2removeTestcasePerformance('groups_3_nonDefaultPatterns_9_blocksPerPattern_10', delta(seconds=12))
+
+  def test_safe2remove_group_3_9_150(self):
+    self.safe2removeTestcasePerformance('groups_3_nonDefaultPatterns_9_blocksPerPattern_150', delta(seconds=60))
+
+  def test_safe2remove_group_4_9_1(self):
+    self.safe2removeTestcasePerformance('groups_4_nonDefaultPatterns_9_blocksPerPattern_1', delta(seconds=15))
+
+  def test_safe2remove_group_4_9_10(self):
+    self.safe2removeTestcasePerformance('groups_4_nonDefaultPatterns_9_blocksPerPattern_10', delta(seconds=20))
+
+  def test_safe2remove_group_4_9_150(self):
+    self.safe2removeTestcasePerformance('groups_4_nonDefaultPatterns_9_blocksPerPattern_150', delta(seconds=88))
+
+  def test_safe2remove_blockflow1(self):
     self.startAllPattern(self.datamaster, 'block-flow1.dot')
     file_name = 'snoop_block-flow1.csv'
     parameter_column = 20
