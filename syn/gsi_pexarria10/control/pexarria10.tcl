c4c0f29c8fad21ce04c392d441f89627a7a9268f
A. Hahn <a.hahn@gsi.de>
2021-03-19 15:27:44 +0100

    pexarria10: tcl update

--- syn/gsi_pexarria10/control/pexarria10.tcl
+++ syn/gsi_pexarria10/control/pexarria10.tcl
@@ -5,4 +5,4 @@ source ../../../modules/pll/arria10_pex10/arria10_pex10_pll.tcl
 source ../../../ip_cores/general-cores/platform/altera/wb_pcie/arria10gx_pex10.tcl
 source ../../../ip_cores/general-cores/platform/altera/networks/arria10gx/arria10gx.tcl
 source ../../../ip_cores/wr-cores/platform/altera/wr_arria10_phy/wr_arria10_pex10_phy/wr_arria10_pex10_phy.tcl
-
+source ../../../modules/lvds/arria10_pex10/arria10_pex10_lvds.tcl
