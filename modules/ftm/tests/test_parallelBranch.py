e4a2e5619cc1410445319f4f0e8b11596cdb8ad6
Martin Skorsky <m.skorsky@gsi.de>
2021-06-04 16:11:15 +0200

    test_parallelBranch.py: snoop for 2 seconds (which is minimal more than a second). Print deadlines after flow commands.

--- modules/ftm/tests/test_parallelBranch.py
+++ modules/ftm/tests/test_parallelBranch.py
@@ -11,27 +11,29 @@ class UnitTestParallelBranch(dm_testbench.DmTestbench):
 
   def doBranch1(self):
     output = self.startAndGetSubprocessStdout(['dm-cmd', self.datamaster, 'deadline'])
-    offsetNanosecondsStr1 = "{:0d}".format(int(output[0][21:]) + 500000000)
-    offsetNanosecondsStr2 = "{:0d}".format(int(output[0][21:]) + 700000000)
-    print()
-    print(f'{offsetNanosecondsStr1} {offsetNanosecondsStr2}')
-    print(f'{dt.fromtimestamp((int(output[0][21:]) + 500000000)/1000000000)} {dt.fromtimestamp((int(output[0][21:]) + 700000000)/1000000000)}')
-    print(f'{dt.utcfromtimestamp((int(output[0][21:]) + 500000000)/1000000000)} {dt.utcfromtimestamp((int(output[0][21:]) + 700000000)/1000000000)}')
+    # add 0.5 seconds (500.000.000ns) to the deadline
+    iDeadline = int(output[0][21:])
+    offsetNanosecondsStr1 = "{:0d}".format(iDeadline + 500000000)
+    # add 0.7 seconds (700.000.000ns) to the deadline
+    offsetNanosecondsStr2 = "{:0d}".format(iDeadline + 700000000)
     self.startAndCheckSubprocess(('dm-cmd', self.datamaster, 'flow', '-q', '1', '-a', '-l', offsetNanosecondsStr1, 'BlockA', 'A2'))
     self.startAndCheckSubprocess(('dm-cmd', self.datamaster, 'flow', '-q', '1', '-a', '-l', offsetNanosecondsStr1, 'BlockB', 'B2'))
     self.startAndCheckSubprocess(('dm-cmd', self.datamaster, 'flow', '-q', '1', '-a', '-l', offsetNanosecondsStr2, 'BlockA', 'A3'))
     self.startAndCheckSubprocess(('dm-cmd', self.datamaster, 'flow', '-q', '1', '-a', '-l', offsetNanosecondsStr2, 'BlockB', 'B3'))
+    print(f'\nUTC deadline: {dt.utcfromtimestamp(int(output[0][21:])/1000000000)}, \
+      UTC deadline + 0.5: {dt.utcfromtimestamp((int(output[0][21:]) + 500000000)/1000000000)}, \
+      UTC deadline + 0.7:  {dt.utcfromtimestamp((int(output[0][21:]) + 700000000)/1000000000)}')
 
   def test_branch1(self):
     self.startAllPattern(self.datamaster, 'branch1.dot')
     file_name = 'snoop_branch1.csv'
     parameter_column = 20
-    self.snoopToCsvWithAction(file_name, self.doBranch1, 1)
+    self.snoopToCsvWithAction(file_name, self.doBranch1, 2)
     self.analyseFrequencyFromCsv(file_name, parameter_column)
-    print(self.startAndGetSubprocessStdout(('grep', '-nHm', '1', 'a1', file_name)))
-    print(self.startAndGetSubprocessStdout(('grep', '-nHm', '1', 'b1', file_name)))
-    print(self.startAndGetSubprocessStdout(('grep', '-nH', 'a2', file_name)))
-    print(self.startAndGetSubprocessStdout(('grep', '-nH', 'b2', file_name)))
-    print(self.startAndGetSubprocessStdout(('grep', '-nH', 'a3', file_name)))
-    print(self.startAndGetSubprocessStdout(('grep', '-nH', 'b3', file_name)))
+    print(self.startAndGetSubprocessStdout(('grep', '-nHm', '1', 'a1', file_name), [0]))
+    print(self.startAndGetSubprocessStdout(('grep', '-nHm', '1', 'b1', file_name), [0]))
+    print(self.startAndGetSubprocessStdout(('grep', '-nH', 'a2', file_name), [0]))
+    print(self.startAndGetSubprocessStdout(('grep', '-nH', 'b2', file_name), [0]))
+    print(self.startAndGetSubprocessStdout(('grep', '-nH', 'a3', file_name), [0]))
+    print(self.startAndGetSubprocessStdout(('grep', '-nH', 'b3', file_name), [0]))
     self.deleteFile(file_name)
