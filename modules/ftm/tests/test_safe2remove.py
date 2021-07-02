0c45eff5b7053fe48208884ecd96c32ca0dfa630
Martin Skorsky <m.skorsky@gsi.de>
2021-07-02 12:09:38 +0200

    test_safe2remove.py: use longer duration for some long running performance tests

--- modules/ftm/tests/test_safe2remove.py
+++ modules/ftm/tests/test_safe2remove.py
@@ -51,7 +51,7 @@ class UnitTestSafe2Remove(dm_testbench.DmTestbench):
 
   @pytest.mark.slow
   def test_safe2remove_group_1_4_1(self):
-    self.safe2removeTestcasePerformance('groups_1_nonDefaultPatterns_4_blocksPerPattern_1', delta(seconds=1.5))
+    self.safe2removeTestcasePerformance('groups_1_nonDefaultPatterns_4_blocksPerPattern_1', delta(seconds=1.6))
 
   @pytest.mark.slow
   def test_safe2remove_group_1_9_1(self):
@@ -59,11 +59,11 @@ class UnitTestSafe2Remove(dm_testbench.DmTestbench):
 
   @pytest.mark.slow
   def test_safe2remove_group_1_9_10(self):
-    self.safe2removeTestcasePerformance('groups_1_nonDefaultPatterns_9_blocksPerPattern_10', delta(seconds=3.1))
+    self.safe2removeTestcasePerformance('groups_1_nonDefaultPatterns_9_blocksPerPattern_10', delta(seconds=3.3))
 
   @pytest.mark.slow
   def test_safe2remove_group_1_9_150(self):
-    self.safe2removeTestcasePerformance('groups_1_nonDefaultPatterns_9_blocksPerPattern_150', delta(seconds=12))
+    self.safe2removeTestcasePerformance('groups_1_nonDefaultPatterns_9_blocksPerPattern_150', delta(seconds=12.3))
 
   @pytest.mark.slow
   def test_safe2remove_group_2_9_1(self):
@@ -75,7 +75,7 @@ class UnitTestSafe2Remove(dm_testbench.DmTestbench):
 
   @pytest.mark.slow
   def test_safe2remove_group_2_9_150(self):
-    self.safe2removeTestcasePerformance('groups_2_nonDefaultPatterns_9_blocksPerPattern_150', delta(seconds=30))
+    self.safe2removeTestcasePerformance('groups_2_nonDefaultPatterns_9_blocksPerPattern_150', delta(seconds=32))
 
   @pytest.mark.slow
   def test_safe2remove_group_3_9_1(self):
@@ -87,7 +87,7 @@ class UnitTestSafe2Remove(dm_testbench.DmTestbench):
 
   @pytest.mark.slow
   def test_safe2remove_group_3_9_150(self):
-    self.safe2removeTestcasePerformance('groups_3_nonDefaultPatterns_9_blocksPerPattern_150', delta(seconds=60))
+    self.safe2removeTestcasePerformance('groups_3_nonDefaultPatterns_9_blocksPerPattern_150', delta(seconds=61))
 
   @pytest.mark.slow
   def test_safe2remove_group_4_9_1(self):
@@ -99,7 +99,7 @@ class UnitTestSafe2Remove(dm_testbench.DmTestbench):
 
   @pytest.mark.slow
   def test_safe2remove_group_4_9_150(self):
-    self.safe2removeTestcasePerformance('groups_4_nonDefaultPatterns_9_blocksPerPattern_150', delta(seconds=88))
+    self.safe2removeTestcasePerformance('groups_4_nonDefaultPatterns_9_blocksPerPattern_150', delta(seconds=95))
 
   def test_safe2remove_blockflow1(self):
     self.startAllPattern(self.datamaster, 'block-flow1.dot')
