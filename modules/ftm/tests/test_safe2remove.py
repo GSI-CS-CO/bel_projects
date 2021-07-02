48bb6b39604e7e3e77635b3b9483b2675e508449
Martin Skorsky <m.skorsky@gsi.de>
2021-07-02 13:13:24 +0200

    test_safe2remove.py: use longer duration for some long running performance tests, 2. try

--- modules/ftm/tests/test_safe2remove.py
+++ modules/ftm/tests/test_safe2remove.py
@@ -55,7 +55,7 @@ class UnitTestSafe2Remove(dm_testbench.DmTestbench):
 
   @pytest.mark.slow
   def test_safe2remove_group_1_9_1(self):
-    self.safe2removeTestcasePerformance('groups_1_nonDefaultPatterns_9_blocksPerPattern_1', delta(seconds=2.6))
+    self.safe2removeTestcasePerformance('groups_1_nonDefaultPatterns_9_blocksPerPattern_1', delta(seconds=2.8))
 
   @pytest.mark.slow
   def test_safe2remove_group_1_9_10(self):
