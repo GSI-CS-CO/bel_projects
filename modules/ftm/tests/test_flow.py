8f05bef84313441153421cfa8da11d4c22b550ae
Martin Skorsky <m.skorsky@gsi.de>
2021-08-05 13:33:24 +0200

    test_flow.py: add description of test cases.

--- modules/ftm/tests/test_flow.py
+++ modules/ftm/tests/test_flow.py
@@ -8,10 +8,19 @@ ToDo: extract
 class UnitTestFlow(dm_testbench.DmTestbench):
 
   def test_dynamic_branch_single_tvalid_rel_0(self):
+    """Load the schedule dynamic-branch-single-schedule.dot and start pattern IN_C0.
+    Check that nodes BLOCK_A and BLOCK_IN0 are visited.
+    Execute a flowpattern command, which is immediatley valid, to change flow from pattern A to B in block BLOCK_IN0.
+    Wait for 0.4 seconds. Check the queues of BLOCK_IN0, the low prio queue should contain the flow command.
+    Start pattern IN_C0. Check that all three nodes are visited.
+    Check the queues of BLOCK_IN0, the low prio queue should contain the executed flow command.
+    """
     self.startPattern(self.datamaster, 'dynamic-branch-single-schedule.dot', 'IN_C0')
     stdoutLines = self.startAndGetSubprocessStdout((self.binary_dm_sched, self.datamaster, 'rawvisited'))
     self.compareExpectedOutput(stdoutLines, self.schedules_folder + 'dynamic-branch-single-expected-0-0.txt')
     self.startAndCheckSubprocess((self.binary_dm_cmd, self.datamaster, 'flowpattern', 'IN_C0', 'B'))
+    # ~ does not work with a delay of 0.0 seconds
+    self.delay(0.4)
     stdoutLines = self.startAndGetSubprocessStdout((self.binary_dm_cmd, self.datamaster, 'rawqueue', 'BLOCK_IN0'))
     self.compareExpectedOutput(stdoutLines, self.schedules_folder + 'dynamic-branch-single-expected-1-1.txt', delete=[6])
     self.startAndCheckSubprocess((self.binary_dm_cmd, self.datamaster, 'startpattern', 'IN_C0'))
@@ -21,11 +30,19 @@ class UnitTestFlow(dm_testbench.DmTestbench):
     self.compareExpectedOutput(stdoutLines, self.schedules_folder + 'dynamic-branch-single-expected-1-4.txt', delete=[24])
 
   def test_dynamic_branch_single_tvalid_rel_1(self):
+    """Load the schedule dynamic-branch-single-schedule.dot and start pattern IN_C0.
+    Check that nodes BLOCK_A and BLOCK_IN0 are visited.
+    Execute a flowpattern command, which is valid after 1 second, to change flow from pattern A to B in block BLOCK_IN0.
+    Wait for 1.4 seconds. Check the queues of BLOCK_IN0, the low prio queue should contain the flow command.
+    Start pattern IN_C0. Check that all three nodes are visited.
+    Check the queues of BLOCK_IN0, the low prio queue should contain the executed flow command.
+    """
     self.startPattern(self.datamaster, 'dynamic-branch-single-schedule.dot', 'IN_C0')
     stdoutLines = self.startAndGetSubprocessStdout((self.binary_dm_sched, self.datamaster, 'rawvisited'))
     self.compareExpectedOutput(stdoutLines, self.schedules_folder + 'dynamic-branch-single-expected-0-0.txt')
     self.startAndCheckSubprocess((self.binary_dm_cmd, self.datamaster, 'flowpattern', 'IN_C0', 'B', '-l', '1000000000'))
-    self.delay(1)
+    # ~ does not work with a delay of 1.0 seconds
+    self.delay(1.4)
     stdoutLines = self.startAndGetSubprocessStdout((self.binary_dm_cmd, self.datamaster, 'rawqueue', 'BLOCK_IN0'))
     self.compareExpectedOutput(stdoutLines, self.schedules_folder + 'dynamic-branch-single-expected-1-1.txt', delete=[6])
     self.startAndCheckSubprocess((self.binary_dm_cmd, self.datamaster, 'startpattern', 'IN_C0'))
@@ -35,6 +52,13 @@ class UnitTestFlow(dm_testbench.DmTestbench):
     self.compareExpectedOutput(stdoutLines, self.schedules_folder + 'dynamic-branch-single-expected-1-4.txt', delete=[24])
 
   def test_dynamic_branch_single_tvalid_abs_0(self):
+    """Load the schedule dynamic-branch-single-schedule.dot and start pattern IN_C0.
+    Check that nodes BLOCK_A and BLOCK_IN0 are visited.
+    Execute a flowpattern command, which is valid at an absolute time, to change flow from pattern A to B in block BLOCK_IN0.
+    Wait for 1.4 seconds. Check the queues of BLOCK_IN0, the low prio queue should contain the flow command.
+    Start pattern IN_C0. Check that all three nodes are visited.
+    Check the queues of BLOCK_IN0, the low prio queue should contain the executed flow command.
+    """
     self.startPattern(self.datamaster, 'dynamic-branch-single-schedule.dot', 'IN_C0')
     stdoutLines = self.startAndGetSubprocessStdout((self.binary_dm_sched, self.datamaster, 'rawvisited'))
     self.compareExpectedOutput(stdoutLines, self.schedules_folder + 'dynamic-branch-single-expected-0-0.txt')
@@ -48,6 +72,13 @@ class UnitTestFlow(dm_testbench.DmTestbench):
     self.compareExpectedOutput(stdoutLines, self.schedules_folder + 'dynamic-branch-single-expected-1-4.txt', delete=[24])
 
   def test_dynamic_branch_single_tvalid_abs_1(self):
+    """Load the schedule dynamic-branch-single-schedule.dot and start pattern IN_C0.
+    Check that nodes BLOCK_A and BLOCK_IN0 are visited.
+    Execute a flowpattern command, which is valid at an absolute time (1 second from now), to change flow from pattern A to B in block BLOCK_IN0.
+    Wait for 1.4 seconds. Check the queues of BLOCK_IN0, the low prio queue should contain the flow command.
+    Start pattern IN_C0. Check that all three nodes are visited.
+    Check the queues of BLOCK_IN0, the low prio queue should contain the executed flow command.
+    """
     self.startPattern(self.datamaster, 'dynamic-branch-single-schedule.dot', 'IN_C0')
     stdoutLines = self.startAndGetSubprocessStdout((self.binary_dm_sched, self.datamaster, 'rawvisited'))
     self.compareExpectedOutput(stdoutLines, self.schedules_folder + 'dynamic-branch-single-expected-0-0.txt')
