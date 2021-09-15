06507d1c1c5598aae434cf420fa1fd23b91d2e5f
A. Hahn <a.hahn@gsi.de>
2021-09-15 16:57:29 +0200

    lvds: added ftm10 Manifest.py

---
+++ modules/lvds/arria10_ftm10/Manifest.py
@@ -0,0 +1,7 @@
+files = [
+  "arria10_ftm10_lvds_ibuf/arria10_ftm10_lvds_ibuf.qsys",
+  "arria10_ftm10_lvds_obuf/arria10_ftm10_lvds_obuf.qsys",
+  "arria10_ftm10_lvds_pll/arria10_ftm10_lvds_pll.qsys",
+  "arria10_ftm10_lvds_rx/arria10_ftm10_lvds_rx.qsys",
+  "arria10_ftm10_lvds_tx/arria10_ftm10_lvds_tx.qsys"
+  ]
