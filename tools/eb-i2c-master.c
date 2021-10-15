240e426cff5766538e6874f81032dcdc8f9a67f3
A. Hahn <a.hahn@gsi.de>
2021-10-15 16:41:05 +0200

    eb-i2c-master: improved stub

--- tools/eb-i2c-master.c
+++ tools/eb-i2c-master.c
@@ -20,6 +20,7 @@
 /* Globals Variables */
 /* ==================================================================================================== */
 const char *program;
+const char *device_name;
 
 /* Main */
 /* ==================================================================================================== */
@@ -27,16 +28,25 @@ int main (int argc, char** argv)
 {
   /* Helpers */
   int opt;
-  struct sdb_device dev;
+  struct sdb_device sdb;
   eb_status_t status;
   eb_socket_t socket;
   eb_device_t device;
+  eb_data_t data;
   int verbose = 0;
   int error = 0;
-  int found_devices = 0;
+  int devices = 1;
+  int base = 0;
+  int current_base = 0;
 
-  /* Default arguments */
+  /* Check argument counter */
+  if (argc < 2)
+  {
+    printf("%s: Missing arguments (got only %i)\n", program, argc);
+    return 1;
+  }
   program = argv[0];
+  device_name = argv[1];
 
   /* Process the command-line arguments */
   while ((opt = getopt(argc, argv, "hv")) != -1)
@@ -48,19 +58,11 @@ int main (int argc, char** argv)
         return 0;
       case 'v':
         verbose = 1;
-        printf("%s: Found %i arguments\n", program, argc);
         break;
       default:
         printf("%s: Bad getopt result\n", program);
         error = 1;
     }
-  }
-
-  /* Check argument counter */
-  if (argc < 2)
-  {
-    printf("%s: Missing arguments (got only %i)\n", program, argc);
-    error = 1;
   }
 
   /* Exit on error(s) */
@@ -73,21 +75,31 @@ int main (int argc, char** argv)
     return 1;
   }
 
-  if ((status = eb_device_open(socket, argv[1], EB_ADDRX|EB_DATAX, 3, &device)) != EB_OK)
+  if ((status = eb_device_open(socket, device_name, EB_ADDRX|EB_DATAX, 3, &device)) != EB_OK)
   {
-    printf("%s: Failed to open device %s\n", program, argv[1]);
+    printf("%s: Failed to open device %s\n", program, device_name);
     return 1;
   }
 
-  if ((status = eb_sdb_find_by_identity(device, I2C_WRAP_GSI_ID, I2C_WRAP_ID, &dev, &found_devices)) != EB_OK)
+  if ((status = eb_sdb_find_by_identity(device, I2C_WRAP_GSI_ID, I2C_WRAP_ID, &sdb, &devices)) != EB_OK)
   {
     printf("%s: Failed find I2C master\n", program);
     return 1;
   }
+  base = (uint32_t) sdb.sdb_component.addr_first;
 
   if (verbose)
   {
-    printf("%s: Found I2C master at 0x%lx\n", program, dev.sdb_component.addr_first);
+    printf("%s: Found I2C master at 0x%x (start)\n", program, (uint32_t) sdb.sdb_component.addr_first);
+    printf("%s: Found I2C master at 0x%x (end)\n", program, (uint32_t) sdb.sdb_component.addr_last);
+  }
+
+  /* Enable core */
+  if (verbose)
+  {
+    current_base = base+(OC_I2C_CTR<<2);
+    eb_device_read(device, (eb_address_t)(current_base), EB_DATA32, &data, 0, NULL);
+    printf("%s: Core status: 0x%x (0x%x)\n", program, (uint32_t) data, current_base);
   }
 
   /* Done */
