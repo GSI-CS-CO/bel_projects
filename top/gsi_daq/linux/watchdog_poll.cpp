70a6262f804c80659069b2ee9eea0e2e3c03dda9
Ulrich Becker <>
2021-11-02 19:11:17 +0100

    Initial of watchdog_poll

---
+++ top/gsi_daq/linux/watchdog_poll.cpp
@@ -0,0 +1,30 @@
+/*!
+ *  @file watchdog_poll.cpp
+ *  @brief Simple watchdog timer for polling routines.
+ *
+ *  @date 02.11.2021
+ *  @copyright (C) 2021 GSI Helmholtz Centre for Heavy Ion Research GmbH
+ *
+ *  @author Ulrich Becker <u.becker@gsi.de>
+ *
+ ******************************************************************************
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library. If not, see <http://www.gnu.org/licenses/>
+ ******************************************************************************
+ */
+#include <daq_calculations.hpp>
+#include "watchdog_poll.hpp"
+
+using namespace Scu;
+
+//================================== EOF ======================================
---
+++ top/gsi_daq/linux/watchdog_poll.hpp
@@ -0,0 +1,70 @@
+/*!
+ *  @file watchdog_poll.hpp
+ *  @brief Simple watchdog timer for polling routines.
+ *
+ *  @date 02.11.2021
+ *  @copyright (C) 2021 GSI Helmholtz Centre for Heavy Ion Research GmbH
+ *
+ *  @author Ulrich Becker <u.becker@gsi.de>
+ *
+ ******************************************************************************
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library. If not, see <http://www.gnu.org/licenses/>
+ ******************************************************************************
+ */
+#ifndef _WATCHDOG_POLL_HPP
+#define _WATCHDOG_POLL_HPP
+
+#include <stdint.h>
+
+namespace Scu
+{
+
+///////////////////////////////////////////////////////////////////////////////
+/*!
+ * @brief Very simple watchdog timer for including in polling loops.
+ */
+class Watchdog
+{
+   uint64_t  m_timeout;
+   uint64_t  m_currentLimit;
+public:
+
+   Watchdog( const uint64_t timeout = 0 ):
+      m_timeout( timeout ),
+      m_currentLimit( 0 )
+   {}
+   
+   ~Watchdog( void ) {}
+
+   /*!
+    * @brief Starts the watchdog timer.
+    *
+    * This function has to be in the initialization part outside
+    * of the polling loop.
+    */
+   void start( const uint64_t timeout = 0 );
+
+   /*!
+    * @brief Returns true if the watchdog is barking, that means when the
+    *        timeout has been occurred.
+    * 
+    * This Function has to be periodically invoked within a polling loop.
+    */
+   bool isBarking( void );
+
+};
+
+} // namespace Scu
+#endif // ifdef _WATCHDOG_POLL_HPP
+//================================== EOF ======================================
