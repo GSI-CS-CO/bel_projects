365ba080e9dfe6c7637a32872bc440658f5e66b7
Dietrich Beck <d.beck@gsi.de>
2021-11-15 17:11:51 +0100

    b2b: fixed a broken startup file on asl

--- modules/b2b/asl/b2b-pro-sis18-rt1-kick.sh
+++ modules/b2b/asl/b2b-pro-sis18-rt1-kick.sh
@@ -14,7 +14,7 @@ log 'copying software and startup script to ramdisk'
 cp -a /opt/$NAME/$ARCH/usr/lib/* /usr/lib/
 ldconfig
 cp -a /opt/$NAME/$ARCH/usr/bin/b2b-ctl /usr/bin/
-cp -a /opt/$NAME/$ARCH/usr/bin/b2b-sis18-pro-rt1-kick_start.sh /usr/bin/
+cp -a /opt/$NAME/$ARCH/usr/bin/b2b-pro-sis18-rt1-kick_start.sh /usr/bin/
 
 log 'copying firmware to ramdisk'
 cp -a /opt/$NAME/firmware/* /
