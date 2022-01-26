218adcebd5d33fa1c1f9abb59ee2db644c356b18
Martin Skorsky <m.skorsky@gsi.de>
2022-01-26 17:04:37 +0100

    Add two more tests for booster / startthread / origin.

--- modules/ftm/tests/test_booster_startthread.py
+++ modules/ftm/tests/test_booster_startthread.py
@@ -1,5 +1,5 @@
 import dm_testbench
-from datetime import datetime as dt
+import pytest
 
 """
 Class collects unit tests for startthread nodes and origin nodes.
@@ -11,7 +11,31 @@ class UnitTestBoosterStartThread(dm_testbench.DmTestbench):
     file_name = 'snoop_startthread.csv'
     parameter_column = 20
     self.snoopToCsv(file_name, 3)
-    print(self.startAndGetSubprocessStdout(('grep', 'EVTNO: 0x0001', file_name), [0]))
-    print(self.startAndGetSubprocessStdout(('grep', 'EVTNO: 0x0002', file_name), [0]))
-    print(self.startAndGetSubprocessStdout(('grep', 'EVTNO: 0x0003', file_name), [0]))
+    self.startAndGetSubprocessStdout(('grep', 'EVTNO: 0x0001', file_name), [0])
+    self.startAndGetSubprocessStdout(('grep', 'EVTNO: 0x0002', file_name), [0])
+    self.startAndGetSubprocessStdout(('grep', 'EVTNO: 0x0003', file_name), [0])
+    self.deleteFile(file_name)
+
+  @pytest.mark.slow
+  def test_threeThreads1(self):
+    self.startPattern('booster_startthread-1.dot', 'BOOST_REQ')
+    file_name = 'snoop_startthread-1.csv'
+    parameter_column = 20
+    self.snoopToCsv(file_name, 15)
+    self.startAndGetSubprocessStdout(('grep', 'EVTNO: 0x0100', file_name), [0])
+    self.startAndGetSubprocessStdout(('grep', 'EVTNO: 0x0200', file_name), [0])
+    self.startAndGetSubprocessStdout(('grep', 'EVTNO: 0x0102', file_name), [0])
+    self.startAndGetSubprocessStdout(('grep', 'EVTNO: 0x0103', file_name), [0])
+    self.deleteFile(file_name)
+
+  @pytest.mark.slow
+  def test_threeThreads2(self):
+    self.startPattern('booster_startthread-2.dot', 'BOOST_REQ')
+    file_name = 'snoop_startthread-2.csv'
+    parameter_column = 20
+    self.snoopToCsv(file_name, 10)
+    self.startAndGetSubprocessStdout(('grep', 'EVTNO: 0x0100', file_name), [0])
+    self.startAndGetSubprocessStdout(('grep', 'EVTNO: 0x0200', file_name), [0])
+    self.startAndGetSubprocessStdout(('grep', 'EVTNO: 0x0102', file_name), [0])
+    self.startAndGetSubprocessStdout(('grep', 'EVTNO: 0x0103', file_name), [0])
     self.deleteFile(file_name)
