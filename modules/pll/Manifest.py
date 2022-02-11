664198ebac1016a6c76b94091ca2f8eacb936197
A. Hahn <a.hahn@gsi.de>
2022-02-11 14:50:59 +0100

    ftm4: adding PLLs, first step

--- modules/pll/Manifest.py
+++ modules/pll/Manifest.py
@@ -2,6 +2,7 @@ def __helper():
   dirs = []
   if syn_device[:4] == "10as":      dirs.extend(["arria10"])
   if syn_device[:9] == "10ax027e3": dirs.extend(["arria10_scu4"])
+  if syn_device[:9] == "10ax048e3": dirs.extend(["arria10_ftm4"])
   if syn_device[:9] == "10ax027h2": dirs.extend(["arria10_pex10"])
   if syn_device[:9] == "10ax066h2": dirs.extend(["arria10_ftm10"])
   if syn_device[:7] == "10ax115":   dirs.extend(["arria10_e3p1"])
@@ -11,4 +12,3 @@ def __helper():
 
 files = [ "pll_pkg.vhd", "altera_butis.vhd", "altera_phase.vhd", "altera_reset.vhd" ]
 modules = {"local": __helper() }
-
