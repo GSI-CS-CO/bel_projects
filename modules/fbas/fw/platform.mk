1d208683e78886e030131d15a2becd91c111c45e
Enkhbold Ochirsuren <E.Ochirsuren@gsi.de>
2022-02-11 15:44:27 +0100

    fbas: build FW binaries for multiple platforms (SCU3, Pexaria5)

---
+++ modules/fbas/fw/platform.mk
@@ -0,0 +1,38 @@
+PLATFMAKEFILE    := $(PLATFPATH)/Makefile
+
+# variables (actually RAM_SIZE) are exported to allow recursive build of ram.ld in top make
+export PLATFORM  := $(shell grep -m1 TARGET    $(PLATFMAKEFILE) | cut -d'=' -f2 | sed 's/[^a-zA-Z0-9]//g')
+export DEVICE    := $(shell grep -m1 DEVICE    $(PLATFMAKEFILE) | cut -d'=' -f2 | sed 's/[^a-zA-Z0-9]//g')
+export FLASH     := $(shell grep -m1 FLASH     $(PLATFMAKEFILE) | cut -d'=' -f2 | sed 's/[^a-zA-Z0-9]//g')
+export SPI_LANES := $(shell grep -m1 SPI_LANES $(PLATFMAKEFILE) | cut -d'=' -f2 | sed 's/[^a-zA-Z0-9]//g')
+export RAM_SIZE  := $(shell grep -m1 RAM_SIZE  $(PLATFMAKEFILE) | cut -d'=' -f2 | sed 's/[^a-zA-Z0-9]//g')
+
+CFLAGS        = -I../include -I../../common-libs/include -I../../wb_timer -I../../../ip_cores/saftlib/drivers -I$(PATHFW) \
+                -DPLATFORM=$(PLATFORM) -DDEBUGLEVEL=$(DEBUGLVL) $(EXTRA_FLAGS)
+SRC_FILES     = $(PATHFW)/$(TARGET).c $(PATHFW)/tmessage.c  \
+		$(PATHFW)/ioctl.c $(PATHFW)/measure.c       \
+		$(PATHFW)/timer.c $(PATHFW)/fwlib.c \
+		$(INCPATH)/ebm.c $(PATHFW)/../../common-libs/fw/common-fwlib.c
+
+$(info    >>>>)
+$(info    building is done by importing the following data from $(PLATFMAKEFILE):)
+$(info    PLATFORM    is $(PLATFORM))
+$(info    DEVICE      is $(DEVICE))
+$(info    FLASH       is $(FLASH))
+$(info    SPI_LANES   is $(SPI_LANES))
+$(info    RAM_SIZE    is $(RAM_SIZE))
+$(info    ----)
+$(info    building firmware using)
+$(info    SHARED_SIZE is $(SHARED_SIZE))
+$(info    USRCPUCLK   is $(USRCPUCLK))
+$(info    VERSION     is $(VERSION))
+$(info    CFLAGS      is $(CFLAGS))
+$(info    SRC_FILES   is $(SRC_FILES))
+$(info    <<<<)
+
+include ../../../syn/build.mk
+
+fwbin: $(TARGET).bin
+	@mv $^ $(TARGET).$(PLATFORM).bin
+
+$(TARGET).elf: $(SRC_FILES)
