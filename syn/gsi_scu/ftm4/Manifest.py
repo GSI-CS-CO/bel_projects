e612e2579f1b729bcb004561af28289679ba693c
A. Hahn <a.hahn@gsi.de>
2022-02-11 13:58:03 +0100

    ftm4: first top version

---
+++ syn/gsi_scu/ftm4/Manifest.py
@@ -0,0 +1,19 @@
+target = "altera"
+action = "synthesis"
+
+fetchto = "../../../ip_cores"
+syn_tool = "quartus"
+syn_grade = "e2sg"
+syn_package = "29"
+syn_device = "10ax027e3f"
+syn_top = "ftm4"
+syn_project = "ftm4"
+syn_family = "Arria 10"
+
+quartus_preflow = "ftm4.tcl"
+
+modules = {
+  "local" : [
+    "../../../top/gsi_scu/ftm4",
+  ]
+}
