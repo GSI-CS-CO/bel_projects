f5524b6a8f61c08abec923aa09525ae8262736ff
Martin Skorsky <m.skorsky@gsi.de>
2022-02-15 16:10:10 +0100

    Add getBuild.sh, startBuild.sh. These scrips start build jobs on Jenkins tsl025 or show the status.

---
+++ modules/ftm/tests/tools/startBuild.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+# hint: add alias startBuild='~/scripts/startBuild.sh <user:token>' to login script.
+echo -e start job
+if [ "$#" -ne 2 ]; then
+  echo "Usage: $0 <user:token> <job name, replace blanks by %20>"
+  echo "Job names are:"
+  echo "   build_schedule_compare"
+  echo "   build_tools"
+  echo "   check%20remote%20snoop"
+  echo "   test_module_ftm_datamaster"
+  echo "   test_module_ftm_datamaster_long_run"
+  echo "   test_module_ftm_datamaster_long_run%20dm-fallout-tests-2"
+
+  exit 1
+fi
+curl -X POST -L --user $1 http://tsl025.acc.gsi.de:8080/job/$2/build/
