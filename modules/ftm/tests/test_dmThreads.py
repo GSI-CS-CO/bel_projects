ebc43b1b8266899875e7747b3f571df58d56e60c
Martin Skorsky <m.skorsky@gsi.de>
2022-02-17 17:22:54 +0100

    test_dmThreads.py: improve check that thread works: extract message counts from dm-cmd and compare with message count a second later. Must increase.

--- modules/ftm/tests/test_dmThreads.py
+++ modules/ftm/tests/test_dmThreads.py
@@ -25,29 +25,80 @@ class UnitTestDatamasterThreads(dm_testbench.DmTestbench):
 
   def test_dmThreads1(self):
     self.run_dmThreads(1)
+    self.check_dmThreads_Cmd()
 
   def test_dmThreads2(self):
     self.run_dmThreads(2)
+    self.check_dmThreads_Cmd()
 
   def test_dmThreads3(self):
     self.run_dmThreads(3)
+    self.check_dmThreads_Cmd()
 
   def test_dmThreads4(self):
     self.run_dmThreads(4)
+    self.check_dmThreads_Cmd()
 
   def test_dmThreads5(self):
     self.run_dmThreads(5)
+    self.check_dmThreads_Cmd()
 
   def test_dmThreads6(self):
     self.run_dmThreads(6)
+    self.check_dmThreads_Cmd()
 
   def test_dmThreads7(self):
     self.run_dmThreads(7)
+    self.check_dmThreads_Cmd()
 
   def test_dmThreads8(self):
     self.run_dmThreads(8)
+    self.check_dmThreads_Cmd()
 
-  def test_dmThreads_Cpu0123(self):
+  def analyse_dm_cmd_output(self, output):
+    msgCounts = {}
+    firstCount = 0
+    index = 0
+    countLines = len(output)
+    offset = 0
+    offset1 = 0
+    cpu = 0
+    thread = 0
+    for line in output:
+      if index > 11 and index < (countLines - 1):
+        try:
+          cpu = int(line[3 + offset1])
+          thread = int(line[9 + offset1])
+          count = int(line[32 + offset:42 + offset])
+        except ValueError:
+          print(f'ValueError:{index} CPU {cpu} Thread {thread}  line: "{line}", "{line[3 + offset1]}", "{line[9 + offset1]}", "{line[32 + offset:42 + offset]}", offset={offset}, offset1={offset1}')
+        msgCounts[str(10*cpu) + str(thread)] = str(count)
+        if offset == 0:
+          offset = 10
+          offset1 = 5
+        else:
+          offset = 0
+          offset1 = 0
+      index = index + 1
+    self.assertEqual(countLines-13, len(msgCounts))
+    return msgCounts
+
+  def check_dmThreads_Cmd(self):
+    output = self.startAndGetSubprocessOutput((self.binary_dm_cmd, self.datamaster), [0])
+    firstCounts = self.analyse_dm_cmd_output(output[0])
+    self.delay(1)
+    output = self.startAndGetSubprocessOutput((self.binary_dm_cmd, self.datamaster), [0])
+    secondCounts = self.analyse_dm_cmd_output(output[0])
+    for key in firstCounts:
+      firstCount = int(firstCounts[key])
+      secondCount = int(secondCounts[key])
+      # ~ print(f'key={key}, firstCount={firstCount}, secondCount={secondCount}')
+      cpu = int(key[0])
+      thread = int(key[1])
+      self.assertGreater(secondCount, firstCount, f'CPU {cpu} Thread {thread} First: {firstCount}, second: {secondCount}')
+      self.assertGreater(firstCount, 0, f'CPU {cpu} Thread {thread} firstCount is {firstCount}')
+
+  def test_dmAllThreads_Cpu0123(self):
     count = 8
     self.startAndCheckSubprocess((self.binary_dm_cmd, self.datamaster, 'reset', 'all'), [0])
     self.startAndCheckSubprocess((self.binary_dm_sched, self.datamaster, 'clear', '-f'), [0])
@@ -69,4 +120,4 @@ class UnitTestDatamasterThreads(dm_testbench.DmTestbench):
         self.startAndCheckSubprocess((self.binary_dm_cmd, self.datamaster, 'startpattern', 'PPS2' + x, '-t', y), [0])
         self.startAndCheckSubprocess((self.binary_dm_cmd, self.datamaster, 'startpattern', 'PPS3' + x, '-t', y), [0])
         index = index + 1
-    output = self.startAndGetSubprocessOutput((self.binary_dm_cmd, self.datamaster), [0], 13 + (4 * count))
+    self.check_dmThreads_Cmd()
