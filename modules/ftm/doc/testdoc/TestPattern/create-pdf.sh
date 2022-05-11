dc23eac131da324c06de3467a3ab453a1ad748fc
Martin Skorsky <m.skorsky@gsi.de>
2022-05-11 14:40:20 +0200

    create-pdf.sh: generate pdf with dot if no positions found in dot file.

--- modules/ftm/doc/testdoc/TestPattern/create-pdf.sh
+++ modules/ftm/doc/testdoc/TestPattern/create-pdf.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-neato -n -Tpdf -o ${1//.dot/.pdf1} $1
+if [ $(grep 'pos=' $1) ] 
+then
+  neato -n -Tpdf -o ${1//.dot/.pdf1} $1
+else
+  dot -Tpdf -o ${1//.dot/.pdf1} $1
+#  neato -Tpdf -o ${1//.dot/.pdf1} $1
+fi
 gs -o ${1//.dot/.pdf} -sDEVICE=pdfwrite -dColorConversionStrategy=/sRGB -dProcessColorModel=/DeviceRGB ${1//.dot/.pdf1}
 rm ${1//.dot/.pdf1}
