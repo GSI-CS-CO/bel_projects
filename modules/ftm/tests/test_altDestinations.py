ce861ccca7bd4e07fa04e847866a9016e25a5e70
Martin Skorsky <m.skorsky@gsi.de>
2022-07-14 13:57:27 +0200

    test_altDestinations.py: adopt tests for DM:lib: fix for vanishing altdst bug

--- modules/ftm/tests/test_altDestinations.py
+++ modules/ftm/tests/test_altDestinations.py
@@ -1,19 +1,19 @@
 import dm_testbench
 import pytest
 
-"""Class UnitTestAltDestinations tests the limit of 10 altdst edges per block.
+"""Class UnitTestAltDestinations tests the limit of 9 altdst edges per block.
 """
 class UnitTestAltDestinations(dm_testbench.DmTestbench):
 
   def test_altDestinationsOk(self):
-    self.startPattern('altdst-flow-10.dot')
+    self.startPattern('altdst-flow-9.dot')
     self.checkRunningThreadsCmd()
 
   def test_altDestinationsOkSwitch(self):
-    self.startPattern('altdst-10.dot')
+    self.startPattern('altdst-9.dot')
     file_name = 'snoop_altDestinationsOkSwitch.csv'
     column_EVTNO = 8
-    listSwitch = [(0, '0a'), (1, '0b'), (2, '0c'), (3, '0d'), (4, '0e'), (5, '0f'), (6, '10'), (7, '11'), (8, '12'), (9, '13')]
+    listSwitch = [(0, '0a'), (1, '0b'), (2, '0c'), (3, '0d'), (4, '0e'), (5, '0f'), (6, '10'), (7, '11'), (8, '12')]
     for x, y in listSwitch:
       self.startAndCheckSubprocess((self.binaryDmCmd, self.datamaster, 'switch', 'Block', 'Msg0' + str(x)), [0], 0, 0)
       self.checkRunningThreadsCmd(0.1)
@@ -22,12 +22,12 @@ class UnitTestAltDestinations(dm_testbench.DmTestbench):
     self.deleteFile(file_name)
 
   def test_altDestinationsFlowFail(self):
-    fileName = self.schedules_folder + 'altdst-flow-11.dot'
+    fileName = self.schedules_folder + 'altdst-flow-10.dot'
     self.startAndCheckSubprocess((self.binaryDmSched, self.datamaster, 'add',
         fileName), [250], linesCout=2, linesCerr=3)
 
   def test_altDestinationsFail(self):
-    fileName = self.schedules_folder + 'altdst-11.dot'
+    fileName = self.schedules_folder + 'altdst-10.dot'
     self.startAndCheckSubprocess((self.binaryDmSched, self.datamaster, 'add',
         fileName), [250], linesCout=2, linesCerr=3)
 
