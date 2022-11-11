7d1387a78a7211db1d4136697ed916828f9ab402
A. Hahn <a.hahn@gsi.de>
2022-11-11 14:20:31 +0100

    monster_pkg.vhd: set default of g_en_tlu to false

--- modules/monster/monster_pkg.vhd
+++ modules/monster/monster_pkg.vhd
@@ -94,7 +94,7 @@ package monster_pkg is
       g_lvds_out             : natural := 0;
       g_fixed                : natural := 0;
       g_lvds_invert          : boolean := false;
-      g_en_tlu               : boolean := true;
+      g_en_tlu               : boolean := false;
       g_en_pcie              : boolean := false;
       g_en_vme               : boolean := false;
       g_en_usb               : boolean := false;
