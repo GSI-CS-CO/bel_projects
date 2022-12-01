a3f3debb1b4cf666dfb28fd78564a69ede53bb9c
Michael Reese <m.reese@gsi.de>
2022-12-01 18:39:13 +0100

    trying to get irqs in lm32

--- testbench/lm32_cluster/test/firmware/firmware.c
+++ testbench/lm32_cluster/test/firmware/firmware.c
@@ -1,24 +1,51 @@
 #include "mini_sdb.h"
+#include "pp-printf.h"
+#include "irq.h"
 
 void* memset(void* s, int c, int n) {
-	for (int i = 0; i < n; ++i) {
-		*(int*)s = c;
+	void *result = s;
+	for (int i = 0; i < n/4; ++i) {
+		((int*)s)[i] = c;
 	}
+	return result;
+}
+void *memcpy(void *dest, const void *src, int n)
+{
+	void *result = dest;
+	for (int i = 0; i < n/4; ++i) {
+		((int*)dest)[i] = ((int*)src)[i];
+	}
+	return result;
 }
 
 
 volatile char *uart_out;
-
-void print_str(char *str) {
+int puts(const char *str) {
 	while(*str) {
 		*uart_out = *str++;
 	}
+	return 1;
+}
+
+void my_isr(void) { 
+	//<evaluate global_msi and do something useful> 
+	puts("my_isr\n");
 }
 
 int main() {
 	uart_out = (volatile char*) find_device_adr(GSI, SDB_UART_SIM);
+	isr_table_clr();
+	isr_ptr_table[0] = my_isr;
+	irq_set_mask(0x01); //Enable used IRQs ...
+	irq_enable(); 
 	for(;;) {
-		print_str("hello, world!\n");
+		puts(".\n");
+		// puts("hello, world!\n");
+		// pp_printf("at_on!\n");
+		// atomic_on();
+		// pp_printf("at_off!\n");
+		// atomic_off();
 	}
+	irq_disable();
 	return 0;
 }
