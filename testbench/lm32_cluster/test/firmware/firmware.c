6e179be07f8bf6fe4904b6ed9ddc42aa77e3ed51
Michael Reese <m.reese@gsi.de>
2022-12-05 18:37:37 +0100

    irq handler ist called

--- testbench/lm32_cluster/test/firmware/firmware.c
+++ testbench/lm32_cluster/test/firmware/firmware.c
@@ -31,24 +31,42 @@ void uart_write_byte(const char c) {
 	*uart_out = c;
 }
 
-void my_isr(void) { 
-	//<evaluate global_msi and do something useful> 
-	puts("my_isr\n");
+void irq_handler(int id) { 
+	mprintf("irq_handler\n");
+}
+
+void init_irq_table() {
+  isr_table_clr();
+  isr_ptr_table[0] = &irq_handler;
+  irq_set_mask(0x01);
+  //msg_buf[IRQ].ring_head = msg_buf[IRQ].ring_tail; // clear msg buffer
+  irq_enable();
+  mprintf("IRQ table configured.\n");
+}
+
+void discover() {
+  sdb_location found_sdb[20];
+  uint32_t idx = 0;
+  pCpuMsiBox   = 0;
+  pMyMsi       = 0; 
+
+  uart_out = (volatile char*) find_device_adr(GSI, SDB_UART_SIM);
+  find_device_multi(&found_sdb[0], &idx, 1, GSI, MSI_MSG_BOX);   
+  if(idx) {
+    pCpuMsiBox    = (uint32_t*)getSdbAdr(&found_sdb[0]); 
+    pMyMsi        = (uint32_t*)getMsiAdr(&found_sdb[0]); 
+  } 
+
 }
 
 int main() {
-	uart_out = (volatile char*) find_device_adr(GSI, SDB_UART_SIM);
-	isr_table_clr();
-	isr_ptr_table[0] = my_isr;
-	irq_set_mask(0x01); //Enable used IRQs ...
-	irq_enable(); 
+	discover();
+	init_irq_table();
 	for(;;) {
 		puts("hello, world!\n");
-		mprintf("0x%x\n",uart_out);
-		// pp_printf("at_on!\n");
-		// atomic_on();
-		// pp_printf("at_off!\n");
-		// atomic_off();
+		mprintf("uart=0x%x\n",uart_out);
+		mprintf("pCpuMsiBox=0x%x\n",pCpuMsiBox);
+		mprintf("pMyMsi=0x%x\n",pMyMsi);
 	}
 	irq_disable();
 	return 0;
