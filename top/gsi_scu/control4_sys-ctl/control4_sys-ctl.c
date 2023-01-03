7c3bd2bccdf29626d3da61890dbecdacae0950f2
k.lueghausen <k.lueghausen@gsi.de>
2023-01-03 13:35:27 +0100

    scu4: basic power power sequencing SM

---
+++ top/gsi_scu/control4_sys-ctl/control4_sys-ctl.c
@@ -0,0 +1,87 @@
+/* 
+ * File: mock-lite.c
+ * Description: Toggels pin PB0 every 500ms
+ * From: C-Programmierung mit AVR-GCC
+ */
+
+//#define F_CPU 32000000UL
+
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+#include "io-func.h"
+
+
+
+
+int main (void) {
+
+    typedef enum {PWR_IDLE = 0,PWR_UP0,PWR_UP1,PWR_UP2,PWR_OK,PWR_DOWN1,PWR_DOWN0} pwr_state_t;
+
+    pwr_state_t state = PWR_IDLE;
+
+ 
+
+   PORTJ.DIRSET = PIN2_bm| PIN3_bm|PIN4_bm|PIN5_bm;
+
+    //Switch to 32MHz internal oscillator 
+    OSC.CTRL |= OSC_RC32MEN_bm;                 // Enable the internal 32MHz  
+    while(!(OSC.STATUS & OSC_RC32MRDY_bm));     // Wait for 32MHz oscillator to stabilize 
+    CCP = CCP_IOREG_gc;                         // Disable register security for clock update
+    CLK.CTRL = CLK_SCLKSEL_RC32M_gc;            // Switch to 32MHz clock
+    OSC.CTRL &= ~OSC_RC2MEN_bm;                 // Disable 2Mhz oscillator
+
+    ADC_init();
+
+    //Output Init
+
+    enableCoreVoltage (low);
+    enable1_8V (low);
+    enable1_8VIO (low);
+    enable5V (low);
+
+   while(1) {
+
+        switch(state)
+        {
+            case PWR_IDLE:     
+                if (MP_ADC_read() >= MP_ON_ADC_THRES)
+                {
+                state = PWR_UP0;
+                }
+            break;
+            case PWR_UP0:
+                enableCoreVoltage (high);
+                state = PWR_UP1;
+            break;
+            case PWR_UP1:
+                enable1_8V (high);
+                state = PWR_UP2;
+            break;
+            case PWR_UP2:
+                enable1_8VIO (high);
+                state = PWR_OK;
+            break;
+            case PWR_OK:
+                enable5V (high);
+                if (MP_ADC_read() <= MP_FAIL_ADC_THRES)           
+                {
+                state = PWR_DOWN1;
+                }
+            break;
+            case PWR_DOWN1:
+                enable5V (low);
+                enable1_8VIO (low);
+                enable1_8V (low);   
+                state = PWR_DOWN0;
+            break;
+            case PWR_DOWN0:
+                enableCoreVoltage (low);
+                state = PWR_IDLE;
+            break;
+        }
+        
+   }
+
+   return 0;
+}
---
+++ top/gsi_scu/control4_sys-ctl/io-func.h
@@ -0,0 +1,28 @@
+#define F_CPU 32000000UL
+
+#define ADC_REF_VOLTAGE 1
+#define ADC_BIT_RES 4096
+
+//Main Power rail ADC
+#define MP_ON_THRESHOLD_V 0.5
+#define MP_FAIL_THRESHOLD_V 0.5
+#define MP_R_SERIES 0
+#define MP_R_MEASURE 1 
+#define MP_ON_ADC_THRES ( ((float)MP_R_MEASURE / (float)(MP_R_SERIES + MP_R_MEASURE) ) * (ADC_BIT_RES / (float)ADC_REF_VOLTAGE) * (float)MP_ON_THRESHOLD_V )
+#define MP_FAIL_ADC_THRES ( ((float)MP_R_MEASURE / (float)(MP_R_SERIES + MP_R_MEASURE) ) * (ADC_BIT_RES / (float)ADC_REF_VOLTAGE) * (float)MP_FAIL_THRESHOLD_V )
+
+typedef enum {low = 0, high = 1, toggle} io_level_t;
+
+void enableCoreVoltage (io_level_t level);
+
+void enable1_8V (io_level_t level);
+
+void enable1_8VIO (io_level_t level);
+
+void enable5V (io_level_t level);
+
+int16_t readPortKPin0 (void);
+
+void ADC_init(void); // Init ADCs
+
+uint16_t MP_ADC_read(void); // Read Main Power Voltage ADC
