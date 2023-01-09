d215e462cf2e3479c8ac1e6ce9c677d775c5c106
k.lueghausen <k.lueghausen@gsi.de>
2023-01-09 11:00:44 +0100

    scu4: power seq astyle -A7

--- top/gsi_scu/control4_sys-ctl/control4_sys-ctl.c
+++ top/gsi_scu/control4_sys-ctl/control4_sys-ctl.c
@@ -1,4 +1,4 @@
-/* 
+/*
  * File: mock-lite.c
  * Description: Toggels pin PB0 every 500ms
  * From: C-Programmierung mit AVR-GCC
@@ -14,7 +14,8 @@
 
 
 
-int main (void) {
+int main (void)
+{
 
     typedef enum {PWR_IDLE = 0,PWR_UP0,PWR_UP1,PWR_UP2,PWR_OK,PWR_DOWN1,PWR_DOWN0} pwr_state_t;
 
@@ -22,11 +23,11 @@ int main (void) {
 
     uint16_t PORflag = 0; //Power on Reset Flag
 
-   PORTJ.DIRSET = PIN2_bm| PIN3_bm|PIN4_bm|PIN5_bm;
+    PORTJ.DIRSET = PIN2_bm| PIN3_bm|PIN4_bm|PIN5_bm;
 
-    //Switch to 32MHz internal oscillator 
-    OSC.CTRL |= OSC_RC32MEN_bm;                 // Enable the internal 32MHz  
-    while(!(OSC.STATUS & OSC_RC32MRDY_bm));     // Wait for 32MHz oscillator to stabilize 
+    //Switch to 32MHz internal oscillator
+    OSC.CTRL |= OSC_RC32MEN_bm;                 // Enable the internal 32MHz
+    while(!(OSC.STATUS & OSC_RC32MRDY_bm));     // Wait for 32MHz oscillator to stabilize
     CCP = CCP_IOREG_gc;                         // Disable register security for clock update
     CLK.CTRL = CLK_SCLKSEL_RC32M_gc;            // Switch to 32MHz clock
     OSC.CTRL &= ~OSC_RC2MEN_bm;                 // Disable 2Mhz oscillator
@@ -43,114 +44,116 @@ int main (void) {
     disableIO();
     performReset();
 
-   while(1) {
-        // Power Sequence State Machine 
-        switch(state)
+    while(1)
         {
-            case PWR_IDLE:
-                PORflag = 0;
-                indicatorLED(red);     
-                if (read_MP_ADC() >= MP_ON_ADC_THRES && readPGood3_3V())
+            // Power Sequence State Machine
+            switch(state)
                 {
-                state = PWR_UP0;
-                }
-            break;
-            case PWR_UP0:
-                enableCoreVoltage (high);
-                indicatorLED(yellow);
-                if (read_MP_ADC() <= MP_FAIL_ADC_THRES || !readPGood3_3V())
-                {
-                state = PWR_DOWN0;
-                }
-                else if (readPGoodCore())
-                {
-                state = PWR_UP1;
-                }
-            break;
-            case PWR_UP1:
-                enable1_8V (high);
-                indicatorLED(yellow);
-                if (read_MP_ADC() <= MP_FAIL_ADC_THRES || !readPGood3_3V() || !readPGoodCore())
-                {
-                state = PWR_DOWN1;
-                }
-                else if (readPGood1_8V())
-                {
-                state = PWR_UP2;
-                }
-            break;
-            case PWR_UP2:
-                enable1_8VIO (high);
-                indicatorLED(yellow);
-                if (read_MP_ADC() <= MP_FAIL_ADC_THRES || !readPGood3_3V() || !readPGoodCore() || !readPGood1_8V())
-                {
-                state = PWR_DOWN1;
-                }
-                else if (read_V1_8IO_ADC() >= V1_8IO_GOOD_ADC_THRES)
-                {
-                state = PWR_OK;
-                }
-            break;
-            //Power Down
-            case PWR_DOWN1:
-                enableComXpowerOk(low);
-                disableIO();
-                enable5V (low);
-                enable1_8VIO (low);
-                enable1_8V (low);
-                indicatorLED(magenta);
-                if (read_V1_8_ADC() <= V1_8_OFF_ADC_THRES && read_V1_8IO_ADC() <= V1_8IO_OFF_ADC_THRES)   
-                {
-                state = PWR_DOWN0;
-                }
-            break;
-            case PWR_DOWN0:
-                enableCoreVoltage (low);
-                indicatorLED(magenta);
-                if (read_CORE_ADC() <= CORE_OFF_ADC_THRES)
-                {
-                state = PWR_IDLE;
-                }
-            break;
-            // Power OK
-            case PWR_OK:
-                if (read_MP_ADC() <= MP_FAIL_ADC_THRES || !readPGood3_3V() || !readPGoodCore() || !readPGood1_8V() || read_V1_8IO_ADC() <= V1_8IO_FAIL_ADC_THRES)           
-                {
-                performReset();
-                state = PWR_DOWN1;
-                }
-                enable5V (high);
-                enableComXpowerOk(high);
-                enableIO();
-                //Reset Behavior 
-                if (!readAllResets())
-                {
-                    //Reset
-                    performReset();
-                    indicatorLED(blue);
-                }
-                else
-                {   if (!PORflag)
-                    {
-                        PORflag = 1;
-                        releaseReset();
-                        indicatorLED(green);
-                        _delay_ms(POR_DELAY);
-                        performReset();
-                        indicatorLED(blue);
-                        _delay_ms(POR_DELAY);
-                    }
+                case PWR_IDLE:
+                    PORflag = 0;
+                    indicatorLED(red);
+                    if (read_MP_ADC() >= MP_ON_ADC_THRES && readPGood3_3V())
+                        {
+                            state = PWR_UP0;
+                        }
+                    break;
+                case PWR_UP0:
+                    enableCoreVoltage (high);
+                    indicatorLED(yellow);
+                    if (read_MP_ADC() <= MP_FAIL_ADC_THRES || !readPGood3_3V())
+                        {
+                            state = PWR_DOWN0;
+                        }
+                    else if (readPGoodCore())
+                        {
+                            state = PWR_UP1;
+                        }
+                    break;
+                case PWR_UP1:
+                    enable1_8V (high);
+                    indicatorLED(yellow);
+                    if (read_MP_ADC() <= MP_FAIL_ADC_THRES || !readPGood3_3V() || !readPGoodCore())
+                        {
+                            state = PWR_DOWN1;
+                        }
+                    else if (readPGood1_8V())
+                        {
+                            state = PWR_UP2;
+                        }
+                    break;
+                case PWR_UP2:
+                    enable1_8VIO (high);
+                    indicatorLED(yellow);
+                    if (read_MP_ADC() <= MP_FAIL_ADC_THRES || !readPGood3_3V() || !readPGoodCore() || !readPGood1_8V())
+                        {
+                            state = PWR_DOWN1;
+                        }
+                    else if (read_V1_8IO_ADC() >= V1_8IO_GOOD_ADC_THRES)
+                        {
+                            state = PWR_OK;
+                        }
+                    break;
+                //Power Down
+                case PWR_DOWN1:
+                    enableComXpowerOk(low);
+                    disableIO();
+                    enable5V (low);
+                    enable1_8VIO (low);
+                    enable1_8V (low);
+                    indicatorLED(magenta);
+                    if (read_V1_8_ADC() <= V1_8_OFF_ADC_THRES && read_V1_8IO_ADC() <= V1_8IO_OFF_ADC_THRES)
+                        {
+                            state = PWR_DOWN0;
+                        }
+                    break;
+                case PWR_DOWN0:
+                    enableCoreVoltage (low);
+                    indicatorLED(magenta);
+                    if (read_CORE_ADC() <= CORE_OFF_ADC_THRES)
+                        {
+                            state = PWR_IDLE;
+                        }
+                    break;
+                // Power OK
+                case PWR_OK:
+                    if (read_MP_ADC() <= MP_FAIL_ADC_THRES || !readPGood3_3V() || !readPGoodCore() || !readPGood1_8V() || read_V1_8IO_ADC() <= V1_8IO_FAIL_ADC_THRES)
+                        {
+                            performReset();
+                            state = PWR_DOWN1;
+                        }
+                    enable5V (high);
+                    enableComXpowerOk(high);
+                    enableIO();
+                    //Reset Behavior
+                    if (!readAllResets())
+                        {
+                            //Reset
+                            performReset();
+                            indicatorLED(blue);
+                        }
                     else
-                    {
-                        //System Running
-                        releaseReset();
-                        indicatorLED(green);
-                    }
+                        {
+                            if (!PORflag)
+                                {
+                                    PORflag = 1;
+                                    releaseReset();
+                                    indicatorLED(green);
+                                    _delay_ms(POR_DELAY);
+                                    performReset();
+                                    indicatorLED(blue);
+                                    _delay_ms(POR_DELAY);
+                                }
+                            else
+                                {
+                                    //System Running
+                                    releaseReset();
+                                    indicatorLED(green);
+                                }
+                        }
+                    break;
                 }
-            break;
-         }
-        
-   }
 
-   return 0;
+        }
+
+    return 0;
 }
