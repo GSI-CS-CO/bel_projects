c21441e17518ca24b256003f8aa03521d18981ce
A. Hahn <a.hahn@gsi.de>
2023-03-02 15:16:27 +0100

    wrpc-import: add w1 files and etherbone.h

---
+++ modules/lm32-include/wrpc-import/hw/etherbone.h
@@ -0,0 +1,12 @@
+/*
+ * This work is part of the White Rabbit project
+ *
+ * Released according to the GNU GPL, version 2 or any later version.
+ */
+
+#ifndef __ETHERBONE_H__
+#define __ETHERBONE_H__
+
+void eb_setIP(unsigned char *IP);
+
+#endif
