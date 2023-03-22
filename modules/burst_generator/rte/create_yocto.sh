746f542fe65f145e21d0547c17a952ab0ef63f9b
Enkhbold Ochirsuren <E.Ochirsuren@gsi.de>
2023-03-22 11:19:36 +0100

    burstgen: add scripts to create the NFSinit symlinks

---
+++ modules/burst_generator/rte/create_yocto.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# pxelinux.cfg = yocto
+# /common/export/nfsinit/<target_scu>
+
+ln -s ../global/timing-rte-yocto-loader 10.tg-fallout-v6-2-1-yocto
+ln -s ../global/timing-rte-yocto-loader 29.backdoor
+ln -s ../global/timing-rte-yocto-loader 30.socat
