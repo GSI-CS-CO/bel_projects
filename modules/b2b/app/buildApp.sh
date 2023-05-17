6e29f2f4de56952b7962e2f790f92bb17e6488bb
Dietrich Beck <d.beck@gsi.de>
2023-05-17 15:54:24 +0200

    b2b: fixing deployment script for apps; fix wrong version number

--- modules/b2b/app/buildApp.sh
+++ modules/b2b/app/buildApp.sh
@@ -5,12 +5,8 @@
 # './buildApp.sh $HOME/consoleApps/b2b b2b-int-transfer-mon b2b-mon'
 # './buildApp.sh $HOME/consoleApps/b2b b2b-pro-transfer-mon b2b-mon'
 
-
-
-
-EB=../../../ip_cores/etherbone-core/api
 SW=../x86
-DIM=/common/usr/timing/b2b/yocto/dim_v20r33/
+USRPATH=/common/usr/timing/b2b/centos7
 APPBUILD=$1/$2/current
 NAME=$2
 BIN=$3
@@ -29,9 +25,9 @@ cp start-app-$NAME.sh $APPBUILD/bin
 cp $SW/libb2blib.so.1.0 $APPBUILD/lib
 ln -sf $APPBUILD/lib/libb2blib.so.1.0 $APPBUILD/lib/libb2blib.so.1
 ln -sf $APPBUILD/lib/libb2blib.so.1 $APPBUILD/lib/libb2blib.so
-cp $DIM/linux/libdim.so $APPBUILD/lib
-cp $EB/.libs/libetherbone.so.5.1.2 $APPBUILD/lib
-ln -sf $APPBUILD/lib/libetherbone.so.5.1.2 $APPBUILD/lib/libetherbone.so.5.1
+cp $USRPATH/usr/lib/libdim.so $APPBUILD/lib
+cp $USRPATH/usr/lib/libetherbone.so.5.1.3 $APPBUILD/lib
+ln -sf $APPBUILD/lib/libetherbone.so.5.1.3 $APPBUILD/lib/libetherbone.so.5.1
 ln -sf $APPBUILD/lib/libetherbone.so.5.1 $APPBUILD/lib/libetherbone.so.5
 cd $APPBUILD
 zip -r $NAME.zip bin lib
