80f08b81c5aaaef4762f7ff2d88d0eb8dc41f772
Martin Skorsky <m.skorsky@gsi.de>
2023-05-24 11:48:37 +0200

    datamasterCheck.sh: include monitoring output. Remove old scripts for specific hosts.

--- modules/ftm/tests/tools/datamasterCheck.sh
+++ modules/ftm/tests/tools/datamasterCheck.sh
@@ -4,6 +4,8 @@ datamasterInfo() {
   echo
   echo " PCI on host $DM_HOST"
   lspci | grep "Class 6800"
+  echo 'Check kernel modules and CERN pci devices.'
+  sudo lspci -nn -vkd 10dc:
   sudo lspci -vs 0000:04:00.0
   sudo lspci -vs 0000:17:00.0
   ls -l /dev/wb*
@@ -17,11 +19,16 @@ datamasterInfo() {
   then
     echo
     echo
-    echo 'Datamaster:' ; eb-mon -yi $DM ; eb-info $DM
+    echo 'Datamaster:' ; eb-info $DM
+    echo '$DM datamaster host time: '; date -Iseconds
+    echo '$DM datamaster IP: '; eb-mon -i $DM
+    echo '$DM datamaster WR sync status: '; eb-mon -y $DM
+    echo '$DM datamaster link status: '; eb-mon -l $DM
     echo
     echo ' Datamaster status:'
     dm-cmd $DM -v
   fi
+  OPTIONS='-k zzz' make -C ~/bel_projects/dev/modules/ftm/tests
   echo
   echo ' Datamaster log space'
   df -h /var/log/
@@ -37,16 +44,24 @@ remoteDatamasterInfo() {
     echo
     echo
     echo 'Timing-Receiver:' ; ssh root@$DM_HOST "eb-mon -vi $TR0" ; ssh root@$DM_HOST "eb-info $TR0"
+    echo '$TR0 WR time: '; ssh root@$DM_HOST "eb-mon -d $TR0"
   fi
   if ! [ -z "$DM" ]
   then
     echo
     echo
     echo 'Datamaster:' ; ssh root@$DM_HOST "eb-mon -yi $DM" ; ssh root@$DM_HOST "eb-info $DM"
+    echo '$DM datamaster host time: '; ssh root@$DM_HOST 'date -Iseconds'
+    echo '$DM datamaster IP: '; ssh root@$DM_HOST "eb-mon -i $DM"
+    echo '$DM datamaster WR sync status: '; ssh root@$DM_HOST "eb-mon -y $DM"
+    echo '$DM datamaster link status: '; ssh root@$DM_HOST "eb-mon -l $DM"
     echo
     echo ' Datamaster status:'
     ssh root@$DM_HOST "dm-cmd -v $DM"
   fi
+  # Test that the tools version and the firmware version are compatible
+  dm-cmd tcp/$DM_HOST
+  #~ OPTIONS='-k zzz' make -C ~/bel_projects/dev/modules/ftm/tests remote
   echo
   echo ' Datamaster log space'
   ssh root@$DM_HOST 'df -h /var/log/'
