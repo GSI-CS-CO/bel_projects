6fb61b360308ad9fd1ba3915616d117dc315f491
Martin Skorsky <m.skorsky@gsi.de>
2023-07-27 16:12:23 +0200

    test_basic.py: add tests for up to 32 threads, run these with --runthread32.

--- modules/ftm/tests/test_basic.py
+++ modules/ftm/tests/test_basic.py
@@ -1,10 +1,12 @@
 import dm_testbench
+import pytest
 
 """Class collects basic tests.
 1. add an remove a schedule.
 """
 class UnitTestBasic(dm_testbench.DmTestbench):
 
+  @pytest.mark.thread8
   def test_static_basic(self):
     """Load the schedule static-basic-schedule.dot.
     """
@@ -13,6 +15,19 @@ class UnitTestBasic(dm_testbench.DmTestbench):
     self.addSchedule('static-basic-schedule.dot')
     stdoutLines = self.startAndGetSubprocessStdout((self.binaryDmSched, self.datamaster, 'status'))
     self.compareExpectedOutput(stdoutLines, self.schedules_folder + 'static-basic-expected-1-0.txt')
+    stdoutLines = self.startAndGetSubprocessStdout((self.binaryDmSched, self.datamaster, 'remove', self.schedules_folder + 'static-basic-schedule.dot'))
+    stdoutLines = self.startAndGetSubprocessStdout((self.binaryDmSched, self.datamaster, 'status'))
+    self.compareExpectedOutput(stdoutLines, self.schedules_folder + 'static-basic-expected-0-0.txt')
+
+  @pytest.mark.thread32
+  def test_static_basic_thread32(self):
+    """Load the schedule static-basic-schedule.dot.
+    """
+    stdoutLines = self.startAndGetSubprocessStdout((self.binaryDmSched, self.datamaster, 'status'))
+    self.compareExpectedOutput(stdoutLines, self.schedules_folder + 'static-basic-expected-0-0.txt')
+    self.addSchedule('static-basic-schedule.dot')
+    stdoutLines = self.startAndGetSubprocessStdout((self.binaryDmSched, self.datamaster, 'status'))
+    self.compareExpectedOutput(stdoutLines, self.schedules_folder + 'static-basic-expected-1-0-thread32.txt')
     stdoutLines = self.startAndGetSubprocessStdout((self.binaryDmSched, self.datamaster, 'remove', self.schedules_folder + 'static-basic-schedule.dot'))
     stdoutLines = self.startAndGetSubprocessStdout((self.binaryDmSched, self.datamaster, 'status'))
     self.compareExpectedOutput(stdoutLines, self.schedules_folder + 'static-basic-expected-0-0.txt')
