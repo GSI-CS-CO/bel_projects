acf3ab45f5368d48c85f605e39e6e5f45482645e
Martin Skorsky <m.skorsky@gsi.de>
2023-07-27 16:23:52 +0200

    test_loop.py: add tests for up to 32 threads, run these with --runthread32.

--- modules/ftm/tests/test_loop.py
+++ modules/ftm/tests/test_loop.py
@@ -1,9 +1,11 @@
 import dm_testbench
+import pytest
 
 """Class collects unit tests for loop.
 """
 class UnitTestLoop(dm_testbench.DmTestbench):
 
+  @pytest.mark.thread8
   def test_dynamic_loop(self):
     """Load the schedule dynamic-loop-schedule.dot and start pattern IN_A.
     Check the visited nodes (rawvisited) and rawstatus.
@@ -21,3 +23,22 @@ class UnitTestLoop(dm_testbench.DmTestbench):
     self.compareExpectedOutput(stdoutLines, self.schedules_folder + 'dynamic-loop-expected-1-0.txt')
     stdoutLines = self.startAndGetSubprocessStdout((self.binaryDmCmd, self.datamaster, 'rawstatus'))
     self.compareExpectedOutput(stdoutLines, self.schedules_folder + 'dynamic-loop-expected-1-1.txt')
+
+  @pytest.mark.thread32
+  def test_dynamic_loop_thread32(self):
+    """Load the schedule dynamic-loop-schedule.dot and start pattern IN_A.
+    Check the visited nodes (rawvisited) and rawstatus.
+    Then start pattern IN_B and check the visited nodes (rawvisited) and rawstatus again.
+    """
+    self.startPattern('dynamic-loop-schedule.dot', 'IN_A')
+    self.delay(0.5)
+    stdoutLines = self.startAndGetSubprocessStdout((self.binaryDmSched, self.datamaster, 'rawvisited'))
+    self.compareExpectedOutput(stdoutLines, self.schedules_folder + 'dynamic-loop-expected-0-0.txt')
+    stdoutLines = self.startAndGetSubprocessStdout((self.binaryDmCmd, self.datamaster, 'rawstatus'))
+    self.compareExpectedOutput(stdoutLines, self.schedules_folder + 'dynamic-loop-expected-0-1-thread32.txt')
+    self.startAndCheckSubprocess((self.binaryDmCmd, self.datamaster, 'startpattern', 'IN_B'))
+    self.delay(0.5)
+    stdoutLines = self.startAndGetSubprocessStdout((self.binaryDmSched, self.datamaster, 'rawvisited'))
+    self.compareExpectedOutput(stdoutLines, self.schedules_folder + 'dynamic-loop-expected-1-0.txt')
+    stdoutLines = self.startAndGetSubprocessStdout((self.binaryDmCmd, self.datamaster, 'rawstatus'))
+    self.compareExpectedOutput(stdoutLines, self.schedules_folder + 'dynamic-loop-expected-1-1-thread32.txt')
