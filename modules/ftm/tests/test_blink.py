a1b9bd180b0503874608aadf2083a8e7bc7625b3
Martin Skorsky <m.skorsky@gsi.de>
2023-08-03 14:53:18 +0200

    test_blink.py: improve comments.

--- modules/ftm/tests/test_blink.py
+++ modules/ftm/tests/test_blink.py
@@ -12,14 +12,15 @@ class DmBlink(dm_testbench.DmTestbench):
     fileName = 'snoop_blink.csv'
     self.addSchedule(schedule)
     self.snoopToCsvWithAction(fileName, self.doAction, 10)
-    # ~ # analyse column 8 which contains the evtno.
-    # ~ # check that evtno 0x0110 and 0x0112 occur.
+    # analyse column 8 which contains the evtno.
+    # check that evtno 0x0110 and 0x0112 occur.
     self.analyseFrequencyFromCsv(fileName, column=8, printTable=True, checkValues={'0x0001': '=1', '0x0110': '>0', '0x0112': '>0'})
-    # ~ # analyse column 8 which contains the parameter.
-    # ~ # check that paramter 0x123, 0x456, 0x789, and 0xABC occur.
+    # analyse column 20 which contains the parameter. It is important to have fid=1 in the timing messages. This ensures that we have 20 columns.
+    # check that paramter 0x123, 0x456, 0x789, and 0xABC occur.
     self.analyseFrequencyFromCsv(fileName, column=20, printTable=True, checkValues={'0x0000000000000001': '=1', '0x0000000000000123': '>0', '0x0000000000000456': '>0', '0x0000000000000789': '>0', '0x0000000000000abc': '>0'})
     self.deleteFile(fileName)
-    self.startAndCheckSubprocess((self.binaryDmCmd, self.datamaster, 'preptime', '-c0',  '-t0', '1000000'), linesCout=0, linesCerr=0)
+    # reset preptime from 1000000000 back to 1000000 after test. This is not done by dm-cmd reset all.
+    self.startAndCheckSubprocess((self.binaryDmCmd, self.datamaster, 'preptime', '-c0', '-t0', '1000000'), linesCout=0, linesCerr=0)
     self.startAndCheckSubprocess((self.binaryDmCmd, self.datamaster, 'preptime', '-c0', '-t1', '1000000'), linesCout=0, linesCerr=0)
 
   def doAction(self):
@@ -27,7 +28,7 @@ class DmBlink(dm_testbench.DmTestbench):
     self.delay(0.5)
     self.startAndCheckSubprocess((self.binaryDmCmd, self.datamaster, 'origin', '-c0', '-t0', 'Evt_POLICE0'), linesCout=0, linesCerr=0)
     self.startAndCheckSubprocess((self.binaryDmCmd, self.datamaster, 'origin', '-c0', '-t1', 'Evt_FIREF0'), linesCout=0, linesCerr=0)
-    self.startAndCheckSubprocess((self.binaryDmCmd, self.datamaster, 'preptime', '-c0',  '-t0', '1000000000'), linesCout=0, linesCerr=0)
+    self.startAndCheckSubprocess((self.binaryDmCmd, self.datamaster, 'preptime', '-c0', '-t0', '1000000000'), linesCout=0, linesCerr=0)
     self.startAndCheckSubprocess((self.binaryDmCmd, self.datamaster, 'preptime', '-c0', '-t1', '1000000000'), linesCout=0, linesCerr=0)
     self.startAndCheckSubprocess((self.binaryDmCmd, self.datamaster, 'starttime', '-c0', '-t0', '0'), linesCout=0, linesCerr=0)
     # ~ self.startAndCheckSubprocess((self.binaryDmCmd, self.datamaster, 'starttime', '-c0', '-t1', '1000'), linesCout=0, linesCerr=0)
