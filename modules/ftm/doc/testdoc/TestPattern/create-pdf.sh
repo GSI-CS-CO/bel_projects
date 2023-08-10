6178c83517ed675ae8842e7e51ded917c235a7d6
Martin Skorsky <m.skorsky@gsi.de>
2023-08-10 16:51:34 +0200

    datamaster-tests.tex: add test_unilac.py description.

--- modules/ftm/doc/testdoc/TestPattern/create-pdf.sh
+++ modules/ftm/doc/testdoc/TestPattern/create-pdf.sh
@@ -8,6 +8,10 @@ else
   then
     echo "dot without positions, arguments: $1 '$2'"
     dot -Tpdf -o ${1//.dot/.pdf1} $1
+  elif [ "neato" = "$2" ]
+  then
+    echo "neato without positions, arguments: $1 $2"
+    neato -Goverlap=compress -Gmodel=subset -Gsep=+0.5 -Tpdf -o ${1//.dot/.pdf1} $1
   else
     echo "$2 without positions, arguments: $1 $2"
     $2 -Tpdf -o ${1//.dot/.pdf1} $1
