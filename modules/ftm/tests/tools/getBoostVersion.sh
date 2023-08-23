5ba3703d0223902dd938b40cbe773ba5707e3a7b
Martin Skorsky <m.skorsky@gsi.de>
2023-08-23 16:38:35 +0200

    Remove BOOSTPATH for asl environments. For Boost 1.75 add -DBOOST_ALLOW_DEPRECATED_HEADERS. Add version 1.0.0 for libcarpedm.so and libdmtest.so.

--- modules/ftm/tests/tools/getBoostVersion.sh
+++ modules/ftm/tests/tools/getBoostVersion.sh
@@ -3,8 +3,8 @@ echo 'Installed Boost version'
 grep -H 'BOOST_LIB_VERSION "' /usr/include/boost/version.hpp
 if [ -z ${BOOSTPATH+x} ];
 then echo "BOOSTPATH is unset";
-else echo "BOOSTAPTH is set to '$BOOSTPATH'";
-grep -H 'BOOST_LIB_VERSION "' $BOOSTPATH/include/version.hpp;
+else echo "BOOSTPATH is set to '$BOOSTPATH'";
+grep -H 'BOOST_LIB_VERSION "' $BOOSTPATH/include/boost/version.hpp;
 fi
 if [ -f boost/version.hpp ];
 then \
