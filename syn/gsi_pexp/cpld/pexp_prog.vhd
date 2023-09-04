dbaafd536241aa6b83dd82e8045caa62ca1a9209
A. Hahn <a.hahn@gsi.de>
2023-09-04 16:32:28 +0200

    pexp: cpld update

--- syn/gsi_pexp/cpld/pexp_prog.vhd
+++ syn/gsi_pexp/cpld/pexp_prog.vhd
@@ -10,8 +10,8 @@ entity pexp_tr_prog is
     confix   : inout std_logic;                    -- input from reset chip (on schematics CONFIG)
     config1  : in    std_logic;                    -- input from reset chip (on schematics CONFIG1)
     con      : out   std_logic_vector(5 downto 1); -- connection to/from fpga
-    pgclk    : in    std_logic;                    -- clock from 50 MHz oscillator
-    sel_clk  : inout std_logic_vector(1 downto 0); -- output to gbit switch
+    pgclk_i  : in    std_logic;                    -- clock from 50 MHz oscillator
+    sel_clk_o: inout std_logic_vector(1 downto 0); -- output to gbit switch
     hsw      : in    std_logic_vector(4 downto 1); -- input from hex switch
     m        : out   std_logic_vector(4 downto 0); -- config mode to FPGA
     fpga_res : in    std_logic;                    -- output to FPGA, optional reset
@@ -33,13 +33,13 @@ begin
 
   -- fixed configuration
   m          <= b"10010"; -- master SPI
-  sel_clk(0) <= '1';      -- in1 to q0 SW1
-  sel_clk(1) <= '1';      -- in1 to q1 SW1
+  sel_clk_o(0) <= '1';      -- in1 to q0 SW1
+  sel_clk_o(1) <= '1';      -- in1 to q1 SW1
   confix     <= '1';      -- immediaty ready
 
   -- counter process
-  process(pgclk) begin
-    if (rising_edge(pgclk)) then
+  process(pgclk_i) begin
+    if (rising_edge(pgclk_i)) then
       countx <= countx + 1;
     end if;
   end process;
@@ -49,7 +49,7 @@ begin
   pled(2) <= confix;     -- red
   pled(3) <= config1;    -- white
   pled(4) <= nstat;      -- blue
-  pled(5) <= pgclk;      -- green
+  pled(5) <= pgclk_i;      -- green
 
   -- connection to fpga
   con(4 downto 1) <= hsw;
