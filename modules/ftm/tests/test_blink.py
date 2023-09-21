049d46ea180abb5a823917b6d83a62e8d3ecd947
Martin Skorsky <m.skorsky@gsi.de>
2023-09-21 14:28:27 +0200

    Enable dm-cmd for running commands for multiple threads. Add tests for this in test_preptime.py.

--- modules/ftm/tests/test_blink.py
+++ modules/ftm/tests/test_blink.py
@@ -38,8 +38,8 @@ class DmBlink(dm_testbench.DmTestbench):
 
   def tearDown(self):
     # reset preptime from 1000000000 back to 1000000 after test. This is not done by dm-cmd reset all.
-    self.startAndCheckSubprocess((self.binaryDmCmd, self.datamaster, 'preptime', '-c0', '-t0', '1000000'), linesCout=0, linesCerr=0)
-    self.startAndCheckSubprocess((self.binaryDmCmd, self.datamaster, 'preptime', '-c0', '-t1', '1000000'), linesCout=0, linesCerr=0)
+    self.startAndCheckSubprocess((self.binaryDmCmd, self.datamaster, 'preptime', '-c0', '-t0', '1000000'), linesCout=1, linesCerr=0)
+    self.startAndCheckSubprocess((self.binaryDmCmd, self.datamaster, 'preptime', '-c0', '-t1', '1000000'), linesCout=1, linesCerr=0)
 
   def doAction(self, numberOfThreads):
     self.delay(0.1)
@@ -47,11 +47,11 @@ class DmBlink(dm_testbench.DmTestbench):
     self.delay(0.5)
     self.startAndCheckSubprocess((self.binaryDmCmd, self.datamaster, 'origin', '-c0', '-t0', 'Evt_POLICE0'), linesCout=0, linesCerr=0)
     self.startAndCheckSubprocess((self.binaryDmCmd, self.datamaster, 'origin', '-c0', '-t1', 'Evt_FIREF0'), linesCout=0, linesCerr=0)
-    self.startAndCheckSubprocess((self.binaryDmCmd, self.datamaster, 'preptime', '-c0', '-t0', '1000000000'), linesCout=0, linesCerr=0)
-    self.startAndCheckSubprocess((self.binaryDmCmd, self.datamaster, 'preptime', '-c0', '-t1', '1000000000'), linesCout=0, linesCerr=0)
-    self.startAndCheckSubprocess((self.binaryDmCmd, self.datamaster, 'starttime', '-c0', '-t0', '0'), linesCout=0, linesCerr=0)
+    self.startAndCheckSubprocess((self.binaryDmCmd, self.datamaster, 'preptime', '-c0', '-t0', '1000000000'), linesCout=1, linesCerr=0)
+    self.startAndCheckSubprocess((self.binaryDmCmd, self.datamaster, 'preptime', '-c0', '-t1', '1000000000'), linesCout=1, linesCerr=0)
+    self.startAndCheckSubprocess((self.binaryDmCmd, self.datamaster, 'starttime', '-c0', '-t0', '0'), linesCout=1, linesCerr=0)
     # ~ self.startAndCheckSubprocess((self.binaryDmCmd, self.datamaster, 'starttime', '-c0', '-t1', '1000'), linesCout=0, linesCerr=0)
-    self.startAndCheckSubprocess((self.binaryDmCmd, self.datamaster, 'starttime', '-c0', '-t1', '0'), linesCout=0, linesCerr=0)
+    self.startAndCheckSubprocess((self.binaryDmCmd, self.datamaster, 'starttime', '-c0', '-t1', '0'), linesCout=1, linesCerr=0)
     stdoutLines = self.startAndGetSubprocessStdout((self.binaryDmCmd, self.datamaster, 'origin', '-c0', '-t0'), linesCout=1, linesCerr=0)
     print(stdoutLines)
     stdoutLines = self.startAndGetSubprocessStdout((self.binaryDmCmd, self.datamaster, 'origin', '-c0', '-t1'), linesCout=1, linesCerr=0)
