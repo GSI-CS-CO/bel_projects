70690ef858a82a5c94f47d13eef9b0abec5fc567
Martin Skorsky <m.skorsky@gsi.de>
2023-10-10 16:09:13 +0200

    test_abort.py: add start of patterns for all threads and then abort some threads and check this with dm-cmd running.

--- modules/ftm/tests/test_abort.py
+++ modules/ftm/tests/test_abort.py
@@ -21,35 +21,57 @@ class AbortTests(dm_testbench.DmTestbench):
     """Load a schedule and start all threads. Check that these are running.
     Abort some threads. Check that these are not running.
     """
+    # Check all CPUs that no thread is running.
+    lines = self.startAndGetSubprocessOutput((self.binaryDmCmd, self.datamaster, '-c', '0xf', 'running'), [0], self.cpuQuantity, 0)
+    # ~ self.printStdOutStdErr(lines)
+    for i in range(self.cpuQuantity):
+      self.assertEqual(lines[0][i], f'CPU {i} Running Threads: 0x0', 'wrong output')
+    # Add schedules for all CPUs and start pattern on all threads.
+    self.addSchedule('pps-all-threads-cpu0.dot')
+    self.addSchedule('pps-all-threads-cpu1.dot')
+    self.addSchedule('pps-all-threads-cpu2.dot')
+    self.addSchedule('pps-all-threads-cpu3.dot')
+    index = 0
+    threadList = [('a', '0'), ('b', '1'), ('c', '2'), ('d', '3'), ('e', '4'), ('f', '5'), ('g', '6'), ('h', '7')]
+    for x, y in threadList:
+      if index < self.threadQuantity:
+        self.startAndCheckSubprocess((self.binaryDmCmd, self.datamaster, 'startpattern', 'PPS0' + x, '-t', y), [0])
+        self.startAndCheckSubprocess((self.binaryDmCmd, self.datamaster, 'startpattern', 'PPS1' + x, '-t', y), [0])
+        self.startAndCheckSubprocess((self.binaryDmCmd, self.datamaster, 'startpattern', 'PPS2' + x, '-t', y), [0])
+        self.startAndCheckSubprocess((self.binaryDmCmd, self.datamaster, 'startpattern', 'PPS3' + x, '-t', y), [0])
+        index = index + 1
+    self.checkRunningThreadsCmd()
+    # Check all CPUs that all threads are running.
+    lines = self.startAndGetSubprocessOutput((self.binaryDmCmd, self.datamaster, '-c', '0xf', 'running'), [0], self.cpuQuantity, 0)
+    # ~ self.printStdOutStdErr(lines)
+    for i in range(self.cpuQuantity):
+      self.assertEqual(lines[0][i], f'CPU {i} Running Threads: 0xff', 'wrong output')
+    # Abort some threads
     cpu = '0x3'
     thread = '0xaa'
-    command = 'abort'
     threadCount = self.bitCount(thread, self.threadQuantity)
     cpuCount = self.bitCount(cpu, self.cpuQuantity)
-    lines = self.startAndGetSubprocessOutput((self.binaryDmCmd, self.datamaster, '-c', f'{cpu}', 'running'), [0], cpuCount, 0)
-    if len(lines[0]) > 0:
-      print(f'{chr(10).join(lines[0])}')
-    if len(lines[1]) > 0:
-      print(f'{chr(10).join(lines[1])}')
-    for i in range(cpuCount):
-      self.assertEqual(lines[0][i], f'CPU {i} Running Threads: 0x0', 'wrong output')
-    lines = self.startAndGetSubprocessOutput((self.binaryDmCmd, self.datamaster, '-c', f'{cpu}', '-t', f'{thread}', command), [0], threadCount * cpuCount, 0)
-    if len(lines[0]) > 0:
-      print(f'{chr(10).join(lines[0])}')
-    if len(lines[1]) > 0:
-      print(f'{chr(10).join(lines[1])}')
+    lines = self.startAndGetSubprocessOutput((self.binaryDmCmd, self.datamaster, '-c', f'{cpu}', '-t', f'{thread}', 'abort'), [0], threadCount * cpuCount, 0)
+    # ~ self.printStdOutStdErr(lines)
     threads = self.listFromBits(thread, self.threadQuantity)
     cpus = self.listFromBits(cpu, self.cpuQuantity)
     for i in range(cpuCount):
       for j in range(threadCount):
         self.assertEqual(lines[0][i*threadCount+j], f'CPU {cpus[i]} Thread {threads[j]} aborted.', 'wrong output')
-    lines = self.startAndGetSubprocessOutput((self.binaryDmCmd, self.datamaster, '-c', f'{cpu}', 'running'), [0], cpuCount, 0)
+    # Check that the remaining threads are running
+    lines = self.startAndGetSubprocessOutput((self.binaryDmCmd, self.datamaster, '-c', '0xf', 'running'), [0], 4, 0)
+    # ~ self.printStdOutStdErr(lines)
+    for i in range(self.cpuQuantity):
+      if i in cpus:
+        self.assertEqual(lines[0][i], f'CPU {i} Running Threads: 0x55', 'wrong output')
+      else:
+        self.assertEqual(lines[0][i], f'CPU {i} Running Threads: 0xff', 'wrong output')
+
+  def printStdOutStdErr(self, lines):
     if len(lines[0]) > 0:
       print(f'{chr(10).join(lines[0])}')
     if len(lines[1]) > 0:
       print(f'{chr(10).join(lines[1])}')
-    for i in range(cpuCount):
-      self.assertEqual(lines[0][i], f'CPU {i} Running Threads: 0x0', 'wrong output')
 
   def runThreadXCommand(self, cpu, thread, command, assertText=''):
     """Test for one thread. If commandSet=True set the time (parameter) with the command.
