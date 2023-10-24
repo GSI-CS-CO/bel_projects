073ee01bbeaf2610fc02b185bd31a62c67c11260
Martin Skorsky <m.skorsky@gsi.de>
2023-10-24 15:44:46 +0200

    test_dm_cmd_noop.py: improve check of test result: run dm-cmd queue before and after noop command.

--- modules/ftm/tests/test_dm_cmd_noop.py
+++ modules/ftm/tests/test_dm_cmd_noop.py
@@ -33,4 +33,32 @@ class NoopTests(dm_testbench.DmTestbench):
     cpu = '0x3'
     thread = '0xaa'
     cpuCount = self.bitCount(cpu, self.cpuQuantity)
+    # check the result with dm-cmd ... queue Block0b.
+    lines = self.startAndGetSubprocessOutput((self.binaryDmCmd, self.datamaster, '-c', f'{cpu}', '-t', f'{thread}', 'queue', 'Block0b'), [0], 10, 0)
+    # verify the output (lines 1 to 9).
+    expectedLine = ['', 'Inspecting Queues of Block Block0b',
+    'Priority 2 (prioil)  Not instantiated',
+    'Priority 1 (priohi)  Not instantiated',
+    'Priority 0 (priolo)  RdIdx: 0 WrIdx: 0    Pending: 0']
+    for i in range(1,5):
+      self.assertEqual(lines[0][i], expectedLine[i], 'wrong output, expected: ' + expectedLine[i])
+    for i in range(5,9):
+      expectedText = '#{variable} empty   -'.format(variable=i-5)
+      messageText = 'wrong output, expected: #{variable} empty   -'.format(variable=i-5)
+      self.assertEqual(lines[0][i], expectedText, messageText)
+
     lines = self.startAndGetSubprocessOutput((self.binaryDmCmd, self.datamaster, '-c', f'{cpu}', '-t', f'{thread}', 'noop', 'Block0b'), [0], 0, 0)
+
+    # check the result with dm-cmd ... queue Block0b.
+    lines = self.startAndGetSubprocessOutput((self.binaryDmCmd, self.datamaster, '-c', f'{cpu}', '-t', f'{thread}', 'queue', 'Block0b'), [0], 10, 0)
+    # verify the output (lines 1 to 9).
+    expectedLine = ['', 'Inspecting Queues of Block Block0b',
+    'Priority 2 (prioil)  Not instantiated',
+    'Priority 1 (priohi)  Not instantiated',
+    'Priority 0 (priolo)  RdIdx: 0 WrIdx: 4    Pending: 4']
+    for i in range(1,5):
+      self.assertEqual(lines[0][i], expectedLine[i], 'wrong output, expected: ' + expectedLine[i])
+    for i in range(5,9):
+      expectedText = '#{variable} pending Valid Time: 0x0000000000000000 0000000000000000000    CmdType: noop    Qty: 1'.format(variable=i-5)
+      messageText = 'wrong output, expected: ' + expectedText
+      self.assertEqual(lines[0][i], expectedText, messageText)
