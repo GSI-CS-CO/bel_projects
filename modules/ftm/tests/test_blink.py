8036cd5f4d7812102f43e420a6259f1a78d4318f
Martin Skorsky <m.skorsky@gsi.de>
2023-11-15 16:17:42 +0100

    test_blink.py: adopt to new firmware for 32 threads.

--- modules/ftm/tests/test_blink.py
+++ modules/ftm/tests/test_blink.py
@@ -1,5 +1,4 @@
 import dm_testbench
-import pytest
 
 """
 Start blink schedule.
@@ -8,26 +7,11 @@ Required: set up of DmTestbench class.
 """
 class DmBlink(dm_testbench.DmTestbench):
 
-  @pytest.mark.thread8
   def test_blink(self):
     schedule = 'blink.dot'
     fileName = 'snoop_blink.csv'
     self.addSchedule(schedule)
-    self.snoopToCsvWithAction(fileName, self.doActionThread8, duration=10)
-    # analyse column 8 which contains the evtno.
-    # check that evtno 0x0110 and 0x0112 occur.
-    self.analyseFrequencyFromCsv(fileName, column=8, printTable=True, checkValues={'0x0001': '=1', '0x0110': '>0', '0x0112': '>0'})
-    # analyse column 20 which contains the parameter. It is important to have fid=1 in the timing messages. This ensures that we have 20 columns.
-    # check that paramter 0x123, 0x456, 0x789, and 0xABC occur.
-    self.analyseFrequencyFromCsv(fileName, column=20, printTable=True, checkValues={'0x0000000000000001': '=1', '0x0000000000000123': '>0', '0x0000000000000456': '>0', '0x0000000000000789': '>0', '0x0000000000000abc': '>0'})
-    self.deleteFile(fileName)
-
-  @pytest.mark.thread32
-  def test_blinkThread32(self):
-    schedule = 'blink.dot'
-    fileName = 'snoop_blink_thread32.csv'
-    self.addSchedule(schedule)
-    self.snoopToCsvWithAction(fileName, self.doActionThread32, duration=10)
+    self.snoopToCsvWithAction(fileName, self.doAction, duration=10)
     # analyse column 8 which contains the evtno.
     # check that evtno 0x0110 and 0x0112 occur.
     self.analyseFrequencyFromCsv(fileName, column=8, printTable=True, checkValues={'0x0001': '=1', '0x0110': '>0', '0x0112': '>0'})
@@ -42,7 +26,7 @@ class DmBlink(dm_testbench.DmTestbench):
     self.startAndCheckSubprocess((self.binaryDmCmd, self.datamaster, 'preptime', '-c0', '-t0', '1000000'), linesCout=1, linesCerr=0)
     self.startAndCheckSubprocess((self.binaryDmCmd, self.datamaster, 'preptime', '-c0', '-t1', '1000000'), linesCout=1, linesCerr=0)
 
-  def doAction(self, numberOfThreads):
+  def doAction(self):
     self.delay(0.1)
     self.startAndCheckSubprocess((self.binaryDmCmd, self.datamaster, 'startpattern', 'ping'), linesCout=1, linesCerr=0)
     self.delay(0.5)
@@ -67,10 +51,4 @@ class DmBlink(dm_testbench.DmTestbench):
     print(stdoutLines)
     self.startAndCheckSubprocess((self.binaryDmCmd, self.datamaster, 'start', '0x3'), linesCout=0, linesCerr=0)
     # we get number of threads plus 1 lines.
-    stdoutLines = self.startAndGetSubprocessStdout((self.binaryDmCmd, self.datamaster, 'heap', '-c', '0'), linesCout=1 + numberOfThreads, linesCerr=0)
-
-  def doActionThread8(self):
-    self.doAction(8)
-
-  def doActionThread32(self):
-    self.doAction(32)
+    stdoutLines = self.startAndGetSubprocessStdout((self.binaryDmCmd, self.datamaster, 'heap', '-c', '0'), linesCout=1 + self.threadQuantity, linesCerr=0)
