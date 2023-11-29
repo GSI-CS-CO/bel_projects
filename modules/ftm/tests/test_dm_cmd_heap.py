cd39ca6023caea5ce9f2b85b670286e757601ff2
Martin Skorsky <m.skorsky@gsi.de>
2023-11-29 12:59:48 +0100

    test_dm_cmd_heap.py: update comments.

--- modules/ftm/tests/test_dm_cmd_heap.py
+++ modules/ftm/tests/test_dm_cmd_heap.py
@@ -29,14 +29,13 @@ class HeapTests(dm_testbench.DmTestbench):
 
   def testHeapSingleThreadHex(self):
     """Loop for all threads aborting this thread.
-    Uses the thread number in decimal form.
+    Uses the thread number in hexadecimal form.
     """
     for cpu in range(self.cpuQuantity):
       for thread in range(self.threadQuantity):
         self.runThreadXCommand(cpu, f'0x{(1 << thread):x}')
 
   def runThreadXCommand(self, cpu, thread):
-    """Test for one thread. If commandSet=True set the time (parameter) with the command.
-    In all cases, read this value. Check the output of both commands.
+    """Test for one thread. Check the number of output lines.
     """
     self.startAndGetSubprocessStdout((self.binaryDmCmd, self.datamaster, '-c', f'{cpu}', '-t', f'{thread}', 'heap'), [0], self.threadQuantity + 1, 0)
