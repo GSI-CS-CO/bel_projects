46e689223edf6c17d4e4be972efe621ab41eb424
Martin Skorsky <m.skorsky@gsi.de>
2023-12-12 12:21:19 +0100

    conftest.py: add marker --development to skip tests which fail with current state of development.

--- modules/ftm/tests/conftest.py
+++ modules/ftm/tests/conftest.py
@@ -4,9 +4,13 @@ def pytest_addoption(parser):
     parser.addoption(
         "--runslow", action="store_true", default=False, help="run slow tests"
     )
+    parser.addoption(
+        "--development", action="store_true", default=False, help="run tests under developement"
+    )
 
 def pytest_configure(config):
     config.addinivalue_line("markers", "slow: mark test as slow to run")
+    config.addinivalue_line("markers", "development: mark test as under development")
 
 def pytest_collection_modifyitems(config, items):
     # --runslow NOT given in cli: skip slow tests
@@ -14,4 +18,10 @@ def pytest_collection_modifyitems(config, items):
       skip_slow = pytest.mark.skip(reason="need --runslow option to run")
       for item in items:
           if "slow" in item.keywords:
+              item.add_marker(skip_slow)
+    # --development NOT given in cli: skip tests under development
+    if not config.getoption("--development"):
+      skip_slow = pytest.mark.skip(reason="need --development option to run")
+      for item in items:
+          if "development" in item.keywords:
               item.add_marker(skip_slow)
