711c6d1699d436abb5303fe03eeb0d43d7c04d4f
Martin Skorsky <m.skorsky@gsi.de>
2024-01-23 14:18:36 +0100

    test_dm_cmd_hex.py: add test for dm-cmd hex.

---
+++ modules/ftm/tests/test_dm_cmd_hex.py
@@ -0,0 +1,34 @@
+import dm_testbench    # contains super class
+
+"""
+Module collects tests for dm-cmd with the command 'hex < node>'.
+"""
+class HexTests(dm_testbench.DmTestbench):
+
+  def testHexdumpTarget(self):
+    """Prepare all threads on all CPUs.
+    Compare the output of 'dm-cmd hex Block0a' and
+    'dm-cmd hex Block3a_ListDst_0' with the expected lines.
+    """
+    expectedLinesBlock0a=[
+      "Block0a:",
+      "  0000  00 00 00 00 3b 9a ca 00 00 00 00 00 10 00 0c a8  ....;...........",
+      "  0010  00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00  ................",
+      "  0020  00 00 00 00 00 00 00 00 22 40 8c 2f 00 10 81 07  ........\"@./....",
+      "  0030  10 00 0b 08"
+    ]
+    expectedLinesBlock3a_ListDst_0=[
+      "Block3a_ListDst_0:",
+      "  0000  10 00 0b 08 00 00 00 00 00 00 00 00 00 00 00 00  ................",
+      "  0010  00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00  ................",
+      "  0020  00 00 00 00 00 00 00 00 17 b1 0d 8f 00 00 00 0c  ................",
+      "  0030  10 00 09 68"
+    ]
+    self.prepareRunThreads()
+    lines = self.startAndGetSubprocessOutput((self.binaryDmCmd, self.datamaster, 'hex', 'Block0a'), [0], 5, 0)
+    for i in range(len(lines[0])):
+      self.assertEqual(expectedLinesBlock0a[i], lines[0][i])
+    lines = self.startAndGetSubprocessOutput((self.binaryDmCmd, self.datamaster, 'hex', 'Block3a_ListDst_0'), [0], 5, 0)
+    for i in range(len(lines[0])):
+      self.assertEqual(expectedLinesBlock3a_ListDst_0[i], lines[0][i])
+
