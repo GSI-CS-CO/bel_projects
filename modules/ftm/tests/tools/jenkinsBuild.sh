ed71f4914cb8f16ea9a4abe586b4c30defba5289
Martin Skorsky <m.skorsky@gsi.de>
2024-01-23 11:36:44 +0100

    tools/jenkinsBuild.sh: add script of Jenkins build steps. Use this in Jenkins jobs. Correction: default 8 threads if no argument supplied.

--- modules/ftm/tests/tools/jenkinsBuild.sh
+++ modules/ftm/tests/tools/jenkinsBuild.sh
@@ -1,5 +1,11 @@
 # create links needed for Rocky-9 environment
 date
+if [ $# -eq 1 ]
+then
+  THR_QTY=$1
+else
+  THR_QTY=8
+fi
 cd res/rocky-9
 ./generate_soft_links.sh
 export PATH=$PATH:$(pwd)
@@ -16,7 +22,7 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$WORKSPACE/ip_cores/etherbone-core/api/.
 EBPATH1=../../../ip_cores/etherbone-core/api make prepare
 export PATH=$PATH:$WORKSPACE/tools/:$WORKSPACE/modules/ftm/bin/:$WORKSPACE/modules/ftm/analysis/scheduleCompare/main/
 # build ftm lm32 firmware
-THR_QTY=$1 PATH=$PATH:$HOME/.local/bin:$WORKSPACE/lm32-toolchain/bin/ make -C $WORKSPACE/syn/gsi_pexarria5/ftm/ ftm.bin
+THR_QTY=$THR_QTY PATH=$PATH:$HOME/.local/bin:$WORKSPACE/lm32-toolchain/bin/ make -C $WORKSPACE/syn/gsi_pexarria5/ftm/ ftm.bin
 # load the required lm32 firmware into fel0069
 $WORKSPACE/syn/gsi_pexarria5/ftm/fwload_all.sh tcp/fel0069.acc.gsi.de $WORKSPACE/syn/gsi_pexarria5/ftm/ftm.bin
 # run all tests; date: timestamp to get duration of tests.
