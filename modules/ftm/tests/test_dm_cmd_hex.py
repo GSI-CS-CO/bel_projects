4636eac270175cb4c4e480e6cd72b3110a7aa10c
Martin Skorsky <m.skorsky@gsi.de>
2024-01-25 10:41:00 +0100

    test_dm_cmd_hex.py: use only CPU 0 for the test to make it faster.

--- modules/ftm/tests/test_dm_cmd_hex.py
+++ modules/ftm/tests/test_dm_cmd_hex.py
@@ -8,8 +8,9 @@ class HexTests(dm_testbench.DmTestbench):
   def testHexdumpTarget(self):
     """Prepare all threads on all CPUs.
     Compare the output of 'dm-cmd hex Block0a' and
-    'dm-cmd hex Block3a_ListDst_0' with the expected lines.
+    'dm-cmd hex Block0a_ListDst_0' with the expected lines.
     """
+    # setup the expected output for 8 and 32 threads.
     expectedLinesBlock0a=[
       "Block0a:",
       "  0000  00 00 00 00 3b 9a ca 00 00 00 00 00 10 00 0c a8  ....;...........",
@@ -20,21 +21,23 @@ class HexTests(dm_testbench.DmTestbench):
     if self.threadQuantity == 32:
       expectedLinesBlock0a[1] = "  0000  00 00 00 00 3b 9a ca 00 00 00 00 00 10 00 12 a8  ....;..........."
       expectedLinesBlock0a[4] = "  0030  10 00 11 08"
-    expectedLinesBlock3a_ListDst_0=[
-      "Block3a_ListDst_0:",
+    expectedLinesBlock0a_ListDst_0=[
+      "Block0a_ListDst_0:",
       "  0000  10 00 0b 08 00 00 00 00 00 00 00 00 00 00 00 00  ................",
       "  0010  00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00  ................",
-      "  0020  00 00 00 00 00 00 00 00 17 b1 0d 8f 00 00 00 0c  ................",
+      "  0020  00 00 00 00 00 00 00 00 06 78 81 46 00 00 00 0c  .........x.F....",
       "  0030  10 00 09 68"
     ]
     if self.threadQuantity == 32:
-      expectedLinesBlock3a_ListDst_0[1] = "  0000  10 00 11 08 00 00 00 00 00 00 00 00 00 00 00 00  ................"
-      expectedLinesBlock3a_ListDst_0[4] = "  0030  10 00 0f 68"
-    self.prepareRunThreads()
+      expectedLinesBlock0a_ListDst_0[1] = "  0000  10 00 11 08 00 00 00 00 00 00 00 00 00 00 00 00  ................"
+      expectedLinesBlock0a_ListDst_0[4] = "  0030  10 00 0f 68"
+    # run schedules on CPU 0.
+    self.prepareRunThreads(1)
+    # the command to test: dm-cmd hex, applied for two nodes.
     lines = self.startAndGetSubprocessOutput((self.binaryDmCmd, self.datamaster, 'hex', 'Block0a'), [0], 5, 0)
     for i in range(len(lines[0])):
       self.assertEqual(expectedLinesBlock0a[i], lines[0][i])
-    lines = self.startAndGetSubprocessOutput((self.binaryDmCmd, self.datamaster, 'hex', 'Block3a_ListDst_0'), [0], 5, 0)
+    lines = self.startAndGetSubprocessOutput((self.binaryDmCmd, self.datamaster, 'hex', 'Block0a_ListDst_0'), [0], 5, 0)
     for i in range(len(lines[0])):
-      self.assertEqual(expectedLinesBlock3a_ListDst_0[i], lines[0][i])
+      self.assertEqual(expectedLinesBlock0a_ListDst_0[i], lines[0][i])
 
