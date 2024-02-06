fa6c757d40eb852be4a79541f2d4a5bad9ff24c6
Mathias Kreider <mkreider@posteo.de>
2024-02-06 16:29:23 +0100

    DM: lib: going for separate Table for statics

---
+++ modules/ftm/include/statictable.h
@@ -0,0 +1,212 @@
+#ifndef _GROUP_TABLE_H_
+#define _GROUP_TABLE_H_
+
+#include <stdint.h>
+#include <string>
+#include <iostream>
+#include <sstream>
+#include <set>
+#include <boost/optional.hpp>
+#include <boost/container/vector.hpp>
+#include <boost/multi_index_container.hpp>
+#include <boost/multi_index/member.hpp>
+#include <boost/multi_index/hashed_index.hpp>
+#include <boost/multi_index/ordered_index.hpp>
+#include <boost/multi_index/composite_key.hpp>
+#include <fstream>
+#include <boost/archive/text_oarchive.hpp>
+#include <boost/archive/text_iarchive.hpp>
+#include "dotstr.h"
+#include "common.h"
+
+
+using boost::multi_index_container;
+using namespace boost::multi_index;
+
+
+using namespace DotStr::Misc;
+
+struct StaticMeta {
+  uint8_t     cpu;
+  uint32_t    adr;
+  uint32_t    hash;
+  uint32_t    offs;
+  uint32_t    size;
+  vertex_t    v;
+
+//ext int / global local statt CPU ? gefährlich (blockade) ?
+  StaticMeta(uint8_t cpu, uint32_t adr, uint32_t hash) : cpu(cpu), adr(adr), hash(hash), size(size), v(null_vertex) {}
+  StaticMeta(uint8_t cpu, uint32_t adr, uint32_t hash) : cpu(cpu), adr(adr), hash(hash), size(4), v(null_vertex) {}
+
+  // Multiindexed Elements are immutable, must use the modify function of the container to change attributes
+};
+
+
+
+
+
+typedef boost::multi_index_container<
+  StaticMeta,
+  indexed_by<
+    hashed_unique<
+      tag<Vertex>,  BOOST_MULTI_INDEX_MEMBER(StaticMeta,vertex_t,v)>,
+    hashed_unique<
+      tag<Hash>,  BOOST_MULTI_INDEX_MEMBER(StaticMeta,uint32_t,hash)>,
+    ordered_unique<
+      tag<CpuAdr>,
+      composite_key<
+        StaticMeta,
+        BOOST_MULTI_INDEX_MEMBER(StaticMeta,uint8_t,cpu),
+        BOOST_MULTI_INDEX_MEMBER(StaticMeta,uint32_t,adr)
+      >
+    >
+  >
+ > StaticMeta_set;
+
+
+
+struct StaticMeta {
+  std::string node;  //name of pattern
+  std::string pattern;  //name of pattern
+  bool patternEntry, patternExit;
+  std::string beamproc; //name of entry node
+  bool beamprocEntry, beamprocExit;
+
+
+  StaticMeta(const std::string& node) : node(node), pattern(sUndefined),  patternEntry(false), patternExit(false), beamproc(sUndefined), beamprocEntry(false), beamprocExit(false) {}
+  StaticMeta() : node(sUndefined), pattern(sUndefined),  patternEntry(false), patternExit(false), beamproc(sUndefined), beamprocEntry(false), beamprocExit(false) {}
+
+  friend class boost::serialization::access;
+  template<class Archive>
+  void serialize(Archive & ar, const unsigned int version)
+  {
+      ar & node;
+      ar & pattern;
+      ar & patternEntry;
+      ar & patternExit;
+      ar & beamproc;
+      ar & beamprocEntry;
+      ar & beamprocExit;
+
+
+  }
+
+};
+
+//necessary to avoid confusion with classnames elsewhere
+namespace Statics {
+
+struct Hash{};
+struct Vertex{};
+struct CpuAdr{};
+
+
+
+}
+
+
+
+
+typedef boost::multi_index_container<
+  StaticMeta,
+  indexed_by<
+    ordered_unique <
+      tag<Groups::Node>,  BOOST_MULTI_INDEX_MEMBER(StaticMeta, std::string, node)>,
+    ordered_non_unique <
+      tag<Groups::Pattern>,  BOOST_MULTI_INDEX_MEMBER(StaticMeta, std::string, pattern)>,
+    ordered_non_unique <
+      tag<Groups::Beamproc>,  BOOST_MULTI_INDEX_MEMBER(StaticMeta, std::string, beamproc)>
+  >
+ > StaticMeta_set;
+
+typedef StaticMeta_set::iterator pmI;
+
+/* accepted
+
+  //the iterator dilemma
+
+ivs.get<1>() gives you index, not iterator. You need to call begin(), end() and other methods on that index to get iterator (like you do on containers). You better use typedef though:
+
+indexed_vertex_set ivs;
+typedef indexed_vertex_set::nth_index<1>::type sorted_index;
+sorted_index &idx = ivs.get<1>();
+for( sorted_index::iterator it = idx.begin(); it != idx.end(); ++it ) {
+    it->vid = 123; // getting access to fields
+}
+
+*/
+
+
+
+class StaticTable {
+private:
+  friend class boost::serialization::access;
+  template<class Archive>
+  void serialize(Archive & ar, const unsigned int version)
+  {
+      ar & a;
+  }
+
+  StaticMeta_set a;
+
+
+public:
+
+  StaticTable(){};
+  ~StaticTable(){};
+  StaticTable(StaticTable const &src) : a(src.a) {};
+
+  StaticTable &operator=(const StaticTable &src)
+  {
+    a = src.a;
+    return *this;
+  }
+
+  std::string store();
+  void load(const std::string& s);
+
+  bool insert(const std::string& sNode);
+
+  template <typename Tag>
+  pmI lookup(const std::string& s)   {auto tmp = a.get<Tag>().find(s); return a.iterator_to(*tmp);}
+
+
+  //Lookup a node, create if non existent. Single return value as node names are uniqu./d
+  pmI lookupOrCreateNode(const std::string& sNode);
+
+
+  template <typename Tag>
+  bool remove(const std::string& s) {
+    auto range = a.get<Tag>().equal_range(s);
+    auto it = a.get<Tag>().erase(range.first, range.second);
+    return (it != a.end() ? true : false);
+  };
+
+  void clear() { a.clear(); }
+
+  void setPattern (pmI it, const std::string& sNew, bool entry, bool exit)  { a.modify(it, [sNew, entry, exit](StaticMeta& p){p.pattern  = sNew; p.patternEntry  = entry; p.patternExit  = exit;}); }
+  void setPattern (pmI it, const std::string& sNew) { setPattern(it, sNew, false, false); }
+  void setPattern (const std::string& sNode, const std::string& sNew, bool entry, bool exit) { setPattern(lookupOrCreateNode(sNode), sNew, entry, exit); }
+  void setPattern (const std::string& sNode, const std::string& sNew) { setPattern(sNode, sNew, false, false); }
+
+  template <typename Tag, std::string StaticMeta::*group>
+  vStrC getGroups(const std::string& sNode) {
+    vStrC res; auto x  = a.get<Tag>().equal_range(sNode);
+    for (auto it = x.first; it != x.second; ++it) {res.push_back(*it.*group); }
+    return res;
+  }
+
+
+
+
+
+  const StaticMeta_set& getTable() const { return a; }
+  const size_t getSize()          const { return a.size(); }
+
+
+  void debug(std::ostream& os);
+
+
+};
+
+#endif
