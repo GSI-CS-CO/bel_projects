6b09e4086ce60b8a22cb4b4eaba716815d330ad2
Martin Skorsky <m.skorsky@gsi.de>
2024-02-14 15:59:55 +0100

    test_overwrite.py: complete testOverwrite2 (overwrite a switch-schedule with itself to rearrange the original edge types).

--- modules/ftm/tests/test_overwrite.py
+++ modules/ftm/tests/test_overwrite.py
@@ -36,34 +36,43 @@ class DmSchedOverwrite(dm_testbench.DmTestbench):
     self.startAndCheckSubprocess([self.binaryDmSched, self.datamaster, 'status', '-o', self.downloadFile1])
 
   def testOverwrite2(self):
-    """Load and start pattern A, a loop of a message and a block.
-    Stop the pattern and remove parts of the pattern which is nearly the same, but
-    the block has no type. The result is a schedule with only the block.
+    """Load a schedule with a switch. Executing the switch interchanges
+    the defdst to EvtA with the altdst to EvtB. Then the schedule loops
+    over EvtB.
+    Overwrite the schedule with the original schedule changes the edges back
+    to defdst to EvtA and altdst to EvtB. Then the switch is executed again.
     """
     snoopFile = 'snoop_overwrite2.csv'
     self.scheduleFile0 = 'overwrite2-0.dot'
-    self.scheduleFile1 = 'overwrite2-1.dot'
+    # ~ self.scheduleFile1 = 'overwrite2-1.dot'
     self.downloadFile0 = self.scheduleFile0.replace('.dot', '-download.dot')
-    self.downloadFile1 = self.scheduleFile1.replace('.dot', '-download.dot')
+    # ~ self.downloadFile1 = self.scheduleFile1.replace('.dot', '-download.dot')
+    self.downloadFile1 = 'overwrite2-1-download.dot'
+    self.downloadFile2 = 'overwrite2-2-download.dot'
     self.snoopToCsvWithAction(snoopFile, self.actionOverwrite2, duration=2)
     self.startAndCheckSubprocess(('scheduleCompare', '-u', self.schedulesFolder + self.scheduleFile0, self.downloadFile0))
     self.deleteFile(self.downloadFile0)
     self.startAndCheckSubprocess(('scheduleCompare', '-u', self.schedulesFolder + self.downloadFile1, self.downloadFile1))
     self.deleteFile(self.downloadFile1)
-    self.analyseFrequencyFromCsv(snoopFile, column=20, printTable=True, checkValues={'0x0000000000000001': '>10'})
+    self.startAndCheckSubprocess(('scheduleCompare', '-u', self.schedulesFolder + self.downloadFile2, self.downloadFile2))
+    self.deleteFile(self.downloadFile2)
+    self.analyseFrequencyFromCsv(snoopFile, column=20, printTable=True, checkValues={'0x0000000000000001': '2', '0x0000000000000002': '>10'})
     self.deleteFile(snoopFile)
 
   def actionOverwrite2(self):
-    """During snoop start pattern A. This produces messages with 10Hz.
-    Download the schedule for later compare.
-    Stop the pattern and remove part of the schedule.
+    """During snoop:
+    Add a schedule, save the status for later compare.
+    Start pattern A and save the status for later compare.
+    Stop the pattern.
+    Overwrite the schedule with the original one and save the status for later compare.
     """
     self.addSchedule(self.scheduleFile0)
-    self.startAndCheckSubprocess((self.binaryDmCmd, self.datamaster, 'startpattern', 'A'), [0], 1, 0)
-    self.delay(1.0)
     self.startAndCheckSubprocess([self.binaryDmSched, self.datamaster, 'status', '-o', self.downloadFile0])
+    self.startAndCheckSubprocess((self.binaryDmCmd, self.datamaster, 'startpattern', 'A'), [0], 1, 0)
+    self.delay(0.5)
+    self.startAndCheckSubprocess([self.binaryDmSched, self.datamaster, 'status', '-o', self.downloadFile1])
     self.startAndCheckSubprocess([self.binaryDmCmd, self.datamaster, 'stoppattern', 'A'])
     self.delay(0.1)
-    self.startAndCheckSubprocess([self.binaryDmSched, self.datamaster, 'overwrite', self.schedulesFolder + self.scheduleFile1], [250], 2, 2)
+    self.startAndCheckSubprocess([self.binaryDmSched, self.datamaster, 'overwrite', self.schedulesFolder + self.scheduleFile0], [0], 0, 0)
+    self.startAndCheckSubprocess([self.binaryDmSched, self.datamaster, 'status', '-o', self.downloadFile2])
     self.startAndCheckSubprocess((self.binaryDmCmd, self.datamaster, 'startpattern', 'A'), [0], 1, 0)
-    self.startAndCheckSubprocess([self.binaryDmSched, self.datamaster, 'status', '-o', self.downloadFile1])
