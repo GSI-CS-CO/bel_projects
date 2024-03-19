5861ae4996dfad8769f8f40cd1d2a3bc63c5aa17
Martin Skorsky <m.skorsky@gsi.de>
2024-03-19 16:02:13 +0100

    test_Cpu0Cpu1.py: add testTwoCpusFlush and testTwoCpusOrigin with edges from CPU 0 to CPU 1.

--- modules/ftm/tests/test_Cpu0Cpu1.py
+++ modules/ftm/tests/test_Cpu0Cpu1.py
@@ -50,3 +50,51 @@ class ConnectCpus(dm_testbench.DmTestbench):
     self.startAndCheckSubprocess((self.binaryDmCmd, self.datamaster, 'startpattern', 'X'), [0], 1, 0)
     self.delay(1.0)
     self.startAndCheckSubprocess((self.binaryDmSched, self.datamaster, 'status', '-o', self.downloadFile0), [0], 0, 0)
+
+  def testTwoCpusFlush(self):
+    """Load a schedule with a flushovr edge that connects a node on CPU 0
+    with a node on CPU 1.
+    """
+    snoopFile = 'snoop_TwoCpusFlush.csv'
+    self.scheduleFile0 = 'cpu0-1-flush-block-flushovr.dot'
+    self.downloadFile0 = self.scheduleFile0.replace('.dot', '-download.dot')
+    self.snoopToCsvWithAction(snoopFile, self.actionTwoCpusFlush, duration=1)
+    self.startAndCheckSubprocess(('scheduleCompare', '-s', '-u', self.schedulesFolder + self.scheduleFile0, self.downloadFile0), [0], 0, 0)
+    self.deleteFile(self.downloadFile0)
+    self.analyseFrequencyFromCsv(snoopFile, column=20, printTable=True, checkValues={'0x0000000000000000': '>35', '0x0000000000000001': '>80'}, addDelayed=True)
+    self.deleteFile(snoopFile)
+
+  def actionTwoCpusFlush(self):
+    """During snoop start pattern A and X. This produces messages.
+    Download the schedule for later compare.
+    """
+    self.addSchedule(self.scheduleFile0)
+    self.startAndCheckSubprocess((self.binaryDmCmd, self.datamaster, 'startpattern', 'A'), [0], 1, 0)
+    self.delay(0.1)
+    self.startAndCheckSubprocess((self.binaryDmCmd, self.datamaster, 'startpattern', 'X'), [0], 1, 0)
+    self.delay(1.0)
+    self.startAndCheckSubprocess((self.binaryDmSched, self.datamaster, 'status', '-o', self.downloadFile0), [0], 0, 0)
+
+  def testTwoCpusOrigin(self):
+    """Load a schedule with a origindst edge that connects a node on CPU 0
+    with a node on CPU 1.
+    """
+    snoopFile = 'snoop_TwoCpusOrigin.csv'
+    self.scheduleFile0 = 'cpu0-1-origin-block-origindst.dot'
+    self.downloadFile0 = self.scheduleFile0.replace('.dot', '-download.dot')
+    self.snoopToCsvWithAction(snoopFile, self.actionTwoCpusOrigin, duration=1)
+    self.startAndCheckSubprocess(('scheduleCompare', '-s', '-u', self.schedulesFolder + self.scheduleFile0, self.downloadFile0), [0], 0, 0)
+    self.deleteFile(self.downloadFile0)
+    self.analyseFrequencyFromCsv(snoopFile, column=20, printTable=True, checkValues={'0x0000000000000000': '>35', '0x0000000000000001': '>10'}, addDelayed=True)
+    self.deleteFile(snoopFile)
+
+  def actionTwoCpusOrigin(self):
+    """During snoop start pattern X and A. This produces messages.
+    Download the schedule for later compare.
+    """
+    self.addSchedule(self.scheduleFile0)
+    self.startAndCheckSubprocess((self.binaryDmCmd, self.datamaster, 'startpattern', 'A'), [0], 1, 0)
+    self.delay(0.1)
+    self.startAndCheckSubprocess((self.binaryDmCmd, self.datamaster, 'startpattern', 'X'), [0], 1, 0)
+    self.delay(1.0)
+    self.startAndCheckSubprocess((self.binaryDmSched, self.datamaster, 'status', '-o', self.downloadFile0), [0], 0, 0)
