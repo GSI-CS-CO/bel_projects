6bed752ff91549c82afe5a9f91537fb2dee1c804
A. Hahn <a.hahn@gsi.de>
2024-03-20 16:04:46 +0100

    a10ts: update Manifest.py

--- modules/a10ts/Manifest.py
+++ modules/a10ts/Manifest.py
@@ -1,4 +1,3 @@
-files = [
-  "a10ts_pkg.vhd",
-  "a10ts.vhd",
-]
+if syn_device[:1] == "5": files = [ "src/hdl/a10ts_pkg.vhd" ]
+if syn_device[:4] == "ep2a": files = [ "src/hdl/a10ts_pkg.vhd" ]
+if syn_device[:2] == "10": files = [ "src/hdl/a10ts_pkg.vhd" "src/hdl/a10ts_pkg.vhd" "src/hdl/a10ts.vhd"]
