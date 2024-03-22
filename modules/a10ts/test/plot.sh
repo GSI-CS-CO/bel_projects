09b180fa4e3cbf003819f038dffc6c6ac44316ef
A. Hahn <a.hahn@gsi.de>
2024-03-22 15:44:50 +0100

    a10ts: plot.sh -> change scaling

--- modules/a10ts/test/plot.sh
+++ modules/a10ts/test/plot.sh
@@ -114,7 +114,7 @@ clean_up_log_files
 while true; do
   sample_data
   if [ $plot_started -eq 0 ]; then
-    gnuplot -p -e 'set title "Temperature Measurements"; set autoscale x; set key top left box; set grid; set xlabel "FPGA Uptime [Decimal Hours]"; set ylabel "Temperature [Degree Celsius]"; set yrange [0:140]; plot "gnuplot_int.pipe" smooth bezier lt 2 lw 2 linecolor rgb "orange" title "FPGA (Internal) Temperature", "gnuplot_ext.pipe" smooth bezier lt 2 lw 2 linecolor rgb "blue" title "Board (External) Temperature"; while (1) { pause 1; replot; };' 2>>/dev/null &
+    gnuplot -p -e 'set title "Temperature Measurements"; set autoscale xfix; set key top left box; set grid; set xlabel "FPGA Uptime [Decimal Hours]"; set ylabel "Temperature [Degree Celsius]"; set yrange [0:140]; plot "gnuplot_int.pipe" smooth bezier lt 2 lw 2 linecolor rgb "orange" title "FPGA (Internal) Temperature", "gnuplot_ext.pipe" smooth bezier lt 2 lw 2 linecolor rgb "blue" title "Board (External) Temperature"; while (1) { pause 1; replot; };' 2>>/dev/null &
     gnuplot_pid=$!
     plot_started=1
     echo "Info: Press Ctrl+C to end the script, then close gnuplot ($gnuplot_pid) ..."
