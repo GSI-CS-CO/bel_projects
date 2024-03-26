1d555059ddab4ae8a2ca2d4a86c06bc54b71d6e0
Martin Skorsky <m.skorsky@gsi.de>
2024-03-26 14:28:11 +0100

    test_schedules.py: tests with longer snoop (10min, 1h) commented out.

--- modules/ftm/tests/test_schedules.py
+++ modules/ftm/tests/test_schedules.py
@@ -1,4 +1,5 @@
 import dm_testbench
+import pytest
 
 """
 Start all pattern in a schedule and analyse the frequency of timing messages for 6 seconds.
@@ -8,7 +9,7 @@ class Schedules(dm_testbench.DmTestbench):
   def test_frequency_schedule1(self):
     self.startAllPattern('schedule1.dot')
     file_name = 'snoop_schedule1.csv'
-    parameter_column = 8 #20
+    parameter_column = 8
     self.snoopToCsv(file_name, duration=6)
     self.analyseFrequencyFromCsv(file_name, parameter_column)
     self.deleteFile(file_name)
@@ -17,31 +18,34 @@ class Schedules(dm_testbench.DmTestbench):
     if self.cpuQuantity > 3:
       self.startAllPattern('schedule2.dot')
       file_name = 'snoop_schedule2.csv'
-      parameter_column = 8 #20
+      parameter_column = 8
       self.snoopToCsv(file_name, duration=6)
       self.analyseFrequencyFromCsv(file_name, parameter_column)
       self.deleteFile(file_name)
 
-  def t1est_frequency_schedule1_0060(self):
-    self.startAllPattern('schedule1.dot')
-    file_name = 'snoop_schedule1.csv'
-    parameter_column = 8
-    self.snoopToCsv(file_name, duration=60)
-    self.analyseFrequencyFromCsv(file_name, parameter_column)
-    self.deleteFile(file_name)
+  # ~ @pytest.mark.development
+  # ~ def test_frequency_schedule1_0060(self):
+    # ~ self.startAllPattern('schedule1.dot')
+    # ~ file_name = 'snoop_schedule1.csv'
+    # ~ parameter_column = 8
+    # ~ self.snoopToCsv(file_name, duration=60)
+    # ~ self.analyseFrequencyFromCsv(file_name, parameter_column)
+    # ~ self.deleteFile(file_name)
 
-  def t1est_frequency_schedule1_0600(self):
-    self.startAllPattern('schedule1.dot')
-    file_name = 'snoop_schedule1.csv'
-    parameter_column = 8
-    self.snoopToCsv(file_name, duration=600)
-    self.analyseFrequencyFromCsv(file_name, parameter_column)
-    self.deleteFile(file_name)
+  # ~ @pytest.mark.development
+  # ~ def test_frequency_schedule1_0600(self):
+    # ~ self.startAllPattern('schedule1.dot')
+    # ~ file_name = 'snoop_schedule1.csv'
+    # ~ parameter_column = 8
+    # ~ self.snoopToCsv(file_name, duration=600)
+    # ~ self.analyseFrequencyFromCsv(file_name, parameter_column)
+    # ~ self.deleteFile(file_name)
 
-  def t1est_frequency_schedule1_3600(self):
-    self.startAllPattern('schedule1.dot')
-    file_name = 'snoop_schedule1.csv'
-    parameter_column = 8
-    self.snoopToCsv(file_name, duration=3600)
-    self.analyseFrequencyFromCsv(file_name, parameter_column)
-    self.deleteFile(file_name)
+  # ~ @pytest.mark.development
+  # ~ def test_frequency_schedule1_3600(self):
+    # ~ self.startAllPattern('schedule1.dot')
+    # ~ file_name = 'snoop_schedule1.csv'
+    # ~ parameter_column = 8
+    # ~ self.snoopToCsv(file_name, duration=3600)
+    # ~ self.analyseFrequencyFromCsv(file_name, parameter_column)
+    # ~ self.deleteFile(file_name)
