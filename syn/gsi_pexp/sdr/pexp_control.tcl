d19408b3bf1d4ea6069188a6ee15c67fe11be891
A. Hahn <a.hahn@gsi.de>
2024-03-27 16:02:41 +0100

    pexp: add SDR edition

---
+++ syn/gsi_pexp/sdr/pexp_control.tcl
@@ -0,0 +1,9 @@
+set platform "pexp"
+source ../../autogen.tcl
+source ../../../modules/build_id/build_id.tcl
+source ../../../ip_cores/general-cores/platform/altera/networks/arria5.tcl
+source ../../../ip_cores/general-cores/platform/altera/wb_pcie/arria5.tcl
+source ../../../modules/pll/arria5/arria5_pll.tcl
+source ../../../ip_cores/wr-cores/platform/altera/wr_arria5_phy/wr_arria5_phy.tcl
+source ../../common/arria5_legacy_flash_patch.tcl
+source ../../common/arria5_serdes_lvds_patch.tcl
