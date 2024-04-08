4530db332b70a54d244176ebe21db300220d7760
Mathias <m.kreider@gsi.de>
2024-04-08 17:32:17 +0200

    DM: lib: added reference location LUT system

--- modules/ftm/include/reflocation.h
+++ modules/ftm/include/reflocation.h
@@ -1,98 +1,60 @@
+#ifndef _REFLOCATION_H_
+#define _REFLOCATION_H_
 
 #include <stdint.h>
 #include <string>
-#include <iostream>
-#include <set>
-#include <boost/optional.hpp>
 #include <boost/bimap.hpp>
-#include <boost/multi_index_container.hpp>
-#include <boost/multi_index/member.hpp>
-#include <boost/multi_index/ordered_index.hpp>
-#include <boost/multi_index/composite_key.hpp>
 
-using boost::multi_index_container;
-using namespace boost::multi_index;
+typedef boost::bimap< std::string, uint32_t > MemLocMap;
+typedef boost::bimap< std::string, uint32_t > MemFieldMap;
+extern MemLocMap mlm;
+extern MemFieldMap mfm;
 
-typedef boost::bimap< const std::string, uint32_t > memLocMap;
-typedef boost::bimap< const std::string, uint32_t > memFieldMap;
+template<typename LeftType, typename RightType>
+void printBimap(const boost::bimap<LeftType, RightType>& bm);
+void showMemLocMap();
+void showMemFieldMap();
 
+struct SearchResult {
+  MemLocMap::iterator itL;
+  MemFieldMap::iterator itF;
 
-struct memoryArea {
-  const std::string& name;
-  const uint32_t&    adr;
-  const uint32_t&    size;
-  vertex_t    v;
-};  
+  SearchResult(const std::string& sLoc, const std::string& sField) {
+    MemLocMap::left_iterator itL    = mlm.left.find(sLoc);
+    MemFieldMap::left_iterator itF  = mfm.left.find(sField);
 
+    this->itL = mlm.project_up(itL);
+    this->itF = mlm.project_up(itF);
+  };
 
-  AllocMeta(uint8_t cpu, uint32_t adr, uint32_t hash) : cpu(cpu), adr(adr), hash(hash) {std::memset(b, 0, sizeof b);}
-  AllocMeta(uint8_t cpu, uint32_t adr, uint32_t hash, vertex_t v) : cpu(cpu), adr(adr), hash(hash), v(v), staged(false) {std::memset(b, 0, sizeof b);}
-  AllocMeta(uint8_t cpu, uint32_t adr, uint32_t hash, vertex_t v, bool staged) : cpu(cpu), adr(adr), hash(hash), v(v), staged(staged) {std::memset(b, 0, sizeof b);}
+  SearchResult(uint32_t searchAdr) {
+    MemLocMap::right_iterator itL = mlm.right.upper_bound(searchAdr);
+    --itL;
+    MemFieldMap::right_iterator itF = mfm.right.upper_bound(searchAdr - itL->first);
+    --itF;
 
-  // Multiindexed Elements are immutable, must use the modify function of the container to change attributes
+    this->itL = mlm.project_up(itL);
+    this->itF = mlm.project_up(itF);
+  };
+
+  SearchResult(MemLocMap::iterator itL, MemFieldMap::iterator itF) : itL(itL), itF(itF) {};
+
+  std::string getLocName() {
+    return itL->left;
+  }
+
+  std::string getFieldName() {
+    return itF->left;
+  }
+
+  uint32_t getLocVal() {
+    return itL->right;
+  }
+
+  uint32_t getFieldVal() {
+    return itF->right;
+  }
+
 };
 
-
-struct Hash{};
-struct Vertex{};
-struct CpuAdr{};
-
-
-typedef boost::multi_index_container<
-  AllocMeta,
-  indexed_by<
-    hashed_unique<
-      tag<Vertex>,  BOOST_MULTI_INDEX_MEMBER(AllocMeta,vertex_t,v)>,
-    hashed_unique<
-      tag<Hash>,  BOOST_MULTI_INDEX_MEMBER(AllocMeta,uint32_t,hash)>,
-    ordered_unique<
-      tag<CpuAdr>,
-      composite_key<
-        AllocMeta,
-        BOOST_MULTI_INDEX_MEMBER(AllocMeta,uint8_t,cpu),
-        BOOST_MULTI_INDEX_MEMBER(AllocMeta,uint32_t,adr)
-      >
-    >
-  >
- > AllocMeta_set;
-
-
-int main() {
-    MemoryManager manager;
-
-    // Registering base addresses with sizes
-    manager.registerBaseAddress("Data1", 1000, 10);
-    manager.registerBaseAddress("Data2", 2000, 20);
-    manager.registerBaseAddress("Data3", 3000, 30);
-
-    // Registering field offsets
-    manager.registerFieldOffset(0);
-    manager.registerFieldOffset(100);
-    manager.registerFieldOffset(200);
-
-    // Simulating lookup using cumulative address sum
-    size_t cumulativeAddressSum = 2305;
-
- 
-
-    // Lookup the base address corresponding to the cumulative address sum
-    size_t baseAddress = 0;
-    for (const auto& entry : manager.baseAddressMap) {
-        size_t addr = entry.first.first;
-        size_t size = entry.first.second;
-        if ((cumulativeAddressSum >= addr) && (cumulativeAddressSum < (addr + size))) {
-            baseAddress = addr;
-            break;
-        }
-    }
-
-       // Lookup the name string corresponding to the cumulative address sum
-    std::string name = manager.lookupName(baseAddress);
-    std::cout << "Corresponding name: " << name << std::endl;
-
-    // Lookup the field offset corresponding to the cumulative address sum
-    size_t fieldOffset = manager.lookupFieldOffset(cumulativeAddressSum, baseAddress);
-    std::cout << "Corresponding field offset: " << fieldOffset << std::endl;
-
-    return 0;
-}
+#endif
