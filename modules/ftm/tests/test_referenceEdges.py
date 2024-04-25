0cc96fd5a151e7e6540f546ba182b4942cbf2cde
Martin Skorsky <m.skorsky@gsi.de>
2024-04-25 16:46:54 +0200

    test_referenceEdges.py: add more tests with 1, 3, 4 reference edges.

--- modules/ftm/tests/test_referenceEdges.py
+++ modules/ftm/tests/test_referenceEdges.py
@@ -34,3 +34,62 @@ class ReferenceEdge(dm_testbench.DmTestbench):
     self.delay(1.0)
     self.startAndCheckSubprocess((self.binaryDmSched, self.datamaster, 'status', '-o', self.downloadFile0), [0], 0, 0)
     self.startAndCheckSubprocess((self.binaryDmCmd, self.datamaster, 'startpattern', 'PAT1'), [0], 1, 0)
+
+  def testReferenceEdgeLoop1(self):
+    """Use a schedule with a loop of a block and two tmsg.
+    The loops run with 1Hz. There is a reference between the two tmsg nodes.
+    Check for the correct gid value in the timing messages.
+    Check for the correct parameter value when using the reference.
+    """
+    snoopFile = 'snoop_reference_loop1.csv'
+    self.scheduleFile0 = 'reference-loop1.dot'
+    if self.threadQuantity == 32:
+      self.scheduleFile0 = self.scheduleFile0.replace('.dot', '-thread32.dot')
+    self.downloadFile0 = self.scheduleFile0.replace('.dot', '-download.dot')
+    self.snoopToCsvWithAction(snoopFile, self.actionReferenceLoop1, duration=2)
+    self.startAndCheckSubprocess(('scheduleCompare', '-s', '-u', self.schedulesFolder + self.scheduleFile0, self.downloadFile0), [0], 0, 0)
+    self.deleteFile(self.downloadFile0)
+    self.analyseFrequencyFromCsv(snoopFile, column=6, printTable=True, checkValues={'0x0001': '>1', '0x0002': '>1'})
+    self.analyseFrequencyFromCsv(snoopFile, column=20, printTable=True, checkValues={'0x0000000000225002': '>3'})
+    self.deleteFile(snoopFile)
+
+  def actionReferenceLoop1(self):
+    """During snoop start pattern ref1. This produces messages with 1Hz.
+    Download the schedule for later compare.
+    """
+    self.startPattern(self.scheduleFile0, 'ref1')
+    self.delay(1.0)
+    self.startAndCheckSubprocess((self.binaryDmSched, self.datamaster, 'status', '-o', self.downloadFile0), [0], 0, 0)
+
+  def testReferenceEdgeLoop3(self):
+    """Use a schedule with a loop of a block and four tmsg.
+    The loops run with 1Hz. There is are references between the tmsg nodes.
+    Check for the correct gid value in the timing messages.
+    Check for the correct parameter value when using the reference.
+    """
+    snoopFile = 'snoop_reference_loop3.csv'
+    self.scheduleFile0 = 'reference-loop3.dot'
+    if self.threadQuantity == 32:
+      self.scheduleFile0 = self.scheduleFile0.replace('.dot', '-thread32.dot')
+    self.downloadFile0 = self.scheduleFile0.replace('.dot', '-download.dot')
+    self.snoopToCsvWithAction(snoopFile, self.actionReferenceLoop3, duration=2)
+    self.startAndCheckSubprocess(('scheduleCompare', '-s', '-u', self.schedulesFolder + self.downloadFile0, self.downloadFile0), [0], 0, 0)
+    self.deleteFile(self.downloadFile0)
+    self.analyseFrequencyFromCsv(snoopFile, column=6, printTable=True, checkValues={'0x0001': '>1', '0x0002': '>1', '0x0003': '>1', '0x0004': '>1'})
+    self.analyseFrequencyFromCsv(snoopFile, column=20, printTable=True, checkValues={'0x0000000000225002': '>3', '0x0000000000225003': '>1', '0x0000000000225004': '>1'})
+    self.deleteFile(snoopFile)
+
+  def actionReferenceLoop3(self):
+    """During snoop start pattern ref1. This produces messages with 1Hz.
+    Download the schedule for later compare.
+    """
+    self.startPattern(self.scheduleFile0, 'ref1')
+    self.delay(1.0)
+    self.startAndCheckSubprocess((self.binaryDmSched, self.datamaster, 'status', '-o', self.downloadFile0), [0], 0, 0)
+
+  def testReferenceEdgeLoop4(self):
+    """Use a schedule with a loop of a block and five tmsg and four references.
+    This is not allowed. Adding the schedule fails.
+    """
+    self.scheduleFile0 = 'reference-loop4.dot'
+    self.startAndCheckSubprocess((self.binaryDmSched, self.datamaster, 'add', self.schedulesFolder + self.scheduleFile0), [250], 2, 3)
