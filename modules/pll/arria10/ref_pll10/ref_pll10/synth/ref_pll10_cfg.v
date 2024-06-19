e104824ae0237e635094491450123b6cdbcc54e5
A. Hahn <a.hahn@gsi.de>
2024-06-19 09:14:47 +0200

    arria10 generic: remove autogenerated files

--- modules/pll/arria10/ref_pll10/ref_pll10/synth/ref_pll10_cfg.v
+++
@@ -1,5 +0,0 @@
-config ref_pll10_cfg;
-		design ref_pll10;
-		instance ref_pll10.iopll_0 use ref_pll10_altera_iopll_160.ref_pll10_altera_iopll_160_d3szf3y;
-endconfig
-
--- modules/pll/arria10/sys_fpll10/sys_fpll10/synth/sys_fpll10_cfg.v
+++
@@ -1,5 +0,0 @@
-config sys_fpll10_cfg;
-		design sys_fpll10;
-		instance sys_fpll10.xcvr_fpll_a10_0 use sys_fpll10_altera_xcvr_fpll_a10_160.altera_xcvr_fpll_a10;
-endconfig
-
