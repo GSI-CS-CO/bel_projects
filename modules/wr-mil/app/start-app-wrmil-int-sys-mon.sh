66e0b88fae2d5c912d1b48312345a47a161778a3
Dietrich Beck <d.beck@gsi.de>
2024-06-21 13:02:15 +0200

    wr-mil: wrf50, re-adjust width of 'apps' to increased number of monitoring data

--- modules/wr-mil/app/start-app-wrmil-int-sys-mon.sh
+++ modules/wr-mil/app/start-app-wrmil-int-sys-mon.sh
@@ -11,4 +11,4 @@ BASEDIR=$(cd "${SCRIPTDIR}/.." >/dev/null; pwd)
 # script for starting the wrmil system viewer on INT
 export DIM_DNS_NODE=asl105
 export LD_LIBRARY_PATH=${BASEDIR}/lib
-xterm -T 'wrmil system status' -fa monaco -fs 10 -geometry 160x24 -e ${BASEDIR}/bin/wrmil-client-mon int
+xterm -T 'wrmil system status' -fa monaco -fs 10 -geometry 173x24 -e ${BASEDIR}/bin/wrmil-client-mon int
