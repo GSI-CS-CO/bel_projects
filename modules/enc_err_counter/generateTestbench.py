9447bb14f67117ed7e2fdb4a5fc4fd1b417dadef
Lucas Herfurth <lucas.herfurth1999@gmail.com>
2024-07-25 17:06:33 +0200

    enc_err_counter: Revert Merge branch enc_err_counter into enc_err_counter_pexp
    This reverts commit fc5b362468d5ed27c0b2087af6612d31b665777d, reversing
    changes made to 650c37771fef9d031a088a4aa018d0977645f474.

--- modules/enc_err_counter/generateTestbench.py
+++ modules/enc_err_counter/generateTestbench.py
@@ -6,7 +6,6 @@ __MODULENAME__ = "enc_err_counter"
 __NERRORS__ = 100 #100
 __MAX_IDLE_CYCLES__ = 100 #100
 __MAX_ERROR_LENGTH__ = 10 #10
-__MAX_TEST_LENGTH__ = __NERRORS__ * (__MAX_ERROR_LENGTH__ + __MAX_IDLE_CYCLES__) 
 
 __REF_CLK_SIGNAL_NAME__ = "s_clk_ref"
 __SYS_CLK_SIGNAL_NAME__ = "s_clk_sys"
@@ -26,99 +25,71 @@ def generate_clock(fout, clk_cycle, clk_signal_name):
 def generate_error(fout, clk_signal_name, en_aux):
     error_length = round(random.random() * __MAX_ERROR_LENGTH__)
     aux_suffix = ""
-    # if en_aux:
-    #     aux_suffix = "_aux"
+    if en_aux:
+        aux_suffix = "_aux"
 
-    fout.write(f"s_err_error <= '1';\n")
-    fout.write(f"wait until rising_edge({clk_signal_name});\n")
-    fout.write(f"s_err_error <= '0';\n")
-    for i in range(error_length-1):
+    fout.write(f"enc_err{aux_suffix} <= '1';\n")
+    for i in range(error_length):
         fout.write(f"wait until rising_edge({clk_signal_name});\n")
+    fout.write(f"enc_err{aux_suffix} <= '0';\n")
     return error_length
 
 def generate_idle(fout, clk_signal_name):
     idle_cycles = round(random.random() * __MAX_IDLE_CYCLES__)
-    
-    fout.write(f"s_err_idle <= '1';\n")
-    fout.write(f"wait until rising_edge({clk_signal_name});\n")
-    fout.write(f"s_err_idle <= '0';\n")
-    for i in range(idle_cycles-1):
+    for i in range(idle_cycles):
         fout.write(f"wait until rising_edge({clk_signal_name});\n")
-    return idle_cycles
 
-def generate_reset(fout, clk_signal_name):
-    fout.write(f"s_ass_reset <= '1';\n")
-    fout.write(f"wait until rising_edge({clk_signal_name});\n")
-    fout.write(f"s_ass_reset <= '0';\n")
-    fout.write(f"wait until rising_edge({clk_signal_name});\n")
-
-def test_wb_output(fout,clk_signal_name, en_aux):
-    fout.write(f"s_ass_set_wb <= '1';\n")
-    fout.write(f"wait until rising_edge({clk_signal_name});\n")
-    fout.write(f"s_ass_set_wb <= '0';\n")
-    fout.write(f"wait until rising_edge({clk_signal_name});\n")
-
-def generate_error_fsm_controller(fout, nerrors, en_aux):
-    #write FSM header
-    fout.write(f"--Encoding Error FSM controller\np_error_fsm_controller : process\nbegin\nwait until rising_edge(s_rst_n);\n")
-    fout.write("wait until rising_edge(s_clk_ref);\n")
-
-    #generate errors
-    for i in range(nerrors):
-        generate_idle(fout, __REF_CLK_SIGNAL_NAME__)
-        generate_error(fout, __REF_CLK_SIGNAL_NAME__, en_aux)
-
-    fout.write("s_err_await <= '1';\n")
-    fout.write("wait until rising_edge(s_clk_ref);\n")
-    fout.write("s_err_await <= '0';\n")
-
-    #wait for reset
-    fout.write("wait until rising_edge(s_done_checking_error);\n")
-
-
-
-    #generate errors
-    for i in range(nerrors):
-        generate_idle(fout, __REF_CLK_SIGNAL_NAME__)
-        generate_error(fout, __REF_CLK_SIGNAL_NAME__, en_aux)
-
-    fout.write("s_err_await <= '1';\n")
-    fout.write("wait until rising_edge(s_clk_ref);\n")
-    fout.write("s_err_await <= '0';\n")
-
-    #process footer
-    fout.write(f'end process;\n')
-
-def generate_assertion_fsm_controller(fout, nerrors, en_aux):
-     #write FSM header
-    fout.write(f"--Encoding Error FSM controller\np_assertion_fsm_controller : process\nbegin\nwait until rising_edge(s_rst_n);\n")
-    fout.write("wait until rising_edge(s_clk_sys);\n")
-
-    fout.write("wait until rising_edge(s_done_generating);\n")
-
-    fout.write("wait until rising_edge(s_clk_sys);\n")
-    fout.write("wait until rising_edge(s_clk_sys);\n")
-    fout.write("wait until rising_edge(s_clk_sys);\n")
-    fout.write("wait until rising_edge(s_clk_sys);\n")
-
-    test_wb_output(fout, __SYS_CLK_SIGNAL_NAME__, False)
-
-    generate_reset(fout, __SYS_CLK_SIGNAL_NAME__)
-
-    fout.write("wait until rising_edge(s_done_generating);\n")
-
-    fout.write("wait until rising_edge(s_clk_sys);\n")
-    fout.write("wait until rising_edge(s_clk_sys);\n")
-    fout.write("wait until rising_edge(s_clk_sys);\n")
-    fout.write("wait until rising_edge(s_clk_sys);\n")
-
-    test_wb_output(fout, __SYS_CLK_SIGNAL_NAME__, False)
-
-    #process footer
-    fout.write(f'end process;\n')
-
-
+def test_wb_output(fout, test_data, en_aux):
+    if en_aux:
+        address = "00000004"
+    else:
+        address = "00000000"
     
+    aux_suffix = ""
+    if en_aux:
+        aux_suffix = "_aux"
+        test_data = "1"
+
+
+    fout.write(f"""s_wb_slave_in <= wb_stim(c_cyc_on, c_str_on, c_we_off, x"{address}", c_reg_all_zero);\n""")
+    fout.write(f"wait until rising_edge(s_wb_slave_out.ack);\nwait until rising_edge({__SYS_CLK_SIGNAL_NAME__});\n")
+    fout.write(f"s_wb_slave_in <= wb_stim(c_cyc_off, c_str_off, c_we_off, c_reg_all_zero, c_reg_all_zero);\n")
+    fout.write(f'assert s_wb_slave_out.dat = x"{test_data}"\nreport "Data read from slave{aux_suffix} is not correct! Expected number of errors: 0x{test_data}." severity NOTE;\n')
+    if not en_aux:
+        fout.write("s_flag <= '1';\n")
+
+def generate_error_controller(fout, nerrors, en_aux):
+    total_errors = 0
+
+    aux_suffix = ""
+    if en_aux:
+        aux_suffix = "_aux"
+
+    #write error header
+    fout.write(f"--Encoding Error Generator\np_enc_err_generator{aux_suffix} : process\nbegin\nwait until rising_edge(s_rst_n);\n")
+    if not en_aux:
+        fout.write("s_flag <= '0';\n")
+
+    #generate errors
+    for i in range(nerrors):
+        generate_idle(fout, __REF_CLK_SIGNAL_NAME__)
+        total_errors += generate_error(fout, __REF_CLK_SIGNAL_NAME__, en_aux)
+
+    #assert counter value
+    generate_idle(fout, __REF_CLK_SIGNAL_NAME__)
+    generate_idle(fout, __REF_CLK_SIGNAL_NAME__)
+    #wait until rising_edge sys_clk
+    fout.write(f"wait until rising_edge({__SYS_CLK_SIGNAL_NAME__});\n")
+    #read slave_o.dat if counter is correct
+    data_string = hex(total_errors).removeprefix('0x')
+    if not en_aux:
+        fout.write(f'nerrors <= x"{data_string.zfill(8)}";\n')
+        fout.write("s_flag <= '1';\n")
+    else:
+        fout.write(f'nerrors_aux <= x"{data_string.zfill(8)}";\n')
+
+    #write error footer
+    fout.write(f"wait for {clk_sys*10000} ns;\nend process;\n")
 
 def write_testbench(tb_name, nerrors):
     fout = open(tb_name, 'w') #add number of errors to the filename?
@@ -133,9 +104,15 @@ def write_testbench(tb_name, nerrors):
     generate_clock(fout, clk_ref, __REF_CLK_SIGNAL_NAME__)
     generate_clock(fout, clk_sys, __SYS_CLK_SIGNAL_NAME__)
 
-    generate_error_fsm_controller(fout, nerrors, False)
+    generate_error_controller(fout, nerrors, False)
+    generate_error_controller(fout, nerrors, True)    
 
-    generate_assertion_fsm_controller(fout, nerrors, False)
+    # for proper testing this should wait the maximum amount of cycles the error generators could take instead of just using the template
+    fin = open("testingTemplate.vhd", 'r')
+    template = fin.read()
+    fin.close()
+    
+    fout.write(template)
     
     #write file footer
     fout.write("end architecture enc_err_counter_tb_arc;\n")
