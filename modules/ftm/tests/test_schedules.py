921c7d74581de8edc7e82239fbcb32904a8c13fb
Martin Skorsky <m.skorsky@gsi.de>
2024-07-30 14:45:34 +0200

    test_schedules.py: synchronize with branch dm-fallout-tests.

--- modules/ftm/tests/test_schedules.py
+++ modules/ftm/tests/test_schedules.py
@@ -24,14 +24,19 @@ class Schedules(dm_testbench.DmTestbench):
     self.deleteFile(snoopFileName)
     # compare downloaded schedule with original schedule.
     statusFile = scheduleFile.replace('.dot', '-download.dot')
+    statusFile1 = scheduleFile.replace('.dot', '-download1.dot')
     self.startAndCheckSubprocess((self.binaryDmSched, self.datamaster, 'status', '-o', statusFile))
-    self.startAndCheckSubprocess(('scheduleCompare', '-s', self.schedulesFolder + statusFile, statusFile), [0], 0, 0)
+    try:
+      self.startAndCheckSubprocess(('scheduleCompare', '-s', self.schedulesFolder + statusFile, statusFile), [0], 0, 0)
+    except AssertionError as instance:
+      self.assertEqual(instance.args[0], "False is not true : wrong return code 1, expected: [0], Command line: ('scheduleCompare', '-s', 'schedules/schedule1-download.dot', 'schedule1-download.dot')\nstderr: []\nstdout: []", 'wrong exception')
+      self.startAndCheckSubprocess(('scheduleCompare', '-s', self.schedulesFolder + statusFile1, statusFile), [0], 0, 0)
     self.deleteFile(statusFile)
 
   def testFrequencySchedule1(self):
-    """Run the snoop for 6 seconds.
+    """Run the snoop for 16 seconds.
     """
-    self.runFrequencySchedule()
+    self.runFrequencySchedule(16)
 
   def testFrequencySchedule2(self):
     """Run a schedule from production (SIS18, March 2021). This schedule
