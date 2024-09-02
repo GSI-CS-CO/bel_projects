1c743ae1ef26bd6616bb8b0961a751939089a8ab
Dietrich Beck <d.beck@gsi.de>
2024-09-02 14:49:45 +0200

    dm-unipz: dm-unidm fixes

--- modules/dm-unipz/x86/dm-unidm-fel0090-int_start.sh
+++ modules/dm-unipz/x86/dm-unidm-fel0090-int_start.sh
@@ -84,6 +84,9 @@ saft-ecpu-ctl tr0 -c 0x112c161000000000 0xfffffff000000000 0 0x8 -d
 # configure ECA for lm32 channel: listen for CMD_UNI_BREQ_NOWAIT, tag "0x9"
 saft-ecpu-ctl tr0 -c 0x112c162000000000 0xfffffff000000000 0 0x9 -d
 
+# configure ECA for lm32 channel: listen for EVT_READY_TO_SIS, tag "0xa" (for testing only)
+saft-ecpu-ctl tr0 -c 0x11c601e000000000 0xfffffff000000000 0 0xa -d
+
 
 ###########################################
 # configure TLU and ECA for UNIPZ
