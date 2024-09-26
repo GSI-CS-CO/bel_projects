f9ef529dd396a960972e00d80310d4e8531f22ef
Martin Skorsky <m.skorsky@gsi.de>
2024-09-26 15:38:41 +0200

    tools/copyLibraries.sh: add descriptions for source and target.

--- modules/ftm/tests/tools/copyLibraries.sh
+++ modules/ftm/tests/tools/copyLibraries.sh
@@ -2,6 +2,7 @@
 
 # script to copy prerequisites and datamaster tools running on Yocto.
 # copying to a stagging area at /common/usr/timing/
+# the libraries are input for /common/export/timing-rte/generator-fel0069-yocto/loader.sh
 # A: three boost libraries needed for libcarpedm
 # B: dm-cmd, dm-sched, libcarpedm
 
@@ -9,9 +10,13 @@
 # B works on ASL cluster when the files are downloaded from Jenkins workspace of job 'YoctoBuild_Datamaster_Tools'
 # B does not work inside Jenkins Job
 
+# the staging area, the productive area is /common/export/timing-rte/generator-fel0069-yocto/
 export TARGET=/common/usr/timing/stagging-fel0069/
+# the source for the boost libraries
 export SOURCE=/common/usr/embedded/yocto/fesa/current/sdk/sysroots/core2-64-ffos-linux
+# the source for the datamaster tools
 export DOWNLOAD=~/Downloads
+
 # only for information:
 ls -l $SOURCE/usr/lib/libboost_{graph,regex,serialization}.so.*
 # A:
