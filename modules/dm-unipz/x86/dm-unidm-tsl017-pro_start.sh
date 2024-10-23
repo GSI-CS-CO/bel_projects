026a06f2928195e4b23eaf9c919f31a134068b11
Dietrich Beck <d.beck@gsi.de>
2024-10-23 09:02:40 +0200

    dm-unidm: fix deployment for PRO .

--- modules/dm-unipz/x86/dm-unidm-tsl017-pro_start.sh
+++ modules/dm-unipz/x86/dm-unidm-tsl017-pro_start.sh
@@ -53,6 +53,7 @@ eb-fwload dev/wbm0 u 0x0 dmunidm.bin
 echo -e dm-unidm - start: set MAC and IP of gateway and Data Master
 
 dmunipz-ctl dev/wbm0 ebmdm 0x00267b00046b 0xc0a880f7
+dmunipz-ctl dev/wbm0 uni 10000
 
 echo -e dm-unidm - start: make firmware operational
 
