83573cc818dbaa8e7f7f4388aeaf1cd4a48931c9
A. Hahn <a.hahn@gsi.de>
2024-10-23 16:40:01 +0200

    pexarria10: Quartus 23.1.1 port

--- modules/wb_arria_reset/arria10_reset/arria10_reset/altera_remote_update_231/synth/arria10_reset_pkg.vhd
+++ modules/wb_arria_reset/arria10_reset/arria10_reset/altera_remote_update_231/synth/arria10_reset_pkg.vhd
@@ -3,7 +3,7 @@ use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 
 package arria10_reset_pkg is
-	component arria10_reset_altera_remote_update_231_qlx46ea is
+	component arria10_reset_altera_remote_update_231_bia2qla is
 		port (
 			busy        : out std_logic;                                        -- busy
 			data_out    : out std_logic_vector(31 downto 0);                    -- data_out
@@ -15,6 +15,6 @@ package arria10_reset_pkg is
 			clock       : in  std_logic                     := 'X';             -- clk
 			reset       : in  std_logic                     := 'X'              -- reset
 		);
-	end component arria10_reset_altera_remote_update_231_qlx46ea;
+	end component arria10_reset_altera_remote_update_231_bia2qla;
 
 end arria10_reset_pkg;
