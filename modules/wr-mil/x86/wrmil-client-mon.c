2db174effede9a837c84def43900659a8958bacf
Dietrich Beck <d.beck@gsi.de>
2024-11-11 11:14:59 +0100

    wr-mil: cosmetic changes, set version number to 00.00.12

--- modules/wr-mil/x86/wrmil-client-mon.c
+++ modules/wr-mil/x86/wrmil-client-mon.c
@@ -34,7 +34,7 @@
  * For all questions and ideas contact: d.beck@gsi.de
  * Last update: 15-April-2019
  *********************************************************************************************/
-#define WRMIL_CLIENT_MON_VERSION 0x000011
+#define WRMIL_CLIENT_MON_VERSION 0x000012
 
 // standard includes 
 #include <unistd.h> // getopt
--- modules/wr-mil/x86/wrmil-serv-mon.cpp
+++ modules/wr-mil/x86/wrmil-serv-mon.cpp
@@ -34,7 +34,7 @@
  * For all questions and ideas contact: d.beck@gsi.de
  * Last update: 15-April-2019
  *********************************************************************************************/
-#define WRMIL_SERV_MON_VERSION 0x000011
+#define WRMIL_SERV_MON_VERSION 0x000012
 
 #define __STDC_FORMAT_MACROS
 #define __STDC_CONSTANT_MACROS
