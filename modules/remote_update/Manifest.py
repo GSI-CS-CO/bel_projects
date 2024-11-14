48ebd1bd18e073913ecb23222030d9cafbda6137
A. Hahn <a.hahn@gsi.de>
2024-11-14 11:14:50 +0100

    asmi10: just one more update to make this work with all targets

--- modules/remote_update/Manifest.py
+++ modules/remote_update/Manifest.py
@@ -11,7 +11,6 @@ files = [
   "wb_asmi_slave.vhd",
   "asmi10/asmi10/synth/asmi10.vhd",
   "asmi10/asmi10/altera_asmi_parallel_231/synth/asmi10_pkg.vhd",
-  "asmi10/asmi10/altera_asmi_parallel_231/synth/asmi10_altera_asmi_parallel_231_47nf7hy.v",
   "asmi5/asmi5/synthesis/asmi5.vhd",
   "asmi5/asmi5/synthesis/submodules/asmi5_asmi_parallel_0.v",
   "asmi_arriaII/asmi_arriaII/synthesis/asmi_arriaII.vhd",
--- modules/remote_update/asmi10/asmi10/altera_asmi_parallel_231/Manifest.py
+++ modules/remote_update/asmi10/asmi10/altera_asmi_parallel_231/Manifest.py
@@ -1,6 +1,5 @@
 library = "altera_asmi_parallel_231"
 
 files = [
-        "synth/asmi10_altera_asmi_parallel_231_47nf7hy.v",
         "synth/asmi10_pkg.vhd"
         ]
