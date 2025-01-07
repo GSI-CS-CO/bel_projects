29ce5a16a0e20d2be875f43b8c7316dad9654782
Antonietta Russo <A.Russo@gsi.de>
2025-01-07 10:36:00 +0100

    DIOB: updated diob manifest file

--- modules/diob/Manifest.py
+++ modules/diob/Manifest.py
@@ -5,5 +5,9 @@ files = [
   "TM_quench_detection.vhd",
   "QuD_masken_reg.vhd",
   "del_quench_detection.vhd",
-  "qud_pkg.vhd"
+  "qud_pkg.vhd",
+   "incoming_signals_modulator.vhd",
+   "modulator_block.vhd",
+   "out_modulator_el.vhd",
+   "front_cards_control.vhd"
 ]
