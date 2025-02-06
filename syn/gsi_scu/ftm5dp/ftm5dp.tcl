22ecde5d73a72d8aad3c0dbe3daeb5a7b5754a71
A. Hahn <a.hahn@gsi.de>
2025-02-06 16:50:48 +0100

    ftm5dp: start project

---
+++ syn/gsi_scu/ftm5dp/ftm5dp.tcl
@@ -0,0 +1,11 @@
+set platform "ftm5dp"
+source ../../autogen.tcl
+source ../../../modules/build_id/build_id.tcl
+source ../../../ip_cores/general-cores/platform/altera/networks/arria10gx/arria10gx.tcl
+source ../../../ip_cores/general-cores/platform/altera/wb_pcie/arria10gx_ftm4.tcl
+source ../../../modules/pll/arria10_ftm4/arria10_ftm4_pll.tcl
+source ../../../ip_cores/wr-cores/platform/altera/wr_arria10_phy/wr_arria10_ftm4_phy/wr_arria10_ftm4_phy.tcl
+source ../../../modules/lvds/arria10_ftm4/arria10_ftm4_lvds.tcl
+source ../../../modules/remote_update/asmi10.tcl
+source ../../../modules/wb_arria_reset/arria10_reset.tcl
+source ../../../modules/a10ts/src/hdl/a10ts_generate.tcl
