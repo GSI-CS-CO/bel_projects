6453eca9c13f9264bb60396a1296db1931a9ae8e
Dietrich Beck <d.beck@gsi.de>
2025-02-20 09:00:56 +0100

    wr-unipz: reduce ahead time for service events 500 -> 250 us

--- modules/wr-unipz/include/wr-unipz.h
+++ modules/wr-unipz/include/wr-unipz.h
@@ -25,8 +25,8 @@
 
 #define WRUNIPZ_MAXPREPOFFSET         2000    // max offset of a prep event within UNILAC cycle [us]; all with events a smaller offset will be predicted from previous cycles
 #define WRUNIPZ_MILCALIBOFFSET       27000    // calibration offset to MIL event bus [ns]; MIL events are always 'late' due its protocol; this offset must be added to WR deadlines
-#define WRUNIPZ_QQOFFSET               500    // offset for sending special service event for QQ [us] /* chk: QQ is breaking the concept of WR */
-#define WRUNIPZ_A4OFFSET               500    // offset for sending special service event for A4 [us] /* chk: A4 is breaking the concept of WR */
+#define WRUNIPZ_QQOFFSET               250    // offset for sending special service event for QQ [us] /* chk: QQ is breaking the concept of WR */
+#define WRUNIPZ_A4OFFSET               250    // offset for sending special service event for A4 [us] /* chk: A4 is breaking the concept of WR */
 #define WRUNIPZ_NOBEAMOFFSET          1320    // offset for sending special service event for EVT_BEAM_OFF [us] /* chk: EVT_NO_BEAM is breaking the concept of WR */
 #define WRUNIPZ_TDIFFMIL                45    // minimmum time difference [us] between sending telegrams to the MIL bus; 25us is enough, but UNIPZ sends service events 45us after last event
 
