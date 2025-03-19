50fd4917a1ab5e84f24e4aa32a3d8582e7ce3bb1
Enkhbold Ochirsuren <E.Ochirsuren@gsi.de>
2025-03-19 12:56:18 +0100

    a10vs: add generated synthesis package to the module manifest

--- modules/a10vs/Manifest.py
+++ modules/a10vs/Manifest.py
@@ -2,7 +2,8 @@ files = [
     "src/hdl/a10vs_pkg.vhd",
     "src/hdl/a10vs_wb.vhd",
     "src/hdl/a10vs.vhd",
-    "src/hdl/a10vs_ip/a10vs_ip/synth/a10vs_ip.vhd"
+    "src/hdl/a10vs_ip/a10vs_ip/synth/a10vs_ip.vhd",
+    "src/hdl/a10vs_ip/a10vs_ip/altera_voltage_sensor_231/synth/a10vs_ip_pkg.vhd"
 ]
 
 modules = {
