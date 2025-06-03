fe1b10c696a2ad566d8983b824202ae8b9e17394
A. Hahn <a.hahn@gsi.de>
2025-06-03 15:32:50 +0200

    ftm5dp: fix warning 127003

--- top/gsi_scu/ftm5dp/ftm5dp.vhd
+++ top/gsi_scu/ftm5dp/ftm5dp.vhd
@@ -301,7 +301,7 @@ architecture rtl of ftm5dp is
   constant c_family       : string := "Arria 10 GX FTM4";
   constant c_project      : string := "scu_control";
   constant c_cores        : natural:= 4;
-  constant c_initf_name   : string := c_project & "_stub.mif";
+  constant c_initf_name   : string := "ftm5dp.mif";
   constant c_profile_name : string := "medium_icache_debug";
   constant c_cr_bits      : natural := 24;
 
