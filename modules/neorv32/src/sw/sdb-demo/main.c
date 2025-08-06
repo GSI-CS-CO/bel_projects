65e701fc603d640d0c8f973a09f7e2ef0238ebe0
A. Hahn <a.hahn@gsi.de>
2025-08-06 10:26:13 +0200

    neorv32: first SDB steps

--- modules/neorv32/src/sw/sdb-demo/main.c
+++ modules/neorv32/src/sw/sdb-demo/main.c
@@ -1,32 +1,27 @@
 #include <stddef.h>
 #include <stdlib.h>
+
 #include <neorv32.h>
 #include <neorv32_uart.h>
 
-#define BAUD_RATE 115200
-//#define BAUD_RATE 19200
+#include <mini_sdb.h>
 
-#define IO_CTL_BASE   0x4010000
-#define IO_GPIO_SET   0x0200
-#define IO_GPIO_RESET 0x0208
+#define BAUD_RATE 115200
 
 int main(void)
 {
+  /* Test SDB */
+  discoverPeriphery();
 
-  int * p_set = (int*) (IO_CTL_BASE+IO_GPIO_SET);
-  int * p_reset = (int*) (IO_CTL_BASE+IO_GPIO_RESET);
-
-  /* Test Wishbone access */
   /* Test UART */
   neorv32_rte_setup();
   neorv32_uart0_setup(BAUD_RATE, 0);
   while (true)
   {
     neorv32_aux_print_logo();
-    //*p_set = 0xffffffff;
     neorv32_uart0_puts("NEORV32: Hello world!\n");
+    neorv32_uart0_printf("NEORV32: Found ECA at %p\n", (void*)&pEca);
   }
-  //*p_reset = 0xffffffff;
 
   /* Test return to start.s */
   return 0;
