6a5a0704bafb7b429f6b0e4a5f523b0b487b035b
Lucas Herfurth <l.herfurth@gsi.de>
2025-09-10 14:44:04 +0200

    wb_dma_rebase: Fixed merge mistake.

--- modules/monster/monster_pkg.vhd
+++ modules/monster/monster_pkg.vhd
@@ -139,7 +139,6 @@ package monster_pkg is
       g_en_a10vs             : boolean := false;
       g_en_cellular_ram      : boolean := false;
       g_en_virtualRAM        : boolean := false;
-      g_en_enc_err_counter   : boolean := false;
       g_en_wb_dma            : boolean := false;
       g_en_wb_master_test    : boolean := false
     );
