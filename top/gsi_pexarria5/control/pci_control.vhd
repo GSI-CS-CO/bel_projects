557d2a870cc58c6ab10868bb884d3eb55af81a75
A. Hahn <a.hahn@gsi.de>
2025-11-26 09:42:45 +0100

    pexarria5: enable ASMI

--- top/gsi_pexarria5/control/pci_control.vhd
+++ top/gsi_pexarria5/control/pci_control.vhd
@@ -280,7 +280,7 @@ begin
       g_lm32_ramsizes      => c_lm32_ramsizes/4,
       g_lm32_init_files    => f_string_list_repeat(c_initf_name, c_cores),
       g_lm32_profiles      => f_string_list_repeat(c_profile_name, c_cores),
-      g_en_asmi            => false
+      g_en_asmi            => true
     )
     port map(
       core_clk_20m_vcxo_i     => clk_20m_vcxo_i,
