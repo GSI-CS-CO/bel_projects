479d2359102b6737d1288f3195d650758608e039
A. Hahn <a.hahn@gsi.de>
2025-11-26 09:43:39 +0100

    scu3: enable ASMI

--- top/gsi_scu/control3/scu_control.vhd
+++ top/gsi_scu/control3/scu_control.vhd
@@ -372,7 +372,7 @@ begin
       g_en_wd_tmr          => true,
       g_en_eca_tap         => true,
       g_en_timer           => true,
-      g_en_asmi            => false
+      g_en_asmi            => true
     )
     port map(
       core_clk_20m_vcxo_i    => clk_20m_vcxo_i,
