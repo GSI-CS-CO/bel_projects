6e7426f681a292877f82d22f1053c0c873835670
Dietrich Beck <d.beck@gsi.de>
2026-01-15 15:34:19 +0100

    wr-mil: setup development system (wrf50 only) for lab in BR3: debug

--- modules/wr-mil/x86/wrmil-int-f50sync-br3-cg_start.sh
+++ modules/wr-mil/x86/wrmil-int-f50sync-br3-cg_start.sh
@@ -6,6 +6,6 @@
 logger 'starting 50 Hz clock'
 # generate 100 kHz clock with 20ns phase offset
 saft-io-ctl  tr1 -n IO3 -o 1 -t 0
-saft-clk-gen tr1 -n IO3 50 20
+saft-clk-gen tr1 -n IO3 -f 50 20
 
 
