24317efea307d374c5aa6872cc9f416ce2a93635
A. Hahn <a.hahn@gsi.de>
2026-08-06 17:04:30 +0200

    neorv32: refactor xbar test #2

--- modules/neorv32/src/sw/sim_xbar/main.c
+++ modules/neorv32/src/sw/sim_xbar/main.c
@@ -2,11 +2,13 @@
 #include <stdlib.h>
 #include <neorv32.h>
 #include <neorv32_uart.h>
+#include <gsi_test_neorv32.h>
+#include <gsi_wishbone_neorv32.h>
 #include <stdbool.h>
 
 // 1152000 for real hardware, 921600 for simulation
 #define BAUD_RATE 921600
-
+#define SEED 0x12345678
 #define N 6
 
 int main(void)
@@ -18,19 +20,18 @@ int main(void)
   int nums[N] = {0};
   int nums_test[N] = {0};
   bool failed;
+  bool stop = false;
 
   /* Test block write access */
   neorv32_rte_setup();
   neorv32_uart0_setup(BAUD_RATE, 0);
   neorv32_uart0_printf("Starting the loop.\n");
 
-  while(true) {
+  while(!stop) {
     failed = false;
     for(int i = 0; i < N; i++) {
-      nums[i] = i; // neorv32_aux_xorshift32();
+      nums[i] = SEED^i;
     }
-
-    neorv32_uart0_printf("#1.\n");
 
     //neorv32_gpio_pin_set(0, 1);
     for(int i = 0; i < N; i++) {
@@ -38,15 +39,12 @@ int main(void)
     }
     //neorv32_gpio_pin_set(0, 0);
 
-    neorv32_uart0_printf("#2.\n");
 
     //neorv32_gpio_pin_set(0, 1);
     for(int i = 0; i < N; i++) {
       nums_test[i] = *(RAM_base_address + i);
     }
     //neorv32_gpio_pin_set(0, 0);
-
-    neorv32_uart0_printf("#3.\n");
 
     for(int i = 0; i < N; i++) {
       if(nums[i] != nums_test[i]) {
@@ -58,12 +56,17 @@ int main(void)
     if(!failed) {
        neorv32_uart0_printf("Data transfers successfull, no data lost.\n");
        for(int i = 0; i < N; i++) {
-         if(nums[i] == nums_test[i]) {
+         if(nums[i] != nums_test[i]) {
            neorv32_uart0_printf("Expected 0x%x, got 0x%x\n", nums[i], nums_test[i]);
+           gsi_test_failed();
          }
        }
     }
+
+    stop = true;
   }
+
+  gsi_test_passed();
 
   return 0;
 }
