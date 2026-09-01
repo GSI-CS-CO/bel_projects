9c9205c3b58d5e4a6f147e60d860556c92f33e7e
A. Hahn <a.hahn@gsi.de>
2026-09-01 15:35:52 +0200

    Manifest.py/urv: fix error 10481 and 10800 -> design library work does not contain primary unit urv_pkg

--- Manifest.py
+++ Manifest.py
@@ -5,6 +5,8 @@ modules = {
     "ip_cores/general-cores",
     "ip_cores/pci-core",
     "ip_cores/wr-cores",
+    "ip_cores/wr-cores/ip_cores/urv-core",
     "ip_cores/fpga-config-space/legacy-vme64x-core/hdl"
   ]
 }
+
