ead1f733041f220943226eed2a831011af16f82b
A. Hahn <a.hahn@gsi.de>
2026-09-18 10:15:07 +0200

    monster: remove g_en_eca_io_channel for now

--- modules/monster/monster.vhd
+++ modules/monster/monster.vhd
@@ -131,7 +131,6 @@ entity monster is
     g_en_a10ts             : boolean;
     g_delay_diagnostics    : boolean;
     g_en_eca               : boolean;
-    g_en_eca_io_channel    : boolean;
     g_en_wd_tmr            : boolean;
     g_en_timer             : boolean;
     g_en_eca_tap           : boolean;
@@ -959,12 +958,11 @@ architecture rtl of monster is
     constant c_ftm_channel_types : t_nat_array(1 downto 0) := c_scu_channel_types(1 downto 0);
     constant c_channel_types     : t_nat_array(2 downto 0) := c_scu_channel_types(2 downto 0);
   begin
+  begin
     if g_en_scubus then
       return c_scu_channel_types;
-    elsif g_en_eca_io_channel then
-      return c_channel_types;
     else
-      return c_ftm_channel_types;
+      return c_channel_types;
     end if;
   end f_channel_types;
 
@@ -973,7 +971,6 @@ architecture rtl of monster is
   signal s_stall_i   : std_logic_vector(c_channel_types'range) := (others => '0');
   signal s_channel_o : t_channel_array(c_channel_types'range);
   signal s_time      : t_time;
-
 
   function TO_INTEGER(x: boolean ) return integer is
   begin
@@ -2995,7 +2992,6 @@ end generate;
           a_stream_o => s_stream_i(1),
           a_stall_i  => s_stall_o(1));
 
-    eca_io_channel_wb : if g_en_eca_io_channel generate
       eca : wr_eca
         generic map(
           g_channel_types  => c_channel_types,
@@ -3022,10 +3018,6 @@ end generate;
           i_rst_n_i   => rstn_sys,
           i_master_i  => dev_msi_slave_o(dev_slaves'pos(devs_eca_ctl)),
           i_master_o  => dev_msi_slave_i(dev_slaves'pos(devs_eca_ctl)));
-    end generate;
-
-    eca_io_channel_wb_n : if not g_en_eca_io_channel generate
-      s_eca_io <= (others => (others => '0'));
     end generate;
 
       -- Legacy 8ns time
@@ -3102,8 +3094,6 @@ end generate;
           master_o    => top_bus_slave_i(top_my_masters'pos(topm_eca_wbm)),
           master_i    => top_bus_slave_o(top_my_masters'pos(topm_eca_wbm)));
 
-
-    eca_io : if g_en_eca_io_channel generate
       c2 : eca_queue
         generic map(
           g_queue_id  => 2)
@@ -3116,7 +3106,6 @@ end generate;
           q_rst_n_i   => rstn_sys,
           q_slave_i   => top_bus_master_o(top_slaves'pos(tops_emb_cpu)),
           q_slave_o   => top_bus_master_i(top_slaves'pos(tops_emb_cpu)));
-    end generate;
 
   end generate;
 
